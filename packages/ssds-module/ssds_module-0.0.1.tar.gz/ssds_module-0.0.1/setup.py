8768ef648d144f5a73e99f4e33248181498f340d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:49 +0000

    Add package ssds-module/0.0.1/ssds_module-0.0.1.tar.gz

---
+++ packages/ssds-module/ssds_module-0.0.1.tar.gz/ssds_module-0.0.1/setup.py
@@ -0,0 +1,26 @@
+# python setup.py sdist bdist_wheel
+#https://www.freecodecamp.org/news/build-your-first-python-package/
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="ssds_module",
+        version=VERSION,
+        author="Shaunak Dhande",
+        author_email="shaunak.dhande@innoplexus.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[],
+        keywords=['python', 'first package'],
+        classifiers = [
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+                        ]
+)
---
+++ packages/ssds-module/ssds_module-0.0.1.tar.gz/ssds_module-0.0.1/ssds_module/__init__.py
@@ -0,0 +1,4 @@
+from sdss_module.add import add
+from sdss_module.subtract import subtract
+from sdss_module.multiply import multiply
+from sdss_module.divide import divide
