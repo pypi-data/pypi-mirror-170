16791657731b408a8c26715c44da396b4cecf37e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:59 +0000

    Add package fwmonitor/1.2.5/fwmonitor-1.2.5.tar.gz

---
+++ packages/fwmonitor/fwmonitor-1.2.5.tar.gz/fwmonitor-1.2.5/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="fwmonitor",
+    version="1.2.5",
+    author="Pouriya Jamshidi",
+    scripts=["fwmonitor"],
+    description="for network traffic analysis, displays your iptables, UFW, or any application that logs in the same format, in a pleasant way.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
