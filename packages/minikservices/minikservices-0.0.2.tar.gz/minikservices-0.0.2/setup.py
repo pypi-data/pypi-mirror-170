c7e7c1496ee93b42b3811772748ab00c46e0575e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:59 +0000

    Add package minikservices/0.0.2/minikservices-0.0.2.tar.gz

---
+++ packages/minikservices/minikservices-0.0.2.tar.gz/minikservices-0.0.2/setup.py
@@ -0,0 +1,56 @@
+"""A setuptools based setup module.
+"""
+
+
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+
+setup(
+
+    name="minikservices",
+    version="0.0.2",
+    license='MIT',
+    description="App to start, stop and view minikube services easily.",
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dszortyka/minikservices",  
+    author="Daniel Szortyka",  
+    author_email="daniel.szortyka@gmail.com",  
+    classifiers=[
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        #"Programming Language :: Python :: 3.7",
+        #"Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+
+    keywords="minikube, k8s, minikube services",
+    package_dir={"": "src"},  
+    packages=find_packages(where="src"),
+    python_requires=">=3.9, <4",
+    
+    entry_points={  
+        "console_scripts": [
+            "kservice=kservice.main:main",
+        ],
+    },
+    
+    project_urls={  
+        "Bug Reports": "https://github.com/dszortyka/minikservices/issues",
+        "Funding": "https://donate.pypi.org",
+        #"Say Thanks!": "http://saythanks.io/to/minikservices",
+        "Source": "https://github.com/dszortyka/minikservices/",
+    },
+)
