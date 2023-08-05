269ac62e635c3ed6d2cd6e4829420bf4d55a7f82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:41 +0000

    Add package ai-automation/2.8.0/ai_automation-2.8.0.tar.gz

---
+++ packages/ai-automation/ai_automation-2.8.0.tar.gz/ai_automation-2.8.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ai_automation',
+    version='2.8.0',
+    license='MIT',
+    author="SheikhChili",
+    author_email='sparrow.shreyas@gmail.com',
+    packages=['aiautomation', 'aiautomation/mlpackage/classification', 'aiautomation/mlpackage',
+              'aiautomation/mlpackage/multiclassification', 'aiautomation/mlpackage/regression',
+              'aiautomation/mlpackage/segmentation'],
+    # package_dir={'': 'src'},
+    url='https://github.com/SheikhChili/aipackage',
+    keywords='AI Automation',
+    install_requires=[
+        'scikit-learn', 'xgboost', 'catboost', 'lightgbm', 'numpy', 'pandas', 'tensorflow', 'imblearn', 'optuna',
+        'tpot', 'hyperopt', 'yellowbrick', 'librosa', 'keras_tuner', 'spacy', 'unidecode', 'bs4', 'dtale',
+        'lime', 'matplotlib', 'shap', 'sweetviz', 'aix360', 'explainerdashboard', 'pandas_profiling',
+        'pandas_visual_analysis'
+    ],
+
+)
