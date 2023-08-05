3af13fc7e378a87b633ee224f7b482cd151cc7a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:10 +0000

    Add package freqtrade/2022.9.1/freqtrade-2022.9.1.tar.gz

---
+++ packages/freqtrade/freqtrade-2022.9.1.tar.gz/freqtrade-2022.9.1/freqtrade/freqai/prediction_models/CatboostClassifier.py
@@ -0,0 +1,44 @@
+import logging
+from typing import Any, Dict
+
+from catboost import CatBoostClassifier, Pool
+
+from freqtrade.freqai.base_models.BaseClassifierModel import BaseClassifierModel
+from freqtrade.freqai.data_kitchen import FreqaiDataKitchen
+
+
+logger = logging.getLogger(__name__)
+
+
+class CatboostClassifier(BaseClassifierModel):
+    """
+    User created prediction model. The class needs to override three necessary
+    functions, predict(), train(), fit(). The class inherits ModelHandler which
+    has its own DataHandler where data is held, saved, loaded, and managed.
+    """
+
+    def fit(self, data_dictionary: Dict, dk: FreqaiDataKitchen, **kwargs) -> Any:
+        """
+        User sets up the training and test data to fit their desired model here
+        :params:
+        :data_dictionary: the dictionary constructed by DataHandler to hold
+        all the training and test data/labels.
+        """
+
+        train_data = Pool(
+            data=data_dictionary["train_features"],
+            label=data_dictionary["train_labels"],
+            weight=data_dictionary["train_weights"],
+        )
+
+        cbr = CatBoostClassifier(
+            allow_writing_files=False,
+            loss_function='MultiClass',
+            **self.model_training_parameters,
+        )
+
+        init_model = self.get_init_model(dk.pair)
+
+        cbr.fit(train_data, init_model=init_model)
+
+        return cbr
