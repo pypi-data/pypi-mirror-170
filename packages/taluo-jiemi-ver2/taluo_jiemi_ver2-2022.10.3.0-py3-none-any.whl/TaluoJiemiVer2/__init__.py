89ea9876e5b7fc253ef7f19bf412fc1f458a6717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:47 +0000

    Add package taluo-jiemi-ver2/2022.10.3.0/taluo_jiemi_ver2-2022.10.3.0-py3-none-any.whl

---
+++ packages/taluo-jiemi-ver2/taluo_jiemi_ver2-2022.10.3.0-py3-none-any.whl/TaluoJiemiVer2/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""taluo-jiemi-ver2
+https://github.com/apachecn/taluo-jiemi-ver2"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.3.0"
