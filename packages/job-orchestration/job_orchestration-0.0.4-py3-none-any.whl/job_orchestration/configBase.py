c8bf9d498857b0047358b349befaca4f675204e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:41 +0000

    Add package job-orchestration/0.0.4/job_orchestration-0.0.4-py3-none-any.whl

---
+++ packages/job-orchestration/job_orchestration-0.0.4-py3-none-any.whl/job_orchestration/configBase.py
@@ -0,0 +1,22 @@
+from abc import ABC
+
+from job_orchestration.TaskBase import TaskBase
+
+
+class Dict2Class(object):
+    def __init__(self, input_dict):
+        self.raw_dict = input_dict
+        for key in input_dict:
+            setattr(self, key, input_dict[key])
+
+    def validate(self):
+        errs = []
+        for param in self.__class__.__annotations__:
+            if param not in vars(self):
+                errs.append("{} missing from {}".format(param, self.__class__.__name__))
+        return errs
+
+
+class TaskWithInitAndValidate(TaskBase, Dict2Class, ABC):
+    def __init__(self, config: dict):
+        Dict2Class.__init__(self, config)
