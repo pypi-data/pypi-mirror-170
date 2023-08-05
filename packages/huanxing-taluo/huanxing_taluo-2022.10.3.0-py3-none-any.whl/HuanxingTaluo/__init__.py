66dbc0f7fea23919bc6431d17d6e0155df513bdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:37 +0000

    Add package huanxing-taluo/2022.10.3.0/huanxing_taluo-2022.10.3.0-py3-none-any.whl

---
+++ packages/huanxing-taluo/huanxing_taluo-2022.10.3.0-py3-none-any.whl/HuanxingTaluo/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""huanxing-taluo
+https://github.com/apachecn/huanxing-taluo"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.3.0"
