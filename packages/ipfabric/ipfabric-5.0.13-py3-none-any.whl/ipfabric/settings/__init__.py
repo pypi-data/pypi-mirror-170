fb55a3a70b3600d2a337910f5cf8728e373cfe56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:25 +0000

    Add package ipfabric/5.0.13/ipfabric-5.0.13-py3-none-any.whl

---
+++ packages/ipfabric/ipfabric-5.0.13-py3-none-any.whl/ipfabric/settings/__init__.py
@@ -0,0 +1,37 @@
+from .api_tokens import APIToken
+from .attributes import Attributes
+from .authentication import Authentication
+from .seeds import Seeds
+from .user_mgmt import UserMgmt
+from .vendor_api import VendorAPI
+from .vendor_api_models import (
+    AWS,
+    Azure,
+    CheckPointApiKey,
+    CheckPointUserAuth,
+    CiscoFMC,
+    Merakiv1,
+    NSXT,
+    SilverPeak,
+    Versa,
+    Viptela,
+)
+
+__all__ = [
+    "Authentication",
+    "Seeds",
+    "APIToken",
+    "UserMgmt",
+    "Attributes",
+    "VendorAPI",
+    "AWS",
+    "Azure",
+    "CheckPointApiKey",
+    "CheckPointUserAuth",
+    "CiscoFMC",
+    "Merakiv1",
+    "NSXT",
+    "SilverPeak",
+    "Versa",
+    "Viptela",
+]
