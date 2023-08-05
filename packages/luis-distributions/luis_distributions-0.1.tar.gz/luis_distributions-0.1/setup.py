eea2ef21eddcd29051704a3a992d4634d523fe7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:31 +0000

    Add package luis-distributions/0.1/luis_distributions-0.1.tar.gz

---
+++ packages/luis-distributions/luis_distributions-0.1.tar.gz/luis_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='luis_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['luis_distributions'],
+      zip_safe=False)
