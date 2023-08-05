8fa5b3cb3b996801893e7d643baf0ba1d03f9e11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:56 +0000

    Add package tw-calculator/0.0.1/tw_calculator-0.0.1.tar.gz

---
+++ packages/tw-calculator/tw_calculator-0.0.1.tar.gz/tw_calculator-0.0.1/tw_calculator/logic.py
@@ -0,0 +1,15 @@
+def tw_sum(num1,num2):
+    result = num1 + num2
+    print('Sum result is : ' + str(result))
+
+def tw_sub(num1,num2):
+    result = num1 - num2
+    print('Sub result is : ' + str(result))
+
+def tw_mutiply(num1,num2):
+    result = num1 * num2
+    print('Multply result is : ' + str(result))
+
+def tw_divide(num1,num2):
+    result = num1 / num2
+    print('Divide result is : ' + str(result))
