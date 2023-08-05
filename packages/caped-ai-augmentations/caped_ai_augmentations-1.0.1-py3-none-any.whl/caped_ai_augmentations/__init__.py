3f3407d0fba3dafdde41a7b2d7e2b0af780a2911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:59 +0000

    Add package caped-ai-augmentations/1.0.1/caped_ai_augmentations-1.0.1-py3-none-any.whl

---
+++ packages/caped-ai-augmentations/caped_ai_augmentations-1.0.1-py3-none-any.whl/caped_ai_augmentations/__init__.py
@@ -0,0 +1,5 @@
+from .TemporalAug import TemporalAug
+from .TemporalZAug import TemporalZAug
+from .Augmentation2D import Augmentation2D
+from .Augmentation2DC import Augmentation2DC
+from .Augmentation3D import Augmentation3D
