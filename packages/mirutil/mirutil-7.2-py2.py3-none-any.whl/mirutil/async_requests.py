5fbfea73301ad8bb42820aa7f2add72d20951ff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:03 +0000

    Add package mirutil/7.2/mirutil-7.2-py2.py3-none-any.whl

---
+++ packages/mirutil/mirutil-7.2-py2.py3-none-any.whl/mirutil/async_requests.py
@@ -0,0 +1,76 @@
+"""
+
+    """
+
+import asyncio
+from functools import partial
+
+import nest_asyncio
+from aiohttp import ClientSession
+
+
+nest_asyncio.apply()
+
+# getting resp text async funcs
+async def get_a_resp_text_async(url ,
+                                headers ,
+                                trust_env ,
+                                params ,
+                                verify_ssl ,
+                                timeout) :
+    async with ClientSession(trust_env = trust_env) as ses :
+        async with ses.get(url ,
+                           params = params ,
+                           headers = headers ,
+                           verify_ssl = verify_ssl ,
+                           timeout = timeout) as resp :
+            if resp.status == 200 :
+                return await resp.text()
+
+async def get_reps_texts_async(urls ,
+                               headers = None ,
+                               trust_env = False ,
+                               params = None ,
+                               verify_ssl = True ,
+                               timeout = None) :
+    fu = partial(get_a_resp_text_async ,
+                 headers = headers ,
+                 trust_env = trust_env ,
+                 params = params ,
+                 verify_ssl = verify_ssl ,
+                 timeout = timeout)
+    co_tasks = [fu(x) for x in urls]
+    return await asyncio.gather(*co_tasks)
+
+# getting resp json async funcs
+
+async def get_a_resp_json_async(url ,
+                                headers ,
+                                trust_env ,
+                                params ,
+                                verify_ssl ,
+                                content_type) :
+    async with ClientSession(trust_env = trust_env) as ses :
+        async with ses.get(url ,
+                           headers = headers ,
+                           params = params ,
+                           verify_ssl = verify_ssl) as resp :
+            if resp.status == 200 :
+                return await resp.json(content_type = content_type)
+
+async def get_reps_jsons_async(urls ,
+                               headers = None ,
+                               trust_env = False ,
+                               params = None ,
+                               verify_ssl = True ,
+                               content_type = None) :
+    fu = partial(get_a_resp_json_async ,
+                 headers = headers ,
+                 trust_env = trust_env ,
+                 params = params ,
+                 verify_ssl = verify_ssl ,
+                 content_type = content_type)
+
+    co_tasks = [fu(x) for x in urls]
+
+    return await asyncio.gather(*co_tasks)
