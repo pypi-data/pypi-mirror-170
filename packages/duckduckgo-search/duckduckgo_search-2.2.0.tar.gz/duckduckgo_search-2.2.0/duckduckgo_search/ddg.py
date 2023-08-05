717d874e0df7a93e9c7d9063d481be940f01b029
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:55 +0000

    Add package duckduckgo-search/2.2.0/duckduckgo_search-2.2.0.tar.gz

---
+++ packages/duckduckgo-search/duckduckgo_search-2.2.0.tar.gz/duckduckgo_search-2.2.0/duckduckgo_search/ddg.py
@@ -0,0 +1,127 @@
+import logging
+
+from requests import ConnectionError
+
+from .utils import SESSION, _do_output, _get_vqd, _normalize
+
+logger = logging.getLogger(__name__)
+
+
+def ddg(
+    keywords,
+    region="wt-wt",
+    safesearch="Moderate",
+    time=None,
+    max_results=25,
+    output=None,
+):
+    """DuckDuckGo text search. Query params: https://duckduckgo.com/params
+
+    Args:
+        keywords (str): keywords for query.
+        region (str, optional): country - wt-wt, us-en, uk-en, ru-ru, etc. Defaults to "wt-wt".
+        safesearch (str, optional): On(kp=1), Moderate(kp=-1), Off(kp=-2). Defaults to "Moderate".
+        time (str, optional): 'd' (day), 'w' (week), 'm' (month), 'y' (year). Defaults to None.
+        max_results (int, optional): return not less than max_results, max=200. Defaults to 25.
+        output (str, optional): csv, json, print. Defaults to None.
+
+    Returns:
+        Optional[List[dict]]: DuckDuckGo text search results.
+    """
+
+    if not keywords:
+        return None
+
+    # get vqd
+    vqd = _get_vqd(keywords)
+    if not vqd:
+        return None
+
+    # search
+    safesearch_base = {"On": 1, "Moderate": -1, "Off": -2}
+    params = {
+        "q": keywords,
+        "l": region,
+        "p": safesearch_base[safesearch],
+        "s": 0,
+        "df": time,
+        "o": "json",
+        "vqd": vqd,
+    }
+
+    results, cache = [], set()
+    while len(results) < max_results and params["s"] < 200:
+        # request search results from duckduckgo
+        page_data = None
+        try:
+            resp = SESSION.get("https://links.duckduckgo.com/d.js", params=params)
+            logger.info(
+                "%s %s %s", resp.status_code, resp.url, resp.elapsed.total_seconds()
+            )
+            page_data = resp.json().get("results", None)
+        except ConnectionError:
+            logger.error("Connection Error.")
+            break
+        except Exception:
+            logger.exception("Exception.", exc_info=True)
+            break
+
+        if not page_data:
+            break
+
+        page_results = []
+        for i, row in enumerate(page_data):
+
+            # try pagination
+            if "n" in row:
+                params["s"] += i
+                break
+
+            # collect results
+            if row["u"] not in cache:
+                cache.add(row["u"])
+                body = _normalize(row["a"])
+                if body:
+                    page_results.append(
+                        {
+                            "title": _normalize(row["t"]),
+                            "href": row["u"],
+                            "body": body,
+                        }
+                    )
+        if not page_results:
+            break
+        results.extend(page_results)
+
+    """ using html method
+    payload = {
+        'q': keywords,
+        'l': region,
+        'p': safesearch_base[safesearch],
+        'df': time
+        }
+    results = []
+    while True:
+        res = SESSION.post('https://html.duckduckgo.com/html', data=payload, **kwargs)
+        tree = html.fromstring(res.text)
+        if tree.xpath('//div[@class="no-results"]/text()'):
+            return results
+        for element in tree.xpath('//div[contains(@class, "results_links")]'):
+            results.append({
+                'title': element.xpath('.//a[contains(@class, "result__a")]/text()')[0],
+                'href': element.xpath('.//a[contains(@class, "result__a")]/@href')[0],
+                'body': ''.join(element.xpath('.//a[contains(@class, "result__snippet")]//text()')),
+            })
+        if len(results) >= max_results:
+            return results
+        next_page = tree.xpath('.//div[@class="nav-link"]')[-1]
+        names = next_page.xpath('.//input[@type="hidden"]/@name')
+        values = next_page.xpath('.//input[@type="hidden"]/@value')
+        payload = {n: v for n, v in zip(names, values)}
+        sleep(2)
+    """
+
+    results = results[:max_results]
+    if output:
+        _do_output(__name__, keywords, output, results)
+    return results
---
+++ packages/duckduckgo-search/duckduckgo_search-2.2.0.tar.gz/duckduckgo_search-2.2.0/duckduckgo_search/utils.py
@@ -0,0 +1,124 @@
+import csv
+import json
+import logging
+import os
+import re
+from datetime import datetime
+from time import sleep
+
+import requests
+from requests import ConnectionError, Timeout
+
+SESSION = requests.Session()
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:102.0) Gecko/20100101 Firefox/102.0",
+    "Referer": "https://duckduckgo.com/",
+}
+SESSION.headers.update(HEADERS)
+
+logger = logging.getLogger(__name__)
+
+RE_CLEAN_HTML = re.compile("<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});")
+RE_VQD = re.compile(r"vqd=([0-9-]+)\&")
+VQD_DICT = dict()
+
+
+def _get_vqd(keywords):
+    global SESSION
+
+    vqd = VQD_DICT.get(keywords, None)
+    if vqd:
+        return vqd
+
+    payload = {"q": keywords}
+    for _ in range(2):
+        try:
+            resp = SESSION.post(
+                "https://duckduckgo.com", data=payload, headers=HEADERS, timeout=10
+            )
+            if resp.status_code == 200:
+                logger.info(
+                    "%s %s %s", resp.status_code, resp.url, resp.elapsed.total_seconds()
+                )
+                vqd = RE_VQD.search(resp.text).group(1)
+                if vqd:
+                    # delete the first key to reduce memory consumption
+                    if len(VQD_DICT) >= 32768:
+                        VQD_DICT.pop(next(iter(VQD_DICT)))
+                    VQD_DICT[keywords] = vqd
+                    logger.info("keywords=%s. Got vqd=%s", keywords, vqd)
+                    return vqd
+            logger.info("get_vqd(). response=%s", resp.status_code)
+        except Timeout:
+            logger.warning("Connection timeout in get_vqd().")
+        except ConnectionError:
+            logger.warning("Connection error in get_vqd().")
+        except Exception:
+            logger.exception("Exception in get_vqd().", exc_info=True)
+
+        # refresh SESSION if not vqd
+        SESSION = requests.Session()
+        SESSION.headers.update(HEADERS)
+        logger.warning(
+            "keywords=%s. _get_vqd() is None. Refresh SESSION and retry...", keywords
+        )
+        VQD_DICT.pop(keywords, None)
+        sleep(1)
+
+    # sleep to prevent blocking
+    sleep(1)
+
+
+def _save_json(jsonfile, data):
+    with open(jsonfile, "w") as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+
+
+def _save_csv(csvfile, data):
+    with open(csvfile, "w", newline="", encoding="utf-8") as file:
+        if data:
+            headers = data[0].keys()
+            writer = csv.DictWriter(file, fieldnames=headers, quoting=csv.QUOTE_MINIMAL)
+            writer.writeheader()
+            writer.writerows(data)
+
+
+def _download_image(image_url, dir_path, filename):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:102.0) Gecko/20100101 Firefox/102.0"
+    }
+    for _ in range(2):
+        try:
+            resp = requests.get(image_url, headers=headers, timeout=10)
+            if resp.status_code == 200:
+                with open(os.path.join(dir_path, filename), "wb") as file:
+                    file.write(resp.content)
+                    logger.info("Image downloaded. image_url=%s", image_url)
+                break
+        except Timeout:
+            logger.warning("Connection timeout. image_url=%s", image_url)
+        except ConnectionError:
+            logger.warning("Connection error. image_url=%s", image_url)
+        except Exception:
+            logger.warning("Exception. {image_url=}.", exc_info=True)
+
+
+def _normalize(raw_html):
+    if raw_html:
+        return re.sub(RE_CLEAN_HTML, "", raw_html)
+
+
+def _do_output(module_name, keywords, output, results):
+    keywords = keywords.replace('"', "'")
+    if output == "csv":
+        _save_csv(
+            f"{module_name}_{keywords}_{datetime.now():%Y%m%d_%H%M%S}.csv", results
+        )
+    elif output == "json":
+        _save_json(
+            f"{module_name}_{keywords}_{datetime.now():%Y%m%d_%H%M%S}.json", results
+        )
+    elif output == "print":
+        for i, result in enumerate(results, start=1):
+            print(f"{i}.", json.dumps(result, ensure_ascii=False, indent=4))
+            input()
