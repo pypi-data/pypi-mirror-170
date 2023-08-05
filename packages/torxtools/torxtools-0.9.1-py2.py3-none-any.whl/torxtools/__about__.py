a5826a7e609dbc412b6851c7e890eec0d853ce6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:57 +0000

    Add package torxtools/0.9.1/torxtools-0.9.1-py2.py3-none-any.whl

---
+++ packages/torxtools/torxtools-0.9.1-py2.py3-none-any.whl/torxtools/__about__.py
@@ -0,0 +1,22 @@
+__all__ = [
+    "__author__",
+    "__copyright__",
+    "__email__",
+    "__license__",
+    "__summary__",
+    "__title__",
+    "__uri__",
+    "__version__",
+]
+
+__title__ = "torxtools"
+__summary__ = "Less generic functionalities than Phillips and Pozidriv tools."
+__uri__ = "https://gitlab.com/jlecomte/projects/torxtools"
+
+__version__ = "0.9.1"
+
+__author__ = "Julien Lecomte"
+__email__ = "julien@lecomte.at"
+
+__license__ = "MIT"
+__copyright__ = "2020-2022 %s" % __author__
