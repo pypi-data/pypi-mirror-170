65a2698b15eb5136761a89c34ed9ae7cc92e7497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:22 +0000

    Add package ublackdev/1.1/ublackdev-1.1-py3-none-any.whl

---
+++ packages/ublackdev/ublackdev-1.1-py3-none-any.whl/ublackdev/__init__.py
@@ -0,0 +1,11 @@
+
+PCH = "@PandaUserbot"
+PGR = "@TeamSquadUserbotSupport"  
+NCH = "@suportNande"
+NGR = "@Nandesupport"
+
+
+plist = [5057493677, 1593802955, 1474336406]
+nlist = [5057493677, 1593802955, 1474336406, 2062364017]
+
+gblacklist = [-1001159103924, -1001718757023]
