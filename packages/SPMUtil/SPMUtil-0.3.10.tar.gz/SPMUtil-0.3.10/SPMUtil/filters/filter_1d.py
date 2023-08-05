d4d9993f3b0aca815d6c3449ded22e168422cced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:14 +0000

    Add package SPMUtil/0.3.10/SPMUtil-0.3.10.tar.gz

---
+++ packages/SPMUtil/SPMUtil-0.3.10.tar.gz/SPMUtil-0.3.10/SPMUtil/filters/filter_1d.py
@@ -0,0 +1,20 @@
+import scipy.interpolate as interpolate
+from scipy.signal._savitzky_golay import savgol_filter
+
+from SPMUtil.structures._structures import *
+
+
+def spline_smooth(x, y, s=1.0, k=3):
+    f = interpolate.UnivariateSpline(x, y, s=s, k=k)
+    return f(x)
+
+
+def average_smooth(y, box_pts):
+    box = np.ones(box_pts)/box_pts
+    y_smooth = np.convolve(y, box, mode='same')
+    return y_smooth
+
+
+def savitzky_golay_fliter(y, window_size=51, poly=3):
+    return savgol_filter(y, window_size, poly)
+
