752e56d016f7b7310ed0232125ece8587b77bdac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:03 +0000

    Add package boreal/0.5.0/boreal-0.5.0-py2.py3-none-any.whl

---
+++ packages/boreal/boreal-0.5.0-py2.py3-none-any.whl/boreal/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for Boreal."""
+
+__author__ = """George Tzanetakis"""
+__email__ = 'gtzan@cs.uvic.ca'
+__version__ = '0.5.0'
+
+
+
+from .boreal import * 
+from .audio_widgets import * 
