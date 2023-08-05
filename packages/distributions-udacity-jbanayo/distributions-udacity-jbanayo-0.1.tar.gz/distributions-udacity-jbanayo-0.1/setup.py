0adbc051f03ccbf1634d96ceb18e536ce46a144b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:52 +0000

    Add package distributions-udacity-jbanayo/0.1/distributions-udacity-jbanayo-0.1.tar.gz

---
+++ packages/distributions-udacity-jbanayo/distributions-udacity-jbanayo-0.1.tar.gz/distributions-udacity-jbanayo-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions-udacity-jbanayo',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_udacity_jbanayo'],
+      zip_safe=False)
