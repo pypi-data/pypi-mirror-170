4b93b5a2251dd6e4b03914909a5a3673ccee9821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:53 +0000

    Add package IntelligentOptimizationAl/0.0.1/IntelligentOptimizationAl-0.0.1.tar.gz

---
+++ packages/IntelligentOptimizationAl/IntelligentOptimizationAl-0.0.1.tar.gz/IntelligentOptimizationAl-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(name='IntelligentOptimizationAl',
+      version='0.0.1',
+      description='Intelligent optimization algorithm,PSO、GWO',
+      author='inswsi',
+      author_email='2471364272@qq.com',
+      requires= ['numpy'],
+      packages=find_packages()
+      )
