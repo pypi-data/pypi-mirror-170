f9a17e19a6b40d5abbf47337f0eddf8546e2df08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:12 +0000

    Add package xiandai-zhanxingxue-jinjie-vol2/2022.10.3.0/xiandai-zhanxingxue-jinjie-vol2-2022.10.3.0.tar.gz

---
+++ packages/xiandai-zhanxingxue-jinjie-vol2/xiandai-zhanxingxue-jinjie-vol2-2022.10.3.0.tar.gz/xiandai-zhanxingxue-jinjie-vol2-2022.10.3.0/XiandaiZhanxingxueJinjieVol2/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xiandai-zhanxingxue-jinjie-vol2
+https://github.com/apachecn/xiandai-zhanxingxue-jinjie-vol2"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.3.0"
