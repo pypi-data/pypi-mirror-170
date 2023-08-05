5fe8ebb6ef3aca78f399ee2ae911b6c661571ca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:43 +0000

    Add package TinyTinyDebugger/0.11/TinyTinyDebugger-0.11.tar.gz

---
+++ packages/TinyTinyDebugger/TinyTinyDebugger-0.11.tar.gz/TinyTinyDebugger-0.11/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+#change to dict
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(r"C:\Users\Gamer\anaconda3\envs\dfdir\_tmp_PyGitUpload_000012\README.md", encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.11'
+DESCRIPTION = "Simple debugger for small scripts, shows the line being executed, local vars, log files as xlsx"
+
+# Setting up
+setup(
+    name="TinyTinyDebugger",
+    version=VERSION,
+    license='MIT',
+    url = 'https://github.com/hansalemaos/TinyTinyDebugger',
+    author="Johannes Fischer",
+    author_email="<aulasparticularesdealemaosp@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    #packages=['cprinter', 'decorator', 'input_timeout', 'pandas', 'PrettyColorPrinter'],
+    keywords=['debugger', 'debugging'],
+    classifiers=['Development Status :: 4 - Beta', 'Programming Language :: Python :: 3 :: Only', 'Topic :: Scientific/Engineering :: Visualization', 'Topic :: Software Development :: Libraries :: Python Modules', 'Topic :: Text Editors :: Text Processing', 'Topic :: Text Processing :: General', 'Topic :: Text Processing :: Indexing', 'Topic :: Text Processing :: Filters', 'Topic :: Utilities'],
+    install_requires=['cprinter', 'decorator', 'input_timeout', 'pandas', 'PrettyColorPrinter'],
+    include_package_data=True
+)
+#python setup.py sdist bdist_wheel
+#twine upload dist/*
