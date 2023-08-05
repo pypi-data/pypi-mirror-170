90da91148eb13111515333f2e68d01c92ca201ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:36 +0000

    Add package redbrick-sdk/2.2.1/redbrick_sdk-2.2.1-py3-none-any.whl

---
+++ packages/redbrick-sdk/redbrick_sdk-2.2.1-py3-none-any.whl/redbrick/repo/shards.py
@@ -0,0 +1,169 @@
+"""Partial queries to prevent duplication."""
+
+TAXONOMY_SHARD = """
+orgId
+name
+version
+createdAt
+categories {
+    name
+    children {
+        name
+        classId
+        disabled
+        children {
+            name
+            classId
+            disabled
+            children {
+                name
+                classId
+                disabled
+                children {
+                    name
+                    classId
+                    disabled
+                    children {
+                        name
+                        classId
+                        disabled
+                        children {
+                            name
+                            classId
+                            disabled
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+attributes {
+    name
+    attrType
+    whitelist
+    disabled
+}
+taskCategories {
+    name
+    children {
+        name
+        classId
+        disabled
+        children {
+            name
+            classId
+            disabled
+            children {
+                name
+                classId
+                disabled
+                children {
+                    name
+                    classId
+                    disabled
+                    children {
+                        name
+                        classId
+                        disabled
+                        children {
+                            name
+                            classId
+                            disabled
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+taskAttributes {
+    name
+    attrType
+    whitelist
+    disabled
+}
+colorMap {
+    name
+    color
+    classid
+    trail
+    taskcategory
+}
+archived
+"""
+
+TASK_DATA_SHARD = """
+    createdAt
+    createdByEmail
+    labelsData(interpolate: true)
+    labelsStorage {
+        storageId
+    }
+    labelsMap(presigned: false) {
+        labelName
+    }
+"""
+
+TASK_SHARD = f"""
+    ... on LabelingTask {{
+        state
+        assignedTo {{
+            userId
+            email
+        }}
+        taskData {{
+            {TASK_DATA_SHARD}
+        }}
+        subTasks {{
+            state
+            assignedTo {{
+                userId
+                email
+            }}
+            taskData {{
+                {TASK_DATA_SHARD}
+            }}
+        }}
+        overallConsensusScore
+        consensusInfo {{
+            user {{
+                userId
+                email
+            }}
+            taskData {{
+                {TASK_DATA_SHARD}
+            }}
+            scores {{
+                user {{
+                    userId
+                    email
+                }}
+                score
+            }}
+        }}
+    }}
+"""
+
+DATAPOINT_SHARD = """
+    dataPoint {{
+        name
+        items(presigned: false)
+        {}
+        createdAt
+        createdByEntity {{
+            email
+        }}
+        metaData
+        seriesInfo {{
+            name
+            itemsIndices
+            dataType
+            numFrames
+            dimensions
+        }}
+        storageMethod {{
+            storageId
+        }}
+    }}
+"""
