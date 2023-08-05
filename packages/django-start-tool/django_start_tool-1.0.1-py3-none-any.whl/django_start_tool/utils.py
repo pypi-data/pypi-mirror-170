5268287e50d2cd802d47e6cbcab08ff5bcc9356d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:49 +0000

    Add package django-start-tool/1.0.1/django_start_tool-1.0.1-py3-none-any.whl

---
+++ packages/django-start-tool/django_start_tool-1.0.1-py3-none-any.whl/django_start_tool/utils.py
@@ -0,0 +1,33 @@
+import random
+from pathlib import Path
+from urllib.request import urlretrieve
+from zipfile import ZipFile
+
+
+def download(url: str) -> None:
+    content, _ = urlretrieve(url)
+
+    with ZipFile(content) as archive:
+        archive.extractall()
+
+    return get_archive_subdirectory_name(url)
+
+
+def entity_matches_one_of_patterns(entity: Path, patterns: list[str]) -> bool:
+    return any(entity.match(pattern) for pattern in patterns)
+
+
+def generate_secret_key() -> str:
+    chars = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)"
+    return "django-insecure-" + "".join(random.choice(chars) for _ in range(50))
+
+
+def get_archive_subdirectory_name(url: str) -> str:
+    url = url.replace("://", "").split("/")
+    repo = url[2]
+    branch = url[4].replace(".zip", "")
+    return f"{repo}-{branch}"
+
+
+def is_url(path: str) -> bool:
+    return "https" in path
