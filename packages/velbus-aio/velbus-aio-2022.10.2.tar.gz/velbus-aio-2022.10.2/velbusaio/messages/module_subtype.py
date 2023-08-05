7d757005e018bcd101c2ea8ae619b8d4579b6cc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:57 +0000

    Add package velbus-aio/2022.10.2/velbus-aio-2022.10.2.tar.gz

---
+++ packages/velbus-aio/velbus-aio-2022.10.2.tar.gz/velbus-aio-2022.10.2/velbusaio/messages/module_subtype.py
@@ -0,0 +1,55 @@
+"""
+:author: Thomas Delaet <thomas@delaet.org>
+"""
+from __future__ import annotations
+
+import json
+import struct
+
+from velbusaio.command_registry import register_command
+from velbusaio.message import Message
+
+COMMAND_CODE = 0xB0
+
+
+class ModuleSubTypeMessage(Message):
+    """
+    send by: VMB6IN, VMB4RYLD
+    received by:
+    """
+
+    # pylint: disable-msg=R0902
+
+    def __init__(self, address=None):
+        Message.__init__(self)
+        self.module_type = 0x00
+        self.sub_address_1 = 0xFF
+        self.sub_address_2 = 0xFF
+        self.sub_address_3 = 0xFF
+        self.sub_address_4 = 0xFF
+        self.set_defaults(address)
+        self.serial = 0
+
+    def module_name(self):
+        """
+        :return: str
+        """
+        return "Unknown"
+
+    def populate(self, priority, address, rtr, data):
+        """
+        :return: None
+        """
+        self.needs_low_priority(priority)
+        self.needs_no_rtr(rtr)
+        # self.needs_data(data, 6)
+        self.set_attributes(priority, address, rtr)
+        self.module_type = data[0]
+        (self.serial,) = struct.unpack(">L", bytes([0, 0, data[1], data[2]]))
+        self.sub_address_1 = data[3]
+        self.sub_address_2 = data[4]
+        self.sub_address_3 = data[5]
+        self.sub_address_4 = data[6]
+
+
+register_command(COMMAND_CODE, ModuleSubTypeMessage)
