ff81de834d9b714987ca5194ef41eabec871a907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:27 +0000

    Add package mypy/0.982/mypy-0.982.tar.gz

---
+++ packages/mypy/mypy-0.982.tar.gz/mypy-0.982/test-data/unit/lib-stub/types.pyi
@@ -0,0 +1,16 @@
+from typing import TypeVar
+import sys
+
+_T = TypeVar('_T')
+
+def coroutine(func: _T) -> _T: pass
+
+class ModuleType:
+    __file__ = ... # type: str
+
+if sys.version_info >= (3, 10):
+    class Union:
+        def __or__(self, x) -> Union: ...
+
+    class NoneType:
+        ...
