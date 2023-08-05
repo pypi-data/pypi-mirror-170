565f6815c340bf2d0b96cf2277352ecdd8c99ca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:45 +0000

    Add package Remilia/2022.10.3.7.4.22/Remilia-2022.10.3.7.4.22.tar.gz

---
+++ packages/Remilia/Remilia-2022.10.3.7.4.22.tar.gz/Remilia-2022.10.3.7.4.22/Remilia/base/Files.py
@@ -0,0 +1,62 @@
+from abc import ABC,abstractmethod
+from ..LiteResource import Path
+
+
+class CommonFileBase(ABC):
+    @abstractmethod
+    def _read(
+        self,
+        *args,
+        **kwargs,
+        ) -> None:
+        return any
+
+    @abstractmethod
+    def write(
+        self,
+        *args,
+        **kwargs,
+        ) -> None:
+        return None
+    
+
+class KVFileBase(CommonFileBase):
+    def __init__(
+        self,
+        path:Path,
+        noneText:str=""
+        ) -> None:
+        self.encoding=path.encoding
+        if path.isexist:
+            if path.text == "":
+                with open(path.abspath,"w",encoding=self.encoding) as File:
+                    File.write(noneText)
+        else:
+            with open(path.abspath,"w",encoding=self.encoding) as File:
+                File.write(noneText)
+        self.path=path
+    
+    @property
+    def Dict(self):
+        return self._read()
+    
+    @abstractmethod
+    def _read(self) -> dict:
+        return {
+            str:...,str:...
+        }
+    
+    def read(
+        self,
+        key,
+        ) -> None:
+        return self._read()[key]
+
+    @abstractmethod
+    def write(
+        self,
+        key,
+        value
+        ) -> None:
+        return None
+    
