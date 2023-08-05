e88b766312cd510c77dbf9f609bf61f7b2ad44cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:11 +0000

    Add package xerxes-protocol/0.0.4/xerxes_protocol-0.0.4.tar.gz

---
+++ packages/xerxes-protocol/xerxes_protocol-0.0.4.tar.gz/xerxes_protocol-0.0.4/xerxes_protocol/units/angle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from math import pi
+from .unit import Unit
+
+class Angle(Unit):
+    @property
+    def degrees(self):
+        return 180*self.value/pi
+
+    @property
+    def rad(self):
+        return self.value
+    
+    @staticmethod
+    def from_degrees(deg):
+        return Angle(pi*deg/180)
+
+    def __repr__(self):
+        return f"Angle({self.value})"
+
+    @property
+    def preferred(self):
+        return self.rad
