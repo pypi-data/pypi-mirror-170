33a93a6786afec1bc1fc945563733a30ef72d040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:15 +0000

    Add package distributions-jcab/0.1/distributions_jcab-0.1.tar.gz

---
+++ packages/distributions-jcab/distributions_jcab-0.1.tar.gz/distributions_jcab-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_jcab',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_jcab'],
+      zip_safe=False)
