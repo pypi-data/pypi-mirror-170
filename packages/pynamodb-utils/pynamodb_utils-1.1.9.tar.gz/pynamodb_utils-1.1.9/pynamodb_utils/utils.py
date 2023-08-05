7208e7d4e2bfcb4512e4a67c2e9ae204b8323b36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:22 +0000

    Add package pynamodb-utils/1.1.9/pynamodb_utils-1.1.9.tar.gz

---
+++ packages/pynamodb-utils/pynamodb_utils-1.1.9.tar.gz/pynamodb_utils-1.1.9/pynamodb_utils/utils.py
@@ -0,0 +1,43 @@
+from datetime import datetime, timezone
+
+from pynamodb.attributes import MapAttribute
+
+from pynamodb_utils.attributes import DynamicMapAttribute
+from pynamodb_utils.exceptions import FilterError
+
+
+def parse_attr(attr):
+    if isinstance(attr, DynamicMapAttribute):
+        return attr.as_dict()
+    elif isinstance(attr, MapAttribute):
+        return parse_attrs_to_dict(attr)
+    elif isinstance(attr, datetime):
+        return datetime.isoformat(attr, sep='T')
+    return attr
+
+
+def parse_attrs_to_dict(obj):
+    return {k: parse_attr(getattr(obj, k, None)) for k in obj.get_attributes().keys()}
+
+
+def get_nested_attribute(model, attr_string, raise_exception=True):
+    attrs = attr_string.split('.')
+    result = model
+    for attr in attrs:
+        if isinstance(result, DynamicMapAttribute):
+            result = result[attr]
+        elif hasattr(result, attr):
+            result = getattr(result, attr)
+        elif raise_exception:
+            raise FilterError(
+                message={attr_string: [f'Parameter {attr} does not exist.'
+                                       f' Choose some of available: {", ".join(result.get_attributes())}']},
+                status_code=400
+            )
+        else:
+            return None
+    return result
+
+
+def get_timestamp(tzinfo=None):
+    return datetime.now(tzinfo or timezone.utc)
