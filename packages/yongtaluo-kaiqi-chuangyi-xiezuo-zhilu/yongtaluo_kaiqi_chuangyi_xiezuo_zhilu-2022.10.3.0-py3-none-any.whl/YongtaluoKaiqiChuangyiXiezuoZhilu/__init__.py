e47bb615e8e20c51d627876d74da523b3a7fae71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:00 +0000

    Add package yongtaluo-kaiqi-chuangyi-xiezuo-zhilu/2022.10.3.0/yongtaluo_kaiqi_chuangyi_xiezuo_zhilu-2022.10.3.0-py3-none-any.whl

---
+++ packages/yongtaluo-kaiqi-chuangyi-xiezuo-zhilu/yongtaluo_kaiqi_chuangyi_xiezuo_zhilu-2022.10.3.0-py3-none-any.whl/YongtaluoKaiqiChuangyiXiezuoZhilu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yongtaluo-kaiqi-chuangyi-xiezuo-zhilu
+https://github.com/apachecn/yongtaluo-kaiqi-chuangyi-xiezuo-zhilu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.3.0"
