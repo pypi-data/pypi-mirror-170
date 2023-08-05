5e43813ff671b713113b5ce27d8e5793ba5da169
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:12 +0000

    Add package pyscooter/0.1/pyscooter-0.1.tar.gz

---
+++ packages/pyscooter/pyscooter-0.1.tar.gz/pyscooter-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = "pyscooter",
+  packages = ["pyscooter"],
+  version = "0.1",
+  license="MIT",
+  author = "Bastel Pichi",
+  author_email = "pichi@pichisdns.com",
+  url = "https://github.com/BastelPichi/pyscooter",
+  download_url = "https://github.com/BastelPichi/pyscooter"
+
+)
