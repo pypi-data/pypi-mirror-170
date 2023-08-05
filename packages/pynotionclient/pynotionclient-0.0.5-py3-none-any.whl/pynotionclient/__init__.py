c125749c5551c490ce52967be57b428230c9c00d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:16 +0000

    Add package pynotionclient/0.0.5/pynotionclient-0.0.5-py3-none-any.whl

---
+++ packages/pynotionclient/pynotionclient-0.0.5-py3-none-any.whl/pynotionclient/__init__.py
@@ -0,0 +1,15 @@
+from src.pynotionclient.block import NotionBlock
+from src.pynotionclient.comment import NotionComment
+from src.pynotionclient.database import NotionDatabase
+from src.pynotionclient.page import NotionPage
+from src.pynotionclient.py_notion import PyNotion
+from src.pynotionclient.user import NotionUser
+
+__all__ = [
+    "NotionBlock",
+    "NotionComment",
+    "NotionDatabase",
+    "NotionPage",
+    "PyNotion",
+    "NotionUser",
+]
---
+++ packages/pynotionclient/pynotionclient-0.0.5-py3-none-any.whl/pynotionclient/schema/database/status_schema.py
@@ -0,0 +1,9 @@
+from src.pynotionclient.schema.database import IdNameSchema, IdTypeSchema
+
+
+class InternalStatusSchema(IdNameSchema):
+    color: str
+
+
+class StatusSchema(IdTypeSchema):
+    status: InternalStatusSchema
