becc3ae4ce9a821c6aec96c472a10a9e672de025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:12 +0000

    Add package yushen-duihua-1to5/2022.10.3.0/yushen_duihua_1to5-2022.10.3.0-py3-none-any.whl

---
+++ packages/yushen-duihua-1to5/yushen_duihua_1to5-2022.10.3.0-py3-none-any.whl/YushenDuihua1to5/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yushen-duihua-1to5
+https://github.com/apachecn/yushen-duihua-1to5"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.3.0"
