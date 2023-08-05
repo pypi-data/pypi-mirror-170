5097912b6c1be6475f40033d0b53e38f9cf5f1b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:46 +0000

    Add package toolkit4life/0.0.79/toolkit4life-0.0.79.tar.gz

---
+++ packages/toolkit4life/toolkit4life-0.0.79.tar.gz/toolkit4life-0.0.79/toolkit4life/clients/postgres.py
@@ -0,0 +1,18 @@
+# Third-party imports
+from ._sqlalchemy import SQLAlchemy
+
+
+class PostgresClient(SQLAlchemy):
+
+    def __init__(self, host: str, port: str, database: str, username: str, password: str) -> None:
+        """
+            Creates and initializes a PostgreSQL engine instance that connects to the database
+
+            Parameters:
+                host (str): Host IP address
+                port (str): Port number
+                database (str): Name of the database
+                username (str): Username for authentication/privileges
+                password (str): Password for authentication
+        """
+        super().__init__(engine = "postgres", host = host, port = port, database = database, username = username, password = password)
