fb2d894299995dd10652be9ce5247fa357be7fa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:19 +0000

    Add package mathme/0.1.0/mathme-0.1.0.tar.gz

---
+++ packages/mathme/mathme-0.1.0.tar.gz/mathme-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name='mathme',
+    packages=find_packages(),
+    version='0.1.0',
+    description='My first Python library',
+    author='KHM',
+    license='MIT',
+    install_requires=['']
+)
