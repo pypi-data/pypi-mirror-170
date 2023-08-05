31953b3bd5e2d34a8a6903a9671e4bcad2c449c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:44 +0000

    Add package bigchin/1.1.9/bigchin-1.1.9.tar.gz

---
+++ packages/bigchin/bigchin-1.1.9.tar.gz/bigchin-1.1.9/bigchin_api/__init__.py
@@ -0,0 +1,5 @@
+from .bigchin import (system, perform)
+
+__author__ = "DFWastaken"
+__version__ = "1.1.9"
+__email__ = "dfwastaken.work@gmail.com"
---
+++ packages/bigchin/bigchin-1.1.9.tar.gz/bigchin-1.1.9/setup.py
@@ -0,0 +1,31 @@
+from io import open
+from setuptools import setup
+from pathlib import Path
+
+version = "1.1.9"
+this_directory = Path(__file__).parent
+long_desc = (this_directory / "README.md").read_text()
+
+setup(
+    name = "bigchin",
+    version = version,
+    author = "DFWastaken",
+    author_email = "dfwastaken.work@gmail.com",
+    description = (
+        u"A small library for your projects"
+    ),
+    long_description = long_desc,
+    long_description_content_type = 'text/markdown',
+    url = "https://github.com/DFekatsaW/Big-Chin.",
+    download_url = "https://raw.githubusercontent.com/DFekatsaW/Big-Chin./main/bigchin.py",
+    license = "Apache License 2.0",
+    packages = ['bigchin_api'],
+    classifiers = [
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9"
+    ]
+)
