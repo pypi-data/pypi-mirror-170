b472fc5d82bea245c8a90abdad8f48d581923333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:16 +0000

    Add package mihai-lib/0.0.1/mihai_lib-0.0.1.tar.gz

---
+++ packages/mihai-lib/mihai_lib-0.0.1.tar.gz/mihai_lib-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name='mihai_lib',
+    version='0.0.1',
+    author='Angelica Barbu',
+    author_email='angibarbu@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    description='A nice library'
+)
