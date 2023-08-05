ac13b163be91c814c71a27bd59ea2bfe15349c53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:58 +0000

    Add package xerxes-protocol/0.0.3/xerxes_protocol-0.0.3-py2.py3-none-any.whl

---
+++ packages/xerxes-protocol/xerxes_protocol-0.0.3-py2.py3-none-any.whl/xerxes_protocol/hierarchy/leaves/utils.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from xerxes_protocol.hierarchy.leaves.inclination import ILeaf
+from xerxes_protocol.ids import DevId
+
+
+leaf_types = {
+    DevId.ANGLE_XY_90: ILeaf,
+}
+
