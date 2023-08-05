25c1c577a3c5f0de6b2b68c80a4bd06ad6fd9408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:12 +0000

    Add package pyautotrace/0.0.2/pyautotrace-0.0.2-cp37-cp37m-win_amd64.whl

---
+++ packages/pyautotrace/pyautotrace-0.0.2-cp37-cp37m-win_amd64.whl/autotrace/__init__.py
@@ -0,0 +1,12 @@
+"""Python bindings for AutoTrace."""
+
+from .autotrace import (  # noqa: F401
+    Bitmap,
+    Color,
+    Path,
+    Point,
+    PolynomialDegree,
+    Spline,
+    TraceOptions,
+    VectorImage,
+)
