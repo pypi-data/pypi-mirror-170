4b7970dba2cd0e7012d8b9a9a06d4f1f4b6c64e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:14 +0000

    Add package avrzero/0.1.dev0/avrzero-0.1.dev0-py3-none-any.whl

---
+++ packages/avrzero/avrzero-0.1.dev0-py3-none-any.whl/avrzero/__main__.py
@@ -0,0 +1,20 @@
+import argparse
+from pprint import pprint
+
+from avrzero.assembler import Assembler
+from avrzero.machine import Machine
+
+
+parser = argparse.ArgumentParser(
+    description="a simple AVR instruction set simulator"
+)
+parser.add_argument("file")
+args = parser.parse_args()
+
+with open(args.file, "r") as asm_file:
+    asm_source = asm_file.read()
+
+assembler = Assembler(asm_source)
+machine = Machine()
+
+print(assembler.assemble())
