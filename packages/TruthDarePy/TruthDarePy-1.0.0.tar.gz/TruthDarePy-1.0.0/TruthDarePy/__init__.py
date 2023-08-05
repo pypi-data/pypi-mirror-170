24df8177a99c6c80caa556ca48f92363ec6f7461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:35 +0000

    Add package TruthDarePy/1.0.0/TruthDarePy-1.0.0.tar.gz

---
+++ packages/TruthDarePy/TruthDarePy-1.0.0.tar.gz/TruthDarePy-1.0.0/TruthDarePy/__init__.py
@@ -0,0 +1,35 @@
+from requests import get
+
+class TD():
+    def __init__(self):
+        self.url = "https://api.truthordarebot.xyz/v1"
+
+    def truth(self):
+        try:
+            url = f"{self.url}/truth"
+            response = get(url, timeout=5)
+            return response.json()
+        except Exception as e:
+            return "Got An Error! Report At @ProgrammerSupport\n\n{}".format(e)
+
+    def dare(self):
+        try:
+            url = f"{self.url}/dare"
+            response = get(url, timeout=5)
+            return response.json()
+        except Exception as e:
+            return "Got An Error! Report At @ProgrammerSupport\n\n{}".format(e)
+
+    def languages():
+        try:
+            return """
+• English - question
+• বাংলা (Bengali) - bn
+• Deutsch (German) - de
+• Español (Spanish) - ea
+• Français (French) - fr
+• हिंदी (Hindi) - hi
+• Tagalog (Filipino) - tl
+  """
+        except Exception as e:
+            return "Got An Error! Report At @ProgrammerSupport\n\n{}".format(e)
