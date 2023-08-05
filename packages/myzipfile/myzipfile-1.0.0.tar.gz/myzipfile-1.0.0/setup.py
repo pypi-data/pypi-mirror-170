cf688c6dd842ccf642bf4f5af89eb1801311b334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:54 +0000

    Add package myzipfile/1.0.0/myzipfile-1.0.0.tar.gz

---
+++ packages/myzipfile/myzipfile-1.0.0.tar.gz/myzipfile-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='myzipfile',
+    version='1.0.0',
+    author='Tiancheng Jiao',
+    author_email='jtc1246@outlook.com',
+    url='https://github.com/jtc1246',
+    description='Fix UTF-8 encoding problems in official zipfile',
+    packages=['myzipfile'],
+    install_requires=[],
+    python_requires='>=3',
+    platforms=["all"],
+    license='GPL-2.0 License'
+)
+
