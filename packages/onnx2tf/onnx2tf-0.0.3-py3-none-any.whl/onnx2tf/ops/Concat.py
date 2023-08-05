c4fb96b6956367793fd460ce9a41694b0d7a42c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:29 +0000

    Add package onnx2tf/0.0.3/onnx2tf-0.0.3-py3-none-any.whl

---
+++ packages/onnx2tf/onnx2tf-0.0.3-py3-none-any.whl/onnx2tf/ops/Concat.py
@@ -0,0 +1,64 @@
+import random
+random.seed(0)
+import numpy as np
+np.random.seed(0)
+import tensorflow as tf
+import onnx_graphsurgeon as gs
+from onnx2tf.utils.common_functions import (
+    convert_axis,
+    get_constant_or_variable,
+    print_node_info,
+)
+
+
+@print_node_info
+def make_node(
+    *,
+    graph_node: gs.Node,
+    tf_layers_dict: dict,
+    **kwargs: dict,
+):
+    """Concat
+
+    Parameters
+    ----------
+    graph_node: gs.Node
+        graph_surgeon Node
+
+    tf_layers_dict: dict
+        optype, shape, dtype, tensorflow graph
+    """
+    values = []
+    for graph_node_input in graph_node.inputs:
+        const_or_var = get_constant_or_variable(graph_node_input)
+        if isinstance(const_or_var, gs.Variable):
+            values.append(tf_layers_dict[const_or_var.name]['tf_node'])
+        else:
+            values.append(const_or_var)
+
+    graph_node_output: gs.Variable = graph_node.outputs[0]
+
+    shape = graph_node_output.shape
+    dtype = graph_node_output.dtype
+
+    axis = graph_node.attrs.get('axis', 0)
+    # NCHW->NHWC, NCDHW->NDHWC
+    axis = convert_axis(
+        axis=axis,
+        tensor_rank=len(shape),
+    )
+
+    # Preserving Graph Structure (Dict)
+    tf_layers_dict[graph_node_output.name] = {
+        'optype': graph_node.op,
+        'shape': shape,
+        'dtype': dtype,
+    }
+
+    # Generation of TF OP
+    tf_layers_dict[graph_node_output.name]['tf_node'] = \
+        tf.concat(
+            values=values,
+            axis=axis,
+            name=graph_node.name,
+        )
---
+++ packages/onnx2tf/onnx2tf-0.0.3-py3-none-any.whl/onnx2tf/ops/ScatterND.py
@@ -0,0 +1,59 @@
+import random
+random.seed(0)
+import numpy as np
+np.random.seed(0)
+import tensorflow as tf
+import onnx_graphsurgeon as gs
+from onnx2tf.utils.common_functions import (
+    get_constant_or_variable,
+    print_node_info,
+)
+
+
+@print_node_info
+def make_node(
+    *,
+    graph_node: gs.Node,
+    tf_layers_dict: dict,
+    **kwargs: dict,
+):
+    """ScatterND
+
+    Parameters
+    ----------
+    graph_node: gs.Node
+        graph_surgeon Node
+
+    tf_layers_dict: dict
+        optype, shape, dtype, tensorflow graph
+    """
+    graph_node_input_1 = get_constant_or_variable(graph_node.inputs[0])
+    graph_node_input_2 = get_constant_or_variable(graph_node.inputs[1])
+    graph_node_input_3 = get_constant_or_variable(graph_node.inputs[2])
+    graph_node_output: gs.Variable = graph_node.outputs[0]
+    shape = graph_node_output.shape
+    dtype = graph_node_output.dtype
+
+    input_tensor = tf_layers_dict[graph_node_input_1.name]['tf_node'] \
+        if isinstance(graph_node_input_1, gs.Variable) else graph_node_input_1
+    indices_tensor = tf_layers_dict[graph_node_input_2.name]['tf_node'] \
+        if isinstance(graph_node_input_2, gs.Variable) else graph_node_input_2
+    updates_tensor = tf_layers_dict[graph_node_input_3.name]['tf_node'] \
+        if isinstance(graph_node_input_3, gs.Variable) else graph_node_input_3
+
+    # Preserving Graph Structure (Dict)
+    tf_layers_dict[graph_node_output.name] = {
+        'optype': graph_node.op,
+        'shape': shape,
+        'dtype': dtype,
+    }
+
+    # Generation of TF OP
+    # TODO: negative indices
+    tf_layers_dict[graph_node_output.name]['tf_node'] = \
+        tf.scatter_nd(
+            indices=indices_tensor,
+            updates=updates_tensor,
+            shape=input_tensor,
+            name=graph_node.name,
+        )
