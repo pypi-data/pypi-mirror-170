4791dc116f58f744b84d101fd5d5d2a91cc6c702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:24 +0000

    Add package ctranslate2/2.24.0/ctranslate2-2.24.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ctranslate2/ctranslate2-2.24.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ctranslate2/__init__.py
@@ -0,0 +1,47 @@
+import sys
+
+if sys.platform == "win32":
+    import ctypes
+    import os
+
+    import pkg_resources
+
+    module_name = sys.modules[__name__].__name__
+    package_dir = pkg_resources.resource_filename(module_name, "")
+
+    add_dll_directory = getattr(os, "add_dll_directory", None)
+    if add_dll_directory is not None:
+        add_dll_directory(package_dir)
+
+    for library in ("libiomp5md", "ctranslate2"):
+        ctypes.CDLL(os.path.join(package_dir, "%s.dll" % library))
+
+try:
+    from ctranslate2.extensions import register_extensions
+    from ctranslate2.translator import (
+        AsyncGenerationResult,
+        AsyncScoringResult,
+        AsyncTranslationResult,
+        GenerationResult,
+        Generator,
+        ScoringResult,
+        TranslationResult,
+        TranslationStats,
+        Translator,
+        contains_model,
+        get_cuda_device_count,
+        get_supported_compute_types,
+        set_random_seed,
+    )
+
+    register_extensions()
+    del register_extensions
+except ImportError as e:
+    # Allow using the Python package without the compiled translator extension.
+    if "No module named" in str(e):
+        pass
+    else:
+        raise
+
+from ctranslate2 import converters, specs
+from ctranslate2.version import __version__
