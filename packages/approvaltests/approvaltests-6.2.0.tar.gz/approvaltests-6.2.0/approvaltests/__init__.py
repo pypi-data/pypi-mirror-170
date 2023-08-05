eda8a4be2cea07eda4cfade65abf1052154b7b8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:11 +0000

    Add package approvaltests/6.2.0/approvaltests-6.2.0.tar.gz

---
+++ packages/approvaltests/approvaltests-6.2.0.tar.gz/approvaltests-6.2.0/approvaltests/__init__.py
@@ -0,0 +1,9 @@
+"""Approvaltests module."""
+from .version import *
+from .utilities import *
+from .approvals import *
+from .asserts import *
+from .combination_approvals import *
+
+__author__ = "Chris Lucian; Llewellyn Falco; Jim Counts; Tim Preece"
+__version__ = version_number
