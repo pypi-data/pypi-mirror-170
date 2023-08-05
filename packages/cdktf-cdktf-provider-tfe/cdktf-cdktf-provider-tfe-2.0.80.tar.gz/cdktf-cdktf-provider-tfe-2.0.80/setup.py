e4308f9b009485268b308c72e79b8cff57de570b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:04 +0000

    Add package cdktf-cdktf-provider-tfe/2.0.80/cdktf-cdktf-provider-tfe-2.0.80.tar.gz

---
+++ packages/cdktf-cdktf-provider-tfe/cdktf-cdktf-provider-tfe-2.0.80.tar.gz/cdktf-cdktf-provider-tfe-2.0.80/setup.py
@@ -0,0 +1,65 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "cdktf-cdktf-provider-tfe",
+    "version": "2.0.80",
+    "description": "Prebuilt tfe Provider for Terraform CDK (cdktf)",
+    "license": "MPL-2.0",
+    "url": "https://github.com/hashicorp/cdktf-provider-tfe.git",
+    "long_description_content_type": "text/markdown",
+    "author": "HashiCorp",
+    "bdist_wheel": {
+        "universal": true
+    },
+    "project_urls": {
+        "Source": "https://github.com/hashicorp/cdktf-provider-tfe.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "cdktf_cdktf_provider_tfe",
+        "cdktf_cdktf_provider_tfe._jsii"
+    ],
+    "package_data": {
+        "cdktf_cdktf_provider_tfe._jsii": [
+            "provider-tfe@2.0.80.jsii.tgz"
+        ],
+        "cdktf_cdktf_provider_tfe": [
+            "py.typed"
+        ]
+    },
+    "python_requires": "~=3.7",
+    "install_requires": [
+        "cdktf>=0.12.2, <0.14.0",
+        "constructs>=10.0.0, <11.0.0",
+        "jsii>=1.69.0, <2.0.0",
+        "publication>=0.0.3",
+        "typeguard~=2.13.3"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Typing :: Typed",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved"
+    ],
+    "scripts": []
+}
+"""
+)
+
+with open("README.md", encoding="utf8") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
