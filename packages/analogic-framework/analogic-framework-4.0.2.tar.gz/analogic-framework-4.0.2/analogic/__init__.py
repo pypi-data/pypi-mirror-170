6f1f7e095629bb9d851d28d9a1f8efc7f0682172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:29 +0000

    Add package analogic-framework/4.0.2/analogic-framework-4.0.2.tar.gz

---
+++ packages/analogic-framework/analogic-framework-4.0.2.tar.gz/analogic-framework-4.0.2/analogic/__init__.py
@@ -0,0 +1,17 @@
+from .analogic import Analogic as Analogic
+from .version import version
+from .analogic import create_app
+from .authentication_provider import get_authentication_provider
+from .authentication_provider import login_required
+from .authentication_provider import endpoint_login_required
+from .authentication_provider import AuthenticationProvider
+from .condition import Condition
+from .cam import Cam
+from .endpoint import AnalogicEndpoint
+from .loader import ClassLoader
+from .email import EmailManager
+from .setting import SettingManager
+from . import pivot
+from .loginbasic import LoginBasic
+from .nologin import NoLogin
+from . import core_endpoints
