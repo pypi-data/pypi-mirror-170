59659f6e608c2e82e1fe06e55eb6d6af43d43b73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:24 +0000

    Add package http2client/0.0.9/http2client-0.0.9.tar.gz

---
+++ packages/http2client/http2client-0.0.9.tar.gz/http2client-0.0.9/setup.py
@@ -0,0 +1,29 @@
+#!python
+# -*- coding:utf-8 -*-
+from __future__ import print_function
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name="http2client",
+    version="0.0.9",
+    author="cyal1",
+    author_email="dsq6115119@gmail.com",
+    description="A simple HTTP/2 client for Cyber Security",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cyal1/http2client",
+    packages=find_packages(),
+    install_requires=[
+        "h2 >= 2.6.2",
+        "urllib3>=1.26.12",
+        "setuptools"
+        ],
+    classifiers=[
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    python_requires='>=3.8'
+)
