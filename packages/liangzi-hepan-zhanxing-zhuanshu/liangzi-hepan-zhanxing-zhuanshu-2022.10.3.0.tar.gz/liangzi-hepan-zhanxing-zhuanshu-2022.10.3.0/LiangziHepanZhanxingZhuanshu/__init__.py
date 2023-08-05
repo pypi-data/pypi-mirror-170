0db3da7d05c881fa34225d6aec3991b27b742149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:52 +0000

    Add package liangzi-hepan-zhanxing-zhuanshu/2022.10.3.0/liangzi-hepan-zhanxing-zhuanshu-2022.10.3.0.tar.gz

---
+++ packages/liangzi-hepan-zhanxing-zhuanshu/liangzi-hepan-zhanxing-zhuanshu-2022.10.3.0.tar.gz/liangzi-hepan-zhanxing-zhuanshu-2022.10.3.0/LiangziHepanZhanxingZhuanshu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""liangzi-hepan-zhanxing-zhuanshu
+https://github.com/apachecn/liangzi-hepan-zhanxing-zhuanshu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.3.0"
