3d8790ccc625692f7b37c14f72e90a0d87b78bd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:47 +0000

    Add package excel-operate-light22/2.1.1/excel_operate_light22-2.1.1-py3-none-any.whl

---
+++ packages/excel-operate-light22/excel_operate_light22-2.1.1-py3-none-any.whl/excel_operate/__init__.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   __init__.py
+@Author  :   北极星光 
+@Contact :   light22@126.com
+'''
+
+from excel_operate.excel_operate import ExcelOperate
+from excel_operate.sheet_copy import SheetCopy
+from excel_operate.sheet_comparison import SheetComparison
+from excel_operate.list_operate import *
