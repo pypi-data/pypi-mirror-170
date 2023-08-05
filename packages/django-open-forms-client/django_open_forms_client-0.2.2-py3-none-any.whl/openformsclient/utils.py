2976555c0b5d3ea30c3ce012cec5667e6204ce9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:29 +0000

    Add package django-open-forms-client/0.2.2/django_open_forms_client-0.2.2-py3-none-any.whl

---
+++ packages/django-open-forms-client/django_open_forms_client-0.2.2-py3-none-any.whl/openformsclient/utils.py
@@ -0,0 +1,19 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def get_form_choices(client=None, use_uuids=False):
+    if client is None:
+        from .models import Configuration
+
+        config = Configuration.get_solo()
+        client = config.client
+
+    response = client.get_forms()
+
+    key = "uuid" if use_uuids else "slug"
+    return sorted(
+        [(item[key], item["name"]) for item in response],
+        key=lambda entry: entry[1],
+    )
