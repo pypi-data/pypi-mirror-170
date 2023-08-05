8967be054176134117c935395cbaef84095dbbee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:53 +0000

    Add package HypergraphAnalysisToolbox/0.0.1/hypergraphanalysistoolbox-0.0.1.tar.gz

---
+++ packages/HypergraphAnalysisToolbox/hypergraphanalysistoolbox-0.0.1.tar.gz/hypergraphanalysistoolbox-0.0.1/HAT/graph.py
@@ -0,0 +1,88 @@
+import numpy as np
+import scipy as sp
+
+class graph:
+    """
+    This class represents pairwise graph structures.
+    """
+    def __init__(self):
+        """
+        Constructs a default graph.
+        """
+        self.A = np.zeros((0,0))
+        self.N = 0
+        self.E = 0
+        
+    def __init__(self, A):
+        """
+        Constructs a graph from an adjacency matrix.
+        """
+        self.A = A
+        self.N = len(A)
+    
+    @property
+    def degree(self):
+        """
+        The degree matrix of a graph.
+        """
+        return np.diag(sum(self.A))
+        
+    @property
+    def laplacian(self):
+        """
+        The Laplacian matrix of a graph.
+        """
+        return self.degree - self.A
+
+    @property
+    def clusteringCoef(self):
+        """
+        The clustering coefficient of the graph.
+        """
+        gammas = 0
+        for vx in range(len(self.A)):
+            gammas += self.clusteringCoef(vx)
+        return gammas / len(self.A)
+        
+    def clusteringCoef(self, vx):
+        """
+        Computes the clustering coefficient of a single vertex.
+        """
+        neighbors = np.where(self.A[vx,:] == 1)[0]
+        if vx not in neighborhood:
+            neighbors = np.append(neighbors, vx)
+        neighborhood = self.A[neighbors, :]
+        neighborhood = neighborhood[:, neighbors]
+        realEdges = sum(neighborhood) / 2
+        possibleEdges = sp.special.binom(len(neighbors), 2)
+        return realEdges / possibleEdges        
+
+    def pairwiseDistance(self, vxi, vxj):
+        """
+        Computes the pairwise distance between vertices i and j.
+        """
+        d = 1
+        N = self.A
+        print(N)
+        while N[vxi,vxj] == 0:
+            print(N)
+            N = np.matmul(N, self.A)
+            d += 1
+        return d
+    
+    def erdosRenyi(n, p):
+        """
+        Constructs a $G_{n,p}$ Erdős-Rényi random graph.
+
+        References
+        ----------
+        .. [1] P. Erdős and A. Rényi, On Random Graphs, Publ. Math. 6, 290 (1959).
+        .. [2] E. N. Gilbert, Random Graphs, Ann. Math. Stat., 30, 1141 (1959).
+        """
+        A = np.zeros((n,n))
+        for i in range(n):
+            for j in range(n):
+                if np.random.rand() < p:
+                    A[i, j] = 1
+                    A[j, i] = 1
+        return graph(A)
---
+++ packages/HypergraphAnalysisToolbox/hypergraphanalysistoolbox-0.0.1.tar.gz/hypergraphanalysistoolbox-0.0.1/HAT/hypergraph.py
@@ -0,0 +1,105 @@
+import numpy as np
+import scipy as sp
+
+from HAT.graph import graph
+
+class hypergraph:
+    """
+    This class represents multiway hypergraph structures.
+    """
+
+    def __init__(self):
+        """
+        This is the default constructor for a hypergraph.
+        """
+        self.W = np.zeros((0,0))
+        self.N = 0
+        self.E = 0
+        
+    def __init__(self, w):
+        """
+        This constructor initializes a hypergraph based on an incidence matrix.
+        """
+        self.W = w
+        self.N = len(self.W)
+        self.E = len(self.W[0])
+    
+    def avgDistance(self):
+        """
+        The average distance is computed between any vertices in the hypergraph based
+        on the average distance of the clique expansion.
+        """
+        g = self.cliqueExpand()
+        return g.avgDistance()
+    
+    def dualGraph(self):
+        """
+        The dual hypergraph is constructed.
+        """
+        W = self.W.T
+        return hypergraph(W)
+
+    def cliqueExpand(self):
+        """
+        The clique expansion graph is constructed.
+        """
+        A = np.zeros((len(self.W), len(self.W)))
+        for e in range(len(self.W[0])):
+            vxc = np.where(self.W[:, e] == 1)[0]
+            for i in range(len(vxc)):
+                for j in range(i+1, len(vxc)):
+                    A[vxc[i], vxc[j]] = 1
+        A = A + A.T
+        return graph(A)
+
+    def lineGraph(self):
+        """
+        The line graph, which is the clique expansion of the dual graph is constructed.
+        """
+        return self.cliqueGraph(self.dualGraph())
+
+    def starGraph(self):
+        """
+        The star graph representation is constructed.
+        """
+        A = np.zeros((len(self.W) + len(self.W[0]), len(self.W) + len(self.W[0])))
+        for e in range(len(self.W[0])):
+            vxc = np.where(self.W[:, e] == 1)[0]
+            A[vxc, len(self.W) + e] = 1
+        A = A + A.T
+        return graph(A)
+        
+    def adjacencyTensor(self):
+        """
+        This constructs the adjacency tensor for uniform hypergraphs.
+        """
+        order = sum(self.W[:,0])
+        modes = len(self.W) * np.ones(order)
+        A = np.zeros(modes.astype(int))
+        for e in range(len(W[0])):
+            vxc = np.where(W[:, e] == 1)[0]
+            vxc = list(permutations(vxc))
+            for p in vxc:
+                A[p] = 1
+        return A
+    
+    def degreeTensor(self):
+        """
+        This constructs the degree tensor for uniform hypergraphs.
+        """
+        order = sum(self.W[:,0])
+        modes = len(self.W) * np.ones(order)
+        D = np.zeros(modes.astype(int))
+        for vx in range(len(W)):
+            D[(np.ones(order) * vx).astype(int)] = sum(W[vx])
+        return D
+    
+    def laplacianTensor(self):
+        """
+        This constructs the Laplacian tensor for uniform hypergraphs.
+        """
+        D = self.degreeTensor()
+        A = self.adjacencyTensor()
+        L = D - A
+        return L
+    
