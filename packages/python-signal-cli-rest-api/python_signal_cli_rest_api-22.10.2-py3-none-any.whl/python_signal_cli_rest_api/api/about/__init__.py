a5984bbeb833c4905fd33886db7beddba6900e90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:39 +0000

    Add package python-signal-cli-rest-api/22.10.2/python_signal_cli_rest_api-22.10.2-py3-none-any.whl

---
+++ packages/python-signal-cli-rest-api/python_signal_cli_rest_api-22.10.2-py3-none-any.whl/python_signal_cli_rest_api/api/about/__init__.py
@@ -0,0 +1,48 @@
+"""
+about
+"""
+
+from dataclasses import dataclass
+from typing import List
+
+from sanic import Blueprint
+from sanic.response import json
+from sanic_ext import openapi
+
+from python_signal_cli_rest_api.lib.jsonrpc import jsonrpc
+
+about_v1 = Blueprint("about_v1", url_prefix="/about")
+
+
+@dataclass
+class AboutGetV1Params:
+    """
+    AboutGetV1Params
+    """
+
+    mode: str
+    versions: List[str]
+    signal_cli: str
+
+
+@about_v1.get("/", version=1)
+@openapi.tag("General")
+@openapi.response(
+    200,
+    {"application/json": AboutGetV1Params},
+    description="OK",
+)
+@openapi.description("Returns the supported API versions.")
+async def about_v1_get(request):  # pylint: disable=unused-argument
+    """
+    Lists general information about the API.
+    """
+    signal_cli = jsonrpc({"method": "version"}).get("result").get("version")
+    return json(
+        {
+            "mode": "json-rpc",
+            "versions": ["v1", "v2"],
+            "signal_cli": signal_cli,
+        },
+        200,
+    )
