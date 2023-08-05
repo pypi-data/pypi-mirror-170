c81023d334f00ef47a357ac592c270a0d8c8e843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:55 +0000

    Add package Remilia/2022.10.3.1.46.31/Remilia-2022.10.3.1.46.31.tar.gz

---
+++ packages/Remilia/Remilia-2022.10.3.1.46.31.tar.gz/Remilia-2022.10.3.1.46.31/Remilia/__init__.py
@@ -0,0 +1,51 @@
+try:
+    from . import (
+    LiteLog,
+    LiteTasks,
+    utils,
+    )
+except:
+    pass
+
+from . import (
+    LiteMixin,
+    LitePGL,
+    LiteEvent,
+    LiteConfig,
+    LiteResource,
+    LiteI18n,
+    LiteThread,
+    base,
+    )
+
+__all__=[
+    "LiteLog",
+    "LiteMixin",
+    "LiteThread",
+    "LitePGL",
+    "LiteResource",
+    "LiteTasks",
+    "LiteEvent",
+    "LiteConfig",
+    "LiteI18n",
+    "base",
+    "utils",
+    ]
+
+__version__="2022.10.03.01.46.31"
+
+def __REQUIREMENTS__():
+    '''
+## if you want to use some class in our lib,you must install the lib below
+### LiteTasks/LiteLog:
+- #### colorama >= 0.4.5
+
+### utils/cli
+- #### click >= 8.0
+- #### prompt_toolkit >= 3.0.31
+
+### utils/net/pixiv
+- #### pixivpy_async >= 1.2.14
+    '''
+    import inspect
+    print(inspect.getsource(__REQUIREMENTS__))
