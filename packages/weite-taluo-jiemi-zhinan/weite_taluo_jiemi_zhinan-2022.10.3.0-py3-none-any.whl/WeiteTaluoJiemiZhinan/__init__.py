2da99de367a84f101d6a3cccd04d628d61a0b18a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:51 +0000

    Add package weite-taluo-jiemi-zhinan/2022.10.3.0/weite_taluo_jiemi_zhinan-2022.10.3.0-py3-none-any.whl

---
+++ packages/weite-taluo-jiemi-zhinan/weite_taluo_jiemi_zhinan-2022.10.3.0-py3-none-any.whl/WeiteTaluoJiemiZhinan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""weite-taluo-jiemi-zhinan
+https://github.com/apachecn/weite-taluo-jiemi-zhinan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.3.0"
