f022daa459d66b5b78f6e611d8e2bad12531d161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:41 +0000

    Add package dbus-fast/1.21.5/dbus-fast-1.21.5.tar.gz

---
+++ packages/dbus-fast/dbus-fast-1.21.5.tar.gz/dbus-fast-1.21.5/build.py
@@ -0,0 +1,36 @@
+"""Build optional cython modules."""
+
+import os
+from distutils.command.build_ext import build_ext
+
+
+class BuildExt(build_ext):
+    def build_extensions(self):
+        try:
+            super().build_extensions()
+        except Exception:
+            pass
+
+
+def build(setup_kwargs):
+    if os.environ.get("SKIP_CYTHON", False):
+        return
+    try:
+        from Cython.Build import cythonize
+
+        setup_kwargs.update(
+            dict(
+                ext_modules=cythonize(
+                    [
+                        "src/dbus_fast/unpack.py",
+                        "src/dbus_fast/_private/marshaller.py",
+                        "src/dbus_fast/_private/unmarshaller.py",
+                    ]
+                ),
+                cmdclass=dict(build_ext=BuildExt),
+            )
+        )
+    except Exception:
+        if os.environ.get("REQUIRE_CYTHON"):
+            raise
+        pass
