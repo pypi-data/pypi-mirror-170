e453d23ddffc4100aface516908ac95cdbf3d9ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:42 +0000

    Add package otlmow-converter/0.1/otlmow_converter-0.1.tar.gz

---
+++ packages/otlmow-converter/otlmow_converter-0.1.tar.gz/otlmow_converter-0.1/otlmow_converter/FileExporter.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+from otlmow_converter.Exceptions.InvalidExtensionError import InvalidExtensionError
+from otlmow_converter.FileFormats.CsvExporter import CsvExporter
+from otlmow_converter.FileFormats.ExcelExporter import ExcelExporter
+from otlmow_converter.FileFormats.JsonExporter import JsonExporter
+from otlmow_converter.FileImporter import FileImporter
+
+
+class FileExporter:
+    def __init__(self, settings: dict):
+        self.settings = settings
+
+    def create_file_from_assets(self, filepath: Path, list_of_objects: list, **kwargs):
+        extension = FileImporter.get_file_extension(filepath, file_must_exist=False)
+        exporter = self.get_exporter_from_extension(extension=extension, settings=self.settings)
+        return exporter.export_to_file(filepath=filepath, list_of_objects=list_of_objects, **kwargs)
+
+    @staticmethod
+    def get_exporter_from_extension(extension: str, settings: dict):
+        if extension == 'csv':
+            return CsvExporter(settings=settings)
+        elif extension == 'json':
+            return JsonExporter(settings=settings)
+        elif extension == ['xls', 'xlsx']:
+            return ExcelExporter(settings=settings)
+        else:
+            raise InvalidExtensionError('This file has an invalid extension. Supported file formats are: csv, json, xlsx')
+
