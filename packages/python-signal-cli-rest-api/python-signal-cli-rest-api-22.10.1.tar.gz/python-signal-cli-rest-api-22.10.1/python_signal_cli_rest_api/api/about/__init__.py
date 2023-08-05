a03a9427d99a5d1714298dbccc07482d1e68ec34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:45 +0000

    Add package python-signal-cli-rest-api/22.10.1/python-signal-cli-rest-api-22.10.1.tar.gz

---
+++ packages/python-signal-cli-rest-api/python-signal-cli-rest-api-22.10.1.tar.gz/python-signal-cli-rest-api-22.10.1/python_signal_cli_rest_api/api/about/__init__.py
@@ -0,0 +1,43 @@
+"""
+about
+"""
+
+from dataclasses import dataclass
+from typing import List
+
+from sanic import Blueprint
+from sanic.response import json
+from sanic_ext import openapi
+
+about_v1 = Blueprint("about_v1", url_prefix="/about")
+
+
+@dataclass
+class AboutGetV1Params:
+    """
+    AboutGetV1Params
+    """
+
+    mode: str
+    versions: List[str]
+
+
+@about_v1.get("/", version=1)
+@openapi.tag("General")
+@openapi.response(
+    200,
+    {"application/json": AboutGetV1Params},
+    description="OK",
+)
+@openapi.description("Returns the supported API versions.")
+async def about_v1_get(request):  # pylint: disable=unused-argument
+    """
+    Lists general information about the API.
+    """
+    return json(
+        {
+            "mode": "json-rpc",
+            "versions": ["v1", "v2"],
+        },
+        200,
+    )
---
+++ packages/python-signal-cli-rest-api/python-signal-cli-rest-api-22.10.1.tar.gz/python-signal-cli-rest-api-22.10.1/python_signal_cli_rest_api/dataclasses/__init__.py
@@ -0,0 +1,51 @@
+"""
+common dataclasses
+"""
+
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class Error:
+    """
+    SearchV1Error
+    """
+
+    error: str
+
+
+@dataclass
+class Recipients:
+    """
+    Recipients
+    """
+
+    recipients: List[str]
+
+
+@dataclass
+class ResponseTimestamp:
+    """
+    ResponseTimestamp
+    """
+
+    timestamp: Recipients
+
+
+@dataclass
+class ResponseTimestamps:
+    """
+    ResponseTimestamps
+    """
+
+    timestamps: ResponseTimestamp
+
+
+@dataclass
+class GroupId:
+    """
+    GroupId
+    """
+
+    group_id: str
