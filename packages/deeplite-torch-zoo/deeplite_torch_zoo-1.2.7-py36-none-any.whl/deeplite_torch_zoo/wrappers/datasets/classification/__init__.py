13f9bb13436a6d435e7ebfc759f57ccbda1cc943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:20 +0000

    Add package deeplite-torch-zoo/1.2.7/deeplite_torch_zoo-1.2.7-py36-none-any.whl

---
+++ packages/deeplite-torch-zoo/deeplite_torch_zoo-1.2.7-py36-none-any.whl/deeplite_torch_zoo/wrappers/datasets/classification/__init__.py
@@ -0,0 +1,10 @@
+try:
+    from .vww import *
+except ImportError:
+    pass
+
+from .cifar import *
+from .imagenet import *
+from .mnist import *
+from .vww import *
+from .tiny_imagenet import *
---
+++ packages/deeplite-torch-zoo/deeplite_torch_zoo-1.2.7-py36-none-any.whl/deeplite_torch_zoo/wrappers/datasets/classification/mnist.py
@@ -0,0 +1,47 @@
+import os
+import torchvision
+
+from ..utils import get_dataloader
+from os.path import expanduser
+from torchvision import transforms
+from deeplite_torch_zoo.wrappers.registries import DATA_WRAPPER_REGISTRY
+
+
+__all__ = ["get_mnist"]
+
+
+@DATA_WRAPPER_REGISTRY.register(dataset_name="mnist")
+def get_mnist(data_root="", batch_size=128, num_workers=4, fp16=False, download=True, device="cuda", distributed=False, **kwargs):
+
+    if len(kwargs):
+        import sys
+        print(f"Warning, {sys._getframe().f_code.co_name}: extra arguments {list(kwargs.keys())}!")
+
+    if data_root == "":
+        data_root = os.path.join(expanduser("~"), ".deeplite-torch-zoo")
+
+    train_dataset = torchvision.datasets.MNIST(
+        root=data_root,
+        train=True,
+        download=download,
+        transform=transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+        ),
+    )
+
+    test_dataset = torchvision.datasets.MNIST(
+        root=data_root,
+        train=False,
+        download=download,
+        transform=transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+        ),
+    )
+
+    train_loader = get_dataloader(train_dataset, batch_size=batch_size, num_workers=num_workers,
+        fp16=fp16, distributed=distributed, shuffle=not distributed, device=device)
+
+    test_loader = get_dataloader(test_dataset, batch_size=batch_size, num_workers=num_workers,
+        fp16=fp16, distributed=distributed, shuffle=False, device=device)
+
+    return {"train": train_loader, "test": test_loader}
