c42d8490c7d60e531f70f9b8bd79291beaa359dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:25 +0000

    Add package weathon/0.0.0.1/weathon-0.0.0.1.tar.gz

---
+++ packages/weathon/weathon-0.0.0.1.tar.gz/weathon-0.0.0.1/weathon/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/10/2 08:28
+# @Author  : LiZhen
+# @FileName: __init__.py.py
+# @github  : https://github.com/Lizhen0628
+# @Description:
+
+
+__version__ = '0.0.0.1'
+__author__ = 'LiZhen'
