8b43670fa401b416ed010be53e349caec4378cdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:15 +0000

    Add package libtorrentx/0.0.4/libtorrentx-0.0.4.tar.gz

---
+++ packages/libtorrentx/libtorrentx-0.0.4.tar.gz/libtorrentx-0.0.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, Extension
+from setuptools import find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="libtorrentx",
+    version="0.0.4",
+    description="libtorrent python api",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Nitin Rai",
+    author_email="mneonizer@gmail.com",
+    url="https://github.com/imneonizer/libtorrentx",
+    license="MIT License",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["libtorrent>=2.0.6", "ujson>=5.2.0", "easydict>=1.9", "loguru>=0.5.3"],
+    platforms=["linux", "unix"],
+    python_requires=">3.5.2",
+)
