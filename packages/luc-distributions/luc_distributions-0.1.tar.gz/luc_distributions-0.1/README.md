3c1c7587b5b3f1565cb4df485524381e3bcfd98a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:05 +0000

    Add package luc-distributions/0.1/luc_distributions-0.1.tar.gz

---
+++ packages/luc-distributions/luc_distributions-0.1.tar.gz/luc_distributions-0.1/README.md
@@ -0,0 +1,9 @@
+# udc-distributions package
+
+Summary of the package
+
+# Files
+
+Explanation of files in the package
+
+# Installation
---
+++ packages/luc-distributions/luc_distributions-0.1.tar.gz/luc_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='luc_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['luc_distributions'],
+      author = 'Lucas Elbert Suryana',
+      author_email = 'lucaselbert@gmail.com',
+      zip_safe=False)
