935ef8eb4043d9b601ca66fc20211e8ce0ee09f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:18 +0000

    Add package bioregistry/0.5.116/bioregistry-0.5.116-py3-none-any.whl

---
+++ packages/bioregistry/bioregistry-0.5.116-py3-none-any.whl/bioregistry/lint.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+"""Linting functions."""
+
+import click
+
+from bioregistry.schema_utils import (
+    read_collections,
+    read_contexts,
+    read_metaregistry,
+    read_registry,
+    write_collections,
+    write_contexts,
+    write_metaregistry,
+    write_registry,
+)
+
+
+@click.command()
+def lint():
+    """Run the lint commands."""
+    registry = read_registry()
+    for resource in registry.values():
+        if resource.synonyms:
+            resource.synonyms = sorted(set(resource.synonyms))
+    write_registry(registry)
+    collections = read_collections()
+    for collection in collections.values():
+        collection.resources = sorted(set(collection.resources))
+    write_collections(collections)
+    write_metaregistry(read_metaregistry())
+    write_contexts(read_contexts())
+
+
+if __name__ == "__main__":
+    lint()
