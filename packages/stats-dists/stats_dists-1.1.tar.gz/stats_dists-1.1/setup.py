0d7dea84bbaf82cfeab00869d213cd735fab61b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:22 +0000

    Add package stats-dists/1.1/stats_dists-1.1.tar.gz

---
+++ packages/stats-dists/stats_dists-1.1.tar.gz/stats_dists-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='stats_dists',
+      version='1.1',
+      description='Gaussian distributions and Binomial',
+      packages=['stats_dists'],
+      author = 'Ahmad Muhammad Tahir',
+      author_email = 'ahmad.tahir49@yahoo.com',
+      zip_safe=False)
