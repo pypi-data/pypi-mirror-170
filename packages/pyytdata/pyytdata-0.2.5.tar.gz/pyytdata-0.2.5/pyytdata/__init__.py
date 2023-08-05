8e93b1c6252db7ea738f8eae6ffd9a47aad0a085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:37 +0000

    Add package pyytdata/0.2.5/pyytdata-0.2.5.tar.gz

---
+++ packages/pyytdata/pyytdata-0.2.5.tar.gz/pyytdata-0.2.5/pyytdata/__init__.py
@@ -0,0 +1,8 @@
+from .pyytdata import PyYtData
+from .util import VidInfo
+
+
+def vid_info(url: str) -> VidInfo:
+    """Takes URL of the video and return the VidInfo object"""
+    id = url.split("=")
+    return VidInfo(id=id[-1])
---
+++ packages/pyytdata/pyytdata-0.2.5.tar.gz/pyytdata-0.2.5/pyytdata/util/__init__.py
@@ -0,0 +1,3 @@
+from .chnlinfo import ChnlInfo
+from .info import Info
+from .vidinfo import VidInfo
