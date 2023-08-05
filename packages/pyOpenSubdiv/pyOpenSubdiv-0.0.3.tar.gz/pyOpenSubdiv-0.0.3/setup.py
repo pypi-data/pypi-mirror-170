beb76130b57f08c22c07a22d2d9359f8b798ecfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:06 +0000

    Add package pyOpenSubdiv/0.0.3/pyOpenSubdiv-0.0.3.tar.gz

---
+++ packages/pyOpenSubdiv/pyOpenSubdiv-0.0.3.tar.gz/pyOpenSubdiv-0.0.3/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+# import distutils.sysconfig
+
+NAME = 'pyOpenSubdiv'
+# VERSION = '0.0.1'
+# Initial 
+# VERSION = '0.0.2'
+# ?
+VERSION = '0.0.3' # [ ] 
+# - A lot of changes associated with handling level <=0 subdivisions better.
+# - Some refactoring. 
+# - Added ctypes_subdivider.cpp flags maxlevel (-m <N>) and verbose (-v)
+# - Slightly better test framework, added some example topologies to try. 
+DESCRIPTION = 'Python Catmull-Clark Subdivision.'
+LONG_DESCRIPTION = 'A Python wrapper for the OpenSubdiv C++ Library far topology refiner.'
+
+# Also needs (maybe)
+# sudo apt-get install libatlas-base-dev (https://stackoverflow.com/questions/53347759/importerror-libcblas-so-3-cannot-open-shared-object-file-no-such-file-or-dire)
+
+# import site
+# import sys
+# print(site.USER_BASE)
+# print(sys.prefix)
+setup(
+    name = NAME,
+    version = VERSION,
+    author = 'GeneralPancake',
+    description = DESCRIPTION,
+    long_description = LONG_DESCRIPTION,
+    packages = find_packages(),
+    include_package_data=True,
+    package_data={f'{NAME}.clib':['*.so','*.dll']}, # https://stackoverflow.com/questions/70334648/how-to-correctly-install-data-files-with-setup-py
+    install_requires = ["numpy"],    
+    keywords = ['subdivision','opensubdiv','Catmull-Clark','hard-surface'],
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",        
+        "Programming Language :: Python :: 3",        
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: Unix",
+    ]
+)
