9b8498ee918957fdd13cee584930f89282fc7a82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:09 +0000

    Add package tons/0.0.31/tons-0.0.31-py3-none-any.whl

---
+++ packages/tons/tons-0.0.31-py3-none-any.whl/tons/ui/interactive_cli/_sets/_entrypoint.py
@@ -0,0 +1,20 @@
+from collections import OrderedDict
+
+from ._base import BaseSet
+from ._keystores import KeystoresSet
+from ._whitelist import WhitelistSet
+
+
+class EntrypointSet(BaseSet):
+    def _handlers(self) -> OrderedDict:
+        ord_dict = OrderedDict()
+        ord_dict["Keystores"] = self._handle_keystores
+        ord_dict["Whitelist"] = self._handle_whitelist
+        ord_dict["Exit"] = self._handle_exit
+        return ord_dict
+
+    def _handle_keystores(self):
+        KeystoresSet(self.ctx).show()
+
+    def _handle_whitelist(self):
+        WhitelistSet(self.ctx).show()
---
+++ packages/tons/tons-0.0.31-py3-none-any.whl/tons/ui/interactive_cli/tons_interactive.py
@@ -0,0 +1,4 @@
+from tons.ui.interactive_cli import main
+
+if __name__ == '__main__':
+    main()
