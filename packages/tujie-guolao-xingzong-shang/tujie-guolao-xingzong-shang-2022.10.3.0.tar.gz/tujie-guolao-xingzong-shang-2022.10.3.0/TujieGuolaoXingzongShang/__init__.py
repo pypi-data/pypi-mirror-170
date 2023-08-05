6daca8c0f929c24f2f5544e5b03276cb299706b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:20 +0000

    Add package tujie-guolao-xingzong-shang/2022.10.3.0/tujie-guolao-xingzong-shang-2022.10.3.0.tar.gz

---
+++ packages/tujie-guolao-xingzong-shang/tujie-guolao-xingzong-shang-2022.10.3.0.tar.gz/tujie-guolao-xingzong-shang-2022.10.3.0/TujieGuolaoXingzongShang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""tujie-guolao-xingzong-shang
+https://github.com/apachecn/tujie-guolao-xingzong-shang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.3.0"
