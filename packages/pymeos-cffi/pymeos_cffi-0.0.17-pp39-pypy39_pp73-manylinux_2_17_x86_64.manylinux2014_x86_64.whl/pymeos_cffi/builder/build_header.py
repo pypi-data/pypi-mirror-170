815141b781075eb9149904460ca8cec7ec8e2ead
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:05 +0000

    Add package pymeos-cffi/0.0.17/pymeos_cffi-0.0.17-pp39-pypy39_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pymeos-cffi/pymeos_cffi-0.0.17-pp39-pypy39_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pymeos_cffi/builder/build_header.py
@@ -0,0 +1,23 @@
+import re
+import sys
+
+from build_helpers import ADDITIONAL_DEFINITIONS
+
+
+def main(header_path):
+    with open(header_path, 'r') as f:
+        content = f.read()
+        content = content.replace('#', '//#')
+        content = content.replace(*ADDITIONAL_DEFINITIONS)
+        # Remove comments
+        content = re.sub(r'//.*', '', content)
+        content = re.sub(r'/\*.*?\*/', '', content, flags=re.RegexFlag.MULTILINE)
+    with open('pymeos_cffi/builder/meos.h', 'w') as f:
+        f.write(content)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        main(sys.argv[1])
+    else:
+        main('/usr/local/include/meos.h')
---
+++ packages/pymeos-cffi/pymeos_cffi-0.0.17-pp39-pypy39_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pymeos_cffi/builder/build_pymeos_functions_modifiers.py
@@ -0,0 +1,44 @@
+def period_shift_tscale_modifier(function: str) -> str:
+    return function \
+        .replace(') ->', ', result: "Optional[\'Period *\']") ->') \
+        .replace("out_result = _ffi.new('Period *')",
+                 "out_result = _ffi.cast('Period *', result) if result is not None else _ffi.new('Period *')")
+
+
+def cstring2text_modifier(_: str) -> str:
+    return """def cstring2text(cstring: str) -> 'text *':
+    cstring_converted = cstring.encode('utf-8')
+    result = _lib.cstring2text(cstring_converted)
+    return result"""
+
+
+def text2cstring_modifier(_: str) -> str:
+    return """def text2cstring(textptr: 'text *') -> str:
+    result = _lib.text2cstring(textptr)
+    result = _ffi.string(result).decode('utf-8')
+    return result"""
+
+
+def timestampset_make_modifier(function: str) -> str:
+    return function \
+        .replace('times: int', 'times: List[int]') \
+        .replace("times_converted = _ffi.cast('const TimestampTz *', times)",
+                 "times_converted = [_ffi.cast('const TimestampTz', x) for x in times]")
+
+
+def gserialized_from_lwgeom_modifier(function: str) -> str:
+    return function \
+        .replace(", size: 'size_t *'", '') \
+        .replace("_ffi.cast('size_t *', size)", '_ffi.NULL')
+
+
+def tpointseq_make_coords_modifier(function: str) -> str:
+    return function \
+        .replace('times: int', "times: 'const TimestampTz *'") \
+        .replace("    xcoords_converted = _ffi.cast('const double *', xcoords)\n", '') \
+        .replace("    ycoords_converted = _ffi.cast('const double *', ycoords)\n", '') \
+        .replace("    times_converted = _ffi.cast('const TimestampTz *', times)\n", '') \
+        .replace("_ffi.cast('const double *', zcoords)", 'zcoords') \
+        .replace('xcoords_converted', 'xcoords') \
+        .replace('ycoords_converted', 'ycoords') \
+        .replace('times_converted', 'times')
