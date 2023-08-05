03a52c18d6c224df5349089cd10f02a43945ac6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:09 +0000

    Add package hashbase/1.0.0/hashbase-1.0.0.tar.gz

---
+++ packages/hashbase/hashbase-1.0.0.tar.gz/hashbase-1.0.0/hashbase/__init__.py
@@ -0,0 +1,6 @@
+from hashbase.md2 import MD2
+from hashbase.md4 import MD4
+from hashbase.md5 import MD5
+from hashbase.sha1 import SHA1
+from hashbase.crc8 import CRC8
+from hashbase.crc16 import CRC16
