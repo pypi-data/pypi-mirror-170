3ca562298ede9ce887c0c8e4e3d337a72f5c291d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:36 +0000

    Add package featimp/0.1.7/featimp-0.1.7-py3-none-any.whl

---
+++ packages/featimp/featimp-0.1.7-py3-none-any.whl/featimp/__init__.py
@@ -0,0 +1,12 @@
+from featimp.base import (
+    get_corr_importances,
+    get_chi2_crosstab_importances,
+    get_chi2_importances,
+    get_anova_importances,
+    get_mutual_info_importances,
+    get_ml_importances,
+    get_permutation_importances,
+    get_feature_importances,
+    get_fi_plots,
+    display_feature_importances,
+)
