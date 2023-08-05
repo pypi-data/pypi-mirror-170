9bfcbd8affba6e2405e14a16b734e933b3cd055e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:56 +0000

    Add package signposting/0.8.3/signposting-0.8.3.tar.gz

---
+++ packages/signposting/signposting-0.8.3.tar.gz/signposting-0.8.3/docs/AUTHORS.rst
@@ -0,0 +1,4 @@
+Authors
+=======
+
+* Stian Soiland-Reyes <https://orcid.org/0000-0001-9842-9718>
---
+++ packages/signposting/signposting-0.8.3.tar.gz/signposting-0.8.3/src/signposting/__init__.py
@@ -0,0 +1,50 @@
+"""Finding signposting in FAIR resources.
+
+This library helps client to discover links that follow the
+`FAIR`_ `signposting`_ conventions.
+
+This can then be used to navigate between:
+
+* Persistent identifiers
+* HTML landing pages
+* File downloads/items
+* Structured metadata
+
+The library works by inspecting the HTTP messages for
+``Link:`` headers from a given URI with :meth:`find_signposting_http`, which
+which categorize them by their ``rel`` `Link relation`_ into a
+:class:`Signposting` object with absolute URIs.
+
+It is up to the clients of this library to decide how to further
+navigate or retrieve the associated resources, e.g. using a
+RDF library like :mod:`rdflib`.
+
+This library also provide ways to discover
+FAIR signposting in HTML ``<link>`` annotations and in
+`Link set`_ documents.  Future versions may provide ways to 
+discover/merge these concurrently.
+
+.. _signposting: https://signposting.org/conventions/
+.. _FAIR: https://signposting.org/FAIR/
+.. _Link Relation: https://www.iana.org/assignments/link-relations/
+.. _rdflib: https://rdflib.readthedocs.io/en/stable/
+.. _Link set: <https://signposting.org/FAIR/#linksetrec>
+"""
+
+__version__ = '0.8.3'
+
+from typing import List
+import warnings
+from .signpost import Signposting, Signpost, AbsoluteURI, MediaType, LinkRel
+from .linkheader import find_signposting_http_link
+from .resolver import find_signposting_http
+from .htmllinks import find_signposting_html
+from .linkset import find_signposting_linkset
+
+def find_signposting(headers: List[str], baseurl: str = None) -> Signposting:
+    """DEPRECATED: Use meth:`find_signposting_http_link` instead"""
+    warnings.warn("find_signposting() is deprecated, use find_signposting_http_link()")
+    return find_signposting_http_link(headers, baseurl)
+
+__all__ = """find_signposting_http_link find_signposting_http find_signposting_html find_signposting_linkset
+Signposting Signpost AbsoluteURI MediaType LinkRel""".split()
