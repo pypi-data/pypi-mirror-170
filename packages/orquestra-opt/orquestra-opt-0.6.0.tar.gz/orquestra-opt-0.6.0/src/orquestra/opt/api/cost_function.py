d1f20cdc49634d9bc535bbd862688b0d397665ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:15 +0000

    Add package orquestra-opt/0.6.0/orquestra-opt-0.6.0.tar.gz

---
+++ packages/orquestra-opt/orquestra-opt-0.6.0.tar.gz/orquestra-opt-0.6.0/src/orquestra/opt/api/cost_function.py
@@ -0,0 +1,43 @@
+################################################################################
+# © Copyright 2022 Zapata Computing Inc.
+################################################################################
+"""Interfaces related to cost functions."""
+from typing import Protocol, Union
+
+import numpy as np
+from orquestra.quantum.utils import ValueEstimate
+
+from .functions import (
+    CallableStoringArtifacts,
+    CallableWithGradient,
+    CallableWithGradientStoringArtifacts,
+)
+
+
+class _CostFunction(Protocol):
+    """Cost function transforming vectors from R^n to numbers or their estimates."""
+
+    def __call__(self, parameters: np.ndarray) -> Union[float, ValueEstimate]:
+        """Compute  value of the cost function for given parameters."""
+        ...
+
+
+CostFunction = Union[
+    _CostFunction,
+    CallableWithGradient,
+    CallableStoringArtifacts,
+    CallableWithGradientStoringArtifacts,
+]
+
+
+class ParameterPreprocessor(Protocol):
+    """Parameter preprocessor.
+
+    Implementer of this protocol should create new array instead of
+    modifying passed parameters in place, which can have unpredictable
+    side effects.
+    """
+
+    def __call__(self, parameters: np.ndarray) -> np.ndarray:
+        """Preprocess parameters."""
+        ...
