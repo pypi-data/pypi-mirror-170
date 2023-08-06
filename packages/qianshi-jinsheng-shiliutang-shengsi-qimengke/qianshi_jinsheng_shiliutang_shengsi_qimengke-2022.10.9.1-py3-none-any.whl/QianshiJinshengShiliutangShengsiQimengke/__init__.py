539dfff5982406f3a2e16fd4c3c7ff12e2d64390
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:20:20 +0000

    Add package qianshi-jinsheng-shiliutang-shengsi-qimengke/2022.10.9.1/qianshi_jinsheng_shiliutang_shengsi_qimengke-2022.10.9.1-py3-none-any.whl

---
+++ packages/qianshi-jinsheng-shiliutang-shengsi-qimengke/qianshi_jinsheng_shiliutang_shengsi_qimengke-2022.10.9.1-py3-none-any.whl/QianshiJinshengShiliutangShengsiQimengke/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qianshi-jinsheng-shiliutang-shengsi-qimengke
+https://github.com/apachecn/qianshi-jinsheng-shiliutang-shengsi-qimengke"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.9.1"
