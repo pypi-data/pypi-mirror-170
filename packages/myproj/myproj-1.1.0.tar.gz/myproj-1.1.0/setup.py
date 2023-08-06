5685d55e766eacacc976f324670efe489c043d3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:46 +0000

    Add package myproj/1.1.0/myproj-1.1.0.tar.gz

---
+++ packages/myproj/myproj-1.1.0.tar.gz/myproj-1.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myproj',
+    version='1.1.0',
+    packages=find_packages(),
+    entry_points='''
+        [console_scripts]
+        myproj=myproj.__main__:run
+    ''', 
+    license="BSD",  # TODO change the license
+    classifiers=[
+    ],
+    install_requires=[
+    ],
+    tests_require=[
+    ],
+)
