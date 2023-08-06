acb424fca96bb009d632b8f5e601b950d93fb26f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:14 +0000

    Add package fake-bpy-module-3.0/20221006/fake_bpy_module_3.0-20221006-py3-none-any.whl

---
+++ packages/fake-bpy-module-3.0/fake_bpy_module_3.0-20221006-py3-none-any.whl/bl_operators/__init__.py
@@ -0,0 +1,48 @@
+import sys
+import typing
+from . import wm
+from . import presets
+from . import view3d
+from . import assets
+from . import bmesh
+from . import geometry_nodes
+from . import spreadsheet
+from . import userpref
+from . import object_randomize_transform
+from . import mesh
+from . import anim
+from . import uvcalc_follow_active
+from . import object_quick_effects
+from . import vertexpaint_dirt
+from . import console
+from . import object_align
+from . import image
+from . import file
+from . import clip
+from . import add_mesh_torus
+from . import rigidbody
+from . import uvcalc_lightmap
+from . import node
+from . import constraint
+from . import freestyle
+from . import sequencer
+from . import screen_play_rendered_anim
+from . import object
+
+GenericType = typing.TypeVar("GenericType")
+
+
+def register():
+    ''' 
+
+    '''
+
+    pass
+
+
+def unregister():
+    ''' 
+
+    '''
+
+    pass
---
+++ packages/fake-bpy-module-3.0/fake_bpy_module_3.0-20221006-py3-none-any.whl/bpy/ops/geometry.py
@@ -0,0 +1,43 @@
+import sys
+import typing
+import bpy.types
+
+GenericType = typing.TypeVar("GenericType")
+
+
+def attribute_add(override_context: typing.
+                  Union[typing.Dict, 'bpy.types.Context'] = None,
+                  execution_context: typing.Union[str, int] = None,
+                  undo: bool = None,
+                  *,
+                  name: str = "Attribute",
+                  domain: typing.Union[str, int] = 'POINT',
+                  data_type: typing.Union[str, int] = 'FLOAT'):
+    ''' Add attribute to geometry
+
+    :type override_context: typing.Union[typing.Dict, 'bpy.types.Context']
+    :type execution_context: typing.Union[str, int]
+    :type undo: bool
+    :param name: Name, Name of new attribute
+    :type name: str
+    :param domain: Domain, Type of element that attribute is stored on * POINT Point -- Attribute on point. * EDGE Edge -- Attribute on mesh edge. * FACE Face -- Attribute on mesh faces. * CORNER Face Corner -- Attribute on mesh face corner. * CURVE Spline -- Attribute on spline.
+    :type domain: typing.Union[str, int]
+    :param data_type: Data Type, Type of data stored in attribute * FLOAT Float -- Floating-point value. * INT Integer -- 32-bit integer. * FLOAT_VECTOR Vector -- 3D vector with floating-point values. * FLOAT_COLOR Color -- RGBA color with floating-point values. * BYTE_COLOR Byte Color -- RGBA color with 8-bit values. * STRING String -- Text string. * BOOLEAN Boolean -- True or false. * FLOAT2 2D Vector -- 2D vector with floating-point values.
+    :type data_type: typing.Union[str, int]
+    '''
+
+    pass
+
+
+def attribute_remove(override_context: typing.
+                     Union[typing.Dict, 'bpy.types.Context'] = None,
+                     execution_context: typing.Union[str, int] = None,
+                     undo: bool = None):
+    ''' Remove attribute from geometry
+
+    :type override_context: typing.Union[typing.Dict, 'bpy.types.Context']
+    :type execution_context: typing.Union[str, int]
+    :type undo: bool
+    '''
+
+    pass
