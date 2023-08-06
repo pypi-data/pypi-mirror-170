5625f888bf535dc66d27ebadec83596e7212fe7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:15 +0000

    Add package direpack/1.0.21/direpack-1.0.21.tar.gz

---
+++ packages/direpack/direpack-1.0.21.tar.gz/direpack-1.0.21/src/direpack/sudire/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 11 17:22:09 2020
+
+@author: Emmanuel Jordy Menvouta
+Edits by Sven Serneels. 
+"""
+
+
+__name__ = "sudire"
+__author__ = "Emmanuel Jordy Menvouta"
+__license__ = "MIT"
+__version__ = "0.1.5"
+__date__ = "2021-10-05"
