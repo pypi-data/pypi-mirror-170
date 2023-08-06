d4a18edc65b32a643a35de1ad853865183e0757b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:02 +0000

    Add package argilla/0.0.1/argilla-0.0.1.tar.gz

---
+++ packages/argilla/argilla-0.0.1.tar.gz/argilla-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='argilla',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://www.example.com',
+    author='argilla-io',
+    author_email='me@example.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
