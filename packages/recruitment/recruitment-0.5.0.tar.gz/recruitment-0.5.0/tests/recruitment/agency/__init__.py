c8b828bac90b28013362b66391ef9a4205a48224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:23 +0000

    Add package recruitment/0.5.0/recruitment-0.5.0.tar.gz

---
+++ packages/recruitment/recruitment-0.5.0.tar.gz/recruitment-0.5.0/tests/recruitment/agency/__init__.py
@@ -0,0 +1,40 @@
+from contextlib import contextmanager
+from io import IOBase as Buffer
+
+from botocore.session import get_session
+
+
+acceptable_broker_names = (
+    'kinesis',
+    's3',
+    'sns',
+    'sqs',
+)
+
+fake_credentials = {
+    'region_name': 'somewhere-in-the-world',
+    'access_key_id': 's3curityBadge!',
+    'secret_access_key': 'p@ssw0rd!',
+    'endpoint_url': 'some-computer.com',
+}
+
+
+def client(client_type: str, region_name: str):
+    return get_session().create_client(client_type, region_name=region_name)
+
+
+def raise_this(**kwargs):
+    exception = kwargs['exception']
+    def will_raise(**kwargs):
+        raise exception
+    return will_raise
+
+
+@contextmanager
+def uncloseable(buffer: Buffer):
+    """Context manager which makes the buffer's close operation a no-op"""
+    close = buffer.close
+    buffer.close = lambda: None
+    yield buffer
+    buffer.close = close
+    buffer.seek(0)  # fake close
