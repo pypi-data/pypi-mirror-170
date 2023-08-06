1ef4328a88533dc79e5c04f80ded80a17b4dddff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:47 +0000

    Add package ElasticTabstops/1.0.1/ElasticTabstops-1.0.1.tar.gz

---
+++ packages/ElasticTabstops/ElasticTabstops-1.0.1.tar.gz/ElasticTabstops-1.0.1/elastictabstops/__init__.py
@@ -0,0 +1,14 @@
+"""
+ElasticTabstops package
+Converts text indented/aligned with elastic tabstops
+see: https://nickgravgaard.com/elastictabstops/
+
+Repo at: https://github.com/nickgravgaard/elastic-tabstops-py
+"""
+
+# Copyright (c) 2007-2022 Nick Gravgaard <nick@nickgravgaard.com>
+# This code is licensed under the MIT Licence - see LICENCE.txt
+
+from elastictabstops.classes import Text, Table
+
+__all__ = ['Text', 'Table']
