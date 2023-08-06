73dd7d9c8f42c324de91698cc9dcfb06941a1d9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:47 +0000

    Add package pyltk/0.0.2/pyltk-0.0.2-py3-none-any.whl

---
+++ packages/pyltk/pyltk-0.0.2-py3-none-any.whl/pyltk/tokenizer.py
@@ -0,0 +1,14 @@
+from typing import Callable, List, Generator
+
+_accept_byte = lambda c: c < 122 if c > 96 else c > 64 or c < 33 or c == 45 if c < 91 else False
+
+def word_tokenize(text: str):
+    for char in ("-", "—"):
+        text = text.replace(char, " ")
+    for char in (
+        char
+        for char in set(text)
+        if not _accept_byte(ord(char))
+    ):
+        text = text.replace(char, "")
+    return text.split()
