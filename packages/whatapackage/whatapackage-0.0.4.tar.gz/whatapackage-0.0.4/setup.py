99f95d1adfc3676ddfbf0e7c88d1d24381ad4592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:47 +0000

    Add package whatapackage/0.0.4/whatapackage-0.0.4.tar.gz

---
+++ packages/whatapackage/whatapackage-0.0.4.tar.gz/whatapackage-0.0.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='whatapackage',
+    version='0.0.4',
+    description='this is the first package i do',
+    long_description='What a very long desc version fake',
+    long_description_content_type='text/plain',
+    packages=find_packages()
+)
