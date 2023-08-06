a4c29105b7d226136cc4eddf6df852d0869643fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:28 +0000

    Add package haizi-doushi-laolinghun/2022.10.9.0/haizi_doushi_laolinghun-2022.10.9.0-py3-none-any.whl

---
+++ packages/haizi-doushi-laolinghun/haizi_doushi_laolinghun-2022.10.9.0-py3-none-any.whl/HaiziDoushiLaolinghun/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""haizi-doushi-laolinghun
+https://github.com/apachecn/haizi-doushi-laolinghun"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.9.0"
