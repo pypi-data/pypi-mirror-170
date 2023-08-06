f399dfc02c615fe1214276602ce80396bbb75929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:04 +0000

    Add package shaq-probability/0.2/shaq_probability-0.2.tar.gz

---
+++ packages/shaq-probability/shaq_probability-0.2.tar.gz/shaq_probability-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='shaq_probability',
+      version='0.2',
+      description='Probability distributions',
+      packages=['shaq_probability'],
+      author = 'Mohammed Shaqura',
+	  author_email = 'mohammad.shaqura@gmail.com',
+      zip_safe=False)
