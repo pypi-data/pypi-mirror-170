8cea3656e6351e4c2a5686c00a8ab8a6d7c57b60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:20 +0000

    Add package generalize-config/1.1.0/generalize_config-1.1.0-py3-none-any.whl

---
+++ packages/generalize-config/generalize_config-1.1.0-py3-none-any.whl/generalize_config/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from generalize_config.config_file import read_config_file, read_config_file_if_readable
+
+__version__ = "1.1.0"
+
+__all__ = (
+    "__version__",
+    "read_config_file",
+    "read_config_file_if_readable",
+)
