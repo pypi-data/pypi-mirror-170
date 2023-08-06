04aaa46fa640088ed8249c7ac075e33712b947c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:28 +0000

    Add package ghazi/1/ghazi-1.tar.gz

---
+++ packages/ghazi/ghazi-1.tar.gz/ghazi-1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="ghazi",
+    version=1,
+    author="Ghazi",
+    author_email="Ghazi123@mej.com",
+    description='Ghazi library',
+    long_description_content_type="text/markdown",
+    long_description='Ghazi library',
+    packages=find_packages(),
+    keywords=['python', 'ghazi'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
