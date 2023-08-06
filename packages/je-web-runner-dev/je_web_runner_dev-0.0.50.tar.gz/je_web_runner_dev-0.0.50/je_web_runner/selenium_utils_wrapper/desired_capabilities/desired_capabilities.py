6e2dc66c10eea70fdbb6dda6cfaec4c9aecf9bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:27 +0000

    Add package je-web-runner-dev/0.0.50/je_web_runner_dev-0.0.50.tar.gz

---
+++ packages/je-web-runner-dev/je_web_runner_dev-0.0.50.tar.gz/je_web_runner_dev-0.0.50/je_web_runner/selenium_utils_wrapper/desired_capabilities/desired_capabilities.py
@@ -0,0 +1,30 @@
+from typing import Any, Union
+
+from selenium.webdriver import DesiredCapabilities
+
+from je_web_runner.utils.exception.exception_tag import selenium_wrapper_web_driver_not_found_error
+from je_web_runner.utils.exception.exceptions import WebRunnerException
+
+desired_capabilities_dict = {
+    "firefox": DesiredCapabilities.FIREFOX,
+    "chrome": DesiredCapabilities.CHROME,
+    "edge": DesiredCapabilities.EDGE,
+    "safari": DesiredCapabilities.SAFARI,
+}
+
+
+def get_desired_capabilities_keys() -> Union[str, Any]:
+    return desired_capabilities_dict.keys()
+
+
+def get_desired_capabilities(webdriver_name: str) -> \
+        [
+            DesiredCapabilities.FIREFOX.copy(),
+            DesiredCapabilities.CHROME.copy(),
+            DesiredCapabilities.EDGE.copy(),
+            DesiredCapabilities.SAFARI.copy(),
+        ]:
+    desired_capabilities = desired_capabilities_dict.get(webdriver_name)
+    if desired_capabilities is None:
+        raise WebRunnerException(selenium_wrapper_web_driver_not_found_error)
+    return desired_capabilities.copy()
