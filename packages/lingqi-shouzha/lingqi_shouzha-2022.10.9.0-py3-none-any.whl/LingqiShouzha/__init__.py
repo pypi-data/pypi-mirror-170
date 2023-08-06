752167002d3cd7e0ae74a12e3285a41e8e5bbfd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:54 +0000

    Add package lingqi-shouzha/2022.10.9.0/lingqi_shouzha-2022.10.9.0-py3-none-any.whl

---
+++ packages/lingqi-shouzha/lingqi_shouzha-2022.10.9.0-py3-none-any.whl/LingqiShouzha/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""lingqi-shouzha
+https://github.com/apachecn/lingqi-shouzha"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.9.0"
