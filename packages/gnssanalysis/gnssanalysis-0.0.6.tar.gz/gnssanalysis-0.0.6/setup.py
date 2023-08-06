1da42e307f1be1a6ae45552b4eb72488c658ce1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:54 +0000

    Add package gnssanalysis/0.0.6/gnssanalysis-0.0.6.tar.gz

---
+++ packages/gnssanalysis/gnssanalysis-0.0.6.tar.gz/gnssanalysis-0.0.6/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    include_package_data=True,
+    name="gnssanalysis",
+    version="0.0.6",
+    description="basic python module for gnss analysis",
+    author="Geoscience Australia",
+    author_email="GNSSAnalysis@ga.gov.au",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "boto3",
+        "click",
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "plotext==4.2",
+        "plotly",
+        "pymongo",
+        "pytest",
+        "scipy",
+        "tqdm",
+        "unlzw",
+    ],
+)
