a1a76aee50d4ebca7d54316f13e4d9cfe02196c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:28 +0000

    Add package FairArticle/4.3.2/FairArticle-4.3.2.tar.gz

---
+++ packages/FairArticle/FairArticle-4.3.2.tar.gz/FairArticle-4.3.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import os
+
+current = os.getcwd()
+
+setup(
+    name='FairArticle',
+    version='4.3.2',
+    description='Article Provider for Sources, Categorizer, Utils and more.',
+    url='https://github.com/chazzcoin/FairArticle',
+    author='ChazzCoin',
+    author_email='chazzcoin@gmail.com',
+    license='BSD 2-clause',
+    packages=find_packages(),
+    package_dir={'res': 'FAui'},
+    package_data={
+        'FAui': ['*', '*.ui']
+    },
+    install_requires=['fairweb>=4.1.1', 'fairresources>=4.0.3', 'nltk>=3.5'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ]
+)
