c8b32b4092ea51a315109f90f0751cb255a48287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:23 +0000

    Add package sudio/1.0.9.2/sudio-1.0.9.2.tar.gz

---
+++ packages/sudio/sudio-1.0.9.2.tar.gz/sudio-1.0.9.2/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='sudio',
+    version='1.0.9.2',
+    packages=['sudio',],
+    author="hussein zahaki",
+    author_email="hussein.zahaki.mansoor@gmail.com",
+    description="Real-Time Audio Processing Platform",
+    long_description=long_description,
+    install_requires=[
+            'scipy>=1.9.1',
+            'numpy>=1.23.3',
+            'pyaudio>=0.2.12',
+            'pandas>=1.5.0',
+            'miniaudio>=1.52',
+            'samplerate>=0.1.0',
+        ],
+    long_description_content_type="text/markdown",
+    url="https://github.com/MrZahaki/sudio",
+    license='Apache License 2.0',
+    classifiers=[
+     "Programming Language :: Python :: 3",
+     'License :: OSI Approved :: Apache Software License',
+    ],
+    license_files=['LICENSE'],
+ )
