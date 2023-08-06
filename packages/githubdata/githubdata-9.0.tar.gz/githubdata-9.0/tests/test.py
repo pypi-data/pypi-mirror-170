e39f1bb2fbaef7673b96472633b516547df76d72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:55 +0000

    Add package githubdata/9.0/githubdata-9.0.tar.gz

---
+++ packages/githubdata/githubdata-9.0.tar.gz/githubdata-9.0/tests/test.py
@@ -0,0 +1,44 @@
+"""
+
+  """
+
+from src.githubdata.githubdata import get_data_from_github
+from src.githubdata.githubdata import GithubData
+
+
+## the most simple usage
+u = 'https://github.com/imahdimir/d-TSETMC_ID-2-FirmTicker'
+df = get_data_from_github(u)
+
+## clone a public repo
+u = 'https://github.com/imahdimir/d-TSETMC_ID-2-FirmTicker'
+repo = GithubData(u)
+repo.overwriting_clone()
+
+##
+repo.rmdir()
+
+## clone a public repo and commit back
+u = 'https://github.com/imahdimir/test-public'
+repo = GithubData(u)
+repo.overwriting_clone()
+
+##
+msg = 'test commit'
+repo.commit_and_push(msg)
+
+##
+repo.rmdir()
+
+## clone a private repo and commit back
+ur = 'https://github.com/imahdimir/test-private'
+rp = GithubData(ur)
+rp.overwriting_clone()
+
+##
+
+
+##
+rp.commit_and_push('test commit')
+
+##
