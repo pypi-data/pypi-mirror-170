396c65398898a8086e867cb62b3fade3c469d2c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:48 +0000

    Add package grpyc/0.1.3/grpyc-0.1.3.tar.gz

---
+++ packages/grpyc/grpyc-0.1.3.tar.gz/grpyc-0.1.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup,find_packages
+
+
+from pip._internal.req import parse_requirements
+
+def load_requirements(fname):
+    reqs = parse_requirements(fname, session="test")
+    return [str(ir.req) for ir in reqs]
+
+#Actual setup
+setup(
+    name="grpyc",
+    version='0.1.3',
+    description='Framework for grpcio (gRPC for python)',
+    long_description="To ease compilation, project structure and tests",
+    long_description_content_type='text/plain',
+    license='MIT',
+    author="gloxounet",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/Gloxounet/gRPyC',
+    keywords='gRPC framework',
+    py_modules=['cli','compilation'],
+    install_requires=['grpcio-tools>=1.49.1','Click'],
+    python_requires='>=3',
+    entry_points='''
+        [console_scripts]
+        grpyc=cli:cli
+    ''',
+)
