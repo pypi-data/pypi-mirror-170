dfe067c4497391149f640cc89c0e893b26420306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:47 +0000

    Add package EarthMC/2.3.0/EarthMC-2.3.0.tar.gz

---
+++ packages/EarthMC/EarthMC-2.3.0.tar.gz/EarthMC-2.3.0/setup.py
@@ -0,0 +1,23 @@
+# Read contents README
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+from setuptools import setup
+setup(
+    name="EarthMC",
+    version="2.3.0",
+    description="Provides data on people, places and more on the EarthMC Minecraft server.",
+    author="Owen3H",
+    license="MIT",
+    url="https://github.com/EarthMC-Toolkit/EarthMC-Py",
+    py_modules=["__init__", "Utils"],
+    package_dir={'': 'src'},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=False
+)
+
+# Build dist using this: python setup.py sdist bdist_wheel
+# Upload using this: python -m twine upload dist/*
