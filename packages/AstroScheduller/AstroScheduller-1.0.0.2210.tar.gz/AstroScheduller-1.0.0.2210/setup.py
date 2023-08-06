1d3f210733ea1ea1d0121211afafd2e0652de88c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:22 +0000

    Add package AstroScheduller/1.0.0.2210/AstroScheduller-1.0.0.2210.tar.gz

---
+++ packages/AstroScheduller/AstroScheduller-1.0.0.2210.tar.gz/AstroScheduller-1.0.0.2210/setup.py
@@ -0,0 +1,34 @@
+from gettext import install
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="AstroScheduller",
+    version="1.0.0.2210",
+    author="Wenky",
+    author_email="wxia1@fandm.edu",
+    description="AstroScheduller algorithms and packages for planning astronomical observations.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AstroScheduller/AstroScheduller",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'requests',
+        'tkinter',
+        'astropy',
+        'Pillow'
+    ],
+    package_data={
+        'astroscheduller': [
+            'ashrel/*.png',
+            'ashrel/*.ico',
+            'ashrel/*.json',
+            'ashrel/*.txt',
+            'ashrel/*.ash'
+        ]
+    }
+)
