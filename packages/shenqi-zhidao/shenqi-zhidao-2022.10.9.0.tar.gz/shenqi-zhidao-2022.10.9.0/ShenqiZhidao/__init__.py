44feabc016c3b0948dd0e28af9474beaa95baebd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:15:02 +0000

    Add package shenqi-zhidao/2022.10.9.0/shenqi-zhidao-2022.10.9.0.tar.gz

---
+++ packages/shenqi-zhidao/shenqi-zhidao-2022.10.9.0.tar.gz/shenqi-zhidao-2022.10.9.0/ShenqiZhidao/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shenqi-zhidao
+https://github.com/apachecn/shenqi-zhidao"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.9.0"
