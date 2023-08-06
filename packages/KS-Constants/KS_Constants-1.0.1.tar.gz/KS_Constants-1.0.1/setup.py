fff84f2705bf887c437584d9e51ec6e730693bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:09 +0000

    Add package KS-Constants/1.0.1/KS_Constants-1.0.1.tar.gz

---
+++ packages/KS-Constants/KS_Constants-1.0.1.tar.gz/KS_Constants-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='KS_Constants',
+    version='1.0.1',
+    license='MIT',
+    author="Steven Su",
+    author_email='ks2devteam@gmail.com',
+    packages=find_packages('ks_constants'),
+    package_dir={'': 'ks_constants'},
+    url='https://github.com/kerrigan-survival-team/ks_constants_pyi'
+)
