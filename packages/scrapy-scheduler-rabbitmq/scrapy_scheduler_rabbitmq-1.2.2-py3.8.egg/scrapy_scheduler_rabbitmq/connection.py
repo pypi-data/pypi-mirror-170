dc283030a31ac774ce492442c4570e4b76493b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:02 +0000

    Add package scrapy-scheduler-rabbitmq/1.2.2/scrapy_scheduler_rabbitmq-1.2.2-py3.8.egg

---
+++ packages/scrapy-scheduler-rabbitmq/scrapy_scheduler_rabbitmq-1.2.2-py3.8.egg/scrapy_scheduler_rabbitmq/connection.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import pika
+
+
+def get_channel(connection, queue_name, durable=True, confirm_delivery=True, is_delay=False, is_lazy=True):
+    """ Init method to return a prepared channel for consuming
+    """
+    channel = connection.channel()
+    args = {'x-max-priority': 10}
+    if is_lazy:
+        args['x-queue-mode'] = 'lazy'
+    channel.queue_declare(queue=queue_name, durable=durable, arguments=args)
+    if confirm_delivery:
+        channel.confirm_delivery()
+
+    if is_delay is True:
+        exchange_name = "{}-delay".format(queue_name)
+        channel.exchange_declare(exchange_name,exchange_type="x-delayed-message",arguments={"x-delayed-type": "direct"})
+        channel.queue_bind(queue=queue_name, exchange=exchange_name, routing_key=queue_name)
+    return channel
+
+
+def connect(connection_url):
+    """ Create and return a fresh connection
+    """
+    return pika.BlockingConnection(pika.URLParameters(connection_url))
