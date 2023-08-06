f7cd35c401e0221580acb08f4da443b58b6a9a87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:43 +0000

    Add package gjcode/0.0.8/gjcode-0.0.8.tar.gz

---
+++ packages/gjcode/gjcode-0.0.8.tar.gz/gjcode-0.0.8/src/gjcode/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.0.8"
+
+default_options = 2
+default_rolls = 100000
+default_details = 0
---
+++ packages/gjcode/gjcode-0.0.8.tar.gz/gjcode-0.0.8/src/gjcode/__main__.py
@@ -0,0 +1,9 @@
+from . import rnd
+
+
+def main():
+    rnd.rnd_print(3, 10001, 1)
+
+
+if __name__ == "__main__":
+    main()
