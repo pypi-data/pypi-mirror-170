eba5be54af94e43f2aba769064b3f8a46401156e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:11 +0000

    Add package shaq-distributions/0.1/shaq_distributions-0.1.tar.gz

---
+++ packages/shaq-distributions/shaq_distributions-0.1.tar.gz/shaq_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='shaq_distributions',
+      version='0.1',
+      description='Probability distributions',
+      packages=['shaq_distributions'],
+      zip_safe=False)
