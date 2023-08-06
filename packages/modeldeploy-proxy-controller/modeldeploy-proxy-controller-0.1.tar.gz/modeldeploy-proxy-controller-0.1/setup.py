ed1d9e6a6cb0d00a9a1a733285d3217b34b43450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:43 +0000

    Add package modeldeploy-proxy-controller/0.1/modeldeploy-proxy-controller-0.1.tar.gz

---
+++ packages/modeldeploy-proxy-controller/modeldeploy-proxy-controller-0.1.tar.gz/modeldeploy-proxy-controller-0.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+setup(
+    name = 'modeldeploy-proxy-controller',
+    version = '0.1',
+    description = 'The JupyterLab backend component for modeldeploy-proxy.',
+    author = 'ever cheng',
+    author_email = 'ever_cheng@asus.com',
+    license = 'Apache License Version 2.0',
+    packages = [
+        'modeldeploy_proxy_controller',
+        'modeldeploy_proxy_controller.common',
+        'modeldeploy_proxy_controller.rpc',
+        'modeldeploy_proxy_controller.processors',
+    ],
+    install_requires = [
+        'IPython >= 7.6.0',
+        'jupyter-client >=5.3.4, <7.0.0',
+        'jupyter-core >= 4.6.0',
+        'nbconvert >=5.6.1, <6.0.0',
+        'nbformat >=4.4, <5.0.0',
+        'ipykernel >= 5.1.4',
+        'notebook >= 6.0.0',
+        'packaging > 20',
+    ],
+    extras_require = {
+        'dev': [
+            'pytest',
+            'pytest-clarity',
+            'testfixtures',
+            'pytest-cov',
+        ]
+    },
+    entry_points = {
+        'console_scripts': [
+            'modeldeploy_proxy_controller=src.cli:main'
+        ]
+    },
+    python_requires = '>=3.6.0',
+    include_package_data = True,
+    zip_safe = False
+)
