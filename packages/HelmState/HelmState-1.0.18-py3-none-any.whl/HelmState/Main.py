763779ae57ac7671a113c0484d25579aefdb4b76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:40 +0000

    Add package HelmState/1.0.18/HelmState-1.0.18-py3-none-any.whl

---
+++ packages/HelmState/HelmState-1.0.18-py3-none-any.whl/HelmState/Main.py
@@ -0,0 +1,95 @@
+import json
+from typing import Union, List
+
+import yaml
+import sys
+from HelmState.Tools import StateHandler
+from HelmState.Tools import GitTools
+from HelmState.Tools import ArgumentTools
+from HelmState.Tools import DumpTools
+
+
+def Main(args: list = None):
+    arguments = ArgumentTools.ParseArguments(args)
+    outputStr = HandleAction(arguments.action,
+                             arguments.helm_chart,
+                             arguments.version,
+                             arguments.namespace,
+                             arguments.resource_group,
+                             arguments.message,
+                             arguments.commits,
+                             arguments.push,
+                             arguments.folder,
+                             arguments.output,
+                             arguments.dumps,
+                             arguments.master_branch,
+                             arguments.remote,
+                             arguments.offline)
+
+    return outputStr
+
+
+def HandleAction(action: str,
+                 helmChart: Union[str, List[str]],
+                 version: str = None,
+                 namespace: str = 'default',
+                 resourceGroup: str = 'default',
+                 message: str = 'Auto state update',
+                 commits: int = 1,
+                 push: bool = False,
+                 folder: str = './',
+                 output: str = 'json',
+                 dumps: str = None,
+                 masterBranch: str = 'master',
+                 remote: str = 'remote',
+                 offline: bool = False):
+    repoHelmCharts = helmChart if isinstance(helmChart, list) else [helmChart]
+    outputData = {}
+    repo = GitTools.GetRepo(folder, initializeIfNotExists=True, push=push, offline=offline)
+    if len(repoHelmCharts) == 0:
+        repoHelmCharts = GitTools.GetHelmChartBranches(repo, resourceGroup, namespace, remote=remote, offline=offline)
+    for repoHelmChart in repoHelmCharts:
+        GitTools.CheckoutHelmChartBranch(repo, resourceGroup, namespace, repoHelmChart,
+                                         masterBranch=masterBranch, remote=remote, offline=offline)
+        if action == 'get':
+            pass
+        elif action == 'commit':
+            if version is None:
+                raise Exception('Please provide a version tag following the helm chart name. '
+                                'Add -h/--help for more info.')
+            state = StateHandler.LoadState(folder)
+            StateHandler.UpdateHelmVersion(state, resourceGroup, namespace, repoHelmChart, version)
+            GitTools.CommitState(repo, state, folder, resourceGroup, namespace, repoHelmChart, message,
+                                 remote=remote, push=push, offline=offline)
+        elif action == 'revert':
+            GitTools.RevertState(repo, resourceGroup, namespace, repoHelmChart, commits, remote=remote, push=push, offline=offline)
+        else:
+            raise Exception('No matching action provided, please add -h/--help to get help.')
+
+        state = StateHandler.LoadState(folder)
+        helmChartData = StateHandler.GetHelmChartData(state, resourceGroup, namespace, repoHelmChart)
+        if len(repoHelmCharts) > 1:
+            outputData = DumpTools.MergeDictData(outputData, {repoHelmChart: helmChartData})
+        else:
+            outputData = helmChartData
+
+    GitTools.CheckoutMasterBranch(repo, masterBranch=masterBranch, remote=remote, offline=offline)
+
+    if output == 'json':
+        outputStr = json.dumps(outputData, sort_keys=True, indent=4)
+    elif output == 'yaml':
+        outputStr = yaml.safe_dump(outputData)
+    elif output == 'env':
+        outputStr = DumpTools.DumpToEnv(outputData)
+    else:
+        raise Exception(f'Invalid output type specified: {output}, please add -h/--help to get help.')
+
+    if dumps is not None:
+        with open(dumps, 'w') as f:
+            f.write(outputStr)
+
+    return outputStr
+
+
+if __name__ == "__main__":
+    sys.stdout.write(Main())
---
+++ packages/HelmState/HelmState-1.0.18-py3-none-any.whl/HelmState/Tools/GitTools.py
@@ -0,0 +1,193 @@
+from typing import List
+
+import git
+import os
+from HelmState.Tools import StateHandler
+
+
+def CreateDefaultReadmeFile(repoFolder: str, filename = 'README.md', content: str = None):
+    if content is None:
+        content = '# Helm State \n'
+        content += 'Get more info about the helm state tool at: https://github.com/hansehe/HelmState'
+
+    defaultFile = os.path.join(repoFolder, filename)
+    if not os.path.isfile(defaultFile):
+        with open(defaultFile, 'w') as f:
+            f.write(content)
+
+    return defaultFile
+
+
+def GetRepo(repoFolder: str, initializeIfNotExists=False,
+            masterBranch: str = 'master', 
+            remote: str = 'origin', 
+            push: bool = True, 
+            offline: bool = False):
+    stateFile = StateHandler.GetStateFilePath(repoFolder)
+    repoFolder = os.path.dirname(stateFile)
+    if not os.path.isdir(os.path.join(repoFolder, '.git')):
+        if initializeIfNotExists:
+            repo = git.Repo.init(repoFolder)
+        else:
+            raise Exception(f'State repoFolder {repoFolder} does not exist!')
+    else:
+        repo = git.Repo(repoFolder)
+
+    if GetCommitCount(repo) == 0:
+        defaultFile = CreateDefaultReadmeFile(repoFolder)
+        CommitFiles(repo, masterBranch, 'initial commit', files=[defaultFile], remote=remote, push=push, offline=offline)
+    if not offline and remote in repo.remotes:
+        repo.remotes[remote].update(prune=True)
+    return repo
+
+
+def GetCommitCount(repo: git.Repo):
+    try:
+        count = repo.git.rev_list('--count', 'HEAD')
+        return int(count)
+    except git.exc.GitCommandError:
+        return 0
+
+
+def NormalizeFile(repo: git.Repo, file: str):
+    normalizedFile = os.path.normpath(file)
+    if not os.path.isabs(normalizedFile):
+        normalizedFile = os.path.join(os.path.normpath(repo.working_dir), normalizedFile)
+    return normalizedFile
+
+
+def NormalizeFiles(repo: git.Repo, files: []):
+    normalizedFiles = []
+    for file in files:
+        normalizedFile = NormalizeFile(repo, file)
+        normalizedFiles.append(normalizedFile)
+    return normalizedFiles
+
+
+def CheckForUntrackedChanges(repo: git.Repo, files: list = None):
+    if files is None:
+        return repo.index.diff(None) or repo.untracked_files
+
+    aFileIsUntracked = False
+    normalizedFiles = NormalizeFiles(repo, files)
+    normalizedUntrackedFiles = NormalizeFiles(repo, repo.untracked_files)
+    for normalizedFile in normalizedFiles:
+        if normalizedFile in normalizedUntrackedFiles:
+            aFileIsUntracked = True
+            break
+
+    return repo.index.diff(None, paths=normalizedFiles) or aFileIsUntracked
+
+
+def CommitFiles(repo: git.Repo, branch: str, message: str, files: list = None,
+                remote: str = 'origin', 
+                push: bool = True, 
+                offline: bool = False):
+    if CheckForUntrackedChanges(repo, files):
+        if files is None:
+            repo.git.add(A=True)
+        else:
+            repo.git.add(files)
+        repo.git.commit(m=message)
+    if remote in repo.remotes and push and not offline:
+        repo.git.push('--set-upstream', remote, branch)
+
+
+def GetBranchState(repo: git.Repo, branch: str):
+    commitsDiff = repo.git.rev_list('--left-right', '--count', f'{branch}...{branch}@{{u}}')
+    numCommitsAhead, numCommitsBehind = commitsDiff.split('\t')
+    return int(numCommitsAhead), int(numCommitsBehind)
+
+
+
+def PullBranch(repo: git.Repo, branch: str, 
+               remote: str = 'origin', 
+               checkoutBranchFromOrigin = False):
+    if remote in repo.remotes:
+        remoteBranches = repo.remotes[remote].refs
+        remoteBranch = f'{remote}/{branch}'
+        if remoteBranch in remoteBranches:
+            repo.index.reset(head=True)
+            if checkoutBranchFromOrigin:
+                repo.git.checkout(remoteBranch, b=branch)
+            else:
+                numCommitsAhead, numCommitsBehind = GetBranchState(repo, branch)
+                if numCommitsBehind > 0:
+                    repo.git.pull(remote, branch)
+                repo.index.reset(head=True)
+            return True
+    return False
+
+
+def CheckoutBranch(repo: git.Repo, branch: str,
+                   masterBranch: str = 'master', 
+                   remote: str = 'origin', 
+                   offline: bool = False):
+    if branch in repo.branches:
+        repo.git.checkout(branch)
+        if not offline:
+            PullBranch(repo, branch, remote=remote)
+    else:
+        remoteExists = PullBranch(repo, branch, remote=remote, checkoutBranchFromOrigin=True) if not offline else False
+        if not remoteExists:
+            repo.git.checkout(masterBranch)
+            if not offline:
+                PullBranch(repo, masterBranch, remote=remote)
+            repo.git.checkout(masterBranch, b=branch)
+
+
+def CheckoutMasterBranch(repo: git.Repo,
+                         masterBranch: str = 'master', 
+                         remote: str = 'origin', 
+                         offline: bool = False):
+    CheckoutBranch(repo, masterBranch, masterBranch=masterBranch, remote=remote, offline=offline)
+
+
+def CheckoutHelmChartBranch(repo: git.Repo, resourceGroup: str, namespace: str, helmChart: str,
+                            masterBranch: str = 'master', 
+                            remote: str = 'origin', 
+                            offline: bool = False):
+    branch = StateHandler.GetStateBranchname(resourceGroup, namespace, helmChart)
+    CheckoutBranch(repo, branch, masterBranch=masterBranch, remote=remote, offline=offline)
+
+
+def GetHelmChartBranches(repo: git.Repo, resourceGroup: str, namespace: str,
+                         remote: str = 'origin', 
+                         offline: bool = False):
+    helmCharts: List[str] = []
+    repoBranches = list(map(lambda repoBranch: str(repoBranch), repo.branches))
+    if not offline and remote in repo.remotes:
+        remoteBranches = repo.remotes[remote].refs
+        for remoteBranch in remoteBranches:
+            localBranch = str(remoteBranch).replace(f'{remote}/', '', 1)
+            if localBranch not in repoBranches:
+                repoBranches.append(localBranch)
+    for branch in repoBranches:
+        if branch.startswith(f'{resourceGroup}/{namespace}/'):
+            helmCharts.append(str(branch).split('/')[2])
+    return helmCharts
+
+
+def CommitState(repo: git.Repo, state: dict, repoFolder: str, resourceGroup: str, namespace: str, helmChart: str, message: str,
+                remote: str = 'origin', 
+                push: bool = True, 
+                offline: bool = False):
+    branch = StateHandler.GetStateBranchname(resourceGroup, namespace, helmChart)
+    stateFile = StateHandler.DumpState(state, repoFolder)
+    CommitFiles(repo, branch, message, files=[stateFile], remote=remote, push=push, offline=offline)
+
+
+def RevertState(repo: git.Repo, resourceGroup: str, namespace: str, helmChart: str,
+                numberOfCommits = 1, 
+                remote: str = 'origin', 
+                push: bool = True, 
+                offline: bool = False):
+    branch = StateHandler.GetStateBranchname(resourceGroup, namespace, helmChart)
+    commitCount = GetCommitCount(repo)
+    if commitCount <= numberOfCommits:
+        raise Exception(f'Number of commits to revert ({numberOfCommits}) '
+                        f'must be lower then counted commits ({commitCount}) in branch {branch}!')
+
+    repo.head.reset(f'HEAD~{numberOfCommits}', index=True, working_tree=True)
+    if remote in repo.remotes and push and not offline:
+        repo.git.push('--set-upstream', '--force', remote, branch)
