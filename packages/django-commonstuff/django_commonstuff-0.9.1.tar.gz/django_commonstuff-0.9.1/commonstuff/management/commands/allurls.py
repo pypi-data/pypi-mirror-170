54accc3a2195f61a44ad0e26a412508ea023e49a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:00 +0000

    Add package django-commonstuff/0.9.1/django_commonstuff-0.9.1.tar.gz

---
+++ packages/django-commonstuff/django_commonstuff-0.9.1.tar.gz/django_commonstuff-0.9.1/commonstuff/management/commands/allurls.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from django.core.management.base import BaseCommand
+from django.urls import get_resolver
+
+
+class Command(BaseCommand):
+    help = 'Shows all project urls (routes) alphabetically in cli environment.'
+    
+    def handle(self, *args, **options):
+        urls = sorted(set(v[1] for k,v in get_resolver(None).reverse_dict.items()))
+        for url in urls:
+            self.stdout.write(url)
