503056edb961ad791cd5e4c762fa43b758aa5899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:10 +0000

    Add package chemiscope/0.4.2/chemiscope-0.4.2.tar.gz

---
+++ packages/chemiscope/chemiscope-0.4.2.tar.gz/chemiscope-0.4.2/python/chemiscope/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from .input import create_input, write_input  # noqa
+from .structures import all_atomic_environments, librascal_atomic_environments  # noqa
+
+try:
+    # only import the chemiscope.show function if we have ipywidgets installed.
+    import ipywidgets  # noqa
+
+    from .jupyter import show  # noqa
+except ImportError:
+    pass
+
+__version__ = "0.4.2"
