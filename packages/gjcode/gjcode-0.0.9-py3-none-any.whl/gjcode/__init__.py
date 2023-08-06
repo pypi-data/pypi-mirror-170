4469c92172190127d9bf41213c05b165d9f23643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:43 +0000

    Add package gjcode/0.0.9/gjcode-0.0.9-py3-none-any.whl

---
+++ packages/gjcode/gjcode-0.0.9-py3-none-any.whl/gjcode/__init__.py
@@ -0,0 +1,11 @@
+import random
+from . import rnd
+
+
+__all__ = ["rnd"]
+
+__version__ = "0.0.9"
+
+default_options = 2
+default_rolls = 100000
+default_details = 0
---
+++ packages/gjcode/gjcode-0.0.9-py3-none-any.whl/gjcode/test.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+import rnd
+
+rnd.rnd_print(4, 1000, 1)
