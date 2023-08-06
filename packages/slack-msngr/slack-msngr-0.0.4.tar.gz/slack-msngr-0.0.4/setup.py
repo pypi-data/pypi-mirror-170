7dd5ace8373527acff8f00521055be6aed5ec5f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:35 +0000

    Add package slack-msngr/0.0.4/slack-msngr-0.0.4.tar.gz

---
+++ packages/slack-msngr/slack-msngr-0.0.4.tar.gz/slack-msngr-0.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.4'
+DESCRIPTION = 'Slack Messanger Client using Incoming Webhooks'
+LONG_DESCRIPTION = 'This is a simple package that helps send Slack Messages via Incoming Webhook URLs'
+
+setup(
+    name="slack-msngr",
+    version=VERSION,
+    author="Louis Tadman",
+    author_email="<noreply@louistadman.com>",
+    url="https://github.com/LouieT/slack-messenger",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=["requests==2.*", "datetime==4.7"],
+    keywords=['python', 'Slack', "Messenger"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
