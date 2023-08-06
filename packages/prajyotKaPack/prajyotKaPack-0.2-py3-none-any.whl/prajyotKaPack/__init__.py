384833ed7414b57ae7c97334ea184cd3cfe72693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:47 +0000

    Add package prajyotKaPack/0.2/prajyotKaPack-0.2-py3-none-any.whl

---
+++ packages/prajyotKaPack/prajyotKaPack-0.2-py3-none-any.whl/prajyotKaPack/__init__.py
@@ -0,0 +1,16 @@
+def ins(a):
+    print("hello world ins")
+    return
+
+def ws(ws):
+    print("hello world ins")
+    return
+
+def gp(s):
+    print("hello world ins")
+    return
+
+
+def linux():
+    print("hello this is function of linux")
+    
