96e8a9b6b90f3644f3e108f5604aeacd2cd77d69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:09 +0000

    Add package getir-locals-swagger-client/1.0.4/getir-locals-swagger-client-1.0.4.tar.gz

---
+++ packages/getir-locals-swagger-client/getir-locals-swagger-client-1.0.4.tar.gz/getir-locals-swagger-client-1.0.4/swagger_client/api/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.auth_api import AuthApi
+from swagger_client.api.health_api import HealthApi
+from swagger_client.api.orders_api import OrdersApi
+from swagger_client.api.products_api import ProductsApi
+from swagger_client.api.shops_api import ShopsApi
+from swagger_client.api.supplier_api import SupplierApi
