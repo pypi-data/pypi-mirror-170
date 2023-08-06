69d2a5c19f37c707525781f3f2eb1b237f406d85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:34 +0000

    Add package pyworxcloud/2.1.20/pyworxcloud-2.1.20-py3-none-any.whl

---
+++ packages/pyworxcloud/pyworxcloud-2.1.20-py3-none-any.whl/pyworxcloud/clouds.py
@@ -0,0 +1,38 @@
+"""Supported cloud endpoints."""
+from __future__ import annotations
+
+WORX = "worx"
+KRESS = "kress"
+LANDXCAPE = "landxcape"
+
+
+class CloudType(object):
+    """Supported cloud types.
+
+    CloudType.WORX: For Worx Landroid devices.
+
+    CloudType.KRESS: For Kress devices.
+
+    CloudType.LANDXCAPE: For Landxcape devices.
+    """
+
+    class WORX(str):
+        """Settings for Worx devices."""
+
+        URL: str = "api.worxlandroid.com"
+        TOKEN_URL: str = "id.eu.worx.com"
+        KEY: str = "150da4d2-bb44-433b-9429-3773adc70a2a"
+
+    # class KRESS(str):
+    #     """Settings for Kress devices."""
+
+    #     URL: str = "api.kress-robotik.com"
+    #     TOKEN_URL: str = "id.eu.kress.com"
+    #     KEY: str = "62FA25FB-3509-4778-A835-D5C50F4E5D88"
+
+    class LANDXCAPE(str):
+        """Settings for Landxcape devices."""
+
+        URL: str = "api.landxcape-services.com"
+        TOKEN_URL: str = "id.landxcape-services.com"
+        KEY: str = "4F1B89F0-230F-410A-8436-D9610103A2A4"
