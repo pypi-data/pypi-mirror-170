947aaeea1d38bd5c8f1506c6cae40da2e9591c67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:26 +0000

    Add package yputils/0.0.1/yputils-0.0.1-py3-none-any.whl

---
+++ packages/yputils/yputils-0.0.1-py3-none-any.whl/yputils/ystring.py
@@ -0,0 +1,13 @@
+
+def replace(ori_text: str, old_text: str, new_text: str) -> str:
+    return ''
+
+
+def clean_null(text: str) -> str:
+
+    return ''
+
+
+def clean_html() -> str:
+
+    return ''
