1685a1aca8d91ca7bd35b9bee26ed6563c4f53f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:58 +0000

    Add package netsock/0.0.2/netsock-0.0.2.tar.gz

---
+++ packages/netsock/netsock-0.0.2.tar.gz/netsock-0.0.2/netsock/__init__.py
@@ -0,0 +1,15 @@
+"""
+:authors: two-it2022
+:license: Apache License, Version 0.2, see LICENSE file
+
+:copyright: (c) 2022 two-it2022
+"""
+
+from .sock import *
+
+
+
+
+__author__ = 'two-it2022'
+__version__ = '0.0.2'
+__email__ = 'kodland.group@gmail.com'
