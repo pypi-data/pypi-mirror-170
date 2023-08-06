c0a946999e031f68f04dd6769b438c6ba2629756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:06 +0000

    Add package pestuary/1.0.2/Pestuary-1.0.2.tar.gz

---
+++ packages/pestuary/Pestuary-1.0.2.tar.gz/Pestuary-1.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+        name="Pestuary",
+        version="1.0.2",
+        py_modules="pestuary",
+        packages=find_packages(),
+        include_package_data=True,
+        install_requires=[
+                'Click',
+                "estuary_client",
+                "requests",
+            ],
+        entry_points='''
+            [console_scripts]
+            pestuary=pestuary_cli:main
+        '''
+)
+        
