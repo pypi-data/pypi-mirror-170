530e91adae31d5f50d4488b67649e38952679c48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:06 +0000

    Add package pidgy/2022.10.5/pidgy-2022.10.5-py3-none-any.whl

---
+++ packages/pidgy/pidgy-2022.10.5-py3-none-any.whl/pidgy/kernel.py
@@ -0,0 +1,25 @@
+"""a modified pidgy kernel.
+
+the command line application installs the kernel"""
+
+from contextlib import suppress
+
+from ipykernel.ipkernel import IPythonKernel
+
+
+class Kernel(IPythonKernel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        from . import load_ipython_extension
+
+        load_ipython_extension(self.shell)
+        with suppress(BaseException):
+            from .widgets import load_ipython_extension
+
+            load_ipython_extension(self.shell)
+
+
+if __name__ == "__main__":
+    import ipykernel.kernelapp
+
+    ipykernel.kernelapp.IPKernelApp.launch_instance(kernel_class=Kernel)
