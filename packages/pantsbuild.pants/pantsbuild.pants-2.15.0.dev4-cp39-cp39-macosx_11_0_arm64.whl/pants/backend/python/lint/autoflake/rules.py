68928d1f2cb513d98cbdcace2db94413c165a1ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:11 +0000

    Add package pantsbuild.pants/2.15.0.dev4/pantsbuild.pants-2.15.0.dev4-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/pantsbuild.pants/pantsbuild.pants-2.15.0.dev4-cp39-cp39-macosx_11_0_arm64.whl/pants/backend/python/lint/autoflake/rules.py
@@ -0,0 +1,73 @@
+# Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from dataclasses import dataclass
+
+from pants.backend.python.lint.autoflake.skip_field import SkipAutoflakeField
+from pants.backend.python.lint.autoflake.subsystem import Autoflake
+from pants.backend.python.target_types import PythonSourceField
+from pants.backend.python.util_rules import pex
+from pants.backend.python.util_rules.pex import PexRequest, VenvPex, VenvPexProcess
+from pants.core.goals.fmt import FmtResult, FmtTargetsRequest
+from pants.core.goals.lint import PartitionerType
+from pants.engine.fs import Digest
+from pants.engine.internals.native_engine import Snapshot
+from pants.engine.process import ProcessResult
+from pants.engine.rules import Get, collect_rules, rule
+from pants.engine.target import FieldSet, Target
+from pants.util.logging import LogLevel
+from pants.util.strutil import pluralize
+
+
+@dataclass(frozen=True)
+class AutoflakeFieldSet(FieldSet):
+    required_fields = (PythonSourceField,)
+
+    source: PythonSourceField
+
+    @classmethod
+    def opt_out(cls, tgt: Target) -> bool:
+        return tgt.get(SkipAutoflakeField).value
+
+
+class AutoflakeRequest(FmtTargetsRequest):
+    field_set_type = AutoflakeFieldSet
+    tool_subsystem = Autoflake
+    partitioner_type = PartitionerType.DEFAULT_SINGLE_PARTITION
+
+
+@rule(desc="Format with Autoflake", level=LogLevel.DEBUG)
+async def autoflake_fmt(request: AutoflakeRequest.SubPartition, autoflake: Autoflake) -> FmtResult:
+    autoflake_pex = await Get(VenvPex, PexRequest, autoflake.to_pex_request())
+
+    result = await Get(
+        ProcessResult,
+        VenvPexProcess(
+            autoflake_pex,
+            argv=(
+                "--in-place",
+                *autoflake.args,
+                *request.files,
+            ),
+            input_digest=request.snapshot.digest,
+            output_files=request.files,
+            description=f"Run Autoflake on {pluralize(len(request.files), 'file')}.",
+            level=LogLevel.DEBUG,
+        ),
+    )
+    output_snapshot = await Get(Snapshot, Digest, result.output_digest)
+    return FmtResult.create(
+        result,
+        request.snapshot,
+        output_snapshot,
+        strip_chroot_path=True,
+        formatter_name=AutoflakeRequest.tool_name,
+    )
+
+
+def rules():
+    return [
+        *collect_rules(),
+        *AutoflakeRequest.rules(),
+        *pex.rules(),
+    ]
---
+++ packages/pantsbuild.pants/pantsbuild.pants-2.15.0.dev4-cp39-cp39-macosx_11_0_arm64.whl/pants/backend/python/lint/docformatter/rules.py
@@ -0,0 +1,70 @@
+# Copyright 2020 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from dataclasses import dataclass
+
+from pants.backend.python.lint.docformatter.skip_field import SkipDocformatterField
+from pants.backend.python.lint.docformatter.subsystem import Docformatter
+from pants.backend.python.target_types import PythonSourceField
+from pants.backend.python.util_rules import pex
+from pants.backend.python.util_rules.pex import PexRequest, VenvPex, VenvPexProcess
+from pants.core.goals.fmt import FmtResult, FmtTargetsRequest
+from pants.core.goals.lint import PartitionerType
+from pants.engine.fs import Digest
+from pants.engine.internals.native_engine import Snapshot
+from pants.engine.process import ProcessResult
+from pants.engine.rules import Get, collect_rules, rule
+from pants.engine.target import FieldSet, Target
+from pants.util.logging import LogLevel
+from pants.util.strutil import pluralize
+
+
+@dataclass(frozen=True)
+class DocformatterFieldSet(FieldSet):
+    required_fields = (PythonSourceField,)
+
+    source: PythonSourceField
+
+    @classmethod
+    def opt_out(cls, tgt: Target) -> bool:
+        return tgt.get(SkipDocformatterField).value
+
+
+class DocformatterRequest(FmtTargetsRequest):
+    field_set_type = DocformatterFieldSet
+    tool_subsystem = Docformatter
+    partitioner_type = PartitionerType.DEFAULT_SINGLE_PARTITION
+
+
+@rule(desc="Format with docformatter", level=LogLevel.DEBUG)
+async def docformatter_fmt(
+    request: DocformatterRequest.SubPartition, docformatter: Docformatter
+) -> FmtResult:
+    docformatter_pex = await Get(VenvPex, PexRequest, docformatter.to_pex_request())
+    result = await Get(
+        ProcessResult,
+        VenvPexProcess(
+            docformatter_pex,
+            argv=(
+                "--in-place",
+                *docformatter.args,
+                *request.files,
+            ),
+            input_digest=request.snapshot.digest,
+            output_files=request.files,
+            description=(f"Run Docformatter on {pluralize(len(request.files), 'file')}."),
+            level=LogLevel.DEBUG,
+        ),
+    )
+    output_snapshot = await Get(Snapshot, Digest, result.output_digest)
+    return FmtResult.create(
+        result, request.snapshot, output_snapshot, formatter_name=DocformatterRequest.tool_name
+    )
+
+
+def rules():
+    return [
+        *collect_rules(),
+        *DocformatterRequest.rules(),
+        *pex.rules(),
+    ]
