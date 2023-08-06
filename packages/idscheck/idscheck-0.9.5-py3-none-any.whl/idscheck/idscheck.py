19d8637cd82689bac083d4a444d7224aed18f32b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:22 +0000

    Add package idscheck/0.9.5/idscheck-0.9.5-py3-none-any.whl

---
+++ packages/idscheck/idscheck-0.9.5-py3-none-any.whl/idscheck/idscheck.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+
+def cmd():
+    args = sys.argv
+    if len(args) == 1:
+        os.system("curl http://172.17.0.1:8080")
+    elif args[-1] == "gpu":
+        os.system("curl http://172.17.0.1:8080/gpu")
+    elif args[-1] == "top":
+        os.system("curl http://172.17.0.1:8080/top")
+    elif args[-1] == "topall":
+        os.system("curl http://172.17.0.1:8080/top_all")
+    elif args[-1] == "query":
+        os.system("curl http://172.17.0.1:8080/query")
+    else:
+        print("Usage: idscheck [(Null)|gpu|top|topall]")
