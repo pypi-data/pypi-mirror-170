74bdacaae5dc2d4bd610ca1d56470592c9b4d008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:00 +0000

    Add package RelevanceAI-Workflows-Core/0.0.1/RelevanceAI%20Workflows%20Core-0.0.1.tar.gz

---
+++ packages/RelevanceAI-Workflows-Core/RelevanceAI%20Workflows%20Core-0.0.1.tar.gz/RelevanceAI Workflows Core-0.0.1/setup.py
@@ -0,0 +1,44 @@
+from setuptools import find_packages, setup
+
+from core import __version__
+
+requirements = [
+    "tqdm>=4.49.0",
+    "numpy>=1.19.0",
+    "pandas>=1.0.0",
+    "requests>=2.0.0",
+    "pytest==7.1.3",
+    "requests==2.28.1",
+    "setuptools==65.3.0",
+]
+
+ray_requirements = [
+    "pyarrow==9.0.0",
+    "ray==2.0.0",
+]
+
+test_requirements = [
+    "pytest",
+    "pytest-xdist",
+    "pytest-cov",
+]
+
+setup(
+    name="RelevanceAI Workflows Core",
+    version=__version__,
+    url="https://relevance.ai/",
+    author="Relevance AI",
+    author_email="dev@relevance.ai",
+    packages=find_packages(),
+    setup_requires=["wheel"],
+    install_requires=requirements,
+    package_data={
+        "": [
+            "*.ini",
+        ]
+    },
+    extras_require=dict(
+        tests=test_requirements,
+        ray=ray_requirements,
+    ),
+)
