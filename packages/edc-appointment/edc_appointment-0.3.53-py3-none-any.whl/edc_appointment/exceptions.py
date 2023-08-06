8ad263a12dfdecd62bfd68fe6fc211aae71cc816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:51 +0000

    Add package edc-appointment/0.3.53/edc_appointment-0.3.53-py3-none-any.whl

---
+++ packages/edc-appointment/edc_appointment-0.3.53-py3-none-any.whl/edc_appointment/exceptions.py
@@ -0,0 +1,18 @@
+class AppointmentStatusError(Exception):
+    pass
+
+
+class AppointmentCreateError(Exception):
+    pass
+
+
+class UnknownVisitCode(Exception):
+    pass
+
+
+class AppointmentWindowError(Exception):
+    pass
+
+
+class AppointmentPermissionsRequired(Exception):
+    pass
