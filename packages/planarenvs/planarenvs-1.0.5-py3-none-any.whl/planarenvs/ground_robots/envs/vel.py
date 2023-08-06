16b16a6d161d8ab989efc1be01a4eaa0016d599a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:01 +0000

    Add package planarenvs/1.0.5/planarenvs-1.0.5-py3-none-any.whl

---
+++ packages/planarenvs/planarenvs-1.0.5-py3-none-any.whl/planarenvs/ground_robots/envs/vel.py
@@ -0,0 +1,47 @@
+import numpy as np
+from gym import spaces
+
+from planarenvs.ground_robots.envs.ground_robot_env import GroundRobotEnv
+
+
+class GroundRobotVelEnv(GroundRobotEnv):
+    def set_spaces(self):
+        self.observation_space = spaces.Dict(
+            {
+                "x": spaces.Box(
+                    low=-self._lim_up_pos,
+                    high=self._lim_up_pos,
+                    dtype=np.float64
+                ),
+                "xdot": spaces.Box(
+                    low=-self._lim_up_vel,
+                    high=self._lim_up_vel,
+                    dtype=np.float64
+                ),
+                "vel": spaces.Box(
+                    low=-self._lim_up_rel_vel,
+                    high=self._lim_up_rel_vel,
+                    dtype=np.float64,
+                ),
+            }
+        )
+        self.action_space = spaces.Box(
+            low=-self._lim_up_rel_vel,
+            high=self._lim_up_rel_vel,
+            dtype=np.float64
+        )
+
+    def integrate(self):
+        super().integrate()
+        self._state["vel"] = self._action
+        self._state["xdot"] = self.compute_xdot(
+            self._state["x"], self._state["vel"]
+        )
+
+    def continuous_dynamics(self, x, t):
+        # x = [x, y, theta, vel_for, vel_rel, xdot, ydot, thetadot]
+        x_pos = x[0:3]
+        xdot = self.compute_xdot(x_pos, self._action)
+        veldot = np.zeros(2)
+        xddot = np.zeros(3)
+        return np.concatenate((xdot, veldot, xddot))
