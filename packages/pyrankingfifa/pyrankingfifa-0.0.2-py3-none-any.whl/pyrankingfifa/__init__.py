dc160d0ed55f7dde792776fb140fa0cdbb25ee3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:43 +0000

    Add package pyrankingfifa/0.0.2/pyrankingfifa-0.0.2-py3-none-any.whl

---
+++ packages/pyrankingfifa/pyrankingfifa-0.0.2-py3-none-any.whl/pyrankingfifa/__init__.py
@@ -0,0 +1,16 @@
+'''
+pyrankingfifa: allows you to get the ranking of all FIFA teams since 2007.
+
+This library wraps the FIFA Ranking through ceroaceros/soccerzz website.
+Note this is not affiliated with FIFA or ceroaceros.
+Just enjoy it and use it for good purposes.
+
+Ceroaceros/soccerzz soruce: https://www.ceroacero.es/ranking_fifa.php
+Official FIFA Ranking: https://www.fifa.com/fifa-world-ranking
+'''
+from pyrankingfifa.__main__ import Ranking
+
+__title__ = 'pyrankingfifa'
+__author__ = 'Manuel Cabral'
+__version__ = '0.0.2'
+__license__ = 'MIT License'
