faff8dac454d2f933b7762f646c2d7983d9c55c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:47 +0000

    Add package wuciyuande-linghun-yangsheng/2022.10.7.0/wuciyuande-linghun-yangsheng-2022.10.7.0.tar.gz

---
+++ packages/wuciyuande-linghun-yangsheng/wuciyuande-linghun-yangsheng-2022.10.7.0.tar.gz/wuciyuande-linghun-yangsheng-2022.10.7.0/WuciyuandeLinghunYangsheng/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""wuciyuande-linghun-yangsheng
+https://github.com/apachecn/wuciyuande-linghun-yangsheng"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.7.0"
