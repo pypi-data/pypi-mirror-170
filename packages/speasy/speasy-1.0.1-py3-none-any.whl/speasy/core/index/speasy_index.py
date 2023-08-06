6e06aecce2ce28957a045a6cab4f4b544c26d084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:17 +0000

    Add package speasy/1.0.1/speasy-1.0.1-py3-none-any.whl

---
+++ packages/speasy/speasy-1.0.1-py3-none-any.whl/speasy/core/index/speasy_index.py
@@ -0,0 +1,17 @@
+import diskcache as dc
+
+from speasy.config import index as index_cfg
+
+
+class SpeasyIndex:
+    def __init__(self):
+        self._index = dc.Index(index_cfg.path())
+
+    def get(self, module, key, default=None):
+        return self._index.get(f'{module}/{key}', default)
+
+    def set(self, module, key, value):
+        self._index[f'{module}/{key}'] = value
+
+    def contains(self, module, key):
+        return f'{module}/{key}' in self._index
