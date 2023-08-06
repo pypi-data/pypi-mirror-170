3b813a34ecd71c8f723ebe6a1f729c1a26650663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:14:13 +0000

    Add package qimen-shiyong-yucexue/2022.10.9.0/qimen-shiyong-yucexue-2022.10.9.0.tar.gz

---
+++ packages/qimen-shiyong-yucexue/qimen-shiyong-yucexue-2022.10.9.0.tar.gz/qimen-shiyong-yucexue-2022.10.9.0/QimenShiyongYucexue/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qimen-shiyong-yucexue
+https://github.com/apachecn/qimen-shiyong-yucexue"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.9.0"
