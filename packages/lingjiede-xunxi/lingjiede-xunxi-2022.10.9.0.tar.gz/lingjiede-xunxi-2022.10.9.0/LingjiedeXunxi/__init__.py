1a7e9283ba59b1b4f64260dca72867d6302b2577
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:49 +0000

    Add package lingjiede-xunxi/2022.10.9.0/lingjiede-xunxi-2022.10.9.0.tar.gz

---
+++ packages/lingjiede-xunxi/lingjiede-xunxi-2022.10.9.0.tar.gz/lingjiede-xunxi-2022.10.9.0/LingjiedeXunxi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""lingjiede-xunxi
+https://github.com/apachecn/lingjiede-xunxi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.9.0"
