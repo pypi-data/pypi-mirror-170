04e4a7b8e49e0a392a8dd891c03a85a0a499b643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:06 +0000

    Add package Peacasso/0.0.13a0/Peacasso-0.0.13a0-py3-none-any.whl

---
+++ packages/Peacasso/Peacasso-0.0.13a0-py3-none-any.whl/peacasso/utils.py
@@ -0,0 +1,24 @@
+import base64
+import json
+from typing import Any, List
+import os
+from PIL import Image
+import io
+
+
+def get_dirs(path: str) -> List[str]:
+    return next(os.walk(path))[1]
+
+
+def base64_to_pil(base64_string: str) -> Image:
+    # base64_string = base64_string.replace("data:image/png;base64,", "")
+    base64_string = base64_string.split(",")[1]
+    img_bytes = base64.b64decode(base64_string)
+    img = Image.open(io.BytesIO(img_bytes))
+    # print number of channels
+    print(img.mode)
+    mask = None
+    if img.mode == "RGBA":
+        mask = img.getchannel("A")
+        img = img.convert("RGB") 
+    return img, mask
