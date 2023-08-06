1952d248f10525154699e251793872b1582d7bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:46 +0000

    Add package whatapackage/0.0.2/whatapackage-0.0.2.tar.gz

---
+++ packages/whatapackage/whatapackage-0.0.2.tar.gz/whatapackage-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='whatapackage',
+    version='0.0.2',
+    description='this is the first package i do',
+    long_description='What a very long desc version 2',
+    long_description_content_type='text/plain',
+    packages=find_packages()
+)
