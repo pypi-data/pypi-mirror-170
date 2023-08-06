86324c7be58f50aea23ed1c241ff8cfae24debd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:04 +0000

    Add package Pywrt/1.0/Pywrt-1.0.tar.gz

---
+++ packages/Pywrt/Pywrt-1.0.tar.gz/Pywrt-1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Pywrt",
+    version="1.0",
+    author="Welison Andrade",
+    author_email="",
+    description="My first Python Package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+    package_dir={'':"src"},
+    packages=find_packages("src"),
+    python_requires=">=3.8",
+)
