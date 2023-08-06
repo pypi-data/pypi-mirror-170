b620be120b11fd1020580c8248b49134783ed2bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:35 +0000

    Add package lightning/2022.10.7/lightning-2022.10.7.tar.gz

---
+++ packages/lightning/lightning-2022.10.7.tar.gz/lightning-2022.10.7/src/lightning/app/cli/commands/connection.py
@@ -0,0 +1,14 @@
+try:
+    from lightning_app.cli.commands.connection import connect  # noqa: F401
+    from lightning_app.cli.commands.connection import disconnect  # noqa: F401
+    from lightning_app.cli.commands.connection import _retrieve_connection_to_an_app  # noqa: F401
+    from lightning_app.cli.commands.connection import _get_commands_folder  # noqa: F401
+    from lightning_app.cli.commands.connection import _resolve_command_path  # noqa: F401
+    from lightning_app.cli.commands.connection import _list_app_commands  # noqa: F401
+
+except ImportError as err:
+
+    from os import linesep
+    from lightning_app import __version__
+    msg = f'Your `lightning` package was built for `lightning_app==0.6.3`, but you are running {__version__}'
+    raise type(err)(str(err) + linesep + msg)
