840b6b5bc654adfc905d7522b7c4004d1017b3e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:34 +0000

    Add package yuding-qimen-dunjia/2022.10.7.0/yuding_qimen_dunjia-2022.10.7.0-py3-none-any.whl

---
+++ packages/yuding-qimen-dunjia/yuding_qimen_dunjia-2022.10.7.0-py3-none-any.whl/YudingQimenDunjia/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yuding-qimen-dunjia
+https://github.com/apachecn/yuding-qimen-dunjia"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.7.0"
