e81384f524516cd7798bdcca2d312f090bad0b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:37 +0000

    Add package purepress-arts/0.4.0/purepress-arts-0.4.0.tar.gz

---
+++ packages/purepress-arts/purepress-arts-0.4.0.tar.gz/purepress-arts-0.4.0/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['purepress']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Flask>=1.1.2,<1.2.0',
+ 'Markdown>=3.2.2,<3.3.0',
+ 'MarkupSafe>=2.0.1,<2.1.0',
+ 'PyYAML>=5.4.1,<5.5.0',
+ 'Werkzeug>=1.0.1,<1.1.0',
+ 'click>=7.1.2,<7.2.0',
+ 'colorama>=0.4.3,<0.5.0',
+ 'py-gfm>=1.0.0,<1.1.0',
+ 'toml>=0.10.2,<0.11.0']
+
+entry_points = \
+{'console_scripts': ['purepress = purepress.__main__:cli.main']}
+
+setup_kwargs = {
+    'name': 'purepress-arts',
+    'version': '0.4.0',
+    'description': 'PurePress for Arts.',
+    'long_description': '# PurePress Arts\n\n[![PyPI](https://img.shields.io/pypi/v/purepress-arts.svg)](https://pypi.org/project/purepress-arts/)\n![Python](https://img.shields.io/badge/python-3.6%2B-blue.svg)\n\n**PurePress Arts** is a variant of [PurePress](https://github.com/verilab/purepress), specialized for displaying artworks.\n\nCases:\n\n- [Amanda Ninji Arts](https://www.amandaninjiarts.com)\n',
+    'author': 'Richard Chien',
+    'author_email': 'stdrc@outlook.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/richardchien/purepress-arts',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<3.11',
+}
+
+
+setup(**setup_kwargs)
