66d21d21c0888688ae08629e06a60fa9eebde08f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:27 +0000

    Add package alexwang/1.0.2/alexwang-1.0.2.tar.gz

---
+++ packages/alexwang/alexwang-1.0.2.tar.gz/alexwang-1.0.2/alexwang/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding:utf-8 -*-
+from . import add_num
+from . import multipul_num
---
+++ packages/alexwang/alexwang-1.0.2.tar.gz/alexwang-1.0.2/alexwang/multipul_num.py
@@ -0,0 +1,7 @@
+"""
+ 
+:Author:  逍遥游
+:Create:  2022/10/5$ 20:32$
+"""
+def multipul_num(a, b):
+    return a * b
