3306e9f5d55245167cd1a865e7683150ef187bf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:14 +0000

    Add package liuren-bianyi/2022.10.6.0/liuren-bianyi-2022.10.6.0.tar.gz

---
+++ packages/liuren-bianyi/liuren-bianyi-2022.10.6.0.tar.gz/liuren-bianyi-2022.10.6.0/LiurenBianyi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""liuren-bianyi
+https://github.com/apachecn/liuren-bianyi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.6.0"
