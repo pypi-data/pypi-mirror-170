65ec1a32ca33eb2904bdd39c151eab1349f07e58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:41 +0000

    Add package mttf/0.17.202210061200/mttf-0.17.202210061200-py3-none-any.whl

---
+++ packages/mttf/mttf-0.17.202210061200-py3-none-any.whl/mt/tf/keras_layers/floor.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+
+
+__all__ = ["Floor"]
+
+
+@tf.custom_gradient
+def floor(x, size: float = 1.0):
+    if size == 1.0:
+        x = tf.math.floor(x)
+    else:
+        x /= size
+        x = tf.math.floor(x)
+        x *= size
+    def grad(upstream): # identity
+        return upstream
+    return x, grad
+
+
+class Floor(tf.keras.layers.Layer):
+    """TensorFlow floor with optional size but gradient is identity."""
+
+    def __init__(self, size: float = 1.0, **kwargs):
+        self._size = size
+        super().__init__(**kwargs)
+
+    def call(self, x):
+        return floor(x, self._size)
+
+    call.__doc__ = tf.keras.layers.Layer.call.__doc__
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+    compute_output_shape.__doc__ = tf.keras.layers.Layer.compute_output_shape.__doc__
