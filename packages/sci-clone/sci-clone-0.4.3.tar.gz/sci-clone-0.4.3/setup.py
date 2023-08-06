6e65722bd170e21a0d48c452dd20bc41c338d37f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:30 +0000

    Add package sci-clone/0.4.3/sci-clone-0.4.3.tar.gz

---
+++ packages/sci-clone/sci-clone-0.4.3.tar.gz/sci-clone-0.4.3/setup.py
@@ -0,0 +1,39 @@
+#-*- coding: UTF-8 -*-
+from setuptools import setup, find_packages
+from sci_clone import config
+
+with open('requirements.txt') as f:
+    REQUIREMENTS = f.readlines()
+
+with open('README.md') as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name=config.__name__,
+    version=config.__version__,    
+    description=config.__description__,
+    url=config.__url__,
+    author=config.__author__,
+    author_email=config.__author_email__,
+    long_description = LONG_DESCRIPTION,
+    long_description_content_type = "text/markdown",
+    license = "MIT",
+    packages = find_packages(),
+    entry_points = {
+        "console_scripts": [
+            "sci-clone=sci_clone.main:app"
+        ]
+    },
+    classifiers = [
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux"
+    ],
+    keywords = "sci-hub sci-clone sci_clone sciclone issn doi paper article journal",
+    install_requires = REQUIREMENTS,
+    zip_safe = False
+)
