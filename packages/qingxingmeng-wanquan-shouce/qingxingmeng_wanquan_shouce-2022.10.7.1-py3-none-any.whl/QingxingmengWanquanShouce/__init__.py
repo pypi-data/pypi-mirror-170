31774019d9b6f485c48b26e44793da9549a28ffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:37 +0000

    Add package qingxingmeng-wanquan-shouce/2022.10.7.1/qingxingmeng_wanquan_shouce-2022.10.7.1-py3-none-any.whl

---
+++ packages/qingxingmeng-wanquan-shouce/qingxingmeng_wanquan_shouce-2022.10.7.1-py3-none-any.whl/QingxingmengWanquanShouce/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qingxingmeng-wanquan-shouce
+https://github.com/apachecn/qingxingmeng-wanquan-shouce"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.7.1"
