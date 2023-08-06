6a99d68fbca3813ecfcc905928764b490edec80e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:04 +0000

    Add package meihua-xinyi/2022.10.7.0/meihua_xinyi-2022.10.7.0-py3-none-any.whl

---
+++ packages/meihua-xinyi/meihua_xinyi-2022.10.7.0-py3-none-any.whl/MeihuaXinyi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""meihua-xinyi
+https://github.com/apachecn/meihua-xinyi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.7.0"
