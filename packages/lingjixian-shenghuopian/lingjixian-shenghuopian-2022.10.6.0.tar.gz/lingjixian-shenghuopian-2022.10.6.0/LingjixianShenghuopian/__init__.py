db48e24281b93755bd8570ba79c8ecca5ce45b81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:06 +0000

    Add package lingjixian-shenghuopian/2022.10.6.0/lingjixian-shenghuopian-2022.10.6.0.tar.gz

---
+++ packages/lingjixian-shenghuopian/lingjixian-shenghuopian-2022.10.6.0.tar.gz/lingjixian-shenghuopian-2022.10.6.0/LingjixianShenghuopian/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""lingjixian-shenghuopian
+https://github.com/apachecn/lingjixian-shenghuopian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.6.0"
