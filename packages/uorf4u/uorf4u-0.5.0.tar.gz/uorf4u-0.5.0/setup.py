d1b2ee0e08669063d53ec3a75268f229596b53e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:46 +0000

    Add package uorf4u/0.5.0/uorf4u-0.5.0.tar.gz

---
+++ packages/uorf4u/uorf4u-0.5.0.tar.gz/uorf4u-0.5.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("pypi.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name="uorf4u",
+      version="0.5.0",
+      description="A tool for short uORF annotation.",
+      url="https://art-egorov.github.io/uorf4u/",
+      author="Artyom Egorov",
+      author_email="artem.egorov@med.lu.se",
+      license="WTFPL",
+      packages=["uorf4u"],
+      install_requires=["biopython", "configs", "argparse", "statistics", "logomaker", "matplotlib", "pandas",
+                        "reportlab"],
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      package_data={"uorf4u": ["*", "uorf4u_data/*", "uorf4u_data/fonts/*", "uorf4u_data/bin/*"]},
+      include_package_data=False,
+      scripts=["bin/uorf4u"],
+      zip_safe=True)
