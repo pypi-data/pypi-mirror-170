61485b94d91a7ac9c494a1c8be19fd212c317129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:55 +0000

    Add package mistat/0.1.7.27/mistat-0.1.7.27-py3-none-any.whl

---
+++ packages/mistat/mistat-0.1.7.27-py3-none-any.whl/mistat/design/__init__.py
@@ -0,0 +1,5 @@
+from .doeUtilities import addTreatments, subgroupOfDefining
+from .graphs import (FDS_Plot, calculateInteractions, calculateMainEffects,
+                     getModelMatrix, interactionPlot, mainEffectsPlot,
+                     marginalInteractionPlot)
+from . import doe
