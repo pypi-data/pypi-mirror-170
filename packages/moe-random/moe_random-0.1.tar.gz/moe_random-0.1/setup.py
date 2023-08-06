d7bf8f1fde5ddc08242b3e74826c46c7c57365d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:59 +0000

    Add package moe-random/0.1/moe_random-0.1.tar.gz

---
+++ packages/moe-random/moe_random-0.1.tar.gz/moe_random-0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['random']
+install_requires = \
+['Moe>=0.15.3,<0.16.0']
+
+entry_points = \
+{'console_scripts': ['moe = moe.cli:main'],
+ 'moe.plugin': ['random = random.py']}
+
+setup_kwargs = {
+    'name': 'moe-random',
+    'version': '0.1',
+    'description': 'Plugin for moe to output a random item from your music library.',
+    'long_description': '# moe_random\nAdds a `random` command to Moe to output a random item from your library.\n',
+    'author': 'Jacob Pavlock',
+    'author_email': 'jtpavlock@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/MoeMusic/moe_random',
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<3.11',
+}
+
+
+setup(**setup_kwargs)
