78ccc2e7f92379d599197ca50414a3e3bd8b855e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:56 +0000

    Add package peitho-data/1.5.3/peitho_data-1.5.3.tar.gz

---
+++ packages/peitho-data/peitho_data-1.5.3.tar.gz/peitho_data-1.5.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="peitho_data",
+    version="1.5.3",
+    description="An opinionated Python package on Big Data Analytics",
+    url="https://github.com/QubitPi/peitho-data",
+    author="Jiaqi liu",
+    author_email="jiaqixy@prontonmail.com",
+    license="Apache-2.0",
+    packages=["peitho_data"],
+    install_requires=[
+        "bs4",
+        "wordcloud",
+        "pycodestyle",
+        "requests",
+        "sphinx-rtd-theme",
+        "matplotlib",
+        "ebooklib"
+    ],
+    zip_safe=False,
+    include_package_data=True
+)
