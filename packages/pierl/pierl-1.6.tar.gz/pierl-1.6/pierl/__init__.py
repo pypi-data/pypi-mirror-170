fd9a4be95fec6ce3799e6b5e434d9ea35132a84c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:16 +0000

    Add package pierl/1.6/pierl-1.6.tar.gz

---
+++ packages/pierl/pierl-1.6.tar.gz/pierl-1.6/pierl/__init__.py
@@ -0,0 +1,8 @@
+from agents.Actor_critics import A2C
+from agents.DQN_based import DDQN, DQN, DQN_RS, DQN_TN
+from agents.Policy_gradients import REINFORCE
+from environments import easy, trumps
+from networks import *
+import pierl
+
+
