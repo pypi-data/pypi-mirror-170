5aefc89b9c4fb3b0039e07087e9098e4ce04cbeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:02 +0000

    Add package aliases/0.2.0/aliases-0.2.0.tar.gz

---
+++ packages/aliases/aliases-0.2.0.tar.gz/aliases-0.2.0/src/aliases/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.2.0"
+
+from aliases.space import AliasSpace
+from aliases.string import AliasAwareString
+from aliases.dict import AliasAwareDict
+
+__all__ = [AliasSpace, AliasAwareDict, AliasAwareString]
