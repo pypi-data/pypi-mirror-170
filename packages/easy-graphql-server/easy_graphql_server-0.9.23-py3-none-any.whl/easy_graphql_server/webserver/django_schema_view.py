b22033398ea74636a877d22455039f4864b0e126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:48 +0000

    Add package easy-graphql-server/0.9.23/easy_graphql_server-0.9.23-py3-none-any.whl

---
+++ packages/easy-graphql-server/easy_graphql_server-0.9.23-py3-none-any.whl/easy_graphql_server/webserver/django_schema_view.py
@@ -0,0 +1,39 @@
+"""
+    Definition of `DjangoSchemaView` class
+"""
+
+from django.http import HttpResponse, JsonResponse
+import django.contrib.auth
+from ._schema_view import SchemaView
+
+
+class DjangoSchemaView(SchemaView):
+
+    """
+        Django schema view. Used when calling `Schema.as_django_view()`.
+    """
+
+    def view(self, request):
+        """
+            Django view to compute GraphQL request
+        """
+        # extract user
+        if request.user and request.user.is_authenticated and not request.user.is_anonymous:
+            authenticated_user = request.user
+        else:
+            try:
+                authenticated_user = django.contrib.auth.authenticate(request)
+            except Exception as error:
+                authenticated_user = None
+        # compute result
+        result = self.compute_response(
+            method = request.method,
+            headers = request.headers,
+            body = request.body,
+            query = dict(request.GET.items()),
+            authenticated_user = authenticated_user,
+        )
+        # return response
+        if isinstance(result, str):
+            return HttpResponse(result)
+        return JsonResponse(result[0], status=result[1])
