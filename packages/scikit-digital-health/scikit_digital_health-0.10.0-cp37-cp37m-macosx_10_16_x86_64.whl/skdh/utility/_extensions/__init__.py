d531f35b618c60c5fead6d13a5a0107aeebafed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:11 +0000

    Add package scikit-digital-health/0.10.0/scikit_digital_health-0.10.0-cp37-cp37m-macosx_10_16_x86_64.whl

---
+++ packages/scikit-digital-health/scikit_digital_health-0.10.0-cp37-cp37m-macosx_10_16_x86_64.whl/skdh/utility/_extensions/__init__.py
@@ -0,0 +1,9 @@
+from .moving_statistics import moving_mean, moving_sd, moving_skewness, moving_kurtosis, moving_median
+
+__all__ = [
+    "moving_mean",
+    "moving_sd",
+    "moving_skewness",
+    "moving_kurtosis",
+    "moving_median",
+]
