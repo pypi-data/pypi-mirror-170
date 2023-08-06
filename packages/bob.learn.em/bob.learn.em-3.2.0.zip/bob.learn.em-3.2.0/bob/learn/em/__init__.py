3e53934472f565564039f95c6f7a841d2ef7218c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:33 +0000

    Add package bob.learn.em/3.2.0/bob.learn.em-3.2.0.zip

---
+++ packages/bob.learn.em/bob.learn.em-3.2.0.zip/bob.learn.em-3.2.0/bob/learn/em/__init__.py
@@ -0,0 +1,43 @@
+import bob.extension
+
+from .factor_analysis import ISVMachine, JFAMachine
+from .gmm import GMMMachine, GMMStats
+from .ivector import IVectorMachine
+from .kmeans import KMeansMachine
+from .linear_scoring import linear_scoring  # noqa: F401
+from .wccn import WCCN
+from .whitening import Whitening
+
+
+def get_config():
+    """Returns a string containing the configuration information."""
+    return bob.extension.get_config(__name__)
+
+
+# gets sphinx autodoc done right - don't remove it
+def __appropriate__(*args):
+    """Says object was actually declared here, an not on the import module.
+
+    Parameters:
+
+      *args: An iterable of objects to modify
+
+    Resolves `Sphinx referencing issues
+    <https://github.com/sphinx-doc/sphinx/issues/3048>`
+    """
+
+    for obj in args:
+        obj.__module__ = __name__
+
+
+__appropriate__(
+    KMeansMachine,
+    GMMMachine,
+    GMMStats,
+    IVectorMachine,
+    WCCN,
+    Whitening,
+    ISVMachine,
+    JFAMachine,
+)
+__all__ = [_ for _ in dir() if not _.startswith("_")]
