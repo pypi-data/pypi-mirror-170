daddada99ccbd324e1c509609eeab1db7161b7d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:55 +0000

    Add package mess-srv156/0.1.0/mess_srv156-0.1.0.tar.gz

---
+++ packages/mess-srv156/mess_srv156-0.1.0.tar.gz/mess_srv156-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="mess_srv156",
+      version="0.1.0",
+      description="mess_srv156",
+      author="android156",
+      author_email="4_registrations@mail.ru",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex']
+      )
