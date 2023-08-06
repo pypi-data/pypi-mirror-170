e861a6431b594e7691d17b0775642ca0cb22feed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:20 +0000

    Add package log2d/0.0.9/log2d-0.0.9.tar.gz

---
+++ packages/log2d/log2d-0.0.9.tar.gz/log2d-0.0.9/log2d/test_log2d.py
@@ -0,0 +1,18 @@
+# Tests for log2d
+import pytest
+from .log2d import *
+
+class Test_Initialisation:
+    def test_something(self):
+        """ Something should happen when you run something() """
+        assert something() == something_else
+
+class Test_Core_Functions:
+    def test_something(self):
+        """ Something should happen when you run something() """
+        assert something() == something_else
+
+class Test_Edge_Cases:
+    def test_something(self):
+        """ Something should happen when you run something() """
+        assert something() == something_else
