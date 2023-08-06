0ae692bc967e9e0c14e0a8896fc25687a4c7be9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:45 +0000

    Add package PyJobRunner/0.0.4/PyJobRunner-0.0.4.tar.gz

---
+++ packages/PyJobRunner/PyJobRunner-0.0.4.tar.gz/PyJobRunner-0.0.4/jobrunner/cli/_jobrunner.py
@@ -0,0 +1,15 @@
+# Standard libraries
+import os
+import subprocess
+
+# Feature libraries
+import click
+
+
+@click.group(name="jobrunner")
+def jobrunner():
+    """
+    \b
+    Command line tool to organize and manage computing jobs.
+    """
+    pass
