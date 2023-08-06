67bd9ba63049b0f473f639dc5d9122220bcd4564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:53 +0000

    Add package frankieyeung-distributions/0.1/frankieyeung_distributions-0.1.tar.gz

---
+++ packages/frankieyeung-distributions/frankieyeung_distributions-0.1.tar.gz/frankieyeung_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='frankieyeung_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['frankieyeung_distributions'],
+      zip_safe=False)
