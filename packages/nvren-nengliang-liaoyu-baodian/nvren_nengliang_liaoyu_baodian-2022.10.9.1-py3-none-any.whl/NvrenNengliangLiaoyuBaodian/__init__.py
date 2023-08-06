38805f93ca5a2a1efc69ed7f2f91d7ac3839a43c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:19:55 +0000

    Add package nvren-nengliang-liaoyu-baodian/2022.10.9.1/nvren_nengliang_liaoyu_baodian-2022.10.9.1-py3-none-any.whl

---
+++ packages/nvren-nengliang-liaoyu-baodian/nvren_nengliang_liaoyu_baodian-2022.10.9.1-py3-none-any.whl/NvrenNengliangLiaoyuBaodian/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""nvren-nengliang-liaoyu-baodian
+https://github.com/apachecn/nvren-nengliang-liaoyu-baodian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.9.1"
