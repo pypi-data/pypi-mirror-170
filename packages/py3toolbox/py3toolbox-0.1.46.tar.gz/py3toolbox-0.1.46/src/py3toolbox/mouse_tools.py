941df356956a00ac17a6cccd5ed23a7359253df0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:50 +0000

    Add package py3toolbox/0.1.46/py3toolbox-0.1.46.tar.gz

---
+++ packages/py3toolbox/py3toolbox-0.1.46.tar.gz/py3toolbox-0.1.46/src/py3toolbox/mouse_tools.py
@@ -0,0 +1,15 @@
+import pyautogui
+import time
+
+
+def get_mouse_pos():
+  pos = pyautogui.position()
+  return (pos.x, pos.y)
+  
+if __name__ == "__main__":
+  print (get_mouse_pos())
+
+"""
+
+
+if __name__ == "__main__":
