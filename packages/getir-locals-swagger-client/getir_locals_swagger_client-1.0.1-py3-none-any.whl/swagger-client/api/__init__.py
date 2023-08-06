9202fe057c20dd7226772f39d049518633916553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:55 +0000

    Add package getir-locals-swagger-client/1.0.1/getir_locals_swagger_client-1.0.1-py3-none-any.whl

---
+++ packages/getir-locals-swagger-client/getir_locals_swagger_client-1.0.1-py3-none-any.whl/swagger-client/api/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from client.api.auth_api import AuthApi
+from client.api.health_api import HealthApi
+from client.api.orders_api import OrdersApi
+from client.api.products_api import ProductsApi
+from client.api.shops_api import ShopsApi
+from client.api.supplier_api import SupplierApi
