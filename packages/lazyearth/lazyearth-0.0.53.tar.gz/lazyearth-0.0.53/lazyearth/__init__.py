10f29f724de89e911c68a884cce6a5e9e55185fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:21 +0000

    Add package lazyearth/0.0.53/lazyearth-0.0.53.tar.gz

---
+++ packages/lazyearth/lazyearth-0.0.53.tar.gz/lazyearth-0.0.53/lazyearth/__init__.py
@@ -0,0 +1,7 @@
+# If there are this code in orther local have to use 
+# from lazyearth.mainfile import objearth
+# from lazyearth.mainfile import water
+
+from lazyearth.mainfile import objearth
+from lazyearth.mainfile import water
+
