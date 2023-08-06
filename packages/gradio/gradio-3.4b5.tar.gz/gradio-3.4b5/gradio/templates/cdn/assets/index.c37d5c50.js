6db1aa22386484fd4fb46dddb1d608171b423ed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:47 +0000

    Add package gradio/3.4b5/gradio-3.4b5.tar.gz

---
+++ packages/gradio/gradio-3.4b5.tar.gz/gradio-3.4b5/gradio/templates/cdn/assets/index.c37d5c50.js
@@ -0,0 +1,7 @@
+export { C as Component } from './Column.6b4a0d64.js';
+import './index.4e854d0e.js';
+import './styles.ed3b21b5.js';
+
+const modes = ["static"];
+
+export { modes };
---
+++ packages/gradio/gradio-3.4b5.tar.gz/gradio-3.4b5/gradio/templates/frontend/assets/index.1f3f5cde.js
@@ -0,0 +1,5 @@
+export { Q as Component } from './index.17b0663f.js';
+
+const modes = ["static"];
+
+export { modes };
