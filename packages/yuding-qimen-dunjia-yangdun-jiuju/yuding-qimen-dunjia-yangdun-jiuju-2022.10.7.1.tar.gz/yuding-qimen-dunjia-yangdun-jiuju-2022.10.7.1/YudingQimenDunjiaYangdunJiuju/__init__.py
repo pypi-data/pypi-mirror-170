9bf0e3e75b50bfd44d34996d76fa710dc82c4c53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:43 +0000

    Add package yuding-qimen-dunjia-yangdun-jiuju/2022.10.7.1/yuding-qimen-dunjia-yangdun-jiuju-2022.10.7.1.tar.gz

---
+++ packages/yuding-qimen-dunjia-yangdun-jiuju/yuding-qimen-dunjia-yangdun-jiuju-2022.10.7.1.tar.gz/yuding-qimen-dunjia-yangdun-jiuju-2022.10.7.1/YudingQimenDunjiaYangdunJiuju/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yuding-qimen-dunjia-yangdun-jiuju
+https://github.com/apachecn/yuding-qimen-dunjia-yangdun-jiuju"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.7.1"
