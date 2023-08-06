e47db9d2caae5d54ff4842b226a93da7adf5ac84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:54 +0000

    Add package joj-auth/0.0.2/joj-auth-0.0.2.tar.gz

---
+++ packages/joj-auth/joj-auth-0.0.2.tar.gz/joj-auth-0.0.2/joj_auth/login.py
@@ -0,0 +1,11 @@
+from jaccount_cli import JaccountCLIAsyncIO
+
+class JaccountCLILogin(JaccountCLIAsyncIO):
+    async def login(self):
+        await self.init()
+        captcha_ascii = self.captcha_generate_ascii()
+        print("", captcha_ascii, "", sep="\n")
+        captcha = input("Please enter the shown captcha: ")
+        username = input("Please enter jaccount username: ")
+        password = input("Please enter password: ")
+        await super().login(username, password, captcha)
