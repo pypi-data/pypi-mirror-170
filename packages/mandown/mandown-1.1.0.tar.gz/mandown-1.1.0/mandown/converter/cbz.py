ad0afcfeb368312213b92bd58cc8a9519aaa802b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:31 +0000

    Add package mandown/1.1.0/mandown-1.1.0.tar.gz

---
+++ packages/mandown/mandown-1.1.0.tar.gz/mandown-1.1.0/mandown/converter/cbz.py
@@ -0,0 +1,36 @@
+import zipfile
+from pathlib import Path
+from typing import Iterator
+
+from .base_converter import BaseConverter
+
+
+class CbzConverter(BaseConverter):
+    def create_file_progress(
+        self, path: Path | str, save_to: Path | str
+    ) -> Iterator[str]:
+        path = Path(path).absolute()
+        save_to = Path(save_to)
+
+        dest_file = save_to / f"{self.comic.metadata.title}.cbz"
+
+        with zipfile.ZipFile(dest_file, "w", zipfile.ZIP_DEFLATED) as file:
+            for outerpath in path.iterdir():
+                if outerpath.is_dir():
+                    for image in outerpath.iterdir():
+                        yield "Compressing"
+                        file.write(
+                            image,
+                            image.absolute().relative_to(path),
+                            zipfile.ZIP_DEFLATED,
+                        )
+                elif outerpath.stem == "cover":
+                    file.write(
+                        outerpath.with_stem("__cover"),  # to be first
+                        outerpath.absolute().relative_to(path),
+                        zipfile.ZIP_DEFLATED,
+                    )
+
+
+def get_class() -> type[CbzConverter]:
+    return CbzConverter
