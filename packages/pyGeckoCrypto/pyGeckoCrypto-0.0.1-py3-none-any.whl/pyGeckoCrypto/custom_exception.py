c848c69f312714249b9398d562de0b57a946c838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:09 +0000

    Add package pyGeckoCrypto/0.0.1/pyGeckoCrypto-0.0.1-py3-none-any.whl

---
+++ packages/pyGeckoCrypto/pyGeckoCrypto-0.0.1-py3-none-any.whl/pyGeckoCrypto/custom_exception.py
@@ -0,0 +1,16 @@
+class InvalidRequestException(Exception):
+    def __init__(self, message: str = "Request is not valid"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class InvalidCoinIDException(Exception):
+    def __init__(self, message: str = "Coin ID is not valid"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class InvalidCurrencyException(Exception):
+    def __init__(self, message: str = "Currency is not valid"):
+        self.message = message
+        super().__init__(self.message)
