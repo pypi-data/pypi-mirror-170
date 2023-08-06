4bab0b6d851dcb40ff8e22b660cb2a34ce478768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:56 +0000

    Add package TCGCSUtils/1.0.9/TCGCSUtils-1.0.9.tar.gz

---
+++ packages/TCGCSUtils/TCGCSUtils-1.0.9.tar.gz/TCGCSUtils-1.0.9/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name = 'TCGCSUtils',
+    version = '1.0.9',
+    author = 'Maycon Vinicius Guimarães',
+    author_email = 'maycon.guimaraes@tc.com.br',
+    packages = ['TCGCSUtils'],
+    install_requires = [
+        "gcsfs==2022.8.2",
+        "ndjson==0.3.1",
+        "numpy==1.23.3",
+        "pandas==1.4.4",
+        "pyarrow==7.0.0",
+        "python-dateutil==2.8.2",
+        "google-cloud-storage==2.5.0",
+    ],
+    description='A lib to easily save or read files in GCS',
+)
