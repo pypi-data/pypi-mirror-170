ca3579233f6b693dc6b6782d342b7259c34b4a7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:51 +0000

    Add package inventory-monitor/0.4.4/inventory-monitor-0.4.4.tar.gz

---
+++ packages/inventory-monitor/inventory-monitor-0.4.4.tar.gz/inventory-monitor-0.4.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'inventory-monitor',
+    version='0.4.4',
+    description = 'Manage inventory discovered by SNMP',
+    install_requires=[],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
