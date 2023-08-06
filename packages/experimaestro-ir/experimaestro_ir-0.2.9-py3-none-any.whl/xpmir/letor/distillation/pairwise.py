150040f2fc1b901829b74810c52259234aaea9f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:49 +0000

    Add package experimaestro-ir/0.2.9/experimaestro_ir-0.2.9-py3-none-any.whl

---
+++ packages/experimaestro-ir/experimaestro_ir-0.2.9-py3-none-any.whl/xpmir/letor/distillation/pairwise.py
@@ -0,0 +1,113 @@
+import sys
+from typing import Iterator, List, Tuple
+import torch
+from torch import nn
+from torch.functional import Tensor
+import torch.nn.functional as F
+from experimaestro import Config, default, Annotated, Param, deprecate
+from xpmir.letor.records import Document, PairwiseRecord, PairwiseRecords
+from xpmir.letor.context import Loss
+from xpmir.letor.trainers import TrainerContext, LossTrainer, TrainingHook
+from xpmir.utils import batchiter, foreach
+from .samplers import DistillationPairwiseSampler, PairwiseDistillationSample
+import numpy as np
+from xpmir.rankers import LearnableScorer, ScorerOutputType
+
+
+class DistillationPairwiseLoss(Config, nn.Module):
+    weight: Param[float] = 1.0
+    NAME = "?"
+
+    def initialize(self, ranker: LearnableScorer):
+        pass
+
+    def process(
+        self, student_scores: Tensor, teacher_scores: Tensor, info: TrainerContext
+    ):
+        loss = self.compute(student_scores, teacher_scores, info)
+        info.add_loss(Loss(f"pairwise-{self.NAME}", loss, self.weight))
+
+    def compute(
+        self, student_scores: Tensor, teacher_scores: Tensor, context: TrainerContext
+    ) -> torch.Tensor:
+        """
+        Compute the loss
+
+        Arguments:
+
+            student_scores: A (batch x 2) tensor
+            teacher_scores: A (batch x 2) tensor
+        """
+        raise NotImplementedError()
+
+
+class MSEDifferenceLoss(DistillationPairwiseLoss):
+    """Computes the MSE between the score differences
+
+    Compute ((student 1 - student 2) - (teacher 1 - teacher 2))**2
+    """
+
+    NAME = "delta-MSE"
+
+    def __init__(self):
+        super().__init__()
+        self.loss = nn.MSELoss()
+
+    def compute(
+        self, student_scores: Tensor, teacher_scores: Tensor, info: TrainerContext
+    ) -> torch.Tensor:
+        return self.loss(
+            student_scores[:, 1] - student_scores[:, 0],
+            teacher_scores[:, 1] - teacher_scores[:, 0],
+        )
+
+
+class DistillationPairwiseTrainer(LossTrainer):
+    """Pairwse trainer
+
+    Arguments:
+
+    lossfn: The loss function to use
+    """
+
+    sampler: Param[DistillationPairwiseSampler]
+
+    lossfn: Param[DistillationPairwiseLoss]
+    """The distillation pairwise batch function"""
+
+    def initialize(
+        self,
+        random: np.random.RandomState,
+        context: TrainerContext,
+    ):
+        super().initialize(random, context)
+        self.train_iter = batchiter(self.batch_size, self.sampler.pairwise_iter())
+        self.lossfn.initialize(self.ranker)
+
+    def train_batch(
+        self, samples: List[PairwiseDistillationSample], context: TrainerContext
+    ):
+        # Builds records and teacher score matrix
+        teacher_scores = torch.empty(len(samples), 2)
+        records = PairwiseRecords()
+        for ix, sample in enumerate(samples):
+            records.add(
+                PairwiseRecord(
+                    sample.query,
+                    Document(None, sample.documents[0].content, None),
+                    Document(None, sample.documents[1].content, None),
+                )
+            )
+            teacher_scores[ix, 0] = sample.documents[0].score
+            teacher_scores[ix, 1] = sample.documents[1].score
+
+        # Get the next batch and compute the scores for each query/document
+        scores = self.ranker(records, self.context).reshape(2, len(records)).T
+
+        if torch.isnan(scores).any() or torch.isinf(scores).any():
+            self.logger.error("nan or inf relevance score detected. Aborting.")
+            sys.exit(1)
+
+        # Call the losses (distillation, pairwise and pointwise)
+        teacher_scores = teacher_scores.to(scores.device)
+        self.lossfn.process(scores, teacher_scores, context)
---
+++ packages/experimaestro-ir/experimaestro_ir-0.2.9-py3-none-any.whl/xpmir/neural/splade.py
@@ -0,0 +1,162 @@
+from typing import List, Optional, Tuple
+from experimaestro import Config, Param
+import torch.nn as nn
+import torch
+from experimaestro import initializer
+from xpmir.context import Context, InitializationHook
+from xpmir.letor import DistributedDeviceInformation
+from xpmir.letor.samplers import PairwiseSampler, PairwiseInBatchNegativesSampler
+from transformers import AutoModelForMaskedLM
+from xpmir.neural import TorchLearnableScorer
+from xpmir.text.huggingface import TransformerVocab
+from xpmir.text.encoders import TextEncoder
+from xpmir.letor.trainers.batchwise import BatchwiseTrainer
+from xpmir.neural.dual import DotDense, FlopsRegularizer
+from xpmir.utils import easylog
+
+logger = easylog()
+
+
+class Aggregation(Config):
+    """The aggregation function for Splade"""
+
+    pass
+
+
+class MaxAggregation(Aggregation):
+    """Aggregate using a max"""
+
+    def __call__(self, logits, mask):
+        values, _ = torch.max(
+            torch.log1p(torch.relu(logits) * mask.to(logits.device).unsqueeze(-1)),
+            dim=1,
+        )
+        return values
+
+
+class SumAggregation(Aggregation):
+    """Aggregate using a sum"""
+
+    def __call__(self, logits, mask):
+        return torch.sum(
+            torch.log1p(torch.relu(logits) * mask.to(logits.device).unsqueeze(-1)),
+            dim=1,
+        )
+
+
+class SpladeTextEncoderModel(nn.Module):
+    def __init__(self, encoder, aggregation):
+        super().__init__()
+        self.encoder = encoder
+        self.aggregation = aggregation
+
+    def forward(self, tokenized):
+        out = self.encoder(tokenized, all_outputs=True)
+        out = self.aggregation(out.logits, tokenized.mask)
+        return out
+
+
+class SpladeTextEncoder(TextEncoder):
+    """Splade model
+
+    It is only a text encoder since the we use `xpmir.neural.dual.DotDense`
+    as the scorer class
+    """
+
+    encoder: Param[TransformerVocab]
+    """The encoder from Hugging Face"""
+
+    aggregation: Param[Aggregation]
+    """How to aggregate the vectors"""
+
+    maxlen: Param[Optional[int]] = None
+    """Max length for texts"""
+
+    @initializer
+    def initialize(self):
+        self.encoder.initialize(automodel=AutoModelForMaskedLM)
+        self.model = SpladeTextEncoderModel(self.encoder, self.aggregation)
+
+    def forward(self, texts: List[str]) -> torch.Tensor:
+        """Returns a batch x vocab tensor"""
+        tokenized = self.encoder.batch_tokenize(texts, mask=True, maxlen=self.maxlen)
+        out = self.model(tokenized)
+        return out
+
+    @property
+    def dimension(self):
+        return self.encoder.model.config.vocab_size
+
+    def static(self):
+        return False
+
+
+class DistributedSpladeTextEncoderHook(InitializationHook):
+    """Hook to distribute the model processing
+
+    When in multiprocessing/multidevice, use `torch.nn.parallel.DistributedDataParallel`,
+    otherwise use `torch.nn.DataParallel`.
+    """
+
+    splade: Param[SpladeTextEncoder]
+    """The splade text encoder"""
+
+    def after(self, state: Context):
+        info = state.device_information
+        if isinstance(info, DistributedDeviceInformation):
+            logger.info("Using a distributed model with rank=%d", info.rank)
+            self.splade.model = nn.parallel.DistributedDataParallel(
+                self.splade.model, device_ids=[info.rank]
+            )
+        else:
+            n_gpus = torch.cuda.device_count()
+            if n_gpus > 1:
+                logger.info(
+                    "Setting up DataParallel for Splade text encoder (%d GPUs)", n_gpus
+                )
+                self.splade.model = torch.nn.DataParallel(self.splade.model)
+            else:
+                logger.warning("Only one GPU detected, not using data parallel")
+
+
+def _splade(lambda_q: float, lambda_d: float, aggregation: Aggregation):
+    encoder = TransformerVocab(model_id="distilbert-base-uncased", trainable=True)
+    doc_encoder = SpladeTextEncoder(
+        aggregation=aggregation, encoder=encoder, maxlen=200
+    )
+    query_encoder = SpladeTextEncoder(
+        aggregation=SumAggregation(), encoder=encoder, maxlen=30
+    )
+    return DotDense(encoder=doc_encoder, query_encoder=query_encoder), FlopsRegularizer(
+        lambda_q=lambda_q, lambda_d=lambda_d
+    )
+
+
+def spladeV1(lambda_q: float, lambda_d: float):
+    """Returns the Splade architecture"""
+    return _splade(lambda_q, lambda_d, SumAggregation())
+
+
+def spladeV2(lambda_q: float, lambda_d: float):
+    """Returns the Splade v2 architecture
+
+    SPLADE v2: Sparse Lexical and Expansion Model for Information Retrieval (arXiv:2109.10086)
+    """
+    return _splade(lambda_q, lambda_d, MaxAggregation())
+
+
+def spladev2___(sampler: PairwiseSampler) -> Tuple[BatchwiseTrainer, DotDense]:
+    """Returns the model described in Splade V2"""
+    from xpmir.letor.optim import Adam
+    from xpmir.letor.trainers.batchwise import SoftmaxCrossEntropy
+
+    ibn_sampler = PairwiseInBatchNegativesSampler(sampler=sampler)
+    trainer = BatchwiseTrainer(
+        batch_size=124,
+        optimizer=Adam(lr=2e-5),
+        sampler=ibn_sampler,
+        lossfn=SoftmaxCrossEntropy(),
+    )
+
+    # Trained with distillation
+    return trainer, spladeV2()
