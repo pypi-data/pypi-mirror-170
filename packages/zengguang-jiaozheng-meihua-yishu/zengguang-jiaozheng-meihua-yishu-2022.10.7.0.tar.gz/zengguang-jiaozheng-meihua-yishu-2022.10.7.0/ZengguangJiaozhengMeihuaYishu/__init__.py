f39609eb8b0fb4de2ef36e44b1872acdf7f6669a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:54 +0000

    Add package zengguang-jiaozheng-meihua-yishu/2022.10.7.0/zengguang-jiaozheng-meihua-yishu-2022.10.7.0.tar.gz

---
+++ packages/zengguang-jiaozheng-meihua-yishu/zengguang-jiaozheng-meihua-yishu-2022.10.7.0.tar.gz/zengguang-jiaozheng-meihua-yishu-2022.10.7.0/ZengguangJiaozhengMeihuaYishu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zengguang-jiaozheng-meihua-yishu
+https://github.com/apachecn/zengguang-jiaozheng-meihua-yishu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.7.0"
