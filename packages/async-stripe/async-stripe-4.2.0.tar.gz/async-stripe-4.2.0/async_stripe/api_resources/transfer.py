e4be90bc1e074e7e62c6d151fd1345db4c34ca86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:27 +0000

    Add package async-stripe/4.2.0/async-stripe-4.2.0.tar.gz

---
+++ packages/async-stripe/async-stripe-4.2.0.tar.gz/async-stripe-4.2.0/async_stripe/api_resources/transfer.py
@@ -0,0 +1,7 @@
+import stripe
+from stripe import util
+from async_stripe.api_resources.abstract import patch_nested_resources
+
+
+netsted_resources = ["reversal"]
+patch_nested_resources(stripe.Transfer, netsted_resources)
