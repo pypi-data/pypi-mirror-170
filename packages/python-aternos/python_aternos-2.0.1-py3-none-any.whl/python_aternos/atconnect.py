8e824af78eb079e006fe6af165dcf6c97d2e4265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:34 +0000

    Add package python-aternos/2.0.1/python_aternos-2.0.1-py3-none-any.whl

---
+++ packages/python-aternos/python_aternos-2.0.1-py3-none-any.whl/python_aternos/atconnect.py
@@ -0,0 +1,298 @@
+"""Stores API connection session and sends requests"""
+
+import re
+import time
+import random
+import logging
+from functools import partial
+
+from typing import Optional, Union
+from typing import Dict, Any
+
+import requests
+
+from cloudscraper import CloudScraper
+
+from . import atjsparse
+from .aterrors import TokenError
+from .aterrors import CloudflareError
+from .aterrors import AternosPermissionError
+
+REQUA = \
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ' \
+    '(KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36 OPR/85.0.4341.47'
+
+
+class AternosConnect:
+
+    """Class for sending API requests,
+    bypassing Cloudflare and parsing responses"""
+
+    def __init__(self) -> None:
+
+        self.session = CloudScraper()
+        self.sec = ''
+        self.token = ''
+
+    def parse_token(self) -> str:
+
+        """Parses Aternos ajax token that
+        is needed for most requests
+
+        Raises:
+            RuntimeWarning: If the parser can not
+                find `<head>` tag in HTML response
+            TokenError: If the parser is unable
+                to extract ajax token from HTML
+
+        Returns:
+            Aternos ajax token
+        """
+
+        loginpage = self.request_cloudflare(
+            'https://aternos.org/go/', 'GET'
+        ).content
+
+        # Using the standard string methods
+        # instead of the expensive xml parsing
+        head = b'<head>'
+        headtag = loginpage.find(head)
+        headend = loginpage.find(b'</head>', headtag + len(head))
+
+        # Some checks
+        if headtag < 0 or headend < 0:
+            pagehead = loginpage
+            raise RuntimeWarning(
+                'Unable to find <head> tag, parsing the whole page'
+            )
+
+        # Extracting <head> content
+        headtag = headtag + len(head)
+        pagehead = loginpage[headtag:headend]
+
+        try:
+            text = pagehead.decode('utf-8', 'replace')
+            js_code = re.findall(r'\(\(\)(.*?)\)\(\);', text)
+
+            token_func = js_code[0]
+            if len(js_code) > 1:
+                token_func = js_code[1]
+
+            ctx = atjsparse.exec_js(token_func)
+            self.token = ctx.window['AJAX_TOKEN']
+
+        except (IndexError, TypeError) as err:
+            raise TokenError(
+                'Unable to parse TOKEN from the page'
+            ) from err
+
+        return self.token
+
+    def generate_sec(self) -> str:
+
+        """Generates Aternos SEC token which
+        is also needed for most API requests
+
+        Returns:
+            Random SEC `key:value` string
+        """
+
+        randkey = self.generate_aternos_rand()
+        randval = self.generate_aternos_rand()
+        self.sec = f'{randkey}:{randval}'
+        self.session.cookies.set(
+            f'ATERNOS_SEC_{randkey}', randval,
+            domain='aternos.org'
+        )
+
+        return self.sec
+
+    def generate_aternos_rand(self, randlen: int = 16) -> str:
+
+        """Generates a random string using
+        Aternos algorithm from main.js file
+
+        Args:
+            randlen (int, optional): Random string length
+
+        Returns:
+            Random string for SEC token
+        """
+
+        # a list with randlen+1 empty strings:
+        # generate a string with spaces,
+        # then split it by space
+        rand_arr = (' ' * (randlen + 1)).split(' ')
+
+        rand = random.random()
+        rand_alphanum = self.convert_num(rand, 36) + ('0' * 17)
+
+        return rand_alphanum[:18].join(rand_arr)[:randlen]
+
+    def convert_num(
+            self, num: Union[int, float, str],
+            base: int, frombase: int = 10) -> str:
+
+        """Converts an integer to specified base
+
+        Args:
+            num (Union[int,float,str]): Integer in any base to convert.
+                If it is a float starting with `0.`,
+                zero and point will be removed to get int
+            base (int): New base
+            frombase (int, optional): Given number base
+
+        Returns:
+            Number converted to a specified base
+        """
+
+        if isinstance(num, str):
+            num = int(num, frombase)
+
+        if isinstance(num, float):
+            sliced = str(num)[2:]
+            num = int(sliced)
+
+        symbols = '0123456789abcdefghijklmnopqrstuvwxyz'
+        basesym = symbols[:base]
+        result = ''
+        while num > 0:
+            rem = num % base
+            result = str(basesym[rem]) + result
+            num //= base
+        return result
+
+    def request_cloudflare(
+            self, url: str, method: str,
+            params: Optional[Dict[Any, Any]] = None,
+            data: Optional[Dict[Any, Any]] = None,
+            headers: Optional[Dict[Any, Any]] = None,
+            reqcookies: Optional[Dict[Any, Any]] = None,
+            sendtoken: bool = False,
+            retry: int = 5) -> requests.Response:
+
+        """Sends a request to Aternos API bypass Cloudflare
+
+        Args:
+            url (str): Request URL
+            method (str): Request method, must be GET or POST
+            params (Optional[Dict[Any, Any]], optional): URL parameters
+            data (Optional[Dict[Any, Any]], optional): POST request data,
+                if the method is GET, this dict will be combined with params
+            headers (Optional[Dict[Any, Any]], optional): Custom headers
+            reqcookies (Optional[Dict[Any, Any]], optional):
+                Cookies only for this request
+            sendtoken (bool, optional): If the ajax and SEC token
+                should be sent
+            retry (int, optional): How many times parser must retry
+                connection to API bypass Cloudflare
+
+        Raises:
+            CloudflareError: When the parser has exceeded retries count
+            NotImplementedError: When the specified method is not GET or POST
+
+        Returns:
+            API response
+        """
+
+        if retry <= 0:
+            raise CloudflareError('Unable to bypass Cloudflare protection')
+
+        old_cookies = self.session.cookies
+        self.session = CloudScraper()
+        self.session.cookies.update(old_cookies)
+        del old_cookies
+
+        params = params or {}
+        data = data or {}
+        headers = headers or {}
+        reqcookies = reqcookies or {}
+
+        method = method or 'GET'
+        method = method.upper().strip()
+        if method not in ('GET', 'POST'):
+            raise NotImplementedError('Only GET and POST are available')
+
+        if sendtoken:
+            params['TOKEN'] = self.token
+            params['SEC'] = self.sec
+            headers['X-Requested-With'] = 'XMLHttpRequest'
+
+        # requests.cookies.CookieConflictError bugfix
+        reqcookies['ATERNOS_SESSION'] = self.atsession
+        del self.session.cookies['ATERNOS_SESSION']
+
+        reqcookies_dbg = {
+            k: str(v or '')[:3]
+            for k, v in reqcookies.items()
+        }
+
+        session_cookies_dbg = {
+            k: str(v or '')[:3]
+            for k, v in self.session.cookies.items()
+        }
+
+        logging.debug('Requesting(%s)%s', method, url)
+        logging.debug('headers=%s', headers)
+        logging.debug('params=%s', params)
+        logging.debug('data=%s', data)
+        logging.debug('req-cookies=%s', reqcookies_dbg)
+        logging.debug('session-cookies=%s', session_cookies_dbg)
+
+        if method == 'POST':
+            sendreq = partial(
+                self.session.post,
+                params=params,
+                data=data
+            )
+        else:
+            sendreq = partial(
+                self.session.get,
+                params={**params, **data}
+            )
+
+        req = sendreq(
+            url,
+            headers=headers,
+            cookies=reqcookies
+        )
+
+        resp_type = req.headers.get('content-type', '')
+        html_type = resp_type.find('text/html') != -1
+        cloudflare = req.status_code == 403
+
+        if html_type and cloudflare:
+            logging.info('Retrying to bypass Cloudflare')
+            time.sleep(0.2)
+            return self.request_cloudflare(
+                url, method,
+                params, data,
+                headers, reqcookies,
+                sendtoken, retry - 1
+            )
+
+        logging.debug('AternosConnect received: %s', req.text[:65])
+        logging.info(
+            '%s completed with %s status',
+            method, req.status_code
+        )
+
+        if req.status_code == 402:
+            raise AternosPermissionError
+
+        req.raise_for_status()
+        return req
+
+    @property
+    def atsession(self) -> str:
+
+        """Aternos session cookie,
+        empty string if not logged in
+
+        Returns:
+            Session cookie
+        """
+
+        return self.session.cookies.get(
+            'ATERNOS_SESSION', ''
+        )
---
+++ packages/python-aternos/python_aternos-2.0.1-py3-none-any.whl/python_aternos/atwss.py
@@ -0,0 +1,274 @@
+"""Connects to Aternos WebSocket API
+for real-time information"""
+
+import enum
+import json
+import asyncio
+import logging
+
+from typing import Union, Any
+from typing import Tuple, Dict
+from typing import Callable, Coroutine
+from typing import TYPE_CHECKING
+
+import websockets
+
+from .atconnect import REQUA
+if TYPE_CHECKING:
+    from .atserver import AternosServer
+
+OneArgT = Callable[[Any], Coroutine[Any, Any, None]]
+TwoArgT = Callable[[Any, Tuple[Any, ...]], Coroutine[Any, Any, None]]
+FunctionT = Union[OneArgT, TwoArgT]
+ArgsTuple = Tuple[FunctionT, Tuple[Any, ...]]
+
+
+class Streams(enum.Enum):
+
+    """WebSocket streams types"""
+
+    status = (0, None)
+    queue = (1, None)
+    console = (2, 'console')
+    ram = (3, 'heap')
+    tps = (4, 'tick')
+    none = (-1, None)
+
+    def __init__(self, num: int, stream: str) -> None:
+
+        self.num = num
+        self.stream = stream
+
+
+class AternosWss:
+
+    """Class for managing websocket connection"""
+
+    def __init__(
+            self,
+            atserv: 'AternosServer',
+            autoconfirm: bool = False) -> None:
+
+        """Class for managing websocket connection
+
+        Args:
+            atserv (AternosServer):
+                atserver.AternosServer instance
+            autoconfirm (bool, optional):
+                Automatically start server status listener
+                when AternosWss connects to API to confirm
+                server launching
+        """
+
+        self.atserv = atserv
+        self.servid = atserv.servid
+
+        cookies = atserv.atconn.session.cookies
+        self.session = cookies['ATERNOS_SESSION']
+
+        recvtype = Dict[Streams, ArgsTuple]
+        self.recv: recvtype = {}
+        self.autoconfirm = autoconfirm
+        self.confirmed = False
+
+        self.socket: Any
+        self.keep: asyncio.Task
+        self.msgs: asyncio.Task
+
+    async def confirm(self) -> None:
+
+        """Simple way to call
+        `AternosServer.confirm`
+        from this class"""
+
+        self.atserv.confirm()
+
+    def wssreceiver(
+            self,
+            stream: Streams,
+            *args: Any) -> Callable[[FunctionT], Any]:
+
+        """Decorator that marks your function as a stream receiver.
+        When websocket receives message from the specified stream,
+        it calls all listeners created with this decorator.
+
+        Args:
+            stream (Streams): Stream that your function should listen
+            *args (tuple, optional): Arguments which will be passed to your function
+
+        Returns:
+            ...
+        """
+
+        def decorator(func: FunctionT) -> None:
+            self.recv[stream] = (func, args)
+        return decorator
+
+    async def connect(self) -> None:
+
+        """Connects to the websocket server
+        and starts all stream listeners"""
+
+        headers = [
+            ('Host', 'aternos.org'),
+            ('User-Agent', REQUA),
+            (
+                'Cookie',
+                f'ATERNOS_SESSION={self.session}; '
+                f'ATERNOS_SERVER={self.servid}'
+            )
+        ]
+        self.socket = await websockets.connect(  # type: ignore
+            'wss://aternos.org/hermes/',
+            origin='https://aternos.org',
+            extra_headers=headers
+        )
+
+        @self.wssreceiver(Streams.status)
+        async def confirmfunc(msg: Dict[str, Any]) -> None:
+
+            """Automatically confirm
+            Minecraft server launching
+
+            Args:
+                msg (Dict[str, Any]): Server info dictionary
+            """
+
+            if not self.autoconfirm:
+                return
+
+            in_queue = (msg['class'] == 'queueing')
+            pending = (msg['queue']['pending'] == 'pending')
+            confirmation = in_queue and pending
+
+            if confirmation and not self.confirmed:
+                await self.confirm()
+
+        @self.wssreceiver(Streams.status)
+        async def streamsfunc(msg: Dict[str, Any]) -> None:
+
+            """Automatically starts streams. Detailed description:
+
+            https://github.com/DarkCat09/python-aternos/issues/22#issuecomment-1146788496
+            According to the websocket messages from the web site,
+            Aternos can't receive any data from a stream (e.g. console) until
+            it requests this stream via the special message
+            to the websocket server: `{"stream":"console","type":"start"}`
+            on which the server responses with: `{"type":"connected"}`
+            Also, there are RAM (used heap) and TPS (ticks per second)
+            streams that must be enabled before trying to get information.
+            Enabling the stream for listening the server status is not needed,
+            these data is sent from API by default, so there's None value in
+            the second item of its stream type tuple
+            (`<Streams.status: (0, None)>`).
+
+            Args:
+                msg (Dict[str, Any]): Server info dictionary
+            """
+
+            if msg['status'] == 2:
+                # Automatically start streams
+                for strm in self.recv:
+
+                    if not isinstance(strm, Streams):
+                        continue
+
+                    if strm.stream:
+                        logging.debug('Requesting %s stream', strm.stream)
+                        await self.send({
+                            'stream': strm.stream,
+                            'type': 'start'
+                        })
+
+        await self.wssworker()
+
+    async def close(self) -> None:
+
+        """Closes websocket connection and stops all listeners"""
+
+        self.keep.cancel()
+        self.msgs.cancel()
+        await self.socket.close()
+        del self.socket
+
+    async def send(self, obj: Union[Dict[str, Any], str]) -> None:
+
+        """Sends a message to websocket server
+
+        Args:
+            obj (Union[Dict[str, Any],str]):
+                Message, may be a string or a dict
+        """
+
+        if isinstance(obj, dict):
+            obj = json.dumps(obj)
+
+        await self.socket.send(obj)
+
+    async def wssworker(self) -> None:
+
+        """Starts async tasks in background
+        for receiving websocket messages
+        and sending keepalive ping"""
+
+        self.keep = asyncio.create_task(self.keepalive())
+        self.msgs = asyncio.create_task(self.receiver())
+
+    async def keepalive(self) -> None:
+
+        """Each 49 seconds sends keepalive ping to websocket server"""
+
+        try:
+            while True:
+                await asyncio.sleep(49)
+                await self.socket.send('{"type":"\u2764"}')
+
+        except asyncio.CancelledError:
+            pass
+
+    async def receiver(self) -> None:
+
+        """Receives messages from websocket servers
+        and calls user's streams listeners"""
+
+        while True:
+            try:
+                data = await self.socket.recv()
+                obj = json.loads(data)
+                msgtype = Streams.none
+
+                if obj['type'] == 'line':
+                    msgtype = Streams.console
+                    msg = obj['data'].strip('\r\n ')
+
+                elif obj['type'] == 'heap':
+                    msgtype = Streams.ram
+                    msg = int(obj['data']['usage'])
+
+                elif obj['type'] == 'tick':
+                    msgtype = Streams.tps
+                    ticks = 1000 / obj['data']['averageTickTime']
+                    msg = 20 if ticks > 20 else ticks
+
+                elif obj['type'] == 'status':
+                    msgtype = Streams.status
+                    msg = json.loads(obj['message'])
+
+                if msgtype in self.recv:
+
+                    # function info tuple
+                    func: ArgsTuple = self.recv[msgtype]
+
+                    # if arguments is not empty
+                    if func[1]:
+                        # call the function with args
+                        coro = func[0](msg, func[1])  # type: ignore
+                    else:
+                        # mypy error: Too few arguments
+                        # looks like bug, so it is ignored
+                        coro = func[0](msg)  # type: ignore
+                    # run
+                    asyncio.create_task(coro)
+
+            except asyncio.CancelledError:
+                break
