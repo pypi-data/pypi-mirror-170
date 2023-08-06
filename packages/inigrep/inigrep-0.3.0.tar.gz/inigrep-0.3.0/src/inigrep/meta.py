7f92b50df8e9daee76a61945bc5b3d91a5354d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:07 +0000

    Add package inigrep/0.3.0/inigrep-0.3.0.tar.gz

---
+++ packages/inigrep/inigrep-0.3.0.tar.gz/inigrep-0.3.0/src/inigrep/meta.py
@@ -0,0 +1,43 @@
+GIT_LASTHASH = """
+5210063fd88d8601e65a6d1e92876e5c557d7fbd
+""".strip()
+
+VERSION = """
+0.3.0
+""".strip()
+
+MAINTAINER = """
+Alois Mahdal <netvor+inigrep@vornet.cz>
+""".strip()
+
+PKGNAME = """
+python-inigrep
+""".strip()
+
+TAGLINE = """
+grep for (some) INIs
+""".strip()
+
+CODENAME = """
+
+""".strip()
+
+NAME = """
+inigrep
+""".strip()
+
+VCS_BROWSER = """
+https://gitlab.com/vornet/python/python-inigrep
+""".strip()
+
+LICENSE = """
+
+""".strip()
+
+VERSION = """
+0.3.0
+""".strip()
+
+GIT_LASTSUMMARY = """
+Bump version to 0.3.0
+""".strip()
