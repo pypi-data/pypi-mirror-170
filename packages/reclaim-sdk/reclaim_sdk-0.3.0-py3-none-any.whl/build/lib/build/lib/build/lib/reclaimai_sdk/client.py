f3b3b4b6ffb49f23568a41e6bd2defc11b28508d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:42 +0000

    Add package reclaim-sdk/0.3.0/reclaim_sdk-0.3.0-py3-none-any.whl

---
+++ packages/reclaim-sdk/reclaim_sdk-0.3.0-py3-none-any.whl/build/lib/build/lib/build/lib/reclaimai_sdk/client.py
@@ -0,0 +1,53 @@
+from httpx import Client
+from os import environ
+import pathlib
+import toml
+
+CONF_FILE = pathlib.Path("~/.reclaimai.toml").expanduser()
+
+
+class ReclaimClient(Client):
+    """
+    The client is a singleton, so we can use it to store the token and
+    the API URL. It should be initialized in the main script once.
+    """
+
+    _instance = None
+    _token = None
+    _api_url = "https://api.app.reclaim.ai"
+
+    @property
+    def is_authenticated(self):
+        return "RECLAIM" in self.cookies
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self, token="", **kwargs):
+        super().__init__(base_url=self._api_url, http2=True)
+
+        if token:
+            self._token = token
+
+        elif "RECLAIM_TOKEN" in environ:
+            self._token = environ["RECLAIM_TOKEN"]
+
+        elif CONF_FILE.exists():
+            try:
+                self._token = toml.load(CONF_FILE)["reclaim_ai"]["token"]
+            except KeyError:
+                raise KeyError(f"Token not found in {CONF_FILE}.")
+
+        if self._token:
+            self.authenticate(self._token)
+
+        else:
+            raise ValueError("No Reclaim.ai token provided.")
+
+    def authenticate(self, token):
+        """
+        Authenticate the client with the given token.
+        """
+        self.cookies.set("RECLAIM", self._token)
