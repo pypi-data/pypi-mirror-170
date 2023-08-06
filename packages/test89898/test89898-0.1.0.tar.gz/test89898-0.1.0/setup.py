c99431cf852a643d4070bfb46f0a4a8dd8f3754d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:59 +0000

    Add package test89898/0.1.0/test89898-0.1.0.tar.gz

---
+++ packages/test89898/test89898-0.1.0.tar.gz/test89898-0.1.0/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['greet']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['arrow>=1.2.3,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['greet = greet.location:cli']}
+
+setup_kwargs = {
+    'name': 'test89898',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': '',
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
