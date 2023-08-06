308ceed7e10e5c915c2566fc8940e1b164da97c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:21 +0000

    Add package django-permafrost/0.2.20/django_permafrost-0.2.20-py3-none-any.whl

---
+++ packages/django-permafrost/django_permafrost-0.2.20-py3-none-any.whl/permafrost/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from permafrost import views
+
+app_name = 'permafrost'
+
+urlpatterns = [
+    path("", views.PermafrostRoleListView.as_view(), name="role-list"),
+    path("manage/", views.PermafrostRoleManageView.as_view(), name="roles-manage"),
+    path("role/create/", views.PermafrostRoleCreateView.as_view(), name="role-create"),
+    path("role/<slug:slug>/", views.PermafrostRoleDetailView.as_view(), name="role-detail"),
+    path("role/<slug:slug>/update/", views.PermafrostRoleUpdateView.as_view(), name="role-update"),
+    path("role/<slug:slug>/permissions/add", views.PermafrostCustomRoleModalView.as_view(), name="custom-role-add-permissions"),
+    path("role/<slug:slug>/permissions/search", views.update_permission_table, name="custom-role-search-permissions"),
+]
+
+
