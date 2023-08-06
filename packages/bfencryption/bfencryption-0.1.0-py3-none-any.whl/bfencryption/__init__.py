4cc9c0163d19aa2d587cd7007ae2f2921fa47bcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:25 +0000

    Add package bfencryption/0.1.0/bfencryption-0.1.0-py3-none-any.whl

---
+++ packages/bfencryption/bfencryption-0.1.0-py3-none-any.whl/bfencryption/__init__.py
@@ -0,0 +1,52 @@
+import hashlib
+import base64
+import random
+
+
+def encrypt_raw(inp, key):
+    if len(inp) != len(key):
+        return -1
+
+    encrypted = [str(hashlib.sha256((str(val) + str(key[idx])).encode()).hexdigest()) for idx, val in enumerate(inp)]
+    return encrypted
+
+
+def decrypt_raw(inp, key):
+    decrypted = []
+    for idx, val in enumerate(inp):
+        i = 0
+        while str(hashlib.sha256((str(i) + str(key[idx])).encode()).hexdigest()) != inp[idx]:
+            i += 1
+        decrypted.append(i)
+    return decrypted
+
+
+def encrypt(inp, key):
+    inp_raw = [ord(c) for c in inp]
+    key_raw = [ord(c) for c in key]
+    result = encrypt_raw(inp_raw, key_raw)
+    b64result = base64.b64encode(".".join(result).encode()).decode()
+    return b64result
+
+
+def decrypt(inp, key):
+    inp = base64.b64decode(inp.encode()).decode().split(".")
+    key_raw = [ord(c) for c in key]
+    decrypted_raw = decrypt_raw(inp, key_raw)
+    output_str = ""
+    for i in decrypted_raw:
+        output_str += chr(i)
+    return output_str
+
+
+def long_encrypt(inp, key):
+    return encrypt(encrypt(inp, key), encrypt(key, key))
+
+def long_decrypt(inp, key):
+    return decrypt(decrypt(inp, encrypt(key, key)), key)
+
+def generate_key(inp):
+    key = ""
+    for i in inp:
+        key += chr(random.choice(range(32, 176)))
+    return key
