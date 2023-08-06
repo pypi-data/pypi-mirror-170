c58f0e5e313f2d6147acb84d3628532d6ea0f3d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:20:13 +0000

    Add package ohka-yuxinling-liaoyu/2022.10.9.1/ohka_yuxinling_liaoyu-2022.10.9.1-py3-none-any.whl

---
+++ packages/ohka-yuxinling-liaoyu/ohka_yuxinling_liaoyu-2022.10.9.1-py3-none-any.whl/OhkaYuxinlingLiaoyu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ohka-yuxinling-liaoyu
+https://github.com/apachecn/ohka-yuxinling-liaoyu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.9.1"
