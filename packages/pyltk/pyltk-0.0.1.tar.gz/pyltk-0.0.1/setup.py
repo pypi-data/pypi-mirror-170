c4daf979fefacf959b26b2409aef43ac08bd6ab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:47 +0000

    Add package pyltk/0.0.1/pyltk-0.0.1.tar.gz

---
+++ packages/pyltk/pyltk-0.0.1.tar.gz/pyltk-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['pyltk']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pyltk',
+    'version': '0.0.1',
+    'description': 'english tokenizer and stemmer',
+    'long_description': '## pyltk\n\nperformant english tokenizer and stemmer',
+    'author': 'wayfaring-stranger',
+    'author_email': 'zw6p226m@duck.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
