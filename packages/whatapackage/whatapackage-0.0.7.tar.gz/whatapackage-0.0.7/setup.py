0d7ba781f9f00c84c6a99de41019e4eb895ee65e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:51 +0000

    Add package whatapackage/0.0.7/whatapackage-0.0.7.tar.gz

---
+++ packages/whatapackage/whatapackage-0.0.7.tar.gz/whatapackage-0.0.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='whatapackage',
+    version='0.0.7',
+    description='this is the first package i do',
+    long_description='What a very long desc version fake 2',
+    long_description_content_type='text/plain',
+    packages=find_packages(),
+    url='https://github.com/python/cpython'
+)
