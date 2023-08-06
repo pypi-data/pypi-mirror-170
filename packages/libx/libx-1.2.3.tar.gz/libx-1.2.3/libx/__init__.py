a2f6abdee34e51e0abe73770a4ebaf5bd9529842
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:55 +0000

    Add package libx/1.2.3/libx-1.2.3.tar.gz

---
+++ packages/libx/libx-1.2.3.tar.gz/libx-1.2.3/libx/__init__.py
@@ -0,0 +1,1 @@
+lmap = lambda f, l, *args, **kwargs: list(map(l, f(*args, **kwargs)))
---
+++ packages/libx/libx-1.2.3.tar.gz/libx-1.2.3/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': '.'}
+
+packages = \
+['libx', 'libx.env', 'libx.log', 'libx.owl', 'libx.sty', 'libx.til']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['beautifulsoup4>=4.11.1,<5.0.0',
+ 'chalky>=1.0.0,<2.0.0',
+ 'fontawesome>=5.10.1.post1,<6.0.0',
+ 'lxml>=4.9.1,<5.0.0',
+ 'pandas>=1.4.4,<2.0.0',
+ 'ptpython>=3.0.20,<4.0.0',
+ 'pydantic>=1.10.2,<2.0.0',
+ 'python-dotenv>=0.21.0,<0.22.0',
+ 'randomname>=0.1.5,<0.2.0',
+ 'requests>=2.28.1,<3.0.0',
+ 'typer>=0.6.1,<0.7.0']
+
+setup_kwargs = {
+    'name': 'libx',
+    'version': '1.2.3',
+    'description': 'alleviates anxiety.',
+    'long_description': '# yes\n',
+    'author': 'lmisto',
+    'author_email': 'louaimisto@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'http://github.com/lmist/libx',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
