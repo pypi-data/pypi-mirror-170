9a5a56c2ae5759e0f4f3b8b353072ad22539072e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:16 +0000

    Add package imodelsx/0.1/imodelsx-0.1.tar.gz

---
+++ packages/imodelsx/imodelsx-0.1.tar.gz/imodelsx-0.1/setup.py
@@ -0,0 +1,39 @@
+from os import path
+
+import setuptools
+
+path_to_repo = path.abspath(path.dirname(__file__))
+with open(path.join(path_to_repo, 'readme.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+required_pypi = [
+    'transformers[torch]',
+    'numpy',
+    'datasets',
+    'scikit-learn',
+    'pandas',
+    'spacy',
+    'torch',
+    'tqdm',
+]
+
+setuptools.setup(
+    name="imodelsx",
+    version="0.01",
+    author="Chandan Singh, John X. Morris",
+    author_email="chansingh@microsoft.com",
+    description="Library to explain *a dataset* in natural language.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/csinva/imodelsX",
+    packages=setuptools.find_packages(
+        exclude=['tests', 'tests.*', '*.test.*']
+    ),
+    install_requires=required_pypi,
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
