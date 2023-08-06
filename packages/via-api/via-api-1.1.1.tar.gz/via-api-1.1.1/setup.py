249866baae8b699b235bdbfb8ffd194d64c26c63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:51 +0000

    Add package via-api/1.1.1/via-api-1.1.1.tar.gz

---
+++ packages/via-api/via-api-1.1.1.tar.gz/via-api-1.1.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import (
+    find_packages,
+    setup
+)
+
+INSTALL_REQUIRES = (
+    'cached_property',
+    'packaging',
+    'reverse_geocoder',
+    'fast_json',
+    'boto3',
+    'haversine',
+    'osmnx',
+    'scikit-learn',
+    'requests',
+    'geopandas',
+    'python-logging-loki'
+)
+
+setup(
+    name='via-api',
+    version='1.1.1',
+    python_requires='>=3.6,<3.10',
+    description='Analysing and serving crowdsourced road quality data',
+    long_description='Analysing and serving crowdsourced road quality data',
+    author='Robert Lucey',
+    url='https://github.com/RobertLucey/via',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=INSTALL_REQUIRES,
+    entry_points={
+        'console_scripts': [
+            'pull_journeys = via.bin.pull_journeys:main',
+            'generate_geojson = via.bin.generate_geojson:main'
+        ]
+    }
+)
