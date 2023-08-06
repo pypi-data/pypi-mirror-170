5d0a2f98278cc075d9cf302a34f5c4ff7109ef9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:24 +0000

    Add package dummy-python-cli/0.1.0a0/dummy-python-cli-0.1.0a0.tar.gz

---
+++ packages/dummy-python-cli/dummy-python-cli-0.1.0a0.tar.gz/dummy-python-cli-0.1.0a0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dummy-python-cli']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click==8.1.3']
+
+entry_points = \
+{'console_scripts': ['dummy-cli = entry:main']}
+
+setup_kwargs = {
+    'name': 'dummy-python-cli',
+    'version': '0.1.0a0',
+    'description': '',
+    'long_description': '# python-cli-pypi-publishing\n\nAdapted from https://levelup.gitconnected.com/how-to-publish-a-python-command-line-application-to-pypi-5b97a6d586f1\n',
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
