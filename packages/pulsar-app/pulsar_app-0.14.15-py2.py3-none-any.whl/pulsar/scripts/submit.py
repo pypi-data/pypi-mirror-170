c63ceaff61a76feb13e93b565ea8fb1de6a06634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:23 +0000

    Add package pulsar-app/0.14.15/pulsar_app-0.14.15-py2.py3-none-any.whl

---
+++ packages/pulsar-app/pulsar_app-0.14.15-py2.py3-none-any.whl/pulsar/scripts/submit.py
@@ -0,0 +1,21 @@
+"""Submit a job and wait for it.
+"""
+import sys
+
+from pulsar.main import ArgumentParser
+from pulsar.scripts.submit_util import (
+    add_common_submit_args,
+    run_server_for_job,
+)
+
+
+def main(args=None):
+    mod_docstring = sys.modules[__name__].__doc__
+    arg_parser = ArgumentParser(description=mod_docstring)
+    add_common_submit_args(arg_parser)
+    args = arg_parser.parse_args(args)
+    run_server_for_job(args)
+
+
+if __name__ == "__main__":
+    main()
