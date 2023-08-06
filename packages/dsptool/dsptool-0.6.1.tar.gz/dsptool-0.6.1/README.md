6ffef05d2c5ae992ec002530ead3e6948fbc8f98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:42 +0000

    Add package dsptool/0.6.1/dsptool-0.6.1.tar.gz

---
+++ packages/dsptool/dsptool-0.6.1.tar.gz/dsptool-0.6.1/README.md
@@ -0,0 +1,6 @@
+[![Build Status](https://drone.tandav.me/api/badges/bhairava/dsptool/status.svg)](https://drone.tandav.me/bhairava/dsptool)
+
+# dsptool
+```sh
+pip install dsptool
+```
---
+++ packages/dsptool/dsptool-0.6.1.tar.gz/dsptool-0.6.1/dsptool/scale.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+def minmax_scaler(value: float | np.ndarray, oldmin, oldmax, newmin=0.0, newmax=1.0) -> float | np.ndarray:
+    if not oldmin < oldmax:
+        raise ValueError('oldmin should be less than oldmax')
+    if not newmin < newmax:
+        raise ValueError('newmin should be less than newmax')
+
+    if isinstance(value, np.ndarray):
+        if not np.all((oldmin <= value) & (value <= oldmax)):
+            raise ValueError('value should be oldmin <= value <= oldmax')
+    else:
+        if not oldmin <= value <= oldmax:
+            raise ValueError('value should be oldmin <= value <= oldmax')
+
+    old_settings = np.seterr(over='raise')
+    out = (value - oldmin) * (newmax - newmin) / (oldmax - oldmin) + newmin
+    np.seterr(**old_settings)
+    return out
