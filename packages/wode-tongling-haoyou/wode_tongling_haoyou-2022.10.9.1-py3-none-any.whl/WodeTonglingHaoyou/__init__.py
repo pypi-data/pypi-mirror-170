32cda8093e1d97569c3488e41e670856a31ca584
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:19:24 +0000

    Add package wode-tongling-haoyou/2022.10.9.1/wode_tongling_haoyou-2022.10.9.1-py3-none-any.whl

---
+++ packages/wode-tongling-haoyou/wode_tongling_haoyou-2022.10.9.1-py3-none-any.whl/WodeTonglingHaoyou/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""wode-tongling-haoyou
+https://github.com/apachecn/wode-tongling-haoyou"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.9.1"
