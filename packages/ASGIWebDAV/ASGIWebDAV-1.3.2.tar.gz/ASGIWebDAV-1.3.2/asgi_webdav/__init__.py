c3b8e6c57ecf4d02ba0c3a6975dad1ad398f967a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:12 +0000

    Add package ASGIWebDAV/1.3.2/ASGIWebDAV-1.3.2.tar.gz

---
+++ packages/ASGIWebDAV/ASGIWebDAV-1.3.2.tar.gz/ASGIWebDAV-1.3.2/asgi_webdav/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+
+"""
+ASGI WebDAV Server
+"""
+
+# from .core import (  # noqa: F401
+#     PyPIPackageProject,
+# )
+
+__name__ = "ASGIWebDAV"
+__version__ = "1.3.2"
+
+__author__ = "Rex Zhang"
+__author_email__ = "rex.zhang@gmail.com"
+__licence__ = "MIT"
+
+__description__ = (
+    "An asynchronous WebDAV server implementation, support multi-provider."
+)
+__project_url__ = "https://github.com/rexzhang/asgi-webdav"
