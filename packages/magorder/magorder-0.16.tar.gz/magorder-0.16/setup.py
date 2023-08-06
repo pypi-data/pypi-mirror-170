8e45f46dd1808ced6d006038ef784526d52e35ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:01 +0000

    Add package magorder/0.16/magorder-0.16.tar.gz

---
+++ packages/magorder/magorder-0.16.tar.gz/magorder-0.16/setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(name='magorder',
+      version='0.16',
+      packages=find_packages(),
+      )
