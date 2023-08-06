c1f03dcb4f32aa2a5c88aebe29021f49686faca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:15:48 +0000

    Add package remoteauthclient/1.4.0/remoteauthclient-1.4.0.tar.gz

---
+++ packages/remoteauthclient/remoteauthclient-1.4.0.tar.gz/remoteauthclient-1.4.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+from os.path import join, dirname
+
+requirements = [
+    "cryptography>=3.4.7",
+    "websockets>=10.1",
+    "aiohttp>=3.8.1"
+]
+
+setup(
+    name='remoteauthclient',
+    version='1.4.0',
+    packages=["remoteauthclient"],
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    long_description_content_type="text/markdown",
+    description='Client for Discord authorization via qr code',
+    url='https://github.com/RuslanUC/RemoteAuthClient',
+    repository='https://github.com/RuslanUC/RemoteAuthClient',
+    author='RuslanUC',
+    install_requires=requirements,
+    python_requires='>=3.7',
+    license='MIT',
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+      ]
+)
