969c1f66605a86f202a4c648f4cbe906fadcae34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:14 +0000

    Add package colors1/0.0.2/colors1-0.0.2.tar.gz

---
+++ packages/colors1/colors1-0.0.2.tar.gz/colors1-0.0.2/src/colors1/colors.py
@@ -0,0 +1,228 @@
+def terminalBlack(userStr):
+    color = '\u001b[30m'
+    return color + userStr
+
+
+def terminalRed(userStr):
+    color = '\u001b[31m'
+    return color + userStr
+
+
+def terminalGreen(userStr):
+    color = '\u001b[32m'
+    return color + userStr
+
+
+def terminalYellow(userStr):
+    color = '\u001b[33m'
+    return color + userStr
+
+
+def terminalBlue(userStr):
+    color = '\u001b[34m'
+    return color + userStr
+
+
+def terminalMagenta(userStr):
+    color = '\u001b[35m'
+    return color + userStr
+
+
+def terminalCyan(userStr):
+    color = '\u001b[36m'
+    return color + userStr
+
+
+def terminalGray(userStr):
+    color = '\u001b[37m'
+    return color + userStr
+
+
+def terminalBrightBlack(userStr):
+    color = '\u001b[30;1m'
+    return color + userStr
+
+
+def terminalBrightRed(userStr):
+    color = '\u001b[31;1m'
+    return color + userStr
+
+
+def terminalBrightGreen(userStr):
+    color = '\u001b[32;1m'
+    return color + userStr
+
+
+def terminalBrightYellow(userStr):
+    color = '\u001b[33;1m'
+    return color + userStr
+
+
+def terminalBrightBlue(userStr):
+    color = '\u001b[34;1m'
+    return color + userStr
+
+
+def terminalBrightMagenta(userStr):
+    color = '\u001b[35;1m'
+    return color + userStr
+
+
+def terminalBrightCyan(userStr):
+    color = '\u001b[36;1m'
+    return color + userStr
+
+
+def terminalBrightGray(userStr):
+    color = '\u001b[37;1m'
+    return color + userStr
+
+
+def terminalBackgroundBlack(userStr):
+    color = '\u001b[40m'
+    return color + userStr
+
+
+def terminalBackgroundRed(userStr):
+    color = '\u001b[41m'
+    return color + userStr
+
+
+def terminalBackgroundGreen(userStr):
+    color = '\u001b[42m'
+    return color + userStr
+
+
+def terminalBackgroundYellow(userStr):
+    color = '\u001b[43m'
+    return color + userStr
+
+
+def terminalBackgroundBlue(userStr):
+    color = '\u001b[44m'
+    return color + userStr
+
+
+def terminalBackgroundMagenta(userStr):
+    color = '\u001b[45m'
+    return color + userStr
+
+
+def terminalBackgroundCyan(userStr):
+    color = '\u001b[46m'
+    return color + userStr
+
+
+def terminalBackgroundWhite(userStr):
+    color = '\u001b[47m'
+    return color + userStr
+
+
+def rgbBlack():
+    color = (0, 0, 0)
+    return color
+
+
+def rgbRed():
+    color = (255, 0, 0)
+    return color
+
+
+def rgbGreen():
+    color = (0, 255, 0)
+    return color
+
+
+def rgbYellow():
+    color = (255, 255, 0)
+    return color
+
+
+def rgbBlue():
+    color = (0, 0, 255)
+    return color
+
+
+def rgbMagenta():
+    color = (255, 0, 255)
+    return color
+
+
+def rgbCyan():
+    color = (0, 255, 255)
+    return color
+
+
+def rgbWhite():
+    color = (255, 255, 255)
+    return color
+
+
+def rgbGray():
+    color = (128, 128, 128)
+    return color
+
+
+def hexBlack():
+    color = '#000000'
+    return color
+
+
+def hexRed():
+    color = '#ff0000'
+    return color
+
+
+def hexGreen():
+    color = '#00ff00'
+    return color
+
+
+def hexYellow():
+    color = '#ffff00'
+    return color
+
+
+def hexBlue():
+    color = '#0000ff'
+    return color
+
+
+def hexMagenta():
+    color = '#ff00ff'
+    return color
+
+
+def hexCyan():
+    color = '#00ffff'
+    return color
+
+
+def hexWhite():
+    color = '#ffffff'
+    return color
+
+
+def hexGray():
+    color = '#808080'
+    return color
+
+
+def terminalBold(userStr):
+    color = '\u001b[1m'
+    return color + userStr
+
+
+def terminalUnderline(userStr):
+    color = '\u001b[4m'
+    return color + userStr
+
+
+def terminalReverse(userStr):
+    color = '\u001b[7m'
+    return color + userStr
+
+
+def end(userStr):
+    color = '\u001b[0m'
+    return color + userStr
