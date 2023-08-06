1f0848707e070f227afe162dbc2fcf987b47f3dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:15 +0000

    Add package hestia-earth-models/0.34.1/hestia_earth_models-0.34.1-py3-none-any.whl

---
+++ packages/hestia-earth-models/hestia_earth_models-0.34.1-py3-none-any.whl/tests/models/utils/test_blank_node.py
@@ -0,0 +1,12 @@
+from hestia_earth.schema import SiteSiteType
+
+from hestia_earth.models.utils.blank_node import _run_required
+
+
+def test_run_required():
+    assert not _run_required('model', 'ch4ToAirAquacultureSystems', {
+        'site': {'siteType': SiteSiteType.CROPLAND.value}
+    })
+    assert _run_required('model', 'ch4ToAirAquacultureSystems', {
+        'site': {'siteType': SiteSiteType.POND.value}
+    }) is True
