72fcc09417b381d3079f8489cbd6babad9a43056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:11 +0000

    Add package sky-excel/1.0.1/sky-excel-1.0.1.tar.gz

---
+++ packages/sky-excel/sky-excel-1.0.1.tar.gz/sky-excel-1.0.1/setup.py
@@ -0,0 +1,25 @@
+# encoding: utf-8
+"""
+@project: djangoModel->setup
+@author: 孙楷炎
+@Email: sky4834@163.com
+@synopsis: 模块打包文件
+@created_time: 2022/6/18 15:14
+"""
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf8') as fp:
+    log_desc = fp.read()
+
+setup(
+    name='sky-excel',  # 模块名称
+    version='1.0.1',  # 模块版本
+    description='excel实验模块',  # 项目 摘要描述
+    long_description=log_desc,  # 项目描述
+    long_description_content_type="text/markdown",  # md文件，markdown格式
+    packages=find_packages(),  # 系统自动从当前目录开始找包
+    license="apache 3.0",
+    install_requires=[
+        'openpyxl',
+    ]
+)
