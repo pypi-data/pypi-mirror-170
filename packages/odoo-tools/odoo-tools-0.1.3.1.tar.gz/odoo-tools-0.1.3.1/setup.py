74343e5825502064b2892467ae06111f059e09f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:14 +0000

    Add package odoo-tools/0.1.3.1/odoo-tools-0.1.3.1.tar.gz

---
+++ packages/odoo-tools/odoo-tools-0.1.3.1.tar.gz/odoo-tools-0.1.3.1/setup.py
@@ -0,0 +1,69 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="odoo-tools",
+    version="0.1.3.1",
+    author="Loïc Faure-Lacroix <lamerstar@gmail.com>",
+    author_email="lamerstar@gmail.com",
+    description="Odoo Tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://odoo-tools.readthedocs.io",
+    project_urls={
+        "Source": "https://github.com/llacroix/odoo-tools",
+        "Documentation": "https://odoo-tools.readthedocs.io",
+    },
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "giturlparse",
+        "toposort",
+        "toml",
+        "requests",
+        "packaging",
+        "lxml",
+        "docutils",
+        "polib",
+        "six>=1.12.0",
+        "cryptography",
+        "click",
+        "passlib",
+        "ptpython",
+        "pip>=10.0",
+        "password-strength",
+        "psycopg2"
+    ],
+    extras_require={
+        "docs": [
+            "sphinx",
+            "furo",
+            "sphinx-argparse",
+            "sphinx-click",
+            "sphinxcontrib.asciinema",
+        ],
+        "test": [
+            "mock",
+            "pytest",
+            "pytest-cov"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        "console_scripts": [
+            "odootools = odoo_tools.cli.odot:command"
+        ],
+    },
+    package_data={
+        "odoo_tools": [
+            "requirements/*.txt",
+            "packages/*.toml",
+        ],
+    }
+)
