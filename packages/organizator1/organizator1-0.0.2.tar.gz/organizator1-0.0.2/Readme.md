c7b9819be71b164e10c77e17b1ef102011111ef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:48 +0000

    Add package organizator1/0.0.2/organizator1-0.0.2.tar.gz

---
+++ packages/organizator1/organizator1-0.0.2.tar.gz/organizator1-0.0.2/Readme.md
@@ -0,0 +1,1 @@
+## Try to upload this to Pypi
---
+++ packages/organizator1/organizator1-0.0.2.tar.gz/organizator1-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+
+setup(
+name='organizator1',
+version='0.0.2',
+author='pk',
+author_email='kali.thot@centrum.cz',
+packages=find_packages(),
+include_package_data=True, 
+description='just playing'
+
+)
