20171c498ccc757ac43ee3d8c97df377cbe60a82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:47 +0000

    Add package ashishinstall/0.0.1/ashishinstall-0.0.1-py3-none-any.whl

---
+++ packages/ashishinstall/ashishinstall-0.0.1-py3-none-any.whl/ashishinstall/ashishinstall.py
@@ -0,0 +1,5 @@
+def info():
+    print("Welcome to backend setup Module.")
+    print("-------------------------------")
+    print("Your Environment is Ready to use.")
+    print("-------------------------------")
