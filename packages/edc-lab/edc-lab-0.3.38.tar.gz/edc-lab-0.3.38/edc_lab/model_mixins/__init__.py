49f24e5e929627e2aacbaccbb73f584924e72a04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:38 +0000

    Add package edc-lab/0.3.38/edc-lab-0.3.38.tar.gz

---
+++ packages/edc-lab/edc-lab-0.3.38.tar.gz/edc-lab-0.3.38/edc_lab/model_mixins/__init__.py
@@ -0,0 +1,16 @@
+from .aliquot import (
+    AliquotIdentifierModelMixin,
+    AliquotLabelMixin,
+    AliquotModelMixin,
+    AliquotShippingMixin,
+    AliquotTypeModelMixin,
+)
+from .panel_model_mixin import LabProfileError, PanelModelError, PanelModelMixin
+from .requisition import (
+    CrfWithRequisitionModelMixin,
+    RequisitionIdentifierMixin,
+    RequisitionModelMixin,
+    RequisitionStatusMixin,
+)
+from .result import ResultItemModelMixin, ResultModelMixin
+from .shipping import ManifestModelMixin, VerifyBoxModelMixin, VerifyModelMixin
