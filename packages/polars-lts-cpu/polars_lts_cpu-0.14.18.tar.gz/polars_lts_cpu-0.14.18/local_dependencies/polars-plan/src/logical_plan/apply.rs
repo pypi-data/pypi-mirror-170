263c97872ba62bc70e979ad5d8ff2ddbdb4311f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:05 +0000

    Add package polars-lts-cpu/0.14.18/polars_lts_cpu-0.14.18.tar.gz

---
+++ packages/polars-lts-cpu/polars_lts_cpu-0.14.18.tar.gz/polars_lts_cpu-0.14.18/local_dependencies/polars-plan/src/logical_plan/apply.rs
@@ -0,0 +1,41 @@
+use std::fmt::{Debug, Formatter};
+
+use polars_core::prelude::*;
+
+pub trait DataFrameUdf: Send + Sync {
+    fn call_udf(&self, df: DataFrame) -> PolarsResult<DataFrame>;
+}
+
+impl<F> DataFrameUdf for F
+where
+    F: Fn(DataFrame) -> PolarsResult<DataFrame> + Send + Sync,
+{
+    fn call_udf(&self, df: DataFrame) -> PolarsResult<DataFrame> {
+        self(df)
+    }
+}
+
+impl Debug for dyn DataFrameUdf {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "dyn DataFrameUdf")
+    }
+}
+
+pub trait UdfSchema: Send + Sync {
+    fn get_schema(&self, input_schema: &Schema) -> PolarsResult<SchemaRef>;
+}
+
+impl<F> UdfSchema for F
+where
+    F: Fn(&Schema) -> PolarsResult<SchemaRef> + Send + Sync,
+{
+    fn get_schema(&self, input_schema: &Schema) -> PolarsResult<SchemaRef> {
+        self(input_schema)
+    }
+}
+
+impl Debug for dyn UdfSchema {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "dyn UdfSchema")
+    }
+}
