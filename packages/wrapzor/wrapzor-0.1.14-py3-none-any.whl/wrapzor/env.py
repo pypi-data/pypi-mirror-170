9b79839314123b1fadd7ecbd10a7daaecb244855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:44 +0000

    Add package wrapzor/0.1.14/wrapzor-0.1.14-py3-none-any.whl

---
+++ packages/wrapzor/wrapzor-0.1.14-py3-none-any.whl/wrapzor/env.py
@@ -0,0 +1,11 @@
+import os
+from pathlib import Path
+
+ZOHO_CLIENT_ID = os.getenv("ZOHO_CLIENT_ID")
+ZOHO_CLIENT_PASSWORD = os.getenv("ZOHO_CLIENT_PASSWORD")
+ZOHO_REFRESH_TOKEN = os.getenv("ZOHO_REFRESH_TOKEN")
+ZOHO_CODE = os.getenv("ZOHO_CODE")
+ZOHO_ACCOUNT_DOMAIN = os.getenv("ZOHO_ACCOUNT_DOMAIN", "https://accounts.zoho.eu")
+CURRENT_DIR = Path(__file__).parent
+HOME = Path.home()
+STATIC_DIR = (HOME / ".wrapzor").mkdir(exist_ok=True)
