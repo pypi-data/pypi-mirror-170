016b0ba39259703a7c068cdd480b154f4b0cf18d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:07 +0000

    Add package pyqir-evaluator/0.6.2/pyqir_evaluator-0.6.2-cp36-abi3-win_amd64.whl

---
+++ packages/pyqir-evaluator/pyqir_evaluator-0.6.2-cp36-abi3-win_amd64.whl/pyqir/evaluator/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+from pyqir.evaluator._gatelogger import GateLogger as GateLogger
+from pyqir.evaluator._gateset import GateSet as GateSet
+from pyqir.evaluator._nonadaptiveevaluator import (
+    NonadaptiveEvaluator as NonadaptiveEvaluator,
+)
