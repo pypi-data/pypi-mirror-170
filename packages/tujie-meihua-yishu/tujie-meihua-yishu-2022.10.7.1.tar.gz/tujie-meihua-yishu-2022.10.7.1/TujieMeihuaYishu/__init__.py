37df1b7989c511db4d67357a4f537e6ef31103a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:47 +0000

    Add package tujie-meihua-yishu/2022.10.7.1/tujie-meihua-yishu-2022.10.7.1.tar.gz

---
+++ packages/tujie-meihua-yishu/tujie-meihua-yishu-2022.10.7.1.tar.gz/tujie-meihua-yishu-2022.10.7.1/TujieMeihuaYishu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""tujie-meihua-yishu
+https://github.com/apachecn/tujie-meihua-yishu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.7.1"
