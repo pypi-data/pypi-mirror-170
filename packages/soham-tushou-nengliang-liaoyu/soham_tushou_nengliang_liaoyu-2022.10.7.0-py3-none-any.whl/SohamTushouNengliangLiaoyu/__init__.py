92d226e67c60777f9debb719e85d15b278d959b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:20 +0000

    Add package soham-tushou-nengliang-liaoyu/2022.10.7.0/soham_tushou_nengliang_liaoyu-2022.10.7.0-py3-none-any.whl

---
+++ packages/soham-tushou-nengliang-liaoyu/soham_tushou_nengliang_liaoyu-2022.10.7.0-py3-none-any.whl/SohamTushouNengliangLiaoyu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""soham-tushou-nengliang-liaoyu
+https://github.com/apachecn/soham-tushou-nengliang-liaoyu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.7.0"
