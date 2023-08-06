400cbcbdb9fb7419b8126615d0f1f9439b938b93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:54 +0000

    Add package stag/0.1.4/stag-0.1.4.tar.gz

---
+++ packages/stag/stag-0.1.4.tar.gz/stag-0.1.4/stag/stag_lib/stag.cpp
@@ -0,0 +1,48 @@
+#include "stag.h"
+
+stag::Graph::Graph(const SprsMat& adjacency_matrix) {
+  adjacency_matrix_ = adjacency_matrix;
+}
+
+stag::Graph::Graph(std::vector<int> &outerStarts, std::vector<int> &innerIndices,
+                   std::vector<double> &values) {
+  // Map the provided data vectors to the sparse matrix type.
+  adjacency_matrix_ = Eigen::Map<SprsMat>(long(outerStarts.size()) - 1,
+                                          long(outerStarts.size()) - 1,
+                                          long(values.size()),
+                                          outerStarts.data(),
+                                          innerIndices.data(),
+                                          values.data());
+}
+
+SprsMat stag::Graph::adjacency() {
+  return adjacency_matrix_;
+}
+
+SprsMat stag::Graph::laplacian() {
+  // Construct the vertex degrees.
+  Eigen::VectorXd degrees = adjacency_matrix_ * Eigen::VectorXd::Ones(adjacency_matrix_.cols());
+  SprsMat degree_matrix(adjacency_matrix_.cols(), adjacency_matrix_.cols());
+  for (int i = 0; i < adjacency_matrix_.cols(); i++) {
+    degree_matrix.insert(i, i) = degrees[i];
+  }
+
+  // Construct and return the laplacian matrix.
+  return degree_matrix - adjacency_matrix_;
+}
+
+double stag::Graph::volume() {
+  Eigen::VectorXd degrees = adjacency_matrix_ * Eigen::VectorXd::Ones(adjacency_matrix_.cols());
+  return degrees.sum();
+}
+
+stag::Graph stag::cycle_graph(int n) {
+  SprsMat adj_mat(n, n);
+  std::vector<Eigen::Triplet<double>> non_zero_entries;
+  for (int i = 0; i < n; i++) {
+    non_zero_entries.emplace_back(i, (i + n + 1) % n, 1);
+    non_zero_entries.emplace_back(i, (i + n - 1) % n, 1);
+  }
+  adj_mat.setFromTriplets(non_zero_entries.begin(), non_zero_entries.end());
+  return stag::Graph(adj_mat);
+}
