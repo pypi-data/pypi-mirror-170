d957825522ed85c85c6362c5b48de3ffc4f75aef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:16 +0000

    Add package nqdc/0.0.3/nqdc-0.0.3.tar.gz

---
+++ packages/nqdc/nqdc-0.0.3.tar.gz/nqdc-0.0.3/setup.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+from setuptools import setup
+
+package_dir = Path(__file__).parent.joinpath("src", "nqdc")
+data_files = [
+    str(f.relative_to(package_dir))
+    for f in package_dir.joinpath("_data").glob("**/*")
+]
+version = package_dir.joinpath("_data", "VERSION").read_text("utf-8").strip()
+setup(package_data={"nqdc": data_files}, version=version)
