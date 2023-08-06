83c1231773350b5f41ecd8ba43693fa9603d22b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:52 +0000

    Add package MeineUtils/0.0.42/MeineUtils-0.0.42.tar.gz

---
+++ packages/MeineUtils/MeineUtils-0.0.42.tar.gz/MeineUtils-0.0.42/MeineUtils/MachineLearning/__init__.py
@@ -0,0 +1,25 @@
+from .linear_regression import LinearRegression
+from .logistic_regression import LogisticRegression
+
+# from .normalization import (
+#     MinMaxScaler,
+#     StandardScaler
+# )
+
+from MeineUtils.MachineLearning.bounding_box import BoundingBoxConverter
+
+from MeineUtils.MachineLearning.augmentation import Augmentation
+from MeineUtils.MachineLearning.augmentation import RandomBrightness
+from MeineUtils.MachineLearning.augmentation import RandomSaltAndPepper
+from MeineUtils.MachineLearning.augmentation import RandomValuedImpulseNoise
+from MeineUtils.MachineLearning.augmentation import RandomRotate
+
+# from .augmentation import (
+#     RandomRotate,
+#     RandomNoise,
+#     RandomBrightness
+# )
+
+from .NaturalLanguageProcessing import (
+    preprocessing
+)
