ee1a1550b3a388547d0c254fceee9d900309ad07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:15:24 +0000

    Add package songbo-yushenxinling-zhenghe-liaoyu/2022.10.9.0/songbo_yushenxinling_zhenghe_liaoyu-2022.10.9.0-py3-none-any.whl

---
+++ packages/songbo-yushenxinling-zhenghe-liaoyu/songbo_yushenxinling_zhenghe_liaoyu-2022.10.9.0-py3-none-any.whl/SongboYushenxinlingZhengheLiaoyu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""songbo-yushenxinling-zhenghe-liaoyu
+https://github.com/apachecn/songbo-yushenxinling-zhenghe-liaoyu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.9.0"
