b32db6898ccd38e002a3fc522d51c8325693b26c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:20 +0000

    Add package sadie-antibody/0.4.27/sadie-antibody-0.4.27.tar.gz

---
+++ packages/sadie-antibody/sadie-antibody-0.4.27.tar.gz/sadie-antibody-0.4.27/src/sadie/airr/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.4.27"
+
+from .airr import Airr
+from sadie.airr.igblast import GermlineData
+from sadie.airr.airrtable import AirrSeries, AirrTable, LinkedAirrTable
+
+__all__ = ["Airr", "AirrSeries", "AirrTable", "LinkedAirrTable", "GermlineData"]
