f85f2c49684abf8da05747865fbeba488d2b4699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:51 +0000

    Add package zeroconnect/0.0.1/zeroconnect-0.0.1-py3-none-any.whl

---
+++ packages/zeroconnect/zeroconnect-0.0.1-py3-none-any.whl/zeroconnect/server.py
@@ -0,0 +1,27 @@
+import socket
+import threading
+
+def onNewClient(sock, addr, callback):
+    callback(sock, addr)
+
+def listenForConnections(s, callback):
+    while True:
+        sock, addr = s.accept()
+        print('Got connection from', addr)
+        threading.Thread(target=onNewClient, args=(sock, addr, callback), daemon=True).start()
+    s.close()
+
+def listen(callback, port=0, host="0.0.0.0"):
+    s = socket.socket()
+
+    print('Server started!')
+    print('Waiting for clients...')
+
+    s.bind((host, port))
+    port = s.getsockname()[1]
+
+    s.listen(5)
+
+    threading.Thread(target=listenForConnections, args=(s, callback), daemon=True).start()
+
+    return port
