1899cefe0d2170a6d0ef6212a3e4469e536a537b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:00 +0000

    Add package pdm-invoke/0.0.1/pdm-invoke-0.0.1.tar.gz

---
+++ packages/pdm-invoke/pdm-invoke-0.0.1.tar.gz/pdm-invoke-0.0.1/pdm_invoke/main.py
@@ -0,0 +1,42 @@
+import argparse
+import subprocess
+import sys
+
+from pdm.cli.utils import PdmFormatter
+from pdm.cli.commands.base import BaseCommand
+from pdm.cli.commands.run import Command as RunCommand
+from pdm.cli.commands.run import TaskRunner
+from pdm.cli.hooks import HookManager
+from pdm.cli.options import skip_option
+from pdm.cli.utils import check_project_file
+
+
+class InvokeCommand(RunCommand):
+  OPTIONS = []
+  COMMAND_PREFIX = ['inv']
+
+  def add_arguments(self, parser):
+    skip_option.add_to_parser(parser)
+    parser.add_argument(
+      "args",
+      nargs=argparse.REMAINDER,
+      help="Arguments that will be passed to the command",
+    )
+
+  def handle(self, project, options):
+    options.list = False
+    options.command = self.COMMAND_PREFIX[0]
+
+    check_project_file(project)
+    hooks = HookManager(project, options.skip)
+    runner = TaskRunner(project, hooks=hooks)
+    sys.exit(runner.run(options.command, self.COMMAND_PREFIX[1:] + options.args))
+
+    hooks.try_emit("pre_run", script=options.command, args=options.args)
+    exit_code = runner.run(options.command, self.COMMAND_PREFIX[1:] + options.args)
+    hooks.try_emit("post_run", script=options.command, args=options.args)
+    sys.exit(exit_code)
+
+
+def reg_commands(core):
+  core.register_command(InvokeCommand, "inv")
