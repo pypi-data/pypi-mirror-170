08cf9f764de3b296ed07408068137a17c86f601e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:51 +0000

    Add package pyrmv/0.2.6/pyrmv-0.2.6.tar.gz

---
+++ packages/pyrmv/pyrmv-0.2.6.tar.gz/pyrmv-0.2.6/pyrmv/enums/filter_mode.py
@@ -0,0 +1,14 @@
+from enum import auto
+from .auto_name import AutoName
+
+class FilterMode(AutoName):
+    """Enumeration used to declare filters for nearby searches."""
+
+    DIST_PERI = auto()
+    "Accentuate matches. Matches in the radius are first."
+
+    EXCL_PERI = auto()
+    "Returns matches inside the radius only."
+
+    SLCT_PERI = auto()
+    "Matches in the radius are excluded. Returns matches outside the radius only."
