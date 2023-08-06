0da225eaaf43c38ea1a531d5e7b94ca4ccfe4e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:28 +0000

    Add package slater-dl-wrapper/0.0.2/slater_dl_wrapper-0.0.2.tar.gz

---
+++ packages/slater-dl-wrapper/slater_dl_wrapper-0.0.2.tar.gz/slater_dl_wrapper-0.0.2/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='slater_dl_wrapper',
+    version='0.0.2',
+    author='Ryan Slater',
+    description='A wrapper for PyTorch to make it easier to train and evaluate models',
+    packages=['slater_dl_wrapper'],
+    url='https://github.com/CodingPenguin1/slater-dl-wrapper'
+)
