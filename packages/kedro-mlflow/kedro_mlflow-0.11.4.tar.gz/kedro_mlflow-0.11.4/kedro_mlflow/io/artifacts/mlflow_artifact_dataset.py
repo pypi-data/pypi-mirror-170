11265f611e28cd0e5dbeae5815f9f6f57b5a6847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:27 +0000

    Add package kedro-mlflow/0.11.4/kedro_mlflow-0.11.4.tar.gz

---
+++ packages/kedro-mlflow/kedro_mlflow-0.11.4.tar.gz/kedro_mlflow-0.11.4/kedro_mlflow/io/artifacts/mlflow_artifact_dataset.py
@@ -0,0 +1,144 @@
+from pathlib import Path
+from typing import Any, Dict, Union
+
+import mlflow
+from kedro.io import AbstractVersionedDataSet
+from kedro.io.core import parse_dataset_definition
+from mlflow.tracking import MlflowClient
+
+
+class MlflowArtifactDataSet(AbstractVersionedDataSet):
+    """This class is a wrapper for any kedro AbstractDataSet.
+    It decorates their ``save`` method to log the dataset in mlflow when ``save`` is called.
+    """
+
+    def __new__(
+        cls,
+        data_set: Union[str, Dict],
+        run_id: str = None,
+        artifact_path: str = None,
+        credentials: Dict[str, Any] = None,
+    ):
+
+        data_set, data_set_args = parse_dataset_definition(config=data_set)
+
+        # fake inheritance : this mlflow class should be a mother class which wraps
+        # all dataset (i.e. it should replace AbstractVersionedDataSet)
+        # instead and since we can't modify the core package,
+        # we create a subclass which inherits dynamically from the data_set class
+        class MlflowArtifactDataSetChildren(data_set):
+            def __init__(self, run_id, artifact_path):
+                super().__init__(**data_set_args)
+                self.run_id = run_id
+                self.artifact_path = artifact_path
+                self._logging_activated = True
+
+            @property
+            def _logging_activated(self):
+                return self.__logging_activated
+
+            @_logging_activated.setter
+            def _logging_activated(self, flag):
+                if not isinstance(flag, bool):
+                    raise ValueError(
+                        f"_logging_activated must be a boolean, got {type(flag)}"
+                    )
+                self.__logging_activated = flag
+
+            def _save(self, data: Any):
+
+                # _get_save_path needs to be called before super, otherwise
+                # it will throw exception that file under path already exist.
+                if hasattr(self, "_version"):
+                    # all kedro datasets inherits from AbstractVersionedDataSet
+                    local_path = self._get_save_path()
+                elif hasattr(self, "_filepath"):
+                    # in case custom datasets inherits from AbstractDataSet without versioning
+                    local_path = self._filepath  # pragma: no cover
+                elif hasattr(self, "_path"):
+                    # special datasets with a folder instead of a specifi files like PartitionedDataSet
+                    local_path = Path(self._path)
+
+                # it must be converted to a string with as_posix()
+                # for logging on remote storage like Azure S3
+                local_path = local_path.as_posix()
+
+                super()._save(data)
+
+                if self._logging_activated:
+                    if self.run_id:
+                        # if a run id is specified, we have to use mlflow client
+                        # to avoid potential conflicts with an already active run
+                        mlflow_client = MlflowClient()
+                        mlflow_client.log_artifact(
+                            run_id=self.run_id,
+                            local_path=local_path,
+                            artifact_path=self.artifact_path,
+                        )
+                    else:
+                        mlflow.log_artifact(local_path, self.artifact_path)
+
+            def _load(self) -> Any:  # pragma: no cover
+                if self.run_id:
+                    # if no run_id is specified, we take the artifact from the local path rather that the active run:
+                    # there are a lot of chances that it has not been saved yet!
+
+                    mlflow_client = MlflowClient()
+
+                    if hasattr(self, "_version"):
+                        # all kedro datasets inherits from AbstractVersionedDataSet
+                        local_path = self._get_load_path()
+                    elif hasattr(self, "_filepath"):
+                        # in case custom datasets inherits from AbstractDataSet without versioning
+                        local_path = self._filepath  # pragma: no cover
+                    elif hasattr(self, "_path"):
+                        # special datasets with a folder instead of a specifi files like PartitionedDataSet
+                        local_path = Path(self._path)
+
+                    artifact_path = (
+                        (self.artifact_path / Path(local_path.name)).as_posix()
+                        if self.artifact_path
+                        else local_path.name
+                    )
+
+                    mlflow_client.download_artifacts(
+                        run_id=self.run_id,
+                        path=artifact_path,
+                        dst_path=local_path.parent.as_posix(),  # must be a **local** **directory**
+                    )
+
+                # finally, read locally
+                return super()._load()
+
+        # rename the class
+        parent_name = data_set.__name__
+        MlflowArtifactDataSetChildren.__name__ = f"Mlflow{parent_name}"
+        MlflowArtifactDataSetChildren.__qualname__ = (
+            f"{parent_name}.Mlflow{parent_name}"
+        )
+
+        mlflow_dataset_instance = MlflowArtifactDataSetChildren(
+            run_id=run_id, artifact_path=artifact_path
+        )
+        return mlflow_dataset_instance
+
+    def _load(self) -> Any:  # pragma: no cover
+        """
+        MlflowArtifactDataSet is a factory for DataSet
+        and consequently does not implements abtracts methods
+        """
+        pass
+
+    def _save(self, data: Any) -> None:  # pragma: no cover
+        """
+        MlflowArtifactDataSet is a factory for DataSet
+        and consequently does not implements abtracts methods
+        """
+        pass
+
+    def _describe(self) -> Dict[str, Any]:  # pragma: no cover
+        """
+        MlflowArtifactDataSet is a factory for DataSet
+        and consequently does not implements abtracts methods
+        """
+        pass
---
+++ packages/kedro-mlflow/kedro_mlflow-0.11.4.tar.gz/kedro_mlflow-0.11.4/tests/config/test_get_mlflow_config.py
@@ -0,0 +1,232 @@
+import json
+from pathlib import Path
+
+import pytest
+import toml
+import yaml
+from dynaconf.validator import Validator
+from kedro import __version__ as kedro_version
+from kedro.config import TemplatedConfigLoader
+from kedro.framework.project import _IsSubclassValidator, _ProjectSettings
+from kedro.framework.session import KedroSession
+from kedro.framework.startup import bootstrap_project
+
+
+def _write_yaml(filepath, config):
+    yaml_str = yaml.dump(config)
+    filepath.write_text(yaml_str)
+
+
+# TODO : reenable this test which is currently failing beacause kedro "import settings"
+# is completetly messing up because we have several projects
+# and the first import wins
+
+
+def test_mlflow_config_default(kedro_project):
+    # kedro_project is a pytest.fixture in conftest
+    dict_config = dict(
+        server=dict(
+            mlflow_tracking_uri="mlruns",
+            credentials=None,
+        ),
+        tracking=dict(
+            disable_tracking=dict(pipelines=["my_disabled_pipeline"]),
+            experiment=dict(name="fake_package", restore_if_deleted=True),
+            run=dict(id="123456789", name="my_run", nested=True),
+            params=dict(
+                dict_params=dict(
+                    flatten=True,
+                    recursive=False,
+                    sep="-",
+                ),
+                long_params_strategy="truncate",
+            ),
+        ),
+        ui=dict(port="5151", host="localhost"),
+    )
+
+    _write_yaml(kedro_project / "conf" / "local" / "mlflow.yml", dict_config)
+    expected = dict_config.copy()
+    expected["server"]["mlflow_tracking_uri"] = (kedro_project / "mlruns").as_uri()
+
+    bootstrap_project(kedro_project)
+    with KedroSession.create(project_path=kedro_project) as session:
+        context = session.load_context()
+        assert context.mlflow.dict(exclude={"project_path"}) == expected
+
+
+@pytest.mark.parametrize("package_name", [None, "fake_project"])
+def test_mlflow_config_in_uninitialized_project(kedro_project, package_name):
+    # config_with_base_mlflow_conf is a pytest.fixture in conftest
+    bootstrap_project(kedro_project)
+    session = KedroSession.create(project_path=kedro_project, package_name=package_name)
+    context = session.load_context()
+    assert context.mlflow.dict() == {
+        "server": {
+            "mlflow_tracking_uri": (kedro_project / "mlruns").as_uri(),
+            "credentials": None,
+        },
+        "tracking": {
+            "disable_tracking": {"pipelines": []},
+            "experiment": {"name": "fake_project", "restore_if_deleted": True},
+            "run": {"id": None, "name": None, "nested": True},
+            "params": {
+                "dict_params": {"flatten": False, "recursive": True, "sep": "."},
+                "long_params_strategy": "fail",
+            },
+        },
+        "ui": {"port": "5000", "host": "127.0.0.1"},
+    }
+
+
+def test_mlflow_config_with_no_experiment_name(kedro_project):
+
+    # create empty conf
+    open((kedro_project / "conf" / "base" / "mlflow.yml").as_posix(), mode="w").close()
+
+    bootstrap_project(kedro_project)
+    session = KedroSession.create(
+        project_path=kedro_project, package_name="fake_project"
+    )
+    context = session.load_context()
+    assert context.mlflow.dict() == {
+        "server": {
+            "mlflow_tracking_uri": (kedro_project / "mlruns").as_uri(),
+            "credentials": None,
+        },
+        "tracking": {
+            "disable_tracking": {"pipelines": []},
+            "experiment": {"name": "fake_project", "restore_if_deleted": True},
+            "run": {"id": None, "name": None, "nested": True},
+            "params": {
+                "dict_params": {"flatten": False, "recursive": True, "sep": "."},
+                "long_params_strategy": "fail",
+            },
+        },
+        "ui": {"port": "5000", "host": "127.0.0.1"},
+    }
+
+
+@pytest.fixture(autouse=True)
+def mock_validate_settings(mocker):
+    # KedroSession eagerly validates that a project's settings.py is correct by
+    # importing it. settings.py does not actually exists as part of this test suite
+    # since we are testing session in isolation, so the validation is patched.
+    mocker.patch("kedro.framework.session.session.validate_settings")
+
+
+def _mock_imported_settings_paths(mocker, mock_settings):
+    for path in [
+        "kedro.framework.project.settings",
+        "kedro.framework.session.session.settings",
+    ]:
+        mocker.patch(path, mock_settings)
+    return mock_settings
+
+
+@pytest.fixture
+def mock_settings_templated_config_loader_class(mocker):
+    class MockSettings(_ProjectSettings):
+        _CONFIG_LOADER_CLASS = _IsSubclassValidator(
+            "CONFIG_LOADER_CLASS", default=lambda *_: TemplatedConfigLoader
+        )
+
+        _CONFIG_LOADER_ARGS = Validator(
+            "CONFIG_LOADER_ARGS", default=dict(globals_pattern="*globals.yml")
+        )
+
+    return _mock_imported_settings_paths(mocker, MockSettings())
+
+
+@pytest.fixture
+def local_logging_config():
+    return {
+        "version": 1,
+        "formatters": {
+            "simple": {"format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s"}
+        },
+        "root": {"level": "INFO", "handlers": ["console"]},
+        "loggers": {
+            "kedro": {"level": "INFO", "handlers": ["console"], "propagate": False}
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "level": "INFO",
+                "formatter": "simple",
+                "stream": "ext://sys.stdout",
+            }
+        },
+        "info_file_handler": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "level": "INFO",
+            "formatter": "simple",
+            "filename": "logs/info.log",
+        },
+    }
+
+
+@pytest.fixture
+def fake_project(tmp_path, local_logging_config):
+    fake_project_dir = Path(tmp_path) / "fake_project"
+    (fake_project_dir / "src").mkdir(parents=True)
+
+    pyproject_toml_path = fake_project_dir / "pyproject.toml"
+    payload = {
+        "tool": {
+            "kedro": {
+                "project_version": kedro_version,
+                "project_name": "fake_project",
+                "package_name": "fake_package",
+            }
+        }
+    }
+    toml_str = toml.dumps(payload)
+    pyproject_toml_path.write_text(toml_str, encoding="utf-8")
+
+    env_logging = fake_project_dir / "conf" / "base" / "logging.yml"
+    env_logging.parent.mkdir(parents=True)
+    env_logging.write_text(json.dumps(local_logging_config), encoding="utf-8")
+    (fake_project_dir / "conf" / "local").mkdir()
+    return fake_project_dir
+
+
+@pytest.mark.usefixtures("mock_settings_templated_config_loader_class")
+def test_mlflow_config_with_templated_config_loader(fake_project):
+    dict_config = dict(
+        server=dict(
+            mlflow_tracking_uri="${mlflow_tracking_uri}",
+            credentials=None,
+        ),
+        tracking=dict(
+            disable_tracking=dict(pipelines=["my_disabled_pipeline"]),
+            experiment=dict(name="fake_package", restore_if_deleted=True),
+            run=dict(id="123456789", name="my_run", nested=True),
+            params=dict(
+                dict_params=dict(
+                    flatten=True,
+                    recursive=False,
+                    sep="-",
+                ),
+                long_params_strategy="truncate",
+            ),
+        ),
+        ui=dict(port="5151", host="localhost"),
+    )
+
+    _write_yaml(fake_project / "conf" / "local" / "mlflow.yml", dict_config)
+
+    _write_yaml(
+        fake_project / "conf" / "local" / "globals.yml",
+        dict(mlflow_tracking_uri="dynamic_mlruns"),
+    )
+
+    expected = dict_config.copy()
+    expected["server"]["mlflow_tracking_uri"] = (
+        fake_project / "dynamic_mlruns"
+    ).as_uri()
+
+    bootstrap_project(fake_project)
+    with KedroSession.create("fake_package", fake_project) as session:
+        context = session.load_context()
+        assert context.mlflow.dict(exclude={"project_path"}) == expected
