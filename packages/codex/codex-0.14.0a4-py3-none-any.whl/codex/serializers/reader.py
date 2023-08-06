2929b18da8ab730314c814a28c0e3a7200c976e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:07 +0000

    Add package codex/0.14.0a4/codex-0.14.0a4-py3-none-any.whl

---
+++ packages/codex/codex-0.14.0a4-py3-none-any.whl/codex/serializers/reader.py
@@ -0,0 +1,57 @@
+"""Codex Reader Serializers."""
+from rest_framework.serializers import (
+    BooleanField,
+    CharField,
+    ChoiceField,
+    DecimalField,
+    IntegerField,
+    Serializer,
+)
+
+from codex.serializers.choices import CHOICES
+
+
+FIT_TO_CHOICES = tuple(CHOICES["fitTo"].keys())
+
+
+class PageRouteSerializer(Serializer):
+    """A comic page route."""
+
+    pk = IntegerField(read_only=True)
+    page = IntegerField(read_only=True)
+
+
+class ReaderSeriesInfoSerializer(Serializer):
+    """Information about the series this comic belongs to."""
+
+    count = IntegerField(read_only=True)
+    index = IntegerField(read_only=True)
+    next = PageRouteSerializer(allow_null=True, read_only=True)
+    prev = PageRouteSerializer(allow_null=True, read_only=True)
+
+
+class ReaderComicSerializer(Serializer):
+    """Components for constructing the title."""
+
+    pk = IntegerField(read_only=True)
+    file_format = CharField(read_only=True)
+    issue = DecimalField(
+        max_digits=None, decimal_places=3, read_only=True, coerce_to_string=False
+    )
+    issue_suffix = CharField(read_only=True)
+    issue_count = IntegerField(read_only=True)
+    max_page = IntegerField(read_only=True)
+    series_name = CharField(read_only=True)
+    volume_name = CharField(read_only=True)
+    series = ReaderSeriesInfoSerializer(read_only=True)
+
+
+class ReaderSettingsSerializer(Serializer):
+    """Reader settings the user can change."""
+
+    fit_to = ChoiceField(
+        choices=FIT_TO_CHOICES,
+        allow_null=True,
+        required=False,
+    )
+    two_pages = BooleanField(allow_null=True, required=False)
