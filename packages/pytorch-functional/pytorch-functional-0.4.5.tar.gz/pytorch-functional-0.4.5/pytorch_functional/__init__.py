23aa8e5691c3e9108f909453aad676fc2bf0ef62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:30 +0000

    Add package pytorch-functional/0.4.5/pytorch-functional-0.4.5.tar.gz

---
+++ packages/pytorch-functional/pytorch-functional-0.4.5.tar.gz/pytorch-functional-0.4.5/pytorch_functional/__init__.py
@@ -0,0 +1,6 @@
+#  Copyright (c) 2022 Szymon Mikler
+
+from .experimental_api_v2 import enable_functional_api_for_new_modules
+from .functional_model import FunctionalModel, Input
+
+enable_functional_api_for_new_modules()
