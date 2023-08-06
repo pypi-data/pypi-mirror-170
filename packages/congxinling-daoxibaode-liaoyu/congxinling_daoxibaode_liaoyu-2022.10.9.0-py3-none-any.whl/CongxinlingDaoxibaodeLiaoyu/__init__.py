b635c6949839147c0693a65ce9042dfad5a56e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:04 +0000

    Add package congxinling-daoxibaode-liaoyu/2022.10.9.0/congxinling_daoxibaode_liaoyu-2022.10.9.0-py3-none-any.whl

---
+++ packages/congxinling-daoxibaode-liaoyu/congxinling_daoxibaode_liaoyu-2022.10.9.0-py3-none-any.whl/CongxinlingDaoxibaodeLiaoyu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""congxinling-daoxibaode-liaoyu
+https://github.com/apachecn/congxinling-daoxibaode-liaoyu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.9.0"
