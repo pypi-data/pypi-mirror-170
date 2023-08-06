7ce288d4eec35d66cd438a9904b1daa7e7475598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:44 +0000

    Add package mlphys/2.7/mlphys-2.7.tar.gz

---
+++ packages/mlphys/mlphys-2.7.tar.gz/mlphys-2.7/app_run_streamlit.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import numpy as np
+import mlphys.deepqis.simulator.Distributions as dist
+import mlphys.deepqis.simulator.Measurements as meas
+import mlphys.deepqis.utils.Purity_Measure as pm
+import mlphys.deepqis.utils.Fidelity_Measure
+
+st.title('MLPHYS')
+st.header('DeepQIS')
+
