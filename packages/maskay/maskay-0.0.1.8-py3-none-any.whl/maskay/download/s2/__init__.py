ce0e6bc73359af527426280f757972170b161b6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:12 +0000

    Add package maskay/0.0.1.8/maskay-0.0.1.8-py3-none-any.whl

---
+++ packages/maskay/maskay-0.0.1.8-py3-none-any.whl/maskay/download/s2/__init__.py
@@ -0,0 +1,3 @@
+from .pc import PC
+from .safe import SAFE
+from .safegee import SAFEGEE
---
+++ packages/maskay/maskay-0.0.1.8-py3-none-any.whl/maskay/torch.py
@@ -0,0 +1,42 @@
+import numpy as np
+import gc
+
+try:
+    import torch
+except ImportError:
+    raise ImportError("Please install the following packages: torch.")
+
+from maskay.module import MaskayModule
+
+class Module(MaskayModule):
+    def __init__(
+        self,
+        cropsize: int = 512,
+        overlap: int = 32,
+        device: str = "cpu",
+        batchsize: int = 1,
+        order: str = "BCHW",
+        quiet: int = False,
+    ):
+        super().__init__(cropsize, overlap, device, batchsize, order, quiet)
+
+    def inProcessing(self, tensor: np.ndarray):
+        pass
+
+    def forward(self, x):
+        pass
+
+    def outProcessing(self, tensor: np.ndarray):
+        pass
+
+    def _run(self, tensor: np.ndarray):
+        tensor = torch.Tensor(tensor)
+
+        # Run the model
+        with torch.no_grad():
+            if self.device == "cuda":
+                tensor = tensor.cuda()
+            tensor = self.forward(tensor).detach().cpu().numpy()
+            torch.cuda.empty_cache()
+            gc.collect()
+        return tensor
