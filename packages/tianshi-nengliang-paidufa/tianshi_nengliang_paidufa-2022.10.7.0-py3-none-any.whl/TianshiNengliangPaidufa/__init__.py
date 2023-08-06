5d42d9e1cb24c5146a97786eb07ccb670c509fea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:23 +0000

    Add package tianshi-nengliang-paidufa/2022.10.7.0/tianshi_nengliang_paidufa-2022.10.7.0-py3-none-any.whl

---
+++ packages/tianshi-nengliang-paidufa/tianshi_nengliang_paidufa-2022.10.7.0-py3-none-any.whl/TianshiNengliangPaidufa/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""tianshi-nengliang-paidufa
+https://github.com/apachecn/tianshi-nengliang-paidufa"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.7.0"
