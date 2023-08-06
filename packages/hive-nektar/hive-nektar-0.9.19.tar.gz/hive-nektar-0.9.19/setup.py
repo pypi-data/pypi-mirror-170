ba84f4496890507f1aeead11ad1eabe4745e7bb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:44:41 +0000

    Add package hive-nektar/0.9.19/hive-nektar-0.9.19.tar.gz

---
+++ packages/hive-nektar/hive-nektar-0.9.19.tar.gz/hive-nektar-0.9.19/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name = "hive-nektar",
+    packages = ["nektar"],
+    version = "0.9.19",
+    license="MIT",
+    description = "Nektar, a Python package to access Hive API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author = "Rodney Maniego Jr.",
+    author_email = "rodney.maniegojr@gmail.com",
+    url = "https://github.com/rmaniego/nektar",
+    download_url = "https://github.com/rmaniego/nektar/archive/v1.0.tar.gz",
+    keywords = ["nektar", "hive", "blockchain", "api"],
+    install_requires=["ecdsa", "requests"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers", 
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3"
+    ],
+    python_requires=">=3.9"
+)
