ffa92ce2746e7ec19b4d6d5b545601af4b29bffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:24 +0000

    Add package liaoyu-chuliao-haojiandan/2022.10.9.0/liaoyu-chuliao-haojiandan-2022.10.9.0.tar.gz

---
+++ packages/liaoyu-chuliao-haojiandan/liaoyu-chuliao-haojiandan-2022.10.9.0.tar.gz/liaoyu-chuliao-haojiandan-2022.10.9.0/LiaoyuChuliaoHaojiandan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""liaoyu-chuliao-haojiandan
+https://github.com/apachecn/liaoyu-chuliao-haojiandan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.9.0"
