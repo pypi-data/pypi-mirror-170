b6ebb0ad994fd0f9eb411a9f261156ae75cc8275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:02 +0000

    Add package objectprogramming/100/objectprogramming-100.tar.gz

---
+++ packages/objectprogramming/objectprogramming-100.tar.gz/objectprogramming-100/op/fnc.py
@@ -0,0 +1,74 @@
+# This file is placed in the Public Domain.
+# pylint: disable=R,C,W
+
+
+"funtion"
+
+
+import types
+
+
+from .obj import keys, register
+
+
+def __dir__():
+    return (
+            'name',
+            'format',
+           )
+
+
+
+def edit(obj, setter):
+    for key, value in items(setter):
+        register(obj, key, value)
+
+
+def format(obj, args="", skip="", plain=False):
+    res = []
+    keyz = []
+    if "," in args:
+        keyz = args.split(",")
+    if not keyz:
+        keyz = keys(obj)
+    for key in keyz:
+        if key.startswith("_"):
+            continue
+        if skip:
+            skips = skip.split(",")
+            if key in skips:
+                continue
+        value = getattr(obj, key, None)
+        if not value:
+            continue
+        if " object at " in str(value):
+            continue
+        txt = ""
+        if plain:
+            txt = str(value)
+        elif isinstance(value, str) and len(value.split()) >= 2:
+            txt = '%s="%s"' % (key, value)
+        else:
+            txt = '%s=%s' % (key, value)
+        res.append(txt)
+    txt = " ".join(res)
+    return txt.rstrip()
+
+
+def name(obj):
+    typ = type(obj)
+    if isinstance(typ, types.ModuleType):
+        return obj.__name__
+    if "__self__" in dir(obj):
+        return "%s.%s" % (obj.__self__.__class__.__name__, obj.__name__)
+    if "__class__" in dir(obj) and "__name__" in dir(obj):
+        return "%s.%s" % (obj.__class__.__name__, obj.__name__)
+    if "__class__" in dir(obj):
+        return obj.__class__.__name__
+    if "__name__" in dir(obj):
+        return obj.__name__
+    return None
+
+
+def register(obj, key, value):
+    setattr(obj, key, value)
