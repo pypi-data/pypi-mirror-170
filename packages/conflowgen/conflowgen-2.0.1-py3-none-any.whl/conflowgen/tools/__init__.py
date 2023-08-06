43aa2da1be0043ec8b6d063f664bcc017dff165a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:26 +0000

    Add package conflowgen/2.0.1/conflowgen-2.0.1-py3-none-any.whl

---
+++ packages/conflowgen/conflowgen-2.0.1-py3-none-any.whl/conflowgen/tools/__init__.py
@@ -0,0 +1,23 @@
+"""
+A collection of tools for which no nicer name has been found yet.
+"""
+from typing import Callable, Any, TypeVar
+
+DecoratedType = TypeVar('DecoratedType')  # pylint: disable=invalid-name
+
+
+def docstring_parameter(*args, **kwargs) -> Callable:
+    def decorator(func: Callable[..., DecoratedType]) -> Callable[..., DecoratedType]:
+        if not hasattr(func, "__doc__"):
+            return func
+        func.__doc__ = func.__doc__.format(*args, **kwargs)
+        return func
+    return decorator
+
+
+def hashable(obj: Any) -> bool:
+    try:
+        hash(obj)
+    except TypeError:
+        return False
+    return True
