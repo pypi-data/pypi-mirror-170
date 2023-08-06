90240bd984175e764f02ad0e1a9ff6cd1c09f06b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:34 +0000

    Add package credit-semaphore/0.0.6/credit_semaphore-0.0.6.tar.gz

---
+++ packages/credit-semaphore/credit_semaphore-0.0.6.tar.gz/credit_semaphore-0.0.6/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='credit_semaphore',
+    version='0.0.6',
+    description='Asynchronous Semaphore Based on Credits for Efficient Credit-Based API Throttling',
+    author='HangukQuant',
+    install_requires=[
+        'opencv-python'
+    ],
+    author_email='',
+    packages=setuptools.find_packages()
+)
