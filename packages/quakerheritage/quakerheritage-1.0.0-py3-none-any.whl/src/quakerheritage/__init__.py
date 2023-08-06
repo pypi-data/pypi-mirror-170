f4d69cf6f46a50d327549d628eda0b56de89c6d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:37 +0000

    Add package quakerheritage/1.0.0/quakerheritage-1.0.0-py3-none-any.whl

---
+++ packages/quakerheritage/quakerheritage-1.0.0-py3-none-any.whl/src/quakerheritage/__init__.py
@@ -0,0 +1,13 @@
+# quakerheritage/__init__.py
+"""Collects text data from online pdfs held by the Quaker Meeting House Heritage Project.
+
+Modules exported by this package:
+
+`callFunctions`: Unifies the functional modules and runs the main code to create a formatted Pandas DataFrame from pdfs held by Britain Yearly Meeting\n
+
+`getWebData`: Main web function that collects url data and extracts text from each pdf examined\n
+
+`cleanseData`: Runs Pandas functionality to merge text data into workable DataFrame and creates consistent data quality\n
+
+"""
+__version__ = "0.0.3"
