9dae579c14ea3bdf73cdcda96ae75d7a8e28d0a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:56 +0000

    Add package accentdatabase/0.0.1/accentdatabase-0.0.1-py3-none-any.whl

---
+++ packages/accentdatabase/accentdatabase-0.0.1-py3-none-any.whl/accentdatabase/__init__.py
@@ -0,0 +1,8 @@
+"""Some useful description."""
+
+__version__ = "0.0.1"
+
+from .base import Base
+from .encoders import json_serializer
+from .engine import engine
+from .session import get_session
