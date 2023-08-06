14565ebd3f55767ae5ca8f109bf752b8dd1b7a03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:04 +0000

    Add package myproj/0.1.0.post1/myproj-0.1.0-1.tar.gz

---
+++ packages/myproj/myproj-0.1.0-1.tar.gz/myproj-0.1.0-1/myproj/__main__.py
@@ -0,0 +1,18 @@
+
+def run():
+    print("Yep, I'm running!")
+    user_in = input("type stop then you want me to stopppppppp")
+    if user_in == "stop":
+        stop()
+    
+def stop():
+    print("stopped")
+    exit()
+        
+    
+    
+  
+
+
+if __name__ == '__main__':
+    run()
