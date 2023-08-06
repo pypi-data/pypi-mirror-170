8085e01327587640eb208cc98074f69eb64f7007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:22:31 +0000

    Add package mmocr/1.0.0rc1/mmocr-1.0.0rc1-py2.py3-none-any.whl

---
+++ packages/mmocr/mmocr-1.0.0rc1-py2.py3-none-any.whl/mmocr/models/textrecog/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .backbones import *  # NOQA
+from .data_preprocessors import *  # NOQA
+from .decoders import *  # NOQA
+from .encoders import *  # NOQA
+from .module_losses import *  # NOQA
+from .plugins import *  # NOQA
+from .postprocessors import *  # NOQA
+from .recognizers import *  # NOQA
