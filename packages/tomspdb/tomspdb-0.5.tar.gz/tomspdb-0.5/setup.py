0f4979ef2b04007c22a043b5651a6be3b966472e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:32 +0000

    Add package tomspdb/0.5/tomspdb-0.5.tar.gz

---
+++ packages/tomspdb/tomspdb-0.5.tar.gz/tomspdb-0.5/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="tomspdb",
+    version="0.5",
+    author="Talwrii",
+    author_email="talwrii@gmail.com",
+    url="https://github.com/talwrii/tomspdb",
+    description="",
+    license="MIT",
+    keywords="debugger",
+    modules=["tomspdb"],
+    long_description=open("readme.md").read(),
+    long_description_content_type="text/markdown",
+    entry_points={"console_scripts": ["tomspdb=tomspdb.tomspdb:main"]},
+    classifiers=[],
+)
