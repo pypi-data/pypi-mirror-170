faeed6944ce01ec4037e5b821df044e325ce9f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:14 +0000

    Add package sgnlp/0.4.0/sgnlp-0.4.0.tar.gz

---
+++ packages/sgnlp/sgnlp-0.4.0.tar.gz/sgnlp-0.4.0/README.md
@@ -0,0 +1,26 @@
+# SG-NLP
+
+Machine learning models from Singapore's natural language processing (NLP) research community.
+
+`sgnlp` is a Python package that allows you to easily get started on using various (NLP) models implemented using the
+Pytorch and Transformers frameworks.
+
+We have an accompanying [demo site](https://sgnlp.aisingapore.net/) where you can interact with our models and get a
+better understanding on how they work.
+
+## Installation
+
+* Python >= 3.8
+
+```
+pip install sgnlp
+```
+
+## Documentation
+
+Visit our [documentation](https://sgnlp.aisingapore.net/docs/) for tutorials.
+
+## License
+
+Code and models from this project are released under the MIT License unless otherwise stated. If a model's code is under
+a separate license, it can be found in the respective model's folder.
---
+++ packages/sgnlp/sgnlp-0.4.0.tar.gz/sgnlp-0.4.0/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+from os import path
+
+dir = path.abspath(path.dirname(__file__))
+with open(path.join(dir, 'README.md')) as f:
+    long_description = f.read()
+
+extras_require = {
+    "lsr": ["networkx==2.4"],
+    "lif_3way_ap": ["allennlp==0.8.4", "scikit-learn==0.22", "overrides==3.1.0"]
+}
+
+setup(
+    name="sgnlp",
+    packages=find_packages(),
+    version="0.4.0",
+    description="Machine learning models from Singapore's NLP research community",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Jonathan Heng, Raymond Ng, Zhi Chong Tan, Benedict Lee, Alon Tenzer",
+    author_email="sg-nlp@aisingapore.org",
+    keywords=["NLP", "machine learning", "deep learning"],
+    install_requires=[
+        "datasets",
+        "nltk",
+        "numpy",
+        "pandas",
+        "scikit-learn",
+        "sentencepiece",
+        "spacy",
+        "tokenizers",
+        "torch>=1.6,<2",
+        "torchtext",
+        "transformers",
+    ],
+    extras_require=extras_require,
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
