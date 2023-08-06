68b7d97c0bf8d94962bdd5efc860b82c177bac0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:03 +0000

    Add package py2exe-gui/0.1.3/py2exe_gui-0.1.3-py3-none-any.whl

---
+++ packages/py2exe-gui/py2exe_gui-0.1.3-py3-none-any.whl/py2exe_gui/Widgets/options_table.py
@@ -0,0 +1,22 @@
+import sys
+from typing import Optional
+
+from PySide6 import QtCore, QtGui, QtWidgets
+
+
+class OptionsHelpTable(QtWidgets.QTableWidget):
+    """
+    用于展示PyInstaller选项详情的表格控件
+    """
+
+    def __init__(self, parent: Optional[QtWidgets.QWidget] = None) -> None:
+        super(OptionsHelpTable, self).__init__(parent)
+
+        pass
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = OptionsHelpTable()
+    window.show()
+    app.exec()
