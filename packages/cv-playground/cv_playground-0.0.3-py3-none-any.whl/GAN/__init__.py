37ef397dfee20795fbd19e3424b7533fa40b7706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:39 +0000

    Add package cv-playground/0.0.3/cv_playground-0.0.3-py3-none-any.whl

---
+++ packages/cv-playground/cv_playground-0.0.3-py3-none-any.whl/GAN/__init__.py
@@ -0,0 +1,5 @@
+from GAN.cyclegan import train_model
+from GAN.cyclegan import composite_model
+from GAN.cyclegan import build_descriminator
+from GAN.cyclegan import build_generator
+from GAN.cyclegan import resnet
