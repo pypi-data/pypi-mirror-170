371c6c1416a3db6315e63dc5b967392243cd7815
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:02 +0000

    Add package wikistats2csv/0.1.5/wikistats2csv-0.1.5.tar.gz

---
+++ packages/wikistats2csv/wikistats2csv-0.1.5.tar.gz/wikistats2csv-0.1.5/wikistats2csv/__init__.py
@@ -0,0 +1,10 @@
+# from helper import Helper
+# from content import Content
+# from reading import Reading
+# from contributing import Contributing
+# from custom_argparse import ArgumentParser
+from .helper import Helper
+from .content import Content
+from .reading import Reading
+from .contributing import Contributing
+from .custom_argparse import ArgumentParser
