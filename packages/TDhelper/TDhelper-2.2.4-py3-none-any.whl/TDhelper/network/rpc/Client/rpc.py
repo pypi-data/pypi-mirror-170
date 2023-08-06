9a1d4b2582c5090bf39b202c9480ee4c257bdb62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:28 +0000

    Add package TDhelper/2.2.4/TDhelper-2.2.4-py3-none-any.whl

---
+++ packages/TDhelper/TDhelper-2.2.4-py3-none-any.whl/TDhelper/network/rpc/Client/rpc.py
@@ -0,0 +1,17 @@
+import logging
+class RPC:
+    def __init__(self, uris: list, token: str, server_cnf: dict = None, logger: "logging" = None,**kwargs) -> None:
+        pass
+
+    def __load__(self):
+        pass
+
+    
+    def __reload__(self):
+        pass
+
+    def __access__(self,conf):
+        pass
+
+    def __remote_call__(self,conf):
+        pass
