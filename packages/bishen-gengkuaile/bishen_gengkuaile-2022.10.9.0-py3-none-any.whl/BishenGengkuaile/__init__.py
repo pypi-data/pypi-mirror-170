6efc38caad299bb0164dadb0d837c60b9fad67b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:50 +0000

    Add package bishen-gengkuaile/2022.10.9.0/bishen_gengkuaile-2022.10.9.0-py3-none-any.whl

---
+++ packages/bishen-gengkuaile/bishen_gengkuaile-2022.10.9.0-py3-none-any.whl/BishenGengkuaile/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""bishen-gengkuaile
+https://github.com/apachecn/bishen-gengkuaile"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.9.0"
