4df0b2d616e0cb697ce8d424e5a2f54d75fc7e3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:27 +0000

    Add package ParUtils/1.0.4/ParUtils-1.0.4-py3-none-any.whl

---
+++ packages/ParUtils/ParUtils-1.0.4-py3-none-any.whl/parutils/changelog.py
@@ -0,0 +1,15 @@
+__VERSION__ = '1.0.4'
+# Improved ttry
+
+# __VERSION__ = '1.0.3'
+# # loging.close_logger()
+# # test -> testing
+
+# __VERSION__ = '1.0.2'
+# # Removed tests from packaging
+
+# __VERSION__ = '1.0.1'
+# # Removed extract_doc
+
+# __VERSION__ = '1.0.0'
+# # Init
