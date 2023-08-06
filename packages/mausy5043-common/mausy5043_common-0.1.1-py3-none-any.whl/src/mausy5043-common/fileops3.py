67b2edd5539196845dbd8ed61b227925aa7ab71b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:26 +0000

    Add package mausy5043-common/0.1.1/mausy5043_common-0.1.1-py3-none-any.whl

---
+++ packages/mausy5043-common/mausy5043_common-0.1.1-py3-none-any.whl/src/mausy5043-common/fileops3.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+"""Provide file operation functions."""
+
+import os
+import syslog
+
+
+def cat(fname):
+    """Read a file (line-by-line) into a variable.
+    Args:
+        fname (str) : file to read from
+
+    Returns:
+          (str) : file contents
+    """
+    ret = ""
+    if os.path.isfile(fname):
+        with open(fname, 'r') as fin:
+            ret = fin.read().strip('\n')
+    return ret
+
+
+def syslog_trace(trace, logerr, out2console):
+    """Log a (multi-line) message to syslog.
+    Args:
+        trace (str): Text to send to log
+        logerr (int): syslog errornumber
+        out2console (bool): If True, will also print the 'trace' to the screen
+    Returns:
+        None
+    """
+    log_lines = trace.split('\n')
+    for line in log_lines:
+        if line and logerr:
+            syslog.syslog(logerr, line)
+        if line and out2console:
+            print(line)
