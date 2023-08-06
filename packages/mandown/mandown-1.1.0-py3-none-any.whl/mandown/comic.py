4210b3693ea0bc1a7f54432d456ac13a1bcb3b6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:31 +0000

    Add package mandown/1.1.0/mandown-1.1.0-py3-none-any.whl

---
+++ packages/mandown/mandown-1.1.0-py3-none-any.whl/mandown/comic.py
@@ -0,0 +1,52 @@
+from . import sources
+from .base import BaseChapter, BaseMetadata
+from .sources.base_source import BaseSource
+
+
+class BaseComic:
+    def __init__(
+        self,
+        metadata: BaseMetadata,
+        chapters: list[BaseChapter],
+    ):
+        self.metadata = metadata
+        self.chapters = chapters
+        try:
+            self.source = sources.get_class_for(self.metadata.url)(self.metadata.url)
+        except ValueError as err:
+            if self.metadata.url == "":  # sentinel value
+                self.source = BaseSource("")
+            else:
+                raise ValueError from err
+
+    def asdict(self) -> dict:
+        return {
+            "metadata": self.metadata.asdict(),
+            "chapters": [c.asdict() for c in self.chapters],
+        }
+
+    def get_chapter_image_urls(self, chapter: BaseChapter) -> list[str]:
+        """
+        Fetch a list of image URLs of a chapter based on the
+        current source.
+        """
+        return self.source.fetch_chapter_image_list(chapter)
+
+    def set_chapter_range(
+        self, *, start: int | None = None, end: int | None = None
+    ) -> None:
+        """
+        `start` and `end` are zero-indexed.
+        """
+        self.chapters = self.chapters[start:end]
+
+    def __str__(self) -> str:
+        return f"""
+Title: {self.metadata.title},
+Author(s): {', '.join(self.metadata.authors)}
+URL: {self.metadata.url}
+Genres: {', '.join(self.metadata.genres)}
+Chapters: {len(self.chapters)}
+Description:
+    {self.metadata.description}
+""".strip()
