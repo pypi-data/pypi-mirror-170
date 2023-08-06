cb9e774e782f740762890844a4552a4e811f63d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:43 +0000

    Add package ppcmd/0.2.7/ppcmd-0.2.7.tar.gz

---
+++ packages/ppcmd/ppcmd-0.2.7.tar.gz/ppcmd-0.2.7/src/ppcmd/ppc/ppc.py
@@ -0,0 +1,29 @@
+from colorama import init
+import fire
+
+from ppcmd.ppc.ppc_processor import PpcProcessor
+
+processor = PpcProcessor()
+
+
+class PPC(object):
+    def update(self):
+        """Update ppc command."""
+        processor.update()
+
+    def test(self):
+        """Run unit test."""
+        processor.test()
+
+    def cov(self):
+        """Run coverage report."""
+        processor.coverage()
+
+    def lint(self):
+        """Run lint report."""
+        processor.lint()
+
+
+def main_():
+    init(autoreset=True)
+    fire.Fire(PPC)
