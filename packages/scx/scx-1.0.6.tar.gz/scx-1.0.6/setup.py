ce84cb02d84037de67dddbae5f781da5725ab0db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:34 +0000

    Add package scx/1.0.6/scx-1.0.6.tar.gz

---
+++ packages/scx/scx-1.0.6.tar.gz/scx-1.0.6/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+  name = 'scx',
+  packages = ['scx'],
+  version = '1.0.6',
+  license='MIT',
+  description = 'MIT Supply Chain Python Package',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  author = 'Connor Makowski',
+  author_email = 'conmak@mit.edu',
+  url = 'https://github.com/connor-makowski/scx',
+  keywords = [],
+  install_requires=["PuLP==2.6.0", "type_enforced==0.0.11"],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+  ],
+)
