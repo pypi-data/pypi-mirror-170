335b84d8cace5a4093c37aac885eef612904491c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:51 +0000

    Add package ppcmd/0.2.3/ppcmd-0.2.3.tar.gz

---
+++ packages/ppcmd/ppcmd-0.2.3.tar.gz/ppcmd-0.2.3/src/ppcmd/ppc/ppc.py
@@ -0,0 +1,27 @@
+import fire
+
+from ppcmd.ppc.ppc_processor import PpcProcessor
+
+processor = PpcProcessor()
+
+
+class PPC(object):
+    def update(self):
+        """Update ppc command."""
+        processor.update()
+
+    def test(self):
+        """Run unit test."""
+        processor.test()
+
+    def cov(self):
+        """Run coverage report."""
+        processor.coverage()
+
+    def lint(self):
+        """Run lint report."""
+        processor.lint()
+
+
+def main_():
+    fire.Fire(PPC)
---
+++ packages/ppcmd/ppcmd-0.2.3.tar.gz/ppcmd-0.2.3/src/ppcmd/ppc/ppc_processor.py
@@ -0,0 +1,16 @@
+from ppcmd.common.console import echo__
+
+
+class PpcProcessor:
+
+    def update(self):
+        echo__('update...')
+
+    def test(self):
+        echo__('test...')
+
+    def coverage(self):
+        echo__('coverage...')
+
+    def lint(self):
+        echo__('lint...')
