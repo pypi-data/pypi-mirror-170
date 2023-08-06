0d57d01c2b01cc53b70a0b386302618bbd5787b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:32 +0000

    Add package evg/2.4.1/evg-2.4.1.tar.gz

---
+++ packages/evg/evg-2.4.1.tar.gz/evg-2.4.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='evg',
+      version='2.4.1',
+      url='https://github.com/IceOne-i/evg',
+      license='MIT',
+      author='Nikita Belan',
+      author_email='nikitabelan9@gmail.com',
+      description='Dicord translator & translation of Discord functions/interactions',
+      zip_safe=False)
