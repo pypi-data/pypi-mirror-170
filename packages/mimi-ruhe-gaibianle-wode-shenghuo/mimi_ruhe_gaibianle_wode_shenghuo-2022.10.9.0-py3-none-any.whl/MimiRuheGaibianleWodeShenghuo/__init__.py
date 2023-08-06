1e2be53b8abfa92403ebcc0f93fb9b350b28d53b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:23 +0000

    Add package mimi-ruhe-gaibianle-wode-shenghuo/2022.10.9.0/mimi_ruhe_gaibianle_wode_shenghuo-2022.10.9.0-py3-none-any.whl

---
+++ packages/mimi-ruhe-gaibianle-wode-shenghuo/mimi_ruhe_gaibianle_wode_shenghuo-2022.10.9.0-py3-none-any.whl/MimiRuheGaibianleWodeShenghuo/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""mimi-ruhe-gaibianle-wode-shenghuo
+https://github.com/apachecn/mimi-ruhe-gaibianle-wode-shenghuo"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.9.0"
