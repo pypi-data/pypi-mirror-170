a872ec152399d90b445bbb09fe7adee3ff443988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:39 +0000

    Add package edc-offstudy/0.3.28/edc-offstudy-0.3.28.tar.gz

---
+++ packages/edc-offstudy/edc-offstudy-0.3.28.tar.gz/edc-offstudy-0.3.28/edc_offstudy/model_mixins/offstudy_non_crf_model_mixin.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from typing import Any
+
+from django.db import models
+
+from ..utils import raise_if_offstudy
+
+
+class OffstudyNonCrfModelError(Exception):
+    pass
+
+
+class OffstudyNonCrfModelMixin(models.Model):
+
+    """Model mixin for non-CRF, PRN, visit, appt models.
+
+    A mixin for non-CRF models to add the ability to determine
+    if the subject is off study as of this non-CRFs report_datetime.
+
+    Requires the fields `subject_identifier`, `report_datetime`.
+    """
+
+    def save(self: Any, *args, **kwargs):
+        raise_if_offstudy(
+            source_obj=self,
+            subject_identifier=self.subject_identifier,
+            report_datetime=self.report_datetime,
+        )
+        super().save(*args, **kwargs)
+
+    class Meta:
+        abstract = True
