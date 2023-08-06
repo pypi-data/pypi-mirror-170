986e0a907f9f59903fb5993ae6d905ada2c9412f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:14 +0000

    Add package mincepy/0.16.5/mincepy-0.16.5.tar.gz

---
+++ packages/mincepy/mincepy-0.16.5.tar.gz/mincepy-0.16.5/test/test_history.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import mincepy
+from mincepy import testing
+
+
+def test_db():
+    """Test using the db() free-function to get type helpers"""
+    helper = mincepy.db(testing.Car)
+    assert isinstance(helper, mincepy.TypeHelper)
+    assert helper.TYPE_ID == testing.Car.TYPE_ID
