2199cd42bf28d1696226b383a3bc7047b4515226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:36 +0000

    Add package sortmyfolder/0.0.1/sortmyfolder-0.0.1.tar.gz

---
+++ packages/sortmyfolder/sortmyfolder-0.0.1.tar.gz/sortmyfolder-0.0.1/setup.py
@@ -0,0 +1,29 @@
+import pathlib
+from setuptools import find_packages, setup
+
+
+
+
+VERSION='0.0.1'
+DESCRIPTION = 'Auto sorting tool to allow you organise any file or folder in a directory using the file extensions'
+
+
+
+setup(
+    name="sortmyfolder",
+    version=VERSION,
+    author="Emmanuel Agyapong",
+    author_email="emmanuelagyapong070@gmail.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['shutil'],
+    keywords=['python', 'automation', 'sorting'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
