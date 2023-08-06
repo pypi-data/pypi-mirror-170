deb859ffbe7dc4a7e830b871f466387558396b89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:53 +0000

    Add package odp-sdk-python-ingest/0.1.0/odp-sdk-python-ingest-0.1.0.tar.gz

---
+++ packages/odp-sdk-python-ingest/odp-sdk-python-ingest-0.1.0.tar.gz/odp-sdk-python-ingest-0.1.0/odp/compute/cli/__main__.py
@@ -0,0 +1,23 @@
+import click
+from dotenv import load_dotenv
+
+from .authenticate import authenticate as _authenticate
+
+from .deploy import deploy as _deploy
+from .parameters import parameters as _parameters
+from .run import run as _run
+
+
+@click.group()
+def cli():
+    pass
+
+
+cli.add_command(_authenticate)
+cli.add_command(_deploy)
+cli.add_command(_parameters)
+cli.add_command(_run)
+
+if __name__ == "__main__":
+    load_dotenv()
+    cli()
