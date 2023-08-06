6092f40315fbcc860330ebb0673dc9b62f25f887
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:11 +0000

    Add package shooker/1.0.4/shooker-1.0.4.tar.gz

---
+++ packages/shooker/shooker-1.0.4.tar.gz/shooker-1.0.4/src/shooker/internal/asm.py
@@ -0,0 +1,92 @@
+import capstone
+
+from lief.ELF import ARCH
+
+from shooker.internal.other import *
+
+
+class Assembler:
+    """Arch-depended stuff"""
+
+    def __init__(self, target: LiefBin, arch: str, mode: str) -> None:
+        self.bin = target
+        self.arch = self.bin.header.machine_type
+        cs_arch, cs_mode = getattr(capstone, arch), getattr(capstone, mode)
+        self.md = capstone.Cs(cs_arch, cs_mode)
+
+    def jump(self, target_addr: int) -> bytes:
+        """Create jmp-instruction to target_addr"""
+        if self.arch == ARCH.x86_64:
+            target_addr -= 5  # length of jmp instr
+            return b"\xE9" + target_addr.to_bytes(4, "little")
+        else:
+            raise Unimplemented(f"Assemble jump for {arch}")
+
+    def brute_ptl(self, section: LiefSect, target_addr: int) -> int:
+        """Find plt-stub for specific function from .got.plt"""
+        cont = section.content.tobytes()
+        plt_offs = section.virtual_address
+
+        if self.arch == ARCH.x86_64:
+            for instr in self.md.disasm(cont, plt_offs):
+                if instr.mnemonic != "jmp" or "word ptr [rip" not in instr.op_str:
+                    continue
+
+                instr_offs = int.from_bytes(instr.bytes[2:], "little")
+
+                # offset from instruction + current entry plt position + len of jmp instruction
+                if instr_offs + instr.address + instr.size == target_addr:
+                    return instr.address
+        else:
+            raise Unimplemented(f"Enumerating plt for {self.arch}")
+
+        raise NotFound("Include function not found")
+
+    def patch_sub_values(self, func_cont: list, func_offs: int) -> list:
+        """Patch values that substracted from rip for relative-call instruction"""
+
+        cont = bytearray(b"".join([i.to_bytes(1, "big") for i in func_cont]))
+        register = None
+        lea_ip = None
+        sub_instr = None
+        call_ip = 0
+
+        if self.arch == ARCH.x86_64:
+            for instr in self.md.disasm(cont, 0):
+                # print(instr)
+                if instr.mnemonic == "lea" and "[rip - 7]" in instr.op_str:
+                    lea_ip = instr.address
+                    register = instr.op_str.replace(", [rip - 7]", "")
+
+                if (
+                    instr.mnemonic == "sub"
+                    and register is not None
+                    and instr.op_str.startswith(register)
+                ):
+                    sub_instr = instr
+
+                if (
+                    instr.mnemonic == "call"
+                    and sub_instr is not None
+                    and 2 <= instr.size <= 3
+                ):
+                    call_ip = instr.address
+                    break
+
+            if lea_ip == 0:
+                raise NotFound(f"Get IP instruction")
+            elif sub_instr is None:
+                raise NotFound(f"Sub {register}, #const")
+            elif call_ip == 0:
+                raise NotFound(f"Register-jump instruction")
+
+            sub_value = int.from_bytes(sub_instr.bytes[2:], "little")
+            relative_offs = func_offs + lea_ip - sub_value
+            sub_bytes = sub_instr.bytes[:2] + relative_offs.to_bytes(4, "little")
+
+            cont[sub_instr.address : sub_instr.address + sub_instr.size] = sub_bytes
+
+        else:
+            raise Unimplemented(f"Patch sub-instruction for {arch}")
+
+        return [i for i in cont]
---
+++ packages/shooker/shooker-1.0.4.tar.gz/shooker-1.0.4/src/shooker/internal/cmpl.py
@@ -0,0 +1,108 @@
+from lief import parse
+from subprocess import run
+
+from shooker.internal.other import *
+
+
+class Compile:
+    """Translate "patch" from xml to asm"""
+
+    def __init__(self, cc: str, path: Path) -> None:
+        self.cc = cc
+        self.code = ""
+        self.path = path
+        self.inc_libs = []
+        self.inc_fncs = []
+        self.whitelist_funcs = []
+
+    def include_lib(self, inc: str) -> None:
+        if not inc:
+            raise NotFound("Include lib")
+
+        self.inc_libs.append(inc)
+
+    def include_func(self, name: str, proto: str, addr: int) -> None:
+        if not name or not proto or not addr:
+            raise NotFound("Name/proto/addr for include func")
+
+        # use Labels as Values https://gcc.gnu.org/onlinedocs/gcc/Labels-as-Values.html
+        decl = f"#define {name} {name}_jmp: (({proto.replace('FUNC', '(*)')})((long)&&{name}_jmp-{addr}))"
+        self.inc_fncs.append(decl)
+
+    def assemble_transl(self) -> None:
+        """Some asm-tricks"""
+        # stuff like #include
+        self.code += "\n".join(self.inc_libs) + "\n"
+
+        # asm-trick to avoid broken xref for string from another segment
+        self.code += "#define _s(string) ((char *)(const char []){string})\n"
+
+        # func declaration stuff
+        self.code += "\n".join(self.inc_fncs) + "\n"
+
+    def add_func_to_transl(self, fnc_name: str, fnc_proto: str, fnc_code: str) -> None:
+        """Assemble hook-function"""
+        if not fnc_name or not fnc_proto or not fnc_code:
+            raise NotFound(f"Name/code/proto to add func's definition")
+
+        self.code += "\n" + fnc_proto.replace("FUNC", fnc_name) + "{" + fnc_code + "}"
+
+        # to avoid extract smth like __do_global_dtors_aux or frame_dummy
+        self.whitelist_funcs.append(fnc_name)
+
+    def compile_transl(self, txt_addr: int) -> FuncsInfo:
+        """Compile patch(s)"""
+
+        (self.path / "translation.c").write_text(self.code)
+
+        # uncomment to look translation.c
+        # __import__("IPython").embed()
+
+        cmd = [
+            self.cc,
+            "-fPIC",
+            "--no-builtin",
+            "-c",
+            "-o",
+            self.path / "translation.o",
+            self.path / "translation.c",
+        ]
+
+        if run(cmd).returncode:
+            raise CompileFail(cmd)
+
+        cmd = [
+            self.cc,
+            "-shared",
+            self.path / "translation.o",
+            "-o",
+            self.path / "libtranslation.so",
+            f"-Wl,--section-start=.text={txt_addr}",
+        ]
+        if run(cmd).returncode:
+            raise CompileFail(cmd)
+
+        funcs_info = {}
+
+        trs_bin = parse((self.path / "libtranslation.so").name)
+        txt = trs_bin.get_section(".text")
+        start = txt.virtual_address
+        end = start + txt.size
+        offset = 0
+
+        # extract bytes of each function
+        for sym in trs_bin.static_symbols:
+            if (
+                start <= sym.value
+                and sym.value < end
+                and sym.name in self.whitelist_funcs
+            ):
+                content = trs_bin.get_content_from_virtual_address(sym.value, sym.size)
+                funcs_info[sym.name] = {"content": content, "offset": offset}
+                offset += len(content)
+
+        (self.path / "translation.c").unlink()
+        (self.path / "translation.o").unlink()
+        (self.path / "libtranslation.so").unlink()
+
+        return funcs_info
