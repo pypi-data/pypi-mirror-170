0cb041f8d33586bad15fc35017d4291f00f348fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:03 +0000

    Add package williamarsecond/0.0.1/williamarsecond-0.0.1.tar.gz

---
+++ packages/williamarsecond/williamarsecond-0.0.1.tar.gz/williamarsecond-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A basic hello package sdaasdasdasdsad'
+
+# Setting up
+setup(
+    name="williamarsecond",
+    version=VERSION,
+    author="William Arsenault",
+    author_email="idk@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
