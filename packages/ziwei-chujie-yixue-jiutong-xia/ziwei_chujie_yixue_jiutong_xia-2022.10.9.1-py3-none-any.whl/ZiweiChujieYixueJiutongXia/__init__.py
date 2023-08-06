cade8cdebcd2cbabddccc1e7f4a0f33d71894fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:21:15 +0000

    Add package ziwei-chujie-yixue-jiutong-xia/2022.10.9.1/ziwei_chujie_yixue_jiutong_xia-2022.10.9.1-py3-none-any.whl

---
+++ packages/ziwei-chujie-yixue-jiutong-xia/ziwei_chujie_yixue_jiutong_xia-2022.10.9.1-py3-none-any.whl/ZiweiChujieYixueJiutongXia/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ziwei-chujie-yixue-jiutong-xia
+https://github.com/apachecn/ziwei-chujie-yixue-jiutong-xia"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.9.1"
