d9dbe6765ccc4f43acc5175c4bf72ab7a346963b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:21:28 +0000

    Add package saisi-zaoqike-1to4/2022.10.9.1/saisi-zaoqike-1to4-2022.10.9.1.tar.gz

---
+++ packages/saisi-zaoqike-1to4/saisi-zaoqike-1to4-2022.10.9.1.tar.gz/saisi-zaoqike-1to4-2022.10.9.1/SaisiZaoqike1to4/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""saisi-zaoqike-1to4
+https://github.com/apachecn/saisi-zaoqike-1to4"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.9.1"
