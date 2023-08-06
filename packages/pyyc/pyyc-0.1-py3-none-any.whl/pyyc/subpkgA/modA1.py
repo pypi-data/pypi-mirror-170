8f346ccd4fa5950deacc35cdc815f82f82249c1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:22 +0000

    Add package pyyc/0.1/pyyc-0.1-py3-none-any.whl

---
+++ packages/pyyc/pyyc-0.1-py3-none-any.whl/pyyc/subpkgA/modA1.py
@@ -0,0 +1,6 @@
+"""
+Documentation du module modA1.
+"""
+
+version = 'A1'
+print("Initialisation ", version)  # À ÉVITER dans un vrai module!
---
+++ packages/pyyc/pyyc-0.1-py3-none-any.whl/pyyc/subpkgA/modA2.py
@@ -0,0 +1,6 @@
+"""
+Documentation du module subpkgA.modA2.
+"""
+
+version = 'A2'                  #: subpkgA.modA2.version
+print("Initialisation ", version)
