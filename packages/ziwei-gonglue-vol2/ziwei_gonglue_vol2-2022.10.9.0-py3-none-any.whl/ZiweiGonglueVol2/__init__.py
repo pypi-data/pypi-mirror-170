52999c16025e5beb772b27c9ee643550354f3a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:18:06 +0000

    Add package ziwei-gonglue-vol2/2022.10.9.0/ziwei_gonglue_vol2-2022.10.9.0-py3-none-any.whl

---
+++ packages/ziwei-gonglue-vol2/ziwei_gonglue_vol2-2022.10.9.0-py3-none-any.whl/ZiweiGonglueVol2/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ziwei-gonglue-vol2
+https://github.com/apachecn/ziwei-gonglue-vol2"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.9.0"
