1d1342c79664b72c1e923175362c9f613da023f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:07 +0000

    Add package darbiadev-shipping/0.5.0/darbiadev-shipping-0.5.0.tar.gz

---
+++ packages/darbiadev-shipping/darbiadev-shipping-0.5.0.tar.gz/darbiadev-shipping-0.5.0/src/darbiadev_shipping/__init__.py
@@ -0,0 +1,5 @@
+"""
+A package wrapping multiple shipping carrier API wrapping packages, providing a higher level multi carrier package.
+"""
+
+from .shipping_services import CarrierEnum, ShippingServices
