57eda3f41ebed78f1997f5968ce94e9c1a953c87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:58 +0000

    Add package chuangzao-shengmingde-qiji/2022.10.6.0/chuangzao_shengmingde_qiji-2022.10.6.0-py3-none-any.whl

---
+++ packages/chuangzao-shengmingde-qiji/chuangzao_shengmingde_qiji-2022.10.6.0-py3-none-any.whl/ChuangzaoShengmingdeQiji/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""chuangzao-shengmingde-qiji
+https://github.com/apachecn/chuangzao-shengmingde-qiji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.6.0"
