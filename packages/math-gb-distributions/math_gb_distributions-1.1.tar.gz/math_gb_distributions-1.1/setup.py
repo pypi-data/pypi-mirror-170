31829fd8d345fdf54e08950c81175ed4208241e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:55 +0000

    Add package math-gb-distributions/1.1/math_gb_distributions-1.1.tar.gz

---
+++ packages/math-gb-distributions/math_gb_distributions-1.1.tar.gz/math_gb_distributions-1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='math_gb_distributions',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['math_gb_distributions'],
+      author='Javad Farshchi',
+      author_email='mjavad.farshchi@gmail.com',
+      long_description="""# Gaussian and Binomial distributions!\n\n* a Python library for helping you with calculation, plotting, and summation of Gaussian and Binomial Distributions.\n""",
+      long_description_content_type='text/markdown',
+      zip_safe=False)
