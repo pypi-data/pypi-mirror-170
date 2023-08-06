f253c3b9992b228fa4d9f6f96fbd62019bdf33f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:59 +0000

    Add package tinyscript/1.26.11/tinyscript-1.26.11.tar.gz

---
+++ packages/tinyscript/tinyscript-1.26.11.tar.gz/tinyscript-1.26.11/tinyscript/__init__.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""Initialization of Tinyscript package.
+
+"""
+from warnings import filterwarnings
+filterwarnings("ignore", "Setuptools is replacing distutils.")
+
+
+from .__info__ import __author__, __copyright__, __license__, __version__
+
+from .deprecation import *
+from .features import *
+from .helpers import *
+from .parser import *
+from .preimports import *
+
+from .deprecation import __features__ as _deprecation
+from .features import __features__ as _features
+from .helpers import __features__ as _helpers
+from .parser import __features__ as _parser
+from .preimports import __features__ as _preimports
+
+
+ts.__author__    = __author__
+ts.__copyright__ = __copyright__
+ts.__license__   = __license__
+ts.__version__   = __version__
+
+
+__all__ = _deprecation + _features + _helpers + _parser + _preimports
+
