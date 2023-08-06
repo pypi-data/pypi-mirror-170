93262836258997edf9dc141fce1c20fd82ad02e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:41 +0000

    Add package ppmi-downloader/0.5/ppmi_downloader-0.5.tar.gz

---
+++ packages/ppmi-downloader/ppmi_downloader-0.5.tar.gz/ppmi_downloader-0.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+DEPS = [ 'webdriver_manager', 'selenium']
+
+setup(
+    name='ppmi_downloader',
+    version='0.5',
+    description='A downloader of PPMI files.',
+    author='Tristan Glatard',
+    author_email='tristan.glatard@concordia.ca',
+    license='MIT',
+    packages=['ppmi_downloader'],
+    setup_requires=DEPS,
+    install_requires=DEPS,
+)
