671e2ca5f7d37af55c413cb30b76079648388e49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:12 +0000

    Add package pierl/1.10/pierl-1.10.tar.gz

---
+++ packages/pierl/pierl-1.10.tar.gz/pierl-1.10/pierl/__init__.py
@@ -0,0 +1,6 @@
+from pierl import agents
+from pierl import environments
+from pierl import network
+from pierl import tools
+
+__all__ = ['A2C', 'DDQN', 'DQN', 'DQN_RS', 'DQN_TN', 'REINFORCE', 'easy', 'trumps']
