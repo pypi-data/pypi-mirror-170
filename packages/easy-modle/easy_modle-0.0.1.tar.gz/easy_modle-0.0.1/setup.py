49a1cd15f016493b7f9b10ec42a32c09a7e85cbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:21 +0000

    Add package easy-modle/0.0.1/easy_modle-0.0.1.tar.gz

---
+++ packages/easy-modle/easy_modle-0.0.1.tar.gz/easy_modle-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+setuptools.setup(
+    name="easy_modle",
+    version="1.0",
+    author="MC_no_circle",
+    author_email="595969887@qq.com",
+    description="Easy ues Python modle",
+    long_description="easy_modle",
+    long_description_content_type="easy_modle",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
