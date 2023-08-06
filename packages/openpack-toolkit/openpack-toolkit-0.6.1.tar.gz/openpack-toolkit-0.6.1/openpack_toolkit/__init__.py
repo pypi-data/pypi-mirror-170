6c07924629d12abe9ab19d4e227acd3e0e1b23d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:35 +0000

    Add package openpack-toolkit/0.6.1/openpack-toolkit-0.6.1.tar.gz

---
+++ packages/openpack-toolkit/openpack-toolkit-0.6.1.tar.gz/openpack-toolkit-0.6.1/openpack_toolkit/__init__.py
@@ -0,0 +1,22 @@
+"""
+.. include:: ../README.md
+"""
+
+__version__ = '0.6.1'
+from . import codalab, configs, data, download, utils
+from .activity import ActClass, ActSet
+from .configs.datasets.annotations import OPENPACK_OPERATIONS
+
+DATASET_VERSION = "v0.3.0"
+
+__all__ = [
+    "ActClass",
+    "ActSet",
+    "codalab",
+    "configs",
+    "data",
+    "download",
+    "utils",
+    "DATASET_VERSION",
+    "OPENPACK_OPERATIONS",
+]
