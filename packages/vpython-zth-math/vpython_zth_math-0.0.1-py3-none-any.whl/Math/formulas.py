e0409f8a75da207cc12c30f855f784d6ab9df5e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:03 +0000

    Add package vpython-zth-math/0.0.1/vpython_zth_math-0.0.1-py3-none-any.whl

---
+++ packages/vpython-zth-math/vpython_zth_math-0.0.1-py3-none-any.whl/Math/formulas.py
@@ -0,0 +1,20 @@
+class Hitung():
+    def jumlah(self, x, y):
+        jumlah = x + y
+        return jumlah
+
+    def kurang(self, x, y):
+        kurang = x - y
+        return kurang
+
+    def kali(self, x, y):
+        kali = x * y
+        return kali
+
+    def bagi(self, x, y):
+        bagi = x / y
+        return bagi
+
+def hitung_luas_lingkaran(r):
+    luas = 3.14 * r * r
+    return luas
