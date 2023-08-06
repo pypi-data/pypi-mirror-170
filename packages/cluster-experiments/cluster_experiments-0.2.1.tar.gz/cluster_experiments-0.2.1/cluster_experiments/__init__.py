6833504321d3bcb65c196e14ab01c58c1ba76455
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:59 +0000

    Add package cluster-experiments/0.2.1/cluster_experiments-0.2.1.tar.gz

---
+++ packages/cluster-experiments/cluster_experiments-0.2.1.tar.gz/cluster_experiments-0.2.1/cluster_experiments/__init__.py
@@ -0,0 +1,38 @@
+from cluster_experiments.cupac import EmptyRegressor, TargetAggregation
+from cluster_experiments.experiment_analysis import (
+    ExperimentAnalysis,
+    GeeExperimentAnalysis,
+    OLSAnalysis,
+)
+from cluster_experiments.perturbator import (
+    BinaryPerturbator,
+    Perturbator,
+    UniformPerturbator,
+)
+from cluster_experiments.power_analysis import PowerAnalysis
+from cluster_experiments.power_config import PowerConfig
+from cluster_experiments.random_splitter import (
+    BalancedClusteredSplitter,
+    ClusteredSplitter,
+    NonClusteredSplitter,
+    RandomSplitter,
+    StratifiedClusteredSplitter,
+)
+
+__all__ = [
+    "ExperimentAnalysis",
+    "GeeExperimentAnalysis",
+    "OLSAnalysis",
+    "BinaryPerturbator",
+    "Perturbator",
+    "UniformPerturbator",
+    "PowerAnalysis",
+    "PowerConfig",
+    "EmptyRegressor",
+    "TargetAggregation",
+    "BalancedClusteredSplitter",
+    "ClusteredSplitter",
+    "RandomSplitter",
+    "NonClusteredSplitter",
+    "StratifiedClusteredSplitter",
+]
