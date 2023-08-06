a7dca6d929faba39fb24bcc65ddd21424c732eac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:25 +0000

    Add package playgroundtools/1.0.1/playgroundtools-1.0.1.tar.gz

---
+++ packages/playgroundtools/playgroundtools-1.0.1.tar.gz/playgroundtools-1.0.1/playgroundtools/playground.py
@@ -0,0 +1,83 @@
+import json
+
+from .exceptions import (
+    PGConfigNotFoundError,
+    PGDoesNotExistError,
+    PGInvalidConfError,
+    PGInvalidSettingError,
+    PGJSONFormatError,
+    PGSettingsNotFoundError,
+    PGTypeNotFoundError,
+)
+from .resources import load_text_resources
+from .util import get_playground_dir
+
+
+def get_config():
+    """Get the configuration for the package."""
+    try:
+        config_json = load_text_resources("config.json")
+        return json.loads(config_json)
+    except FileNotFoundError:
+        raise PGConfigNotFoundError
+    except json.JSONDecodeError as err:
+        raise PGJSONFormatError("config.json", str(err))
+
+
+def get_settings(playground_dir):
+    """Retrieve the settings for a given playground."""
+    settings_path = playground_dir / "settings.json"
+    try:
+        with open(settings_path) as f:
+            return json.load(f)
+    except json.JSONDecodeError as err:
+        raise PGJSONFormatError(settings_path, str(err))
+    except FileNotFoundError:
+        raise PGSettingsNotFoundError(playground_dir)
+
+
+def clean_config(args, raw_config={}):
+    """Returns config options in a more usable format."""
+    playground_dir = get_playground_dir(args)
+    config = {"dir": playground_dir}
+    if args.command == "new":
+        try:
+            type_config = raw_config[args.type]
+        except KeyError:
+            raise PGTypeNotFoundError(args.type)
+        lib = type_config["lib"] + args.lib
+        try:
+            config.update(
+                {
+                    "folders": type_config["folders"] + ["requirements"],
+                    "files": {
+                        **type_config["files"],
+                        "requirements/requirements.in": lib,
+                    },
+                    "lib": lib,
+                    "settings": {
+                        "module": type_config["module"],
+                        "args": type_config["args"],
+                    },
+                }
+            )
+        except KeyError as err:
+            raise PGInvalidConfError(err.args, args.type)
+    else:
+        if not playground_dir.exists():
+            raise PGDoesNotExistError(playground_dir)
+        if args.command == "run":
+            settings = get_settings(playground_dir)
+            try:
+                config.update(
+                    {
+                        "settings": {
+                            "python": settings["python"],
+                            "module": settings["module"],
+                            "args": settings["args"],
+                        }
+                    }
+                )
+            except KeyError as err:
+                raise PGInvalidSettingError(err.args)
+    return config
---
+++ packages/playgroundtools/playgroundtools-1.0.1.tar.gz/playgroundtools-1.0.1/tests/test_playground.py
@@ -0,0 +1,169 @@
+from argparse import Namespace
+from pathlib import Path
+
+import pytest
+
+from ..playgroundtools import playground
+
+
+@pytest.fixture
+def raw_config():
+    """Represents part of the config.json file."""
+    return {
+        "console": {
+            "folders": [],
+            "files": {"main.py": ["print('Hello, World!')"]},
+            "lib": [],
+            "module": "main",
+            "args": [],
+        },
+        "jupyter": {
+            "folders": [],
+            "files": {
+                "dataprep.ipynb": [
+                    "{",
+                    ' "cells": [',
+                    "  {",
+                    '   "cell_type": "code",',
+                    '   "execution_count": null,',
+                    '   "metadata": {},',
+                    '   "outputs": [],',
+                    '   "source": []',
+                    "  }",
+                    " ],",
+                    ' "metadata": {},',
+                    ' "nbformat": 4,',
+                    ' "nbformat_minor": 1',
+                    "}",
+                ],
+                "analysis.ipynb": [
+                    "{",
+                    ' "cells": [',
+                    "  {",
+                    '   "cell_type": "code",',
+                    '   "execution_count": null,',
+                    '   "metadata": {},',
+                    '   "outputs": [],',
+                    '   "source": []',
+                    "  }",
+                    " ],",
+                    ' "metadata": {},',
+                    ' "nbformat": 4,',
+                    ' "nbformat_minor": 1',
+                    "}",
+                ],
+            },
+            "lib": [
+                "jupyter",
+                "jupyterlab",
+                "numpy",
+                "pandas",
+                "matplotlib",
+                "faker",
+                "arrow",
+            ],
+            "module": "jupyter",
+            "args": ["notebook", "analysis.ipynb"],
+        },
+    }
+
+
+class TestPlayground:
+    """Tests functions in the playground module."""
+
+    def test_get_config(self, raw_config):
+        retrieved_config = playground.get_config()
+        modified_config = {
+            "console": retrieved_config["console"],
+            "jupyter": retrieved_config["jupyter"],
+        }
+        assert modified_config == raw_config
+
+    @pytest.mark.parametrize(
+        ["args", "clean_config"],
+        [
+            (
+                Namespace(command="new", name="test", type="console", lib=[]),
+                {
+                    "dir": Path("test").resolve(),
+                    "folders": ["requirements"],
+                    "files": {
+                        "main.py": ["print('Hello, World!')"],
+                        "requirements/requirements.in": [],
+                    },
+                    "lib": [],
+                    "settings": {"module": "main", "args": []},
+                },
+            ),
+            (
+                Namespace(
+                    command="new",
+                    name="test/subfolder",
+                    type="jupyter",
+                    lib=[],
+                ),
+                {
+                    "dir": Path("test/subfolder").resolve(),
+                    "folders": ["requirements"],
+                    "files": {
+                        "dataprep.ipynb": [
+                            "{",
+                            ' "cells": [',
+                            "  {",
+                            '   "cell_type": "code",',
+                            '   "execution_count": null,',
+                            '   "metadata": {},',
+                            '   "outputs": [],',
+                            '   "source": []',
+                            "  }",
+                            " ],",
+                            ' "metadata": {},',
+                            ' "nbformat": 4,',
+                            ' "nbformat_minor": 1',
+                            "}",
+                        ],
+                        "analysis.ipynb": [
+                            "{",
+                            ' "cells": [',
+                            "  {",
+                            '   "cell_type": "code",',
+                            '   "execution_count": null,',
+                            '   "metadata": {},',
+                            '   "outputs": [],',
+                            '   "source": []',
+                            "  }",
+                            " ],",
+                            ' "metadata": {},',
+                            ' "nbformat": 4,',
+                            ' "nbformat_minor": 1',
+                            "}",
+                        ],
+                        "requirements/requirements.in": [
+                            "jupyter",
+                            "jupyterlab",
+                            "numpy",
+                            "pandas",
+                            "matplotlib",
+                            "faker",
+                            "arrow",
+                        ],
+                    },
+                    "lib": [
+                        "jupyter",
+                        "jupyterlab",
+                        "numpy",
+                        "pandas",
+                        "matplotlib",
+                        "faker",
+                        "arrow",
+                    ],
+                    "settings": {
+                        "module": "jupyter",
+                        "args": ["notebook", "analysis.ipynb"],
+                    },
+                },
+            ),
+        ],
+    )
+    def test_clean_config(self, args, clean_config, raw_config):
+        assert playground.clean_config(args, raw_config) == clean_config
