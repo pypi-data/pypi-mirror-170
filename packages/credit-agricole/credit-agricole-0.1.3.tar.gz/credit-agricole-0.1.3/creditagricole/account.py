4cddec930d05c4ad641c34aee3144740087f6dae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:59 +0000

    Add package credit-agricole/0.1.3/credit-agricole-0.1.3.tar.gz

---
+++ packages/credit-agricole/credit-agricole-0.1.3.tar.gz/credit-agricole-0.1.3/creditagricole/account.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+from datetime import date
+from dataclasses import dataclass, field
+
+from typing import List
+
+@dataclass(frozen=True)
+class Transaction:
+    id: str
+    label: str
+    type_label: str
+    description: str
+    date: date
+    amount: float
+
+@dataclass(frozen=True)
+class Account:
+    id: str
+    index: int = field(repr=False)
+    owner: str
+    balance: float
+    currency: str
+    label: str
+    transactions: List[Transaction] = field(repr=False)
