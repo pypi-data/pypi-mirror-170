13e1e9e793d2e3ebddb87533b3dab65821b3eec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:54 +0000

    Add package fengye1966-distributions/0.1/fengye1966_distributions-0.1.tar.gz

---
+++ packages/fengye1966-distributions/fengye1966_distributions-0.1.tar.gz/fengye1966_distributions-0.1/setup.py
@@ -0,0 +1,18 @@
+'''
+@copyright Copyright (c) 2022: Trunk Tech
+Descripttion: 
+Author: Bin Feng
+version: 
+Date: 2022-10-04 12:49:46
+LastEditors: Bin Feng
+LastEditTime: 2022-10-04 13:07:09
+'''
+from setuptools import setup
+
+setup(name='fengye1966_distributions',
+      version='0.1',
+      description='Gaussian distributions by fengye1966',
+      packages=['fengye1966_distributions'],
+      author='fengye1966',
+      author_email='fengye1966@163.com',
+      zip_safe=False)
