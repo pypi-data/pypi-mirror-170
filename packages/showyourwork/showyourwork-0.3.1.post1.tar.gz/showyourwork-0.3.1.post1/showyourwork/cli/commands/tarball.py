c1fb2b10d596f09bd8bd841a61b0ab53684ccd74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:47 +0000

    Add package showyourwork/0.3.1.post1/showyourwork-0.3.1.post1.tar.gz

---
+++ packages/showyourwork/showyourwork-0.3.1.post1.tar.gz/showyourwork-0.3.1.post1/showyourwork/cli/commands/tarball.py
@@ -0,0 +1,19 @@
+import os
+from pathlib import Path
+
+from ... import paths
+from ..conda_env import run_in_env
+
+
+def tarball(options=""):
+    """Build the article tarball.
+
+    Args:
+        options (str, optional): Additional options to pass to Snakemake.
+    """
+    snakefile = snakefile = Path("${SYW_PATH}") / "workflow" / "build.smk"
+    snakemake = f"SNAKEMAKE_OUTPUT_CACHE={paths.user().cache} SNAKEMAKE_RUN_TYPE='tarball' snakemake -c1 --use-conda --reason --cache"
+    command = f"{snakemake} {options} -s {snakefile} syw__arxiv_entrypoint"
+    result = run_in_env(command, check=False)
+    if result.returncode > 0:
+        os._exit(1)
