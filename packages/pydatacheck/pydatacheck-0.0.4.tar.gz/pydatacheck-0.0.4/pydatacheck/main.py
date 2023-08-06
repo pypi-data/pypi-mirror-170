5def2f71020d78c80d4acc33ce9f93e345111faa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:22 +0000

    Add package pydatacheck/0.0.4/pydatacheck-0.0.4.tar.gz

---
+++ packages/pydatacheck/pydatacheck-0.0.4.tar.gz/pydatacheck-0.0.4/pydatacheck/main.py
@@ -0,0 +1,32 @@
+import pylogconf.core
+from pytconf import register_endpoint, register_main, config_arg_parse_and_launch
+
+from pydatacheck.static import APP_NAME, VERSION_STR
+
+
+@register_endpoint(
+    description="check videos",
+)
+def check_videos() -> None:
+    print("tbd")
+
+
+@register_endpoint(
+    description="check books",
+)
+def check_books() -> None:
+    print("tbd")
+
+
+@register_main(
+    main_description="pydatacheck will check your yaml files for you",
+    app_name=APP_NAME,
+    version=VERSION_STR,
+)
+def main():
+    pylogconf.core.setup()
+    config_arg_parse_and_launch()
+
+
+if __name__ == '__main__':
+    main()
