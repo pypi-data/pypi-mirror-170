8daa6d10274987226f256fa6f7a9da4cd5c0a558
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:26 +0000

    Add package nnabla/1.31.0/nnabla-1.31.0-cp38-cp38-win_amd64.whl

---
+++ packages/nnabla/nnabla-1.31.0-cp38-cp38-win_amd64.whl/nnabla/backward_function/randint.py
@@ -0,0 +1,26 @@
+# Copyright 2019,2020,2021 Sony Corporation.
+# Copyright 2021 Sony Group Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+def randint_backward(inputs, low=0, high=1, shape=[], seed=-1):
+    """
+    Args:
+      inputs (list of nn.Variable): Incomming grads/inputs to/of the forward function.
+      kwargs (dict of arguments): Dictionary of the corresponding function arguments.
+
+    Return:
+      list of Variable: Return the gradients wrt inputs of the corresponding function.
+    """
+    return [None] * len(inputs)
