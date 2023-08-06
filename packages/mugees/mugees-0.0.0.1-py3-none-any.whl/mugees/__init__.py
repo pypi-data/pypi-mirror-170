318e8b5e88ef7c2bd3d343a609fee8889a5196c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:35 +0000

    Add package mugees/0.0.0.1/mugees-0.0.0.1-py3-none-any.whl

---
+++ packages/mugees/mugees-0.0.0.1-py3-none-any.whl/mugees/__init__.py
@@ -0,0 +1,17 @@
+from mugees import (
+    Depression,
+    multilingual_depression_xlm_r,
+    depression_albert,
+    depression_bert,
+    unbiased_albert,
+    unbiased_depression_roberta,
+)
+
+__all__ = [
+    "Depression",
+    "depression_bert",
+    "depression_albert",
+    "unbiased_depression_roberta",
+    "unbiased_albert",
+    "multilingual_depression_xlm_r",
+]
