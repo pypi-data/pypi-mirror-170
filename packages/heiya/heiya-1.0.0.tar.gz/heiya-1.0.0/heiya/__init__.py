e8b2db1542b6f672fccc763f9f8d6e51ff0d6b7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:04 +0000

    Add package heiya/1.0.0/heiya-1.0.0.tar.gz

---
+++ packages/heiya/heiya-1.0.0.tar.gz/heiya-1.0.0/heiya/__init__.py
@@ -0,0 +1,13 @@
+from . import to_hei
+from . import from_hei
+from . import he_img
+from . import tools
+from . import extensions
+
+__all__ = [
+    'to_hei',
+    'from_hei',
+    'he_img',
+    'tools',
+    'extensions'
+]
