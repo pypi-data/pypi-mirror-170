9701a00660c0ee6bd97428d3b60267b5285e5a3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:17 +0000

    Add package nnabla/1.31.0/nnabla-1.31.0-cp38-cp38-macosx_10_15_x86_64.whl

---
+++ packages/nnabla/nnabla-1.31.0-cp38-cp38-macosx_10_15_x86_64.whl/nnabla/backward_function/log_softmax.py
@@ -0,0 +1,36 @@
+# Copyright 2019,2020,2021 Sony Corporation.
+# Copyright 2021 Sony Group Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import nnabla.functions as F
+from .utils import positive_axis, get_output
+
+
+def log_softmax_backward(inputs, axis=None):
+    """
+    Args:
+      inputs (list of nn.Variable): Incomming grads/inputs to/of the forward function.
+      kwargs (dict of arguments): Dictionary of the corresponding function arguments.
+
+    Return:
+      list of Variable: Return the gradients wrt inputs of the corresponding function.
+    """
+    dy = inputs[0]
+    x0 = inputs[1]
+    y0 = get_output(x0, "LogSoftmax")
+    D = len(x0.shape)
+    axis = positive_axis(axis, D)
+    dx0 = dy - F.exp(y0) * F.sum(dy, axis=axis, keepdims=True)
+    return dx0
