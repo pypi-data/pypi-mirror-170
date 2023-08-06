236193c5ab805da6a922fca81cfa6c0ab8a66100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:35 +0000

    Add package librapid/0.5.8/librapid-0.5.8-cp38-cp38-win_amd64.whl

---
+++ packages/librapid/librapid-0.5.8-cp38-cp38-win_amd64.whl/librapid/modified/modified.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+// I DO NOT TAKE CREDIT FOR THIS CODE -- I SIMPLY MODIFIED IT
+
+#include "inplace/reducedMath.hpp"
+#include "inplace/index.hpp"
+#include "inplace/transpose.hpp"
+#include "float16/float16.hpp"
