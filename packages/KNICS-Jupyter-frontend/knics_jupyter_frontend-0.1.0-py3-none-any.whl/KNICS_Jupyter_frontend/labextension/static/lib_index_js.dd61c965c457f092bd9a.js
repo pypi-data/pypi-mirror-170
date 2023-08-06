3e4972ca16f6fe4a0a91f257acf7d08fde489a93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:38 +0000

    Add package KNICS-Jupyter-frontend/0.1.0/knics_jupyter_frontend-0.1.0-py3-none-any.whl

---
+++ packages/KNICS-Jupyter-frontend/knics_jupyter_frontend-0.1.0-py3-none-any.whl/KNICS_Jupyter_frontend/labextension/static/lib_index_js.dd61c965c457f092bd9a.js
@@ -0,0 +1,30 @@
+"use strict";
+(self["webpackChunkKNICS_Jupyter_frontend"] = self["webpackChunkKNICS_Jupyter_frontend"] || []).push([["lib_index_js"],{
+
+/***/ "./lib/index.js":
+/*!**********************!*\
+  !*** ./lib/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/**
+ * Initialization data for the KNICS_Jupyter_frontend extension.
+ */
+const plugin = {
+    id: 'KNICS_Jupyter_frontend:plugin',
+    autoStart: true,
+    activate: (app) => {
+        console.log('JupyterLab extension KNICS_Jupyter_frontend is activated!');
+    }
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (plugin);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=lib_index_js.dd61c965c457f092bd9a.js.map
