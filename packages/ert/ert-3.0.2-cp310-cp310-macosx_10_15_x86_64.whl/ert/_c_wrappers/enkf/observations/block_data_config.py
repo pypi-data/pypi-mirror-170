678a6eaec919a75c422047f68b264a2339e1b350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:31 +0000

    Add package ert/3.0.2/ert-3.0.2-cp310-cp310-macosx_10_15_x86_64.whl

---
+++ packages/ert/ert-3.0.2-cp310-cp310-macosx_10_15_x86_64.whl/ert/_c_wrappers/enkf/observations/block_data_config.py
@@ -0,0 +1,43 @@
+#  Copyright (C) 2016  Equinor ASA, Norway.
+#
+#  This file is part of ERT - Ensemble based Reservoir Tool.
+#
+#  ERT is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  ERT is distributed in the hope that it will be useful, but WITHOUT ANY
+#  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#  FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+#  for more details.
+import ctypes
+
+from cwrap import BaseCClass
+
+from ert._c_wrappers.enkf.config import FieldConfig
+
+
+class BlockDataConfig(BaseCClass):
+    TYPE_NAME = "block_data_config"
+
+    def __init__(self):
+        raise NotImplementedError("Cannot instantiate BlockDataConfig!")
+
+    @classmethod
+    def from_param(cls, c_class_object):
+        if c_class_object is None:
+            return ctypes.c_void_p()
+        elif isinstance(c_class_object, FieldConfig):
+            return FieldConfig.from_param(c_class_object)
+
+        # The Container class which is used to support summary based
+        # source in the BLOCK_OBS configuration is not yet supported
+        # in Python.
+
+        # elif isinstance(instance , ContainerConfig):
+        #    return ContainerConfig.from_param( instance )
+        else:
+            raise ValueError("Currently ONLY field data is supported")
