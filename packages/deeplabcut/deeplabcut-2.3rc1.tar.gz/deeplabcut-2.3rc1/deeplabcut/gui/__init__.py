10e21ab71f5c04837c2d016cfc5b3e589f2815ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:23 +0000

    Add package deeplabcut/2.3rc1/deeplabcut-2.3rc1.tar.gz

---
+++ packages/deeplabcut/deeplabcut-2.3rc1.tar.gz/deeplabcut-2.3rc1/deeplabcut/gui/__init__.py
@@ -0,0 +1,14 @@
+"""
+DeepLabCut2.0 Toolbox (deeplabcut.org)
+© A. & M. Mathis Labs
+https://github.com/DeepLabCut/DeepLabCut
+Please see AUTHORS for contributors.
+
+https://github.com/DeepLabCut/DeepLabCut/blob/master/AUTHORS
+Licensed under GNU Lesser General Public License v3.0
+
+"""
+import os
+
+
+BASE_DIR = os.path.dirname(__file__)
