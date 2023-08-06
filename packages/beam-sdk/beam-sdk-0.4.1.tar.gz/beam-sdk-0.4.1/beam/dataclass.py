3b0b0122ba99be9be095f2bea0b21ea4bb5710bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:04 +0000

    Add package beam-sdk/0.4.1/beam-sdk-0.4.1.tar.gz

---
+++ packages/beam-sdk/beam-sdk-0.4.1.tar.gz/beam-sdk-0.4.1/beam/dataclass.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass
+from typing import Dict, List
+
+from beam.base import BaseDataClass
+from beam.types import OutputType, PythonVersion, Types
+
+
+@dataclass()
+class AppConfiguration(BaseDataClass):
+    name: str
+    cpu: str
+    gpu: int
+    memory: str
+    apt_install: PythonVersion
+    python_version: List[str]
+    python_packages: List[str]
+    workspace: str
+
+
+@dataclass
+class WebhookConfiguration(BaseDataClass):
+    inputs: Dict[str, Types]
+    handler: str
+    trigger_type: str = "webhook"
+
+
+@dataclass
+class CronJobConfiguration(BaseDataClass):
+    inputs: Dict[str, Types]
+    cron_schedule: str
+    handler: str
+    trigger_type: str = "cron_job"
+
+
+@dataclass
+class RestAPIConfiguration(BaseDataClass):
+    inputs: Dict[str, Types]
+    outputs: Dict[str, Types]
+    handler: str
+    trigger_type: str = "rest_api"
+
+
+@dataclass
+class FileConfiguration(BaseDataClass):
+    path: str
+    name: str
+    output_type: OutputType
