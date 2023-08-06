ef598a5e4ec50e3471d5a8020494338631b664e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:16:18 +0000

    Add package xinlingde-benzhi/2022.10.9.0/xinlingde_benzhi-2022.10.9.0-py3-none-any.whl

---
+++ packages/xinlingde-benzhi/xinlingde_benzhi-2022.10.9.0-py3-none-any.whl/XinlingdeBenzhi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xinlingde-benzhi
+https://github.com/apachecn/xinlingde-benzhi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.9.0"
