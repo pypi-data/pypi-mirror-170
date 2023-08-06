7529d69fa2b5fc980b9ac672390003ca4fd62d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:49 +0000

    Add package amulet-core/1.9.8/amulet-core-1.9.8.tar.gz

---
+++ packages/amulet-core/amulet-core-1.9.8.tar.gz/amulet-core-1.9.8/amulet/__pyinstaller/hook-amulet.py
@@ -0,0 +1,20 @@
+import pkgutil
+import sys
+import os
+
+import amulet
+from amulet import level
+from amulet import operations
+
+AMULET_PATH = amulet.__path__[0]
+
+hiddenimports = (
+    [name for _, name, _ in pkgutil.walk_packages(level.__path__, level.__name__ + ".")]
+    + [
+        name
+        for _, name, _ in pkgutil.walk_packages(
+            operations.__path__, operations.__name__ + "."
+        )
+    ]
+    + ["amulet.api.structure"]
+)
