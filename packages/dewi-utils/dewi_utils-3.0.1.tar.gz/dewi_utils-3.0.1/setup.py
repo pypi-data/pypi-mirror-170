e56cbf06c3484fd56954a9da04a2b8092fcccef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:28 +0000

    Add package dewi-utils/3.0.1/dewi_utils-3.0.1.tar.gz

---
+++ packages/dewi-utils/dewi_utils-3.0.1.tar.gz/dewi_utils-3.0.1/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+#
+# DEWI utils: Small utilities
+# Copyright 2012-2021  Laszlo Attila Toth
+# Distributed under the terms of the Apache License, Version 2.0
+
+import sys
+
+if sys.hexversion < 0x03080000:
+    raise RuntimeError("Required python version: 3.8 or newer (current: %s)" % sys.version)
+
+try:
+    from setuptools import setup, find_packages
+
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="dewi_utils",
+    description="A toolchain and framework for everyday tasks",
+    license="Apache License, Version 2.0",
+    license_files=('COPYING',),
+    version="3.0.1",
+    author="Laszlo Attila Toth",
+    author_email="python-dewi@laszloattilatoth.me",
+    maintainer="Laszlo Attila Toth",
+    maintainer_email="python-dewi@laszloattilatoth.me",
+    keywords='tool framework development synchronization',
+    url="https://github.com/LA-Toth/dewi_utils",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: System :: Filesystems',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+    ],
+    zip_safe=True,
+    use_2to3=False,
+    python_requires='>=3.8',
+    packages=find_packages(exclude=['pylintcheckers', '*test*']),
+    install_requires=[
+        'dewi_core >=5.0.2, <6',
+        'Jinja2',
+    ]
+)
