cbda4164b0724f64eb89a37ade146e2d5ffed592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:34 +0000

    Add package dnalignment/0.0.6/dnalignment-0.0.6.tar.gz

---
+++ packages/dnalignment/dnalignment-0.0.6.tar.gz/dnalignment-0.0.6/README.md
@@ -0,0 +1,3 @@
+# seq-alignment
+
+run test - use pytest
---
+++ packages/dnalignment/dnalignment-0.0.6.tar.gz/dnalignment-0.0.6/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.6'
+DESCRIPTION = 'dna sequence alignment'
+LONG_DESCRIPTION = ''
+
+# Setting up
+setup(
+    name="dnalignment",
+    version=VERSION,
+    author="Eden Nagar",
+		license="GNU",
+    author_email="<edenagar@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'dna', 'sequence', 'alignment'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
