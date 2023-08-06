905111486a151e499133a4aa04023049352e97be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:17:13 +0000

    Add package pynir/0.2/pynir-0.2.tar.gz

---
+++ packages/pynir/pynir-0.2.tar.gz/pynir-0.2/src/pynir/CalibrationTransfer.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 28 11:00:35 2022
+
+@author: chinn
+"""
+
---
+++ packages/pynir/pynir-0.2.tar.gz/pynir-0.2/src/pynir/OutlierDection.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 28 11:00:35 2022
+
+@author: chinn
+"""
+
+class data():
+    
+    
