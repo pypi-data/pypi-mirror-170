d4d2ef808bb66080804186b78748058ef2ba8188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:46 +0000

    Add package brizuelapdf/1.1.0/brizuelapdf-1.1.0-py3-none-any.whl

---
+++ packages/brizuelapdf/brizuelapdf-1.1.0-py3-none-any.whl/brizuelapdf/pdftotext.py
@@ -0,0 +1,20 @@
+'''
+This module provides functions to convert PDF files to text files.
+'''
+
+
+def convert(path):
+    '''
+    Converts a PDF file to a text file.
+
+    Parameters
+    ----------
+    path : str
+        Path to the PDF file.
+
+    Returns
+    -------
+    str
+        Path to the text file.
+    '''
+    print(f"Converting {path} to text...")
