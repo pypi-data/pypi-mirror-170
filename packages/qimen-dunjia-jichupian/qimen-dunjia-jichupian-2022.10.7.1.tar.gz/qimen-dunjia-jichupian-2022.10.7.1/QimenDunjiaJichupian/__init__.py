1b0bbee9ce0c5ad752477653e650e22aa9d92af6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:49 +0000

    Add package qimen-dunjia-jichupian/2022.10.7.1/qimen-dunjia-jichupian-2022.10.7.1.tar.gz

---
+++ packages/qimen-dunjia-jichupian/qimen-dunjia-jichupian-2022.10.7.1.tar.gz/qimen-dunjia-jichupian-2022.10.7.1/QimenDunjiaJichupian/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qimen-dunjia-jichupian
+https://github.com/apachecn/qimen-dunjia-jichupian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.7.1"
