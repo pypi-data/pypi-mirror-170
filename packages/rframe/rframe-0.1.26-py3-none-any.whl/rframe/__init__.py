e230c8b8d99f11a2a4ecee365ec76f5996ef25eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:32 +0000

    Add package rframe/0.1.26/rframe-0.1.26-py3-none-any.whl

---
+++ packages/rframe/rframe-0.1.26-py3-none-any.whl/rframe/__init__.py
@@ -0,0 +1,23 @@
+"""Top-level package for rframe."""
+
+__author__ = """Yossi Mosbacher"""
+__email__ = "joe.mosbacher@gmail.com"
+__version__ = "0.1.26"
+
+from loguru import logger
+
+from . import schema, indexes, utils, dispatchers
+
+from .indexes import Index, InterpolatingIndex, IntervalIndex
+from .types import Interval, IntegerInterval, TimeInterval
+from .rframe import RemoteFrame
+from .schema import BaseSchema
+from .rest_client import BaseRestClient, RestClient
+from .utils import jsonable
+
+try:
+    from .rest_server import SchemaRouter
+except ImportError:
+    pass
+
+logger.disable("rframe")
