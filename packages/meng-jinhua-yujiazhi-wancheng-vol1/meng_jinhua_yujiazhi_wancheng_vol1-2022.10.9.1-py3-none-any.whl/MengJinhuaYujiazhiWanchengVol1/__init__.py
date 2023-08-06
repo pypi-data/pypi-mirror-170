e3ef9dcc1f57bf00071a2552c6f2a9681ce0b8e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:18:42 +0000

    Add package meng-jinhua-yujiazhi-wancheng-vol1/2022.10.9.1/meng_jinhua_yujiazhi_wancheng_vol1-2022.10.9.1-py3-none-any.whl

---
+++ packages/meng-jinhua-yujiazhi-wancheng-vol1/meng_jinhua_yujiazhi_wancheng_vol1-2022.10.9.1-py3-none-any.whl/MengJinhuaYujiazhiWanchengVol1/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""meng-jinhua-yujiazhi-wancheng-vol1
+https://github.com/apachecn/meng-jinhua-yujiazhi-wancheng-vol1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.9.1"
