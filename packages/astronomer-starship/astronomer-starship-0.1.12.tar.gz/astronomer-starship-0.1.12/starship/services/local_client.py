c9f4e80a07f379293324817df2605cf6f5e24ad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:50 +0000

    Add package astronomer-starship/0.1.12/astronomer-starship-0.1.12.tar.gz

---
+++ packages/astronomer-starship/astronomer-starship-0.1.12.tar.gz/astronomer-starship-0.1.12/starship/services/local_client.py
@@ -0,0 +1,25 @@
+from airflow.utils.session import provide_session
+from sqlalchemy.orm import Session
+
+
+class LocalAirflowClient:
+    @provide_session
+    def get_variables_from_metastore(self, session: Session):
+        from airflow.models import Variable
+
+        variables = session.query(Variable).all()
+        return variables
+
+    @provide_session
+    def get_connections(self, session: Session):
+        from airflow.models import Connection
+
+        connections = session.query(Connection).all()
+        return connections
+
+    @provide_session
+    def get_variables(self, session: Session):
+        from airflow.models import Variable
+
+        vars = session.query(Variable).all()
+        return vars
