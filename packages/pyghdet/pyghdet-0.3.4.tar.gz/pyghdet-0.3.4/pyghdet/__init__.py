5572f813680acbc0d81eb94d0bcf6fcf04a2596e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:25 +0000

    Add package pyghdet/0.3.4/pyghdet-0.3.4.tar.gz

---
+++ packages/pyghdet/pyghdet-0.3.4.tar.gz/pyghdet-0.3.4/pyghdet/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.3.4"
+__author__  = "Rejuan Haque and Laura Kubatko"
+
+from pyghdet.pytorn import cct
+from pyghdet.pytorn import cmc
+from pyghdet.pytorn import mcm
+from pyghdet.pytorn import spcomb
+from pyghdet.pytorn import comb_indiv
+from pyghdet.pytorn import comb_species
