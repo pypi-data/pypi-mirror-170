efbf149fd64f22ce7182ffa726b60f8a2cfc77d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:56 +0000

    Add package liquidData/0.1.0/liquidData-0.1.0-py3-none-any.whl

---
+++ packages/liquidData/liquidData-0.1.0-py3-none-any.whl/liquidData/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for liquidData."""
+
+__author__ = """Alexander Kast"""
+__email__ = 'alexander.kast@mailbox.org'
+__version__ = '0.1.0'
+
+import liquidData
+import liquidData.dataFrames
