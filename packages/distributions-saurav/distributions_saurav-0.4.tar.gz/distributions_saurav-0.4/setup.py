a4f12c564aff34cb89e0212d8d266af8bcbfcb24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:05 +0000

    Add package distributions-saurav/0.4/distributions_saurav-0.4.tar.gz

---
+++ packages/distributions-saurav/distributions_saurav-0.4.tar.gz/distributions_saurav-0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = 'distributions_saurav',
+    version = '0.4',
+    description = 'Gaussian and Binomial Distribution',
+    packages = ['distributions_saurav'],
+    author = 'Saurav Panchal',
+    author_email = 'saurav.panchal.08@gmail.com',
+    zip_safe = False
+)
