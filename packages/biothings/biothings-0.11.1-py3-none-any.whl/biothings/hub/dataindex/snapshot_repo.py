509c84d0f5947f31c101e599053226d745c5df19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:32 +0000

    Add package biothings/0.11.1/biothings-0.11.1-py3-none-any.whl

---
+++ packages/biothings/biothings-0.11.1-py3-none-any.whl/biothings/hub/dataindex/snapshot_repo.py
@@ -0,0 +1,50 @@
+
+import elasticsearch
+
+
+class Repository():
+
+    def __init__(self, client, repository):
+        # Corresponds to HTTP operations on
+        # /_snapshot/<repository>
+
+        self.client = client
+        self.name = repository
+
+    def exists(self):
+        try:
+            self.client.snapshot.get_repository(self.name)
+        except elasticsearch.exceptions.NotFoundError:
+            return False
+        return True
+
+    def create(self, **body):
+        # https://www.elastic.co/guide/en/elasticsearch/plugins/current/repository-s3-client.html
+        return self.client.snapshot.create_repository(self.name, body=body)
+
+    def delete(self):
+        self.client.snapshot.delete_repository(self.name)
+
+    def __str__(self):
+        return (
+            f"<Repository {'READY' if self.exists() else 'MISSING'}"
+            f" name='{self.name}'"
+            f" client={self.client}"
+            f">"
+        )
+
+def test_01():
+    from elasticsearch import Elasticsearch
+    client = Elasticsearch()
+    snapshot = Repository(client, "mynews")
+    print(snapshot)
+
+def test_02():
+    from elasticsearch import Elasticsearch
+    client = Elasticsearch()
+    snapshot = Repository(client, "______")
+    print(snapshot)
+
+
+if __name__ == "__main__":
+    test_01()
---
+++ packages/biothings/biothings-0.11.1-py3-none-any.whl/biothings/web/settings/validators.py
@@ -0,0 +1,57 @@
+
+class WebAPIValidator():
+
+    def validate(self, config):
+
+        # Compatibility
+
+        if getattr(config, "API_PREFIX", None) is not None:
+            config.APP_PREFIX = getattr(config, "API_PREFIX")
+        if getattr(config, "API_VERSION", None) is not None:
+            config.APP_VERSION = getattr(config, "API_VERSION")
+
+        assert config.APP_VERSION or config.APP_PREFIX, (
+            "Require at least one of the follwing settings:"
+            "(APP_VERSION, APP_PREFIX) to create a layer of"
+            "separation for the default biothings routes."
+        )
+
+
+class DBParamValidator():
+
+    def validate(self, config):
+
+        assert isinstance(config.ES_INDICES, dict)
+
+        # compatibility settings
+        if getattr(config, "ES_INDEX", None) and \
+           getattr(config, "ES_DOC_TYPE", None):
+            from biothings.web.settings import default
+            if config.ES_INDICES is default.ES_INDICES:
+                config.ES_INDICES = {}
+            else:  # combine with the user provided value
+                config.ES_INDICES = dict(config.ES_INDICES)
+            config.ES_INDICES[config.ES_DOC_TYPE] = config.ES_INDEX
+
+        ERROR = "UNSUPPORTED SETTINGS."
+        # encountering the following attributes indicate
+        # the application is built for a previous version.
+        # to ensure delivering intended behaviors,
+        # upgrade the config module of the application.
+        assert not hasattr(config, 'ES_SNIFF'), ERROR
+        assert not hasattr(config, 'ES_CLIENT_TIMEOUT'), ERROR
+
+class SubmoduleValidator():
+
+    def __init__(self):
+        self._prefixes = set()
+
+    def validate(self, config):
+        assert config.APP_PREFIX, "APP_PREFIX must be defined for a submodule."
+        assert config.APP_PREFIX not in self._prefixes, "APP_PREFIX conflicts."
+        self._prefixes.add(config.APP_PREFIX)
+
+class MongoParamValidaor():
+
+    def validate(self, config):
+        pass
