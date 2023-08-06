175ae2f3401ae2ee86780441f9e685525afbd477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:17 +0000

    Add package forcetable/0.0.4/forcetable-0.0.4.tar.gz

---
+++ packages/forcetable/forcetable-0.0.4.tar.gz/forcetable-0.0.4/forcetable/__init__.py
@@ -0,0 +1,22 @@
+from forcetable._record import record
+from forcetable._field import field
+from forcetable._field import file_field
+
+from forcetable._table import table
+from forcetable._table import records_table
+from forcetable._table import primary_table
+
+from forcetable._table import records_to_dicts
+from forcetable._table import records_to_fields
+from forcetable._table import records_to_table
+
+from forcetable._table import dicts_to_records
+from forcetable._table import extract_record_primary_item
+
+from forcetable._table import json_to_table
+from forcetable._table import csv_to_table_fp
+from forcetable._table import csv_to_table
+
+
+__name__ = "forcetable"
+___version__ = "0.0.2"
