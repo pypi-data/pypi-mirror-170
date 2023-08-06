3e0b6c33f0f01351b435a99645ba0138423e8bad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:44:46 +0000

    Add package nonebot-adapter-kook/0.0.8/nonebot_adapter_kook-0.0.8-py3-none-any.whl

---
+++ packages/nonebot-adapter-kook/nonebot_adapter_kook-0.0.8-py3-none-any.whl/nonebot/adapters/kook/bot.py
@@ -0,0 +1,348 @@
+import re
+from os import PathLike
+from typing import Any, Union, TYPE_CHECKING, BinaryIO, Dict, Optional, Literal, Callable, Tuple, Sequence
+
+from nonebot.adapters import Bot as BaseBot
+from nonebot.message import handle_event
+from nonebot.typing import overrides
+
+from .event import Event, MessageEvent
+from .message import Message, MessageSegment, MessageSerializer
+from .utils import log
+
+if TYPE_CHECKING:
+    from .event import Event
+    from .adapter import Adapter
+    from .message import Message, MessageSegment
+
+
+def _check_at_me(bot: "Bot", event: MessageEvent):
+    """
+    :说明:
+
+      检查消息开头或结尾是否存在 @机器人，去除并赋值 ``event.to_me``
+
+    :参数:
+
+      * ``bot: Bot``: Bot 对象
+      * ``event: Event``: Event 对象
+    """
+    if not isinstance(event, MessageEvent):
+        return
+
+    # ensure message not empty
+    # if not event.message:
+    #     event.message.append(MessageSegment.text(""))
+
+    if event.message_type == "private":
+        event.to_me = True
+    else:
+        if event.message[0].type == "kmarkdown" and bot.self_id in event.extra.mention:
+            event.to_me = True
+            met_str = f"(met){bot.self_id}(met)"
+            raw_met_str = f"@{bot.self_name}"
+
+            content: str = event.message[0].data["content"].strip()
+            raw_content: str = event.message[0].data["raw_content"].strip()
+            if content.startswith(met_str):
+                content = content[len(met_str):].lstrip()
+                raw_content = raw_content[len(raw_met_str):].lstrip()
+            elif content.endswith(met_str):
+                content = content[:-len(met_str)].rstrip()
+                raw_content = raw_content[:-len(raw_met_str)].rstrip()
+            event.message[0].data["content"] = content
+            event.message[0].data["raw_content"] = raw_content
+
+
+def _check_nickname(bot: "Bot", event: MessageEvent):
+    """
+    :说明:
+
+      检查消息开头是否存在昵称，去除并赋值 ``event.to_me``
+
+    :参数:
+
+      * ``bot: Bot``: Bot 对象
+      * ``event: Event``: Event 对象
+    """
+    first_msg_seg = event.message[0]
+    if first_msg_seg.type != "text":
+        return
+
+    first_text = first_msg_seg.data["content"]
+
+    nicknames = set(filter(lambda n: n, bot.config.nickname))
+    if nicknames:
+        # check if the user is calling me with my nickname
+        nickname_regex = "|".join(nicknames)
+        m = re.search(rf"^({nickname_regex})([\s,，]*|$)", first_text, re.IGNORECASE)
+        if m:
+            nickname = m.group(1)
+            log("DEBUG", f"User is calling me {nickname}")
+            event.to_me = True
+            first_msg_seg.data["content"] = first_text[m.end():]
+
+
+async def send(
+        bot: "Bot",
+        event: Event,
+        message: Union[str, Message, MessageSegment],
+        reply_sender: bool = False,
+        is_temp_msg: bool = False,
+        **kwargs: Any,
+) -> Any:
+    # 构造参数
+    params = {**kwargs}
+
+    # quote
+    if reply_sender:
+        params.setdefault("quote", getattr(event, "message_id"))
+
+    # message_type
+    if event.channel_type == 'GROUP':
+        params.setdefault("message_type", "channel")
+
+        # temp_target_id
+        if is_temp_msg:
+            params.setdefault("user_id", getattr(event, "user_id"))
+
+        # target_id
+        if getattr(event, "target_id", None):
+            params.setdefault("channel_id", getattr(event, "target_id"))
+    else:
+        params.setdefault("message_type", "private")
+
+        # target_id
+        if getattr(event, "target_id", None):
+            params.setdefault("user_id", getattr(event, "user_id"))
+
+    params.setdefault("message", message)
+
+    return await bot.send_msg(**params)
+
+
+class Bot(BaseBot):
+    """
+    Kook Bot 适配。
+    """
+
+    send_handler: Callable[
+        ["Bot", Event, Union[str, Message, MessageSegment], bool, bool], Any
+    ] = send
+
+    def __init__(self, adapter: "Adapter", self_id: str, name: str, token: str):
+        """
+        :参数:
+
+          * ``adapter: Adapter``: 适配器
+          * ``self_id: str``: 机器人 ID
+          * ``name: str``: 机器人用户名
+          * ``token``: 机器人 token
+
+        """
+        super().__init__(adapter, self_id)
+        self.self_name: str = name
+        self.token: str = token
+
+    @overrides(BaseBot)
+    async def call_api(self, api: str, **data) -> Any:
+        """
+        :说明:
+
+          调用 kook 协议 API
+
+        :参数:
+
+          * ``api: str``: API 名称
+          * ``**data: Any``: API 参数
+
+        :返回:
+
+          - ``Any``: API 调用返回数据
+
+        :异常:
+
+          - ``NetworkError``: 网络错误
+          - ``ActionFailed``: API 调用失败
+        """
+        return await super().call_api(api, **data)
+
+    async def handle_event(self, event: Event) -> None:
+        if isinstance(event, MessageEvent):
+            _check_at_me(self, event)
+            _check_nickname(self, event)
+            pass
+
+        await handle_event(self, event)
+
+    @overrides(BaseBot)
+    async def send(
+            self,
+            event: Event,
+            message: Union[str, Message, MessageSegment],
+            reply_sender: bool = False,
+            is_temp_msg: bool = False,
+            **kwargs,
+    ) -> Any:
+        """
+        :说明:
+
+          根据 ``event``  向触发事件的主体发送消息。
+
+        :参数:
+
+          * ``event: Event``: Event 对象
+          * ``message: Union[str, Message, MessageSegment]``: 要发送的消息
+          * ``reply_sender: bool``: 是否回复原消息
+          * ``is_temp_msg: bool``: 是否临时消息
+          * ``**kwargs``: 覆盖默认参数
+
+        :返回:
+
+          - ``Any``: API 调用返回数据
+
+        :异常:
+
+          - ``ValueError``: 缺少 ``user_id``, ``channel_id``
+          - ``NetworkError``: 网络错误
+          - ``ActionFailed``: API 调用失败
+        """
+        return await self.__class__.send_handler(self, event, message, reply_sender, is_temp_msg, **kwargs)
+
+    async def send_private_msg(
+            self,
+            *,
+            user_id: str,
+            message: Union[str, Message, MessageSegment],
+            quote: Optional[str] = None
+    ) -> Dict[str, Any]:
+        """发送私聊消息。
+
+            user_id: 对方用户ID
+            message: 要发送的内容，字符串类型将作为纯文本消息发送
+            quote: 回复某条消息的消息ID
+        """
+        return await self.send_msg(message_type="private",
+                                   user_id=user_id,
+                                   message=message,
+                                   quote=quote)
+
+    async def send_channel_msg(
+            self,
+            *,
+            channel_id: str,
+            message: Union[str, Message, MessageSegment],
+            quote: Optional[str] = None
+    ) -> Dict[str, Any]:
+        """发送频道消息。
+
+            channel_id: 频道ID
+            message: 要发送的内容，字符串类型将作为纯文本消息发送
+            quote: 回复某条消息的消息ID
+        """
+        return await self.send_msg(message_type="channel",
+                                   channel_id=channel_id,
+                                   message=message,
+                                   quote=quote)
+
+    async def send_temp_msg(
+            self,
+            *,
+            user_id: str,
+            channel_id: str,
+            message: Union[str, Message, MessageSegment],
+            quote: Optional[str] = None
+    ) -> Dict[str, Any]:
+        """发送频道临时消息。该消息不会存数据库，但是会在频道内只给该用户推送临时消息。用于在频道内针对用户的操作进行单独的回应通知等。
+
+            channel_id: 频道ID
+            message: 要发送的内容，字符串类型将作为纯文本消息发送
+            quote: 回复某条消息的消息ID
+        """
+        return await self.send_msg(message_type="temp",
+                                   user_id=user_id,
+                                   channel_id=channel_id,
+                                   message=message,
+                                   quote=quote)
+
+    async def send_msg(
+            self,
+            *,
+            message_type: Literal['private', 'channel', 'temp', ''] = '',
+            user_id: Optional[str] = None,
+            channel_id: Optional[str] = None,
+            message: Union[str, Message, MessageSegment],
+            quote: Optional[str] = None
+    ) -> Dict[str, Any]:
+        """发送消息。
+
+        参数:
+            message_type: 消息类型，支持 `private`、`channel`、`temp`，分别对应私聊、频道，如不传入，则根据传入的 `*_id` 参数判断
+            user_id: 对方用户ID（消息类型为 `private`、`temp` 时需要）
+            channel_id: 频道ID（消息类型为 `channel`、`temp` 时需要）
+            message: 要发送的内容，字符串类型将作为纯文本消息发送
+            quote: 回复某条消息的消息ID
+        """
+        # 接口文档：
+        # https://developer.kookapp.cn/doc/http/direct-message#%E5%8F%91%E9%80%81%E7%A7%81%E4%BF%A1%E8%81%8A%E5%A4%A9%E6%B6%88%E6%81%AF
+        # https://developer.kookapp.cn/doc/http/message#%E5%8F%91%E9%80%81%E9%A2%91%E9%81%93%E8%81%8A%E5%A4%A9%E6%B6%88%E6%81%AF
+        params = {}
+
+        # type & content
+        if isinstance(message, Message):
+            params["type"], params["content"] = await MessageSerializer(message).serialize()
+        elif isinstance(message, MessageSegment):
+            params["type"], params["content"] = await MessageSerializer(Message(message)).serialize()
+        else:
+            params["type"], params["content"] = 1, message
+
+        # quote
+        if quote:
+            params["quote"] = quote
+
+        # target_id & api
+        if message_type == "channel":
+            params["target_id"] = channel_id
+            api = "message/create"
+        elif message_type == "private":
+            params["target_id"] = user_id
+            api = "direct-message/create"
+        elif message_type == "temp":
+            params["target_id"] = channel_id
+            params["temp_target_id"] = user_id
+            api = "message/create"
+        else:
+            if channel_id and not user_id:
+                params["target_id"] = channel_id
+                api = "message/create"
+            elif not channel_id and user_id:
+                params["target_id"] = user_id
+                api = "direct-message/create"
+            elif channel_id and user_id:
+                params["target_id"] = channel_id
+                params["temp_target_id"] = user_id
+                api = "message/create"
+            else:
+                raise ValueError(f"channel_id 和 user_id 不能同时为 None")
+
+        return await self.call_api(api, **params)
+
+    async def upload_file(self, file: Union[str, PathLike[str], BinaryIO, bytes,
+                                            Tuple[str, Union[str, PathLike[str], BinaryIO, bytes], str]],
+                          filename: Optional[str] = None) -> str:
+        """
+        上传文件。
+
+        参数:
+            file: 文件，可以是文件路径（str, PathLike[str]）、打开的文件流（BinaryIO）、或二进制数据（bytes）
+            filename: 文件名
+
+        返回值:
+            文件的 URL
+        """
+        # 旧版本API是直接把三元组传参到file，这里处理兼容性
+        # 经过测试，服务器会用从文件读取到的mime覆盖掉我们传过去的mime
+        if not isinstance(file, Sequence) or len(file) != 3:
+            file = (filename or "upload-file", file, "application/octet-stream")
+        result = await self.call_api("asset/create", file=file)
+        return result.get("url")
---
+++ packages/nonebot-adapter-kook/nonebot_adapter_kook-0.0.8-py3-none-any.whl/nonebot/adapters/kook/message.py
@@ -0,0 +1,278 @@
+from dataclasses import dataclass
+from typing import Any, Type, Tuple, Union, Mapping, Iterable, Dict, cast
+
+from deprecated import deprecated
+from nonebot.adapters import Message as BaseMessage
+from nonebot.adapters import MessageSegment as BaseMessageSegment
+from nonebot.typing import overrides
+
+from .exception import UnsupportedMessageType, UnsupportedMessageOperation
+from .utils import unescape_kmarkdown
+
+msg_type_map = {
+    "text": 1,
+    "image": 2,
+    "video": 3,
+    "file": 4,
+    "audio": 8,
+    "kmarkdown": 9,
+    "card": 10,
+}
+
+rev_msg_type_map = {}
+for msg_type, code in msg_type_map.items():
+    rev_msg_type_map[code] = msg_type
+
+# 根据协议消息段类型显示消息段内容
+segment_text = {
+    "text": "[文字]",
+    "image": "[图片]",
+    "video": "[视频]",
+    "file": "[文件]",
+    "audio": "[音频]",
+    "kmarkdown": "[KMarkdown消息]",
+    "card": "[卡片消息]",
+}
+
+
+class MessageSegment(BaseMessageSegment["Message"]):
+    """
+    Kook 协议 MessageSegment 适配。具体方法参考协议消息段类型或源码。
+
+    https://developer.kookapp.cn/doc/event/message
+    """
+
+    # 已知：
+    # command/shell_command使用message_seg = Message[0]; str(message_seg) if message_seg.is_text()
+    # startswith/endswith/keyword使用event.get_plaintext()
+    # regex使用str(Message)
+
+    @classmethod
+    @overrides(BaseMessageSegment)
+    def get_message_class(cls) -> Type["Message"]:
+        return Message
+
+    def __str__(self) -> str:
+        if self.type in ["text", "kmarkdown"]:
+            return str(self.data["content"])
+        elif self.type == "at":
+            return str(f"@{self.data['user_name']}")
+        else:
+            return segment_text.get(msg_type_map.get(self.type, ""), "[未知类型消息]")
+
+    @property
+    def plain_text(self):
+        if self.type == "text":
+            return self.data["content"]
+        elif self.type == "kmarkdown":
+            return self.data["raw_content"]
+        else:
+            return ""
+
+    @overrides(BaseMessageSegment)
+    def __add__(self, other: Union[str, "MessageSegment", Iterable["MessageSegment"]]) -> "Message":
+        return Message(self.conduct(other))
+
+    @overrides(BaseMessageSegment)
+    def __radd__(self, other: Union[str, "MessageSegment", Iterable["MessageSegment"]]) -> "Message":
+        if isinstance(other, str):
+            other = MessageSegment(self.type, {"content": other})
+        return Message(other.conduct(self))
+
+    def _conduct_single(self, other: "MessageSegment") -> "MessageSegment":
+        if self.type == "text":
+            return MessageSegment("text", {"content": self.data["content"] + other.data["content"]})
+        elif self.type == "kmarkdown":
+            return MessageSegment("kmarkdown", {"content": self.data["content"] + other.data["content"],
+                                                "raw_content": self.data["raw_content"] + other.data["raw_content"]})
+        else:
+            raise UnsupportedMessageOperation()
+
+    def conduct(self, other: Union[str, "MessageSegment", Iterable["MessageSegment"]]) -> "MessageSegment":
+        """
+        连接两个或多个 MessageSegment，必须同时为 text 或 KMarkdown
+        """
+        if self.type != "text" and self.type != "kmarkdown":
+            raise UnsupportedMessageOperation()
+
+        if isinstance(other, str):
+            other = [MessageSegment.text(other)]
+        elif isinstance(other, MessageSegment):
+            other = [other]
+
+        seg = self
+        for o in other:
+            if o.type == seg.type:
+                seg = self._conduct_single(o)
+            else:
+                raise UnsupportedMessageOperation()
+        return seg
+
+    @overrides(BaseMessageSegment)
+    def is_text(self) -> bool:
+        if self.type == "kmarkdown":
+            return self.data["is_plain_text"]
+        else:
+            return self.type == "text"
+
+    @staticmethod
+    @deprecated("用 KMarkdown 语法 (met)用户id/here/all(met) 代替")
+    def at(user_id: str) -> "MessageSegment":
+        return MessageSegment("at", {"user_id": user_id})
+
+    @staticmethod
+    def text(text: str) -> "MessageSegment":
+        return MessageSegment("text", {"content": text})
+
+    @staticmethod
+    def image(file_key: str) -> "MessageSegment":
+        return MessageSegment("image", {"file_key": file_key})
+
+    @staticmethod
+    def video(file_key: str) -> "MessageSegment":
+        return MessageSegment("video", {
+            "file_key": file_key,
+        })
+
+    @staticmethod
+    def file(file_key: str) -> "MessageSegment":
+        return MessageSegment("file", {
+            "file_key": file_key
+        })
+
+    @staticmethod
+    def audio(file_key: str, duration: int) -> "MessageSegment":
+        return MessageSegment("audio", {
+            "file_key": file_key,
+            "duration": duration
+        })
+
+    @staticmethod
+    def KMarkdown(content: str, raw_content: str) -> "MessageSegment":
+        # 去除转义字符
+        unescaped = unescape_kmarkdown(content)
+        is_plain_text = unescaped.strip() == raw_content
+
+        # raw_content默认strip掉首尾空格，这里还原了（遵循Kook本体的行为）
+        if is_plain_text:
+            raw_content = unescaped
+
+        return MessageSegment("kmarkdown", {
+            "content": content,
+            "raw_content": raw_content,
+            "is_plain_text": is_plain_text,
+        })
+
+    @staticmethod
+    def Card(content: str) -> "MessageSegment":
+        return MessageSegment("card", {
+            "content": content
+        })
+
+
+class Message(BaseMessage[MessageSegment]):
+    """
+    Kook v3 协议 Message 适配。
+    """
+
+    @classmethod
+    @overrides(BaseMessage)
+    def get_segment_class(cls) -> Type[MessageSegment]:
+        return MessageSegment
+
+    @staticmethod
+    @overrides(BaseMessage)
+    def _construct(
+            msg: Union[str, Mapping, Iterable[Mapping]]
+    ) -> Iterable[MessageSegment]:
+        if isinstance(msg, Mapping):
+            msg = cast(Mapping[str, Any], msg)
+            yield MessageSegment(msg["type"], msg.get("content") or {})
+        elif isinstance(msg, Iterable) and not isinstance(msg, str):
+            for seg in msg:
+                yield MessageSegment(seg["type"], seg.get("content") or {})
+        elif isinstance(msg, str):
+            yield MessageSegment("text", {"content": msg})
+
+    @overrides(BaseMessage)
+    def extract_plain_text(self) -> str:
+        return "".join(seg.plain_text for seg in self)
+
+    def reduce(self) -> None:
+        """合并消息内连续的纯文本段和 KMarkdown 段。"""
+        index = 1
+        while index < len(self):
+            prev = self[index - 1]
+            cur = self[index]
+            if prev.type == "text" and cur.type == "text" \
+                    or prev.type == "kmarkdown" and cur.type == "kmarkdown":
+                # 重新构造MessageSegment，确保lazy字段更新
+                self[index - 1] = MessageSegment(prev.type, {
+                    "content": prev.data["content"] + cur.data["content"]
+                })
+                del self[index]
+            else:
+                index += 1
+
+
+@dataclass
+class MessageSerializer:
+    """
+    Kook 协议 Message 序列化器。
+    """
+    message: Message
+
+    async def serialize(self, for_send: bool = True) -> Tuple[int, str]:
+        if len(self.message) != 1:
+            self.message = self.message.copy()
+            self.message.reduce()
+
+            if len(self.message) != 1:
+                raise UnsupportedMessageOperation()
+
+        msg_type = self.message[0].type
+        msg_type_code = msg_type_map[msg_type]
+        # bot 发送消息只支持"text", "kmarkdown", "card"
+        # 经测试还支持"image", "video", "file"
+        if msg_type in ("text", "kmarkdown", "card"):
+            return msg_type_code, self.message[0].data['content']
+        elif msg_type in ("image", "video", "file"):
+            return msg_type_code, self.message[0].data['file_key']
+        elif msg_type == "audio" and not for_send:
+            return msg_type_code, self.message[0].data['file_key']
+        else:
+            raise UnsupportedMessageType()
+
+
+@dataclass
+class MessageDeserializer:
+    """
+    Kook 协议 Message 反序列化器。
+    """
+    type_code: int
+    data: Dict
+
+    def __post_init__(self):
+        self.type = rev_msg_type_map.get(self.type_code, "")
+
+    def deserialize(self) -> Message:
+        if self.type == "text":
+            return Message(MessageSegment.text(self.data["content"]))
+        elif self.type == "image":
+            return Message(MessageSegment.image(self.data["content"]))
+        elif self.type == "video":
+            return Message(MessageSegment.video(self.data["attachments"]["url"]))
+        elif self.type == "file":
+            return Message(MessageSegment.file(self.data["attachments"]["url"]))
+        elif self.type == "kmarkdown":
+            msg_seg = MessageSegment.KMarkdown(self.data["content"], self.data["kmarkdown"]["raw_content"])
+            # 如果是KMarkdown消息是纯文本，直接构造纯文本消息
+            # 目的是让on_command等依赖__str__的规则能够在消息存在转义字符时正常工作
+            if msg_seg.data["is_plain_text"]:
+                return Message(MessageSegment.text(msg_seg.data["raw_content"]))
+            else:
+                return Message(msg_seg)
+        elif self.type == "card":
+            return Message(MessageSegment.Card(self.data["content"]))
+        else:
+            return Message(MessageSegment(self.type, self.data))
