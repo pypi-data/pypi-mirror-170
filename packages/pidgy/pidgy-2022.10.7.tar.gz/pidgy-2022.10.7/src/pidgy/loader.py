822fade9c5c8ddb16ef2b0cc230d9e7944eef4a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:26 +0000

    Add package pidgy/2022.10.7/pidgy-2022.10.7.tar.gz

---
+++ packages/pidgy/pidgy-2022.10.7.tar.gz/pidgy-2022.10.7/src/pidgy/loader.py
@@ -0,0 +1,26 @@
+from importnb import Notebook
+
+from .extras import ReturnDisplay
+from .markdown import Renderer
+from .tangle import Python
+
+
+class Markdown(Notebook):
+    """an importnb extension for pidgy documents"""
+
+    extensions = ".py.md", ".md", ".md.ipynb"
+    tangle = Renderer(renderer_cls=Python)
+
+    def get_data(self, path):
+        if self.path.endswith(".md"):
+            self.source = self.decode()
+            return self.code(self.source)
+        return super(Notebook, self).get_data(path)
+
+    def code(self, str):
+        return super().code(self.tangle.render("".join(str)))
+
+    def visit(self, node):
+        return super().visit(ReturnDisplay().visit(node))
+
+    get_source = get_data = get_data
