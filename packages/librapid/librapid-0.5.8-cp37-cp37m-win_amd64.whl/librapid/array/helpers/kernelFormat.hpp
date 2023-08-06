9fa63df8c83240dec16ea31cf3cc6a2518f394ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:25 +0000

    Add package librapid/0.5.8/librapid-0.5.8-cp37-cp37m-win_amd64.whl

---
+++ packages/librapid/librapid-0.5.8-cp37-cp37m-win_amd64.whl/librapid/array/helpers/kernelFormat.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace librapid::detail {
+	template<typename T, typename std::enable_if_t<std::is_floating_point_v<T>, int> = 0>
+	LR_INLINE std::string kernelFormat(const T &val) {
+		return fmt::format("(({}) {:.50f})", internal::traits<T>::Name, val);
+	}
+
+	template<typename T, typename std::enable_if_t<!std::is_floating_point_v<T>, int> = 0>
+	LR_INLINE std::string kernelFormat(const T &val) {
+		return fmt::format(
+		  "(({}) {})", internal::traits<typename internal::traits<T>::BaseScalar>::Name, val);
+	}
+
+	std::string kernelGenerator(const std::string &opKernel,
+								const std::vector<std::string> &headerFiles = customHeaders);
+} // namespace librapid::detail
