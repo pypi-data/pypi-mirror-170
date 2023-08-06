97073b9e3352eed1d22e6892c35c10b71725b75e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:08 +0000

    Add package hexlib/2.0.2/hexlib-2.0.2.tar.gz

---
+++ packages/hexlib/hexlib-2.0.2.tar.gz/hexlib-2.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as r:
+    desc = r.read()
+
+setup(
+    name="hexlib",
+    version="2.0.2",
+    author="5f0",
+    url="https://github.com/5f0ne/hexlib",
+    description="Basic tools to read, filter, format and display hex values",
+    classifiers=[
+        "Operating System :: OS Independent ",
+        "Programming Language :: Python :: 3 ",
+        "License :: OSI Approved :: MIT License "
+    ],
+    license="MIT",
+    long_description=desc,
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=find_packages(where='src'),
+    install_requires=[
+
+    ]
+)
