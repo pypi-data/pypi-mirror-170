dd2f38021acc554b1a18050b5aeff905ff13cd1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:37 +0000

    Add package wiki-random/0.1.0/wiki_random-0.1.0-py3-none-any.whl

---
+++ packages/wiki-random/wiki_random-0.1.0-py3-none-any.whl/wiki_random/wiki.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+def wiki_random():
+    """
+    - Get random article from Wikipedia. 
+
+    Returns:
+        title and url
+    """
+    url = requests.get("https://en.wikipedia.org/wiki/Special:Random")
+    soup = BeautifulSoup(url.content, "html.parser")
+    title = soup.find(class_="firstHeading").text
+
+    url = "https://en.wikipedia.org/wiki/%s" % title
+
+    return url, title
