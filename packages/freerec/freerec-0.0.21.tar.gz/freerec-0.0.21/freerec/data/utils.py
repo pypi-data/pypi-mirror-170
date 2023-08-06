7fab91599f9abc2e4c698ca4a1cc565e8aec19fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:25 +0000

    Add package freerec/0.0.21/freerec-0.0.21.tar.gz

---
+++ packages/freerec/freerec-0.0.21.tar.gz/freerec-0.0.21/freerec/data/utils.py
@@ -0,0 +1,26 @@
+
+
+from typing import TypeVar, Callable, Dict, List
+
+import numpy as np
+
+from ..utils import errorLogger
+
+T = TypeVar('T')
+
+def safe_cast(val: T, dest_type: Callable[[T], T], default: T) -> T:
+    try:
+        if val:
+            return dest_type(val)
+        else: # fill_na
+            return default
+    except ValueError:
+        errorLogger(
+            f"Using '{dest_type.__name__}' to convert '{val}' where the default value is {default}", 
+            ValueError
+        )
+
+
+def collate_dict(batch: List[Dict]):
+    elem = batch[0]
+    return {key: np.array([d[key] for d in batch]) for key in elem}
