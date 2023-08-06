06c50446f31f266a125fde3d97fa8fe406bc16c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:01 +0000

    Add package py-qgis-server/1.8.4/py-qgis-server-1.8.4.tar.gz

---
+++ packages/py-qgis-server/py-qgis-server-1.8.4.tar.gz/py-qgis-server-1.8.4/pyqgisserver/handlers/__init__.py
@@ -0,0 +1,15 @@
+
+from .basehandler import ( # noqa F401
+    BaseHandler, 
+    NotFoundHandler,
+    ErrorHandler
+)
+from .statushandler import ( # noqa F401 
+    StatusHandler, 
+    PingHandler
+)
+from .owshandler import ( # noqa F401
+    OwsHandler, 
+    OwsApiHandler, 
+)
+
---
+++ packages/py-qgis-server/py-qgis-server-1.8.4.tar.gz/py-qgis-server-1.8.4/pyqgisserver/handlers/statushandler.py
@@ -0,0 +1,62 @@
+#
+# Copyright 2018 3liz
+# Author: David Marteau
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import os
+import tornado
+import logging
+
+from .basehandler import BaseHandler
+from ..version import __version__
+from ..config import config_to_dict
+
+LOGGER=logging.getLogger('SRVLOG')
+
+
+class PingHandler(BaseHandler):
+    
+    def set_default_headers(self) -> None:
+        super().set_default_headers()
+        # Disable cache because this is healtcheck requests
+        self.set_header("Cache-control","no-store")
+        self.set_header("X-Server-Status","ok")
+        
+    def get(self):
+        self.write_json({ 'status': 'ok' })
+
+    def head(self):
+        pass
+
+
+class StatusHandler(BaseHandler):
+    def get(self):
+
+        stats = self.application.stats
+
+        try:
+            from qgis.core import Qgis
+            QGIS_VERSION="{} ({})".format(Qgis.QGIS_VERSION_INT,Qgis.QGIS_RELEASE_NAME)
+        except Exception as e:
+            LOGGER.error("Cannot get Qgis version %s", e)
+            QGIS_VERSION="n/a"
+
+        response = dict(tornado_ver=tornado.version,
+                        version = __version__,
+                        author="3Liz",
+                        author_url="http://3liz.com",
+                        qgis_version=QGIS_VERSION,
+                        documentation="http://{}/doc/".format(self.request.host),
+                        configuration=config_to_dict(),
+                        env=dict(os.environ))
+                        
+        response.update(stats.json())
+
+        self.write_json(response)
+
+       
+
+
