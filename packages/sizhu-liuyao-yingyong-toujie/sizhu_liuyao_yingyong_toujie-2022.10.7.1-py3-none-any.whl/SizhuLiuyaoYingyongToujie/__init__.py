57204cc1c0ccac88a3e4661e948e7760f00989e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:21 +0000

    Add package sizhu-liuyao-yingyong-toujie/2022.10.7.1/sizhu_liuyao_yingyong_toujie-2022.10.7.1-py3-none-any.whl

---
+++ packages/sizhu-liuyao-yingyong-toujie/sizhu_liuyao_yingyong_toujie-2022.10.7.1-py3-none-any.whl/SizhuLiuyaoYingyongToujie/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""sizhu-liuyao-yingyong-toujie
+https://github.com/apachecn/sizhu-liuyao-yingyong-toujie"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.7.1"
