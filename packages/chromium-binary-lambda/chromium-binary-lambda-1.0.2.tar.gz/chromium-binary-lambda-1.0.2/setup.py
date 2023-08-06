435f3ce4fe7e6504946bbfa6797c09dd202ae867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:22 +0000

    Add package chromium-binary-lambda/1.0.2/chromium-binary-lambda-1.0.2.tar.gz

---
+++ packages/chromium-binary-lambda/chromium-binary-lambda-1.0.2.tar.gz/chromium-binary-lambda-1.0.2/setup.py
@@ -0,0 +1,50 @@
+from setuptools import  find_packages, setup   
+from setuptools.command.build_py import build_py
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+
+
+class CustomInstall(build_py):
+        
+    def run(self):
+        from chromium_binary_lambda import check_chromium, download_chromium
+
+        if not check_chromium():
+            print("Baixando")
+            download_chromium()
+        build_py.run(self)
+
+
+setup(
+    name = "chromium-binary-lambda",
+    version = "1.0.2",
+    author = "Fabricio Silva",
+    author_email = "fabricioadenir@gmail.com",
+    description = "Binary of chromium for usin in lambda functions",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/fabricioadenir/chromium-binary-lambda",
+    project_urls = {
+        "Bug Tracker": "https://github.com/fabricioadenir/chromium-binary-lambda/issues",
+        "repository": "https://github.com/fabricioadenir/chromium-binary-lambda"
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    package_data={
+        'chromium_binary_lambda': ['*']
+    },
+    entry_points={
+        'console_scripts': ['chromedriver-path=chromium_binary_lambda:download_chromium'],
+    },
+    python_requires = ">=3.8",
+    install_requires=['certifi==2022.9.24', 'urllib3==1.26.12', 'tqdm==4.64.1'],
+    cmdclass={
+        'build_py': CustomInstall
+        }
+)
