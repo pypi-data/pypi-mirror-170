7648688635a13db34aff13c82861d72b8e89cc01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:07 +0000

    Add package aliases/0.4.0/aliases-0.4.0-py3-none-any.whl

---
+++ packages/aliases/aliases-0.4.0-py3-none-any.whl/aliases/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.4.0"
+
+from aliases.space import AliasSpace
+from aliases.string import AliasAwareString
+from aliases.dict import AliasAwareDict
+
+__all__ = [AliasSpace, AliasAwareDict, AliasAwareString]
