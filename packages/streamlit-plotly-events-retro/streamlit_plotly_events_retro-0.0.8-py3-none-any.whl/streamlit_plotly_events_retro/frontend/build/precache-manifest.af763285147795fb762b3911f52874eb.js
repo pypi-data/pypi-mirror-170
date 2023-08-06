96662f39b1b366481d18837605f7219e3a70af17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:26 +0000

    Add package streamlit-plotly-events-retro/0.0.8/streamlit_plotly_events_retro-0.0.8-py3-none-any.whl

---
+++ packages/streamlit-plotly-events-retro/streamlit_plotly_events_retro-0.0.8-py3-none-any.whl/streamlit_plotly_events_retro/frontend/build/precache-manifest.af763285147795fb762b3911f52874eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33a0dc367e9c67d6e87d344979178a96",
+    "url": "./index.html"
+  },
+  {
+    "revision": "64caee64bd9cf2724d47",
+    "url": "./static/js/2.02ea19d3.chunk.js"
+  },
+  {
+    "revision": "4a84cb0487aa20911c252a5e9703b6bd",
+    "url": "./static/js/2.02ea19d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9652664c1475ebfbc945",
+    "url": "./static/js/main.d2c5e4b3.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
