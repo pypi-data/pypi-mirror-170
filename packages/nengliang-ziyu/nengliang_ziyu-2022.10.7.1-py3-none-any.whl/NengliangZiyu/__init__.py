96929082eb6a47be894334f2993fbbe73057f69b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:34 +0000

    Add package nengliang-ziyu/2022.10.7.1/nengliang_ziyu-2022.10.7.1-py3-none-any.whl

---
+++ packages/nengliang-ziyu/nengliang_ziyu-2022.10.7.1-py3-none-any.whl/NengliangZiyu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""nengliang-ziyu
+https://github.com/apachecn/nengliang-ziyu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.7.1"
