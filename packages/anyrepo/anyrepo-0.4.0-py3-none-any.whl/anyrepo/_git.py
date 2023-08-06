b1adf5ffcf56aed281f9eb86d27ef4588e78a0ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:15 +0000

    Add package anyrepo/0.4.0/anyrepo-0.4.0-py3-none-any.whl

---
+++ packages/anyrepo/anyrepo-0.4.0-py3-none-any.whl/anyrepo/_git.py
@@ -0,0 +1,122 @@
+"""Git Utilities."""
+
+from pathlib import Path
+from typing import Optional
+
+from ._util import run
+from .exceptions import NoGitError
+
+
+class Git:
+
+    """
+    Our Git REPO Helper.
+
+    We know, that there are libraries for that.
+    But we just want to have a lean programmatic interface to git.
+    """
+
+    def __init__(self, path):
+        self.path = path
+
+    @staticmethod
+    def find_path(path: Optional[Path]) -> Path:
+        """Determine Top Directory of Git Clone."""
+        path = path or Path.cwd()
+        result = run(("git", "rev-parse", "--show-cdup"), capture_output=True, check=False, cwd=path)
+        if result.stderr:
+            raise NoGitError()
+        cdup = result.stdout.decode("utf-8").strip()
+        return (path / cdup).resolve()
+
+    @staticmethod
+    def from_path(path: Optional[Path]) -> "Git":
+        """Create GIT Repo Helper from `path`."""
+        path = Git.find_path(path=path)
+        return Git(path=path)
+
+    def is_cloned(self) -> bool:
+        """Check if clone already exists."""
+        if not self.path.exists() or not self.path.is_dir():
+            return False
+        result = self._run(("rev-parse", "--show-cdup"), capture_output=True, check=False)
+        return not result.stderr and not result.stdout.strip()
+
+    def init(self, branch="main"):
+        """Initialize."""
+        self._run(("init", "-b", branch))
+
+    def set_config(self, name, value):
+        """Configure."""
+        self._run(("config", name, value))
+
+    def clone(self, url, revision=None):
+        """Clone."""
+        cmd = ["git", "clone"]
+        if revision:
+            cmd += ["--branch", revision]
+        cmd += ["--", str(url), str(self.path)]
+        run(cmd)
+
+    def get_branch(self) -> Optional[str]:
+        """Get Branch."""
+        return self._run2str(("branch", "--show-current")) or None
+
+    def get_tag(self) -> Optional[str]:
+        """Get Tag."""
+        return self._run2str(("describe", "--exact-match", "--tags"), check=False) or None
+
+    def get_sha(self, revision="HEAD") -> str:
+        """Get SHA."""
+        return self._run2str(("rev-parse", revision))
+
+    def get_url(self) -> Optional[str]:
+        """Get actual url."""
+        return self._run2str(("remote", "get-url", "origin"), check=False) or None
+
+    def checkout(self, revision):
+        """Checkout Revision."""
+        self._run(("checkout", revision))
+
+    def fetch(self):
+        """Pull."""
+        self._run(("fetch",))
+
+    def merge(self):
+        """Merge."""
+        self._run(("merge",))
+
+    def pull(self):
+        """Pull."""
+        self._run(("pull",))
+
+    def rebase(self):
+        """Rebase."""
+        self._run(("rebase",))
+
+    def add(self, file):
+        """Add."""
+        self._run(("add", str(file)))
+
+    def commit(self, msg):
+        """Commit."""
+        self._run(("commit", "-m", msg))
+
+    def tag(self, name, msg=None):
+        """Tag."""
+        cmd = ["tag", name]
+        if msg:
+            cmd += ["-m", msg]
+        self._run(cmd)
+
+    def _run(self, cmd, cwd=None, **kwargs):
+        cwd = cwd or self.path
+        cmd = ("git",) + tuple(cmd)
+        return run(cmd, cwd=cwd, **kwargs)
+
+    def _run2str(self, cmd, cwd=None, check=True) -> str:
+        result = self._run(cmd, cwd=cwd, check=check, capture_output=True)
+        if result.stderr.strip():
+            return ""
+        stdout = result.stdout.decode("utf-8").strip()
+        return stdout
---
+++ packages/anyrepo/anyrepo-0.4.0-py3-none-any.whl/anyrepo/_util.py
@@ -0,0 +1,69 @@
+"""Utilities."""
+import logging
+import subprocess
+from pathlib import Path
+from typing import Optional
+
+_LOGGER = logging.getLogger("anyrepo")
+# Dependencies to any anyrepo module are forbidden here!
+
+
+def run(cmd, cwd=None, capture_output=False, check=True):
+    """Simplified wrapper around :any:`subprocess.run`."""
+    cwdrel = resolve_relative(cwd) if cwd else None
+    try:
+        result = subprocess.run(cmd, capture_output=capture_output, check=check, cwd=cwd)
+        _LOGGER.info("run(%r, cwd='%s') OK stdout=%r stderr=%r", cmd, cwdrel, result.stdout, result.stderr)
+        return result
+    except subprocess.CalledProcessError as error:
+        _LOGGER.info("run(%r, cwd='%s') FAILED stdout=%r stderr=%r", cmd, cwdrel, error.stdout, error.stderr)
+        raise error
+
+
+def no_echo(text: str, **kwargs):
+    """Just suppress `text`."""
+
+
+def resolve_relative(path: Path, base: Optional[Path] = None):
+    """
+    Return resolved `path` relative to `base`.
+
+    :param path (Path): Path
+    :param base (Path): Base Path. Current Working Directory by default.
+    """
+    if not base:
+        base = Path.cwd()
+    path = base / path
+    path = path.resolve()
+    try:
+        return path.relative_to(base)
+    except ValueError:
+        return path.resolve()
+
+
+def get_repr(args=None, kwargs=None):
+    """Return `repr()` string."""
+    parts = list(args or [])
+    for item in kwargs:
+        try:
+            name, value = item
+            parts.append(f"{name}={value!r}")
+        except ValueError:
+            name, value, default = item
+            if value != default:
+                parts.append(f"{name}={value!r}")
+    return ", ".join(parts)
+
+
+def removesuffix(text, suffix):
+    """
+    Remove Suffix identical to function available since python 3.9.
+
+    >>> removesuffix('my text', 'xt')
+    'my te'
+    >>> removesuffix('my text', 'other')
+    'my text'
+    """
+    if text.endswith(suffix):
+        return text[: -len(suffix)]
+    return text
