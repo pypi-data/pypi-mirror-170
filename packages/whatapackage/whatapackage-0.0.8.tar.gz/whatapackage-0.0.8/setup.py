8e778c35d2e63addfda314e16a4a361b72ae2c23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:57 +0000

    Add package whatapackage/0.0.8/whatapackage-0.0.8.tar.gz

---
+++ packages/whatapackage/whatapackage-0.0.8.tar.gz/whatapackage-0.0.8/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='whatapackage',
+    version='0.0.8',
+    description='this is the first package i do',
+    long_description='What a very long desc version fake 2',
+    long_description_content_type='text/plain',
+    packages=find_packages(),
+    url='https://github.com/python/cpython',
+    project_urls={
+        'link 1': 'https://youtube.com',
+        'FBB': 'https://facebook.com',
+    }
+)
