747ccc5e048a2029a89837e36a30e956afb30e3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:49 +0000

    Add package nonebot-plugin-datastore/0.4.0/nonebot-plugin-datastore-0.4.0.tar.gz

---
+++ packages/nonebot-plugin-datastore/nonebot-plugin-datastore-0.4.0.tar.gz/nonebot-plugin-datastore-0.4.0/nonebot_plugin_datastore/plugin.py
@@ -0,0 +1,260 @@
+""" 插件数据 """
+import json
+import os
+import pickle
+from pathlib import Path
+from typing import IO, Any, Callable, Generic, Optional, TypeVar, Union, overload
+
+import httpx
+from nonebot.log import logger
+
+from .config import plugin_config
+
+T = TypeVar("T")
+R = TypeVar("R")
+
+
+class Config:
+    """插件配置管理"""
+
+    def __init__(self, path: Path) -> None:
+        self._path = path
+        self._data = {}
+        if self._path.exists():
+            self._load_config()
+        else:
+            self._save_config()
+
+    def _load_config(self) -> None:
+        """读取配置"""
+        with self._path.open("r", encoding="utf8") as f:
+            self._data = json.load(f)
+
+    def _save_config(self) -> None:
+        """保存配置"""
+        with self._path.open("w", encoding="utf8") as f:
+            json.dump(self._data, f, ensure_ascii=False, indent=2)
+
+    def _get(self, key: str) -> Any:
+        """获取配置键值"""
+        # TODO: 支持从数据库读取数据
+        return self._data[key]
+
+    @overload
+    def get(self, __key: str) -> Union[Any, None]:
+        ...
+
+    @overload
+    def get(self, __key: str, __default: T) -> T:
+        ...
+
+    def get(self, key, default=None):
+        """获得配置
+
+        如果配置获取失败则使用 `default` 值并保存
+        如果不提供 `default` 默认返回 None
+        """
+        try:
+            value = self._get(key)
+        except:
+            value = default
+            # 保存默认配置
+            self.set(key, value)
+        return value
+
+    def set(self, key: str, value: Any) -> None:
+        """设置配置"""
+        self._data[key] = value
+        self._save_config()
+
+
+class NetworkFile(Generic[T, R]):
+    """从网络获取文件
+
+    暂时只支持 json 格式
+    """
+
+    def __init__(
+        self,
+        url: str,
+        filename: str,
+        plugin_data: "PluginData",
+        process_data: Optional[Callable[[T], R]] = None,
+        cache: bool = False,
+    ) -> None:
+        self._url = url
+        self._filename = filename
+        self._plugin_data = plugin_data
+        self._process_data = process_data
+        self._cache = cache
+
+        self._data: Optional[R] = None
+
+    async def load_from_network(self) -> T:
+        """从网络加载文件"""
+        logger.info("正在从网络获取数据")
+        content = await self._plugin_data.download_file(
+            self._url, self._filename, self._cache
+        )
+        rjson = json.loads(content)
+        return rjson
+
+    def load_from_local(self) -> T:
+        """从本地获取数据"""
+        logger.info("正在加载本地数据")
+        data = self._plugin_data.load_json(self._filename)
+        return data
+
+    @property
+    async def data(self) -> R:
+        """数据
+
+        先从本地加载，如果本地文件不存在则从网络加载
+        """
+        if self._data is None:
+            if self._plugin_data.exists(self._filename):
+                data = self.load_from_local()
+            else:
+                data = await self.load_from_network()
+            # 处理数据
+            if self._process_data:
+                self._data = self._process_data(data)
+            else:
+                self._data = data
+        return self._data  # type: ignore
+
+    async def update(self) -> None:
+        """从网络更新数据"""
+        self._data = await self.load_from_network()
+        if self._process_data:
+            self._data = self._process_data(self._data)
+
+
+class Singleton(type):
+    """单例
+
+    每个相同名称的插件数据只需要一个实例
+    """
+
+    _instances = {}
+
+    def __call__(cls, name: str):
+        if not cls._instances.get(name):
+            cls._instances[name] = super().__call__(name)
+        return cls._instances[name]
+
+
+class PluginData(metaclass=Singleton):
+    """插件数据管理
+
+    将插件数据保存在 `data` 文件夹对应的目录下。
+    提供保存和读取文件/数据的方法。
+    """
+
+    def __init__(self, name: str) -> None:
+        # 插件名，用来确定插件的文件夹位置
+        self._name = name
+
+        # 插件配置
+        self._config = None
+
+        self.init_dir()
+
+    def init_dir(self) -> None:
+        """初始化目录"""
+        os.makedirs(self.data_dir, exist_ok=True)
+        os.makedirs(self.cache_dir, exist_ok=True)
+        os.makedirs(self.config_dir, exist_ok=True)
+
+    @property
+    def cache_dir(self) -> Path:
+        """缓存目录"""
+        path = plugin_config.datastore_cache_dir / self._name
+        return path
+
+    @property
+    def config_dir(self) -> Path:
+        """配置目录"""
+        # 配置都放置在统一的目录下
+        path = plugin_config.datastore_config_dir
+        return path
+
+    @property
+    def data_dir(self) -> Path:
+        """数据目录"""
+        path = plugin_config.datastore_data_dir / self._name
+        return path
+
+    @property
+    def config(self) -> Config:
+        """获取配置管理"""
+        if not self._config:
+            self._config = Config(self.config_dir / f"{self._name}.json")
+        return self._config
+
+    def dump_pkl(self, data: Any, filename: str, cache: bool = False, **kwargs) -> None:
+        with self.open(filename, "wb", cache=cache) as f:
+            pickle.dump(data, f, **kwargs)
+
+    def load_pkl(self, filename: str, cache: bool = False, **kwargs) -> Any:
+        with self.open(filename, "rb", cache=cache) as f:
+            data = pickle.load(f, **kwargs)
+        return data
+
+    def dump_json(
+        self,
+        data: Any,
+        filename: str,
+        cache: bool = False,
+        ensure_ascii: bool = False,
+        **kwargs,
+    ) -> None:
+        with self.open(filename, "w", cache=cache, encoding="utf8") as f:
+            json.dump(data, f, ensure_ascii=ensure_ascii, **kwargs)
+
+    def load_json(self, filename: str, cache: bool = False, **kwargs) -> Any:
+        with self.open(filename, "r", cache=cache, encoding="utf8") as f:
+            data = json.load(f, **kwargs)
+        return data
+
+    def open(self, filename: str, mode: str = "r", cache: bool = False, **kwargs) -> IO:
+        """打开文件，默认打开数据文件夹下的文件"""
+        if cache:
+            path = self.cache_dir / filename
+        else:
+            path = self.data_dir / filename
+        return open(path, mode, **kwargs)
+
+    def exists(self, filename: str, cache: bool = False) -> bool:
+        """判断文件是否存在，默认判断数据文件夹下的文件"""
+        if cache:
+            path = self.cache_dir / filename
+        else:
+            path = self.data_dir / filename
+        return path.exists()
+
+    async def download_file(
+        self, url: str, filename: str, cache: bool = False, **kwargs
+    ) -> bytes:
+        """下载文件"""
+        async with httpx.AsyncClient() as client:
+            r = await client.get(url, **kwargs)
+            content = r.content
+            with self.open(filename, "wb", cache=cache) as f:
+                f.write(content)
+            logger.info(f"已下载文件 {url} -> {filename}")
+            return content
+
+    def network_file(
+        self,
+        url: str,
+        filename: str,
+        process_data: Optional[Callable[[T], R]] = None,
+        cache: bool = False,
+    ) -> NetworkFile[T, R]:
+        """网络文件
+
+        从网络上获取数据，并缓存至本地，仅支持 json 格式
+        且可以在获取数据之后同时处理数据
+        """
+        return NetworkFile[T, R](url, filename, self, process_data, cache)
---
+++ packages/nonebot-plugin-datastore/nonebot-plugin-datastore-0.4.0.tar.gz/nonebot-plugin-datastore-0.4.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['nonebot_plugin_datastore']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiosqlite>=0.17.0,<0.18.0',
+ 'nonebot-plugin-localstore>=0.2.0,<0.3.0',
+ 'nonebot2[httpx]>=2.0.0-rc.1,<3.0.0',
+ 'sqlmodel>=0.0.8,<0.1.0']
+
+setup_kwargs = {
+    'name': 'nonebot-plugin-datastore',
+    'version': '0.4.0',
+    'description': '适用于 Nonebot2 的数据存储插件',
+    'long_description': '<!-- markdownlint-disable MD033 MD036 MD041 -->\n\n<p align="center">\n  <a href="https://v2.nonebot.dev/"><img src="https://v2.nonebot.dev/logo.png" width="200" height="200" alt="nonebot"></a>\n</p>\n\n<div align="center">\n\n# NoneBot Plugin DataStore\n\n_✨ NoneBot 数据存储插件 ✨_\n\n</div>\n\n<p align="center">\n  <a href="https://raw.githubusercontent.com/he0119/nonebot-plugin-datastore/main/LICENSE">\n    <img src="https://img.shields.io/github/license/he0119/nonebot-plugin-datastore.svg" alt="license">\n  </a>\n  <a href="https://pypi.python.org/pypi/nonebot-plugin-datastore">\n    <img src="https://img.shields.io/pypi/v/nonebot-plugin-datastore.svg" alt="pypi">\n  </a>\n  <img src="https://img.shields.io/badge/python-3.8+-blue.svg" alt="python">\n  <a href="https://codecov.io/gh/he0119/nonebot-plugin-datastore">\n    <img src="https://codecov.io/gh/he0119/nonebot-plugin-datastore/branch/main/graph/badge.svg?token=jd5ufc1alv"/>\n  </a>\n</p>\n\n## 使用方式\n\n加载插件后直接导入\n\n```python\n# 先声明依赖\nfrom nonebot import require\nrequire("nonebot_plugin_datastore")\n# 接着直接导入\nfrom nonebot.params import Depends\nfrom nonebot_plugin_datastore import PluginData, get_session\nfrom sqlmodel.ext.asyncio.session import AsyncSession\n\nDATA = PluginData("plugin_name")\n\n@matcher.handle()\ndef handle(session: AsyncSession = Depends(get_session)):\n    await session.exec()\n```\n\n## 配置项\n\n配置方式：直接在 `NoneBot` 全局配置文件中添加以下配置项即可。\n\n### datastore_cache_dir\n\n- 类型: `str`\n- 默认: [nonebot_plugin_localstore](https://github.com/nonebot/plugin-localstore) 插件提供的缓存目录\n- 说明: 缓存目录\n\n### datastore_config_dir\n\n- 类型: `str`\n- 默认: [nonebot_plugin_localstore](https://github.com/nonebot/plugin-localstore) 插件提供的配置目录\n- 说明: 配置目录\n\n### datastore_data_dir\n\n- 类型: `str`\n- 默认: [nonebot_plugin_localstore](https://github.com/nonebot/plugin-localstore) 插件提供的数据目录\n- 说明: 数据目录\n\n### datastore_enable_database\n\n- 类型: `bool`\n- 默认: `True`\n- 说明: 是否启动数据库\n\n### datastore_database_url\n\n- 类型: `str`\n- 默认: `sqlite+aiosqlite:///data_dir/data.db`\n- 说明: 数据库连接字符串，默认使用 SQLite 数据库\n\n### datastore_database_echo\n\n- 类型: `bool`\n- 默认: `False`\n- 说明: 是否显示数据库执行的语句与其参数列表\n\n## 计划\n\n- [x] 调整配置为 K-V 存储\n- [x] 调整配置存放位置至专门的配置目录\n- [x] 数据库为可选项\n- [ ] 支持将配置存放至数据库中\n- [ ] 支持 Alembic\n',
+    'author': 'hemengyang',
+    'author_email': 'hmy0119@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/he0119/nonebot-plugin-datastore',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
