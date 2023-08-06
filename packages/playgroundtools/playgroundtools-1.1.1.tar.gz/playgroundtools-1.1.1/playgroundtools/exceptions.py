2e94af91e15757850dbafcba965b3e2a6730a839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:29 +0000

    Add package playgroundtools/1.1.1/playgroundtools-1.1.1.tar.gz

---
+++ packages/playgroundtools/playgroundtools-1.1.1.tar.gz/playgroundtools-1.1.1/playgroundtools/exceptions.py
@@ -0,0 +1,65 @@
+from contextlib import contextmanager
+
+from .util import get_playground_dir, remove_if_exists
+
+
+class PlaygroundException(Exception):
+    pass
+
+
+class PGConfigNotFoundError(PlaygroundException):
+    pass
+
+
+class PGSettingsNotFoundError(PlaygroundException):
+    pass
+
+
+class PGJSONFormatError(PlaygroundException):
+    pass
+
+
+class PGTypeNotFoundError(PlaygroundException):
+    pass
+
+
+class PGInvalidConfError(PlaygroundException):
+    pass
+
+
+class PGInvalidSettingError(PlaygroundException):
+    pass
+
+
+class PGDoesNotExistError(PlaygroundException):
+    pass
+
+
+@contextmanager
+def playground_manager(args):
+    try:
+        yield args.func(args)
+    except Exception as err:
+        yield get_result(err)
+        cleanup(args)
+
+
+def get_result(err):
+    results = {
+        PGDoesNotExistError: "The playground {0} does not exist.",
+        PGConfigNotFoundError: "The configuration could not be found.",
+        PGSettingsNotFoundError: "Settings for playground '{0}' was not found.",
+        PGTypeNotFoundError: "The playground type '{0}' is not configured.",
+        PGInvalidConfError: "'{0}' is not set for type '{1}'.",
+        PGInvalidSettingError: "Settings options missing: {0}",
+        PGJSONFormatError: "JSON format error in '{0}': {1}",
+    }
+    result = results.get(type(err), str(err))
+    return result.format(*err.args)
+
+
+def cleanup(args):
+    """Cleans up the environment in case of an error."""
+    if args.command == "new":
+        playground_dir = get_playground_dir(args.name)
+        remove_if_exists(playground_dir)
