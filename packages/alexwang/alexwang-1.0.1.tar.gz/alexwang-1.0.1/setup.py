5d80a473e9dde08b411acfef439545bcb1fd941b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:25 +0000

    Add package alexwang/1.0.1/alexwang-1.0.1.tar.gz

---
+++ packages/alexwang/alexwang-1.0.1.tar.gz/alexwang-1.0.1/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+packages = ['alexwang']  # 唯一的包名，自己取名
+setup(name='alexwang',
+      version='1.0.1',
+      author='xyy',
+      packages=packages,
+      package_dir={'requests': 'requests'}, )
