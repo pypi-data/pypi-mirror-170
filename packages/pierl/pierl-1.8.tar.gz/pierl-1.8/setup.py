cdc9c6d82766b73c9fa90a6b87a714765dcd13bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:01 +0000

    Add package pierl/1.8/pierl-1.8.tar.gz

---
+++ packages/pierl/pierl-1.8.tar.gz/pierl-1.8/setup.py
@@ -0,0 +1,19 @@
+# setup.py
+from setuptools import find_namespace_packages, setup
+
+# setup.py
+setup(
+    name="pierl",
+    version='1.8',
+    description="Environment Agnostic RL algorithm implementations using Pytorch.",
+    author="Charlie Gaynor",
+    author_email="charliejackcoding@gmail.com",
+    python_requires=">=3.10.1",
+    install_requires=[
+        'gym==0.25.0',
+        'matplotlib==3.5.3',
+        'torch==1.12.1',
+        'numpy==1.23.2',
+        ],
+    packages=['pierl']
+)
