cdbea0323cd46010ff3095b66724786c66517aed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:46 +0000

    Add package nonebot-plugin-flash-silentsave/1.1.1/nonebot_plugin_flash_silentsave-1.1.1.tar.gz

---
+++ packages/nonebot-plugin-flash-silentsave/nonebot_plugin_flash_silentsave-1.1.1.tar.gz/nonebot_plugin_flash_silentsave-1.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup,find_namespace_packages
+
+setup(
+name='nonebot_plugin_flash_silentsave',
+version='1.1.1',
+description='静默保存群聊中的闪照',
+#long_description=open('README.md','r').read(),
+author='karisaya',
+author_email='1048827424@qq.com',
+license='MIT license',
+include_package_data=True,
+packages=find_namespace_packages(include=["nonebot_plugin_flash_silentsave"]),
+platforms='all',
+install_requires=["nonebot2","nonebot-adapter-onebot",],
+url='https://github.com/KarisAya/nonebot_plugin_flash_silentsave',
+)
