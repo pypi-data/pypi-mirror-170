3409a83fda9fa57d73af7fa8db3427fa16f2068d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:47 +0000

    Add package nisa-di/0.3.0/nisa-di-0.3.0.tar.gz

---
+++ packages/nisa-di/nisa-di-0.3.0.tar.gz/nisa-di-0.3.0/nisa_di/context_inject.py
@@ -0,0 +1,43 @@
+import inspect
+import functools
+from contextvars import ContextVar, copy_context
+from hashlib import md5
+from collections import OrderedDict
+import pickle
+
+inject_context: ContextVar[dict] = ContextVar('Context injection nisa di')
+
+def get_hash(path: str, *args, **kwargs):
+    data = frozenset(OrderedDict(kwargs).items())
+    return hash((path, *args, data))
+
+
+def run_in_new_context(callable, *args, **kwarg):
+    context = copy_context()
+    
+    @functools.wraps(callable)
+    def wrapper(*warg, **wkwarg):
+        
+        inject_context.set({})
+        
+        return callable(*warg, **wkwarg)
+    
+    return context.run(wrapper, *args, **kwarg)
+
+
+def get_context_dependency(depend, *args, **kwargs):
+    injection_data = inject_context.get()
+    
+    
+    path = inspect.getfile(depend)
+    name = depend.__name__
+    
+    hash = f'{path}::{name}'
+    hash = get_hash(hash, *args, **kwargs)
+    
+    hasil = injection_data.get(hash)
+    if hasil == None:
+        hasil = depend(*args, **kwargs)
+        injection_data[hash] = hasil
+    
+    return hasil
